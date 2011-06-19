bc93aeb45087ea6c3db4579d6688855d261aea26
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2011-06-19 20:12:21 +0000

    Fixing the logic to disable the native png encoder, it will blow up with 2 bits as well (this time without changing data dir)
    git-svn-id: https://svn.codehaus.org/geoserver/trunk@16063 ef1d6e69-97e5-0310-af46-8a06194da32a

--- src/wms/src/main/java/org/geoserver/wms/map/PNGMapResponse.java
+++ src/wms/src/main/java/org/geoserver/wms/map/PNGMapResponse.java
@@ -85,7 +85,7 @@ public class PNGMapResponse extends RenderedImageMapResponse {
         SampleModel sm = image.getSampleModel();
         int numBits = sm.getSampleSize(0);
         // png acceleration only works on 2 bit and 8 bit images, crashes on 4 bits
-        boolean nativeAcceleration = PNGNativeAcc.booleanValue() && (numBits > 1 && numBits < 8);
+        boolean nativeAcceleration = PNGNativeAcc.booleanValue() && !(numBits > 1 && numBits < 8);
         new ImageWorker(image).writePNG(outStream, "FILTERED", quality,
                 nativeAcceleration, image.getColorModel() instanceof IndexColorModel);
 

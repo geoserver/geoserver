9ba7a1681731653d805934a0829d80b3cfef07fa
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2011-06-20 15:39:27 +0000

    Adding missing class
    git-svn-id: https://svn.codehaus.org/geoserver/trunk@16069 ef1d6e69-97e5-0310-af46-8a06194da32a

---
+++ src/extension/wps/wps-core/src/main/java/org/geoserver/wps/gs/GetFullCoverage.java
@@ -0,0 +1,62 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.wps.gs;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.CoverageInfo;
+import org.geoserver.data.util.CoverageUtils;
+import org.geoserver.wps.WPSException;
+import org.geoserver.wps.jts.DescribeParameter;
+import org.geoserver.wps.jts.DescribeProcess;
+import org.geoserver.wps.jts.DescribeResult;
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.opengis.coverage.grid.GridCoverageReader;
+import org.opengis.filter.Filter;
+import org.opengis.parameter.GeneralParameterDescriptor;
+import org.opengis.parameter.GeneralParameterValue;
+import org.opengis.parameter.ParameterValueGroup;
+
+/**
+ * A process that returns a coverage fully (something which is un-necessarily hard in WCS)
+ * 
+ * @author Daniele Romagnoli, GeoSolutions SAS
+ * @author Andrea Aime, GeoSolutions SAS
+ */
+@DescribeProcess(title = "GetFullCoverage", description = "Returns a full coverage from the catalog, in its native form")
+public class GetFullCoverage implements GeoServerProcess {
+
+    private Catalog catalog;
+
+    public GetFullCoverage(Catalog catalog) {
+        this.catalog = catalog;
+    }
+
+    @DescribeResult(name = "result", description = "The grid coverage", type = GridCoverage2D.class)
+    public GridCoverage2D execute(
+            @DescribeParameter(name = "name", description = "The grid coverage name, possibly fully qualified (workspace:name)") String name,
+            @DescribeParameter(name = "filter", description = "An eventual filter over the data", min = 0) Filter filter)
+            throws IOException {
+        CoverageInfo ci = catalog.getCoverageByName(name);
+        if (ci == null) {
+            throw new WPSException("Could not find coverage " + name);
+        }
+
+        GridCoverageReader reader = ci.getGridCoverageReader(null, null);
+        final ParameterValueGroup readParametersDescriptor = reader.getFormat().getReadParameters();
+        final List<GeneralParameterDescriptor> parameterDescriptors = readParametersDescriptor
+                .getDescriptor().descriptors();
+        GeneralParameterValue[] params = new GeneralParameterValue[0];
+        if (filter != null) {
+            params = CoverageUtils.mergeParameter(parameterDescriptors, params, filter, "FILTER",
+                    "Filter");
+        }
+
+        return (GridCoverage2D) reader.read(params);
+    }
+
+}

10e62b4d513d975a77bd71e6e06797f1fffc4e38
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2011-08-29 18:39:01 +0000

    GEOS-4743, allows OwsUtils get/set to handle property 'paths'
    git-svn-id: https://svn.codehaus.org/geoserver/branches/2.1.x@16285 ef1d6e69-97e5-0310-af46-8a06194da32a

---
+++ src/ows/src/test/java/org/geoserver/ows/util/OwsUtilsTest.java
@@ -0,0 +1,85 @@
+/* Copyright (c) 2001 - 2011 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.geoserver.ows.util;
+
+import junit.framework.TestCase;
+
+public class OwsUtilsTest extends TestCase {
+
+    public void testSimple() throws Exception {
+        Foo foo = new Foo();
+        foo.setA("a");
+
+        assertEquals("a", OwsUtils.get(foo, "a"));
+        assertNull(OwsUtils.get(foo, "b"));
+
+        OwsUtils.set(foo, "b", 5);
+        assertEquals(5, OwsUtils.get(foo, "b"));
+
+        assertEquals(0f, OwsUtils.get(foo, "c"));
+        OwsUtils.set(foo, "c", 5f);
+        assertEquals(5f, OwsUtils.get(foo, "c"));
+    }
+
+    public void testExtended() throws Exception {
+        Bar bar = new Bar();
+        assertNull(OwsUtils.get(bar, "foo"));
+        assertNull(OwsUtils.get(bar, "foo.a"));
+
+        Foo foo = new Foo();
+        bar.setFoo(foo);
+        assertEquals(foo, OwsUtils.get(bar, "foo"));
+        assertNull(OwsUtils.get(bar, "foo.a"));
+
+        foo.setA("abc");
+        assertEquals("abc", OwsUtils.get(bar, "foo.a"));
+
+        OwsUtils.set(bar, "foo.b", 123);
+        assertEquals(123, OwsUtils.get(bar, "foo.b"));
+    }
+
+    class Foo {
+        String a;
+        Integer b;
+        float c;
+
+        public String getA() {
+            return a;
+        }
+        public void setA(String a) {
+            this.a = a;
+        }
+        public Integer getB() {
+            return b;
+        }
+        public void setB(Integer b) {
+            this.b = b;
+        }
+        public float getC() {
+            return c;
+        }
+        public void setC(float c) {
+            this.c = c;
+        }
+    }
+
+    class Bar {
+        Foo foo;
+        Double d;
+
+        public Foo getFoo() {
+            return foo;
+        }
+        public void setFoo(Foo foo) {
+            this.foo = foo;
+        }
+        public Double getD() {
+            return d;
+        }
+        public void setD(Double d) {
+            this.d = d;
+        }
+    }
+}

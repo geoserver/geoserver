5cba3f0a92e8cdceb0b6143a58b4c7efc966a77d
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2011-08-29 18:47:33 +0000

    added some additional Icon constructors accepting a model, and added a model for specifing a tooltip for the icon
    git-svn-id: https://svn.codehaus.org/geoserver/branches/2.1.x@16287 ef1d6e69-97e5-0310-af46-8a06194da32a

--- src/web/core/src/main/java/org/geoserver/web/wicket/Icon.java
+++ src/web/core/src/main/java/org/geoserver/web/wicket/Icon.java
@@ -4,9 +4,12 @@
  */
 package org.geoserver.web.wicket;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ResourceReference;
 import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 
 /**
  * A simple {@link Image} in a panel. For when you need to add an icon in a repeater without
@@ -18,8 +21,36 @@ import org.apache.wicket.markup.html.panel.Panel;
 @SuppressWarnings("serial")
 public class Icon extends Panel {
 
+    /**
+     * Constructs an Icon from a resource reference.
+     */
     public Icon(String id, ResourceReference resourceReference) {
-        super(id);
-        add(new Image("img", resourceReference));
+        this(id, new Model(resourceReference));
     }
+
+    /**
+     * Constructs an icon from a resource reference for the image and resource model for the "title"
+     * attribute to apply to the rendered "&lt;img>" tag.
+     */
+    public Icon(String id, ResourceReference resourceReference, IModel title) {
+        this(id, new Model(resourceReference), title);
+    }
+    
+    /**
+     * Constructs an Icon from a model.
+     */
+    public Icon(String id, IModel model) {
+        super(id);
+        add(new Image("img", model));
+    }
+
+    /**
+     * Constructs an Icon from a model for the resource reference and a resource model for the
+     * "title" attribute to apply to the rendered "&lt;img>" tag. 
+     */
+    public Icon(String id, IModel model, IModel title) {
+        super(id);
+        add(new Image("img", model).add(new AttributeModifier("title", true, title)));
+    }
+
 }

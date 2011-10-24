27599e0fec44f5eb7e8537aecd0d76885217286a
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2011-10-24 22:40:27 +0000

    Sleuth out some wicket structure and start fleshing out tests
    git-svn-id: https://svn.codehaus.org/geoserver/trunk@16471 ef1d6e69-97e5-0310-af46-8a06194da32a

--- src/community/css/src/test/scala/org/geoserver/community/css/web/CssDemoPageTest.scala
+++ src/community/css/src/test/scala/org/geoserver/community/css/web/CssDemoPageTest.scala
@@ -1,14 +1,56 @@
 package org.geoserver.community.css
 package web
 
+import collection.JavaConverters._
+
+import org.apache.wicket.ajax.markup.html.AjaxLink
+import org.apache.wicket.extensions.ajax.markup.html.tabs.AjaxTabbedPanel
+import org.apache.wicket.markup.html.panel.EmptyPanel
+import org.apache.wicket.markup.html.panel.Panel
 import org.geoserver.web.GeoServerWicketTestSupport
 
 class CssDemoPageTest extends GeoServerWicketTestSupport {
   import GeoServerWicketTestSupport.tester
 
-  def testTestSuite() {
+  def testBasicLayout() {
     login()
     tester.startPage(classOf[CssDemoPage])
     tester.assertRenderedPage(classOf[CssDemoPage])
+    tester.assertComponent("main-content:context", classOf[AjaxTabbedPanel])
+    tester.assertComponent("main-content:context:panel", classOf[EmptyPanel])
+    tester.assertComponent("main-content:change.style", classOf[AjaxLink[_]])
+    tester.assertComponent("main-content:change.layer", classOf[AjaxLink[_]])
+    tester.assertComponent("main-content:create.style", classOf[AjaxLink[_]])
+    tester.assertComponent("main-content:associate.styles" , classOf[AjaxLink[_]])
+    tester.assertComponent("main-content:style.editing" , classOf[StylePanel])
+  }
+
+  def testOpenLayersMapPanel() {
+    login()
+    tester.startPage(classOf[CssDemoPage])
+    tester.clickLink("main-content:context:tabs-container:tabs:1:link")
+    tester.assertComponent("main-content:context:panel", classOf[OpenLayersMapPanel])
+  }
+
+  // need to make sure there's a non-empty layer loaded before testing this
+  // def testDataPanel() {
+  //   login()
+  //   tester.startPage(classOf[CssDemoPage])
+  //   tester.clickLink("main-content:context:tabs-container:tabs:2:link")
+  //   tester.assertComponent("main-content:context:panel", classOf[DataPanel])
+  // }
+
+  def testSLDPreviewPanel() {
+    login()
+    tester.startPage(classOf[CssDemoPage])
+    tester.clickLink("main-content:context:tabs-container:tabs:3:link")
+    tester.assertComponent("main-content:context:panel", classOf[SLDPreviewPanel])
+  }
+  
+  def testDocsPanel() {
+    login()
+    tester.startPage(classOf[CssDemoPage])
+    tester.clickLink("main-content:context:tabs-container:tabs:4:link")
+    tester.assertComponent("main-content:context:panel", classOf[DocsPanel])
   }
 }

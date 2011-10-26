92642bf767eef55e41a953a04ff2d10136d7b0b0
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2011-10-26 14:29:02 +0000

    Start moving SummaryTable over to Java
    git-svn-id: https://svn.codehaus.org/geoserver/trunk@16493 ef1d6e69-97e5-0310-af46-8a06194da32a

--- src/community/css/src/main/scala/org/geoserver/community/css/web/SummaryTable.scala
+++ src/community/css/src/main/scala/org/geoserver/community/css/web/SummaryTable.scala
@@ -2,7 +2,7 @@ package org.geoserver.community.css.web
 
 import java.io.Serializable
 
-import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 import scala.collection.mutable.LinkedHashMap
 
 import org.apache.wicket.Component
@@ -12,6 +12,7 @@ import org.apache.wicket.model.IModel
 
 import com.vividsolutions.jts.geom.Geometry
 import org.geotools.feature.FeatureCollection
+import org.geotools.feature.FeatureIterator
 import org.opengis.feature.simple.SimpleFeature
 import org.opengis.feature.simple.SimpleFeatureType
 import org.opengis.feature.`type`.AttributeDescriptor
@@ -21,75 +22,27 @@ import org.geoserver.web.wicket.GeoServerDataProvider.AbstractProperty
 import org.geoserver.web.wicket.GeoServerDataProvider.Property
 import org.geoserver.web.wicket.GeoServerTablePanel
 
-
-/**
- * The Summary class represents an entry in the SummaryTable; including a
- * propertyname, minimum, and maximum value.
- */
-case class Summary(name: String, min: AnyRef, max: AnyRef) extends Serializable
-
-object Summary {
-  def summarize(data: FeatureCollection[SimpleFeatureType, SimpleFeature])
-    : List[Summary] = {
-    val (comparable, noncomparable) = 
-      data.getSchema.getAttributeDescriptors.toList.partition { x => 
-        classOf[Comparable[_]].isAssignableFrom(x.getType.getBinding) &&
-        !classOf[Geometry].isAssignableFrom(x.getType.getBinding)
-      }
-    var maxima = new LinkedHashMap[AttributeDescriptor,Comparable[AnyRef]]
-    var minima = new LinkedHashMap[AttributeDescriptor,Comparable[AnyRef]]
-    var it = data.features
-    try {
-      while (it.hasNext) {
-        val feature = it.next
-        comparable.foreach { prop =>
-          val v = 
-            feature.getAttribute(prop.getName).asInstanceOf[Comparable[AnyRef]]
-          if (v != null) {
-            val min = minima.getOrElse(prop, v)
-            val max = maxima.getOrElse(prop, v)
-            minima.put(prop, if (v.compareTo(min) < 0) v else min)
-            maxima.put(prop, if (v.compareTo(max) > 0) v else max)
-          }
-        }
-      }
-    } finally {
-      it.close()
-    }
-
-    val comparableTable = comparable map { prop =>
-      Summary(prop.getLocalName, minima(prop), maxima(prop))
-    }
-
-    val noncomparableTable = noncomparable map { prop =>
-      Summary(prop.getLocalName, "[n/a]", "[n/a]")
-    }
-
-    comparableTable ++ noncomparableTable
-  }
-}
-
 /**
  * A provider to make summaries available to the SummaryTable.
  */
-class SummaryProvider(summaries: List[Summary])
+class SummaryProvider(summaries: Seq[Summary])
 extends GeoServerDataProvider[Summary] {
 
   def this(data: FeatureCollection[SimpleFeatureType, SimpleFeature])
-    = this(Summary.summarize(data))
+    = this(Summary.summarize(data).asScala)
 
   // override def newModel(a: Serializable): IModel[Serializable] = new Model(a)
 
   override def getProperties(): java.util.List[Property[Summary]] = {
     val list = new java.util.ArrayList[Property[Summary]]
     list.add(new AbstractProperty[Summary]("Name"){
-      override def getPropertyValue(sum: Summary) = sum.name
+      override def getPropertyValue(sum: Summary) = sum.getName
     })
     list.add(new AbstractProperty[Summary]("Minimum"){
-      override def getPropertyValue(sum: Summary) = sum.min
+      override def getPropertyValue(sum: Summary) = sum.getMin
     })
     list.add(new AbstractProperty[Summary]("Maximum"){
-      override def getPropertyValue(sum: Summary) = sum.max
+      override def getPropertyValue(sum: Summary) = sum.getMax
     })
     return list
   }
@@ -101,7 +54,6 @@ extends GeoServerDataProvider[Summary] {
     }
     return list
   }
-
 }
 
 /**

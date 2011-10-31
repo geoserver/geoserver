8b5fbfb1ca59226334f71e90d1756188a910bf1d
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2011-10-31 16:35:18 +0000

    Move SummaryProvider over to Java
    git-svn-id: https://svn.codehaus.org/geoserver/trunk@16521 ef1d6e69-97e5-0310-af46-8a06194da32a

---
+++ src/community/css/src/main/java/org/geoserver/community/css/web/SummaryProvider.java
@@ -0,0 +1,60 @@
+package org.geoserver.community.css.web;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.geoserver.web.wicket.GeoServerDataProvider;
+import org.geotools.feature.FeatureCollection;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+
+/**
+ * A provider to make summaries available to the SummaryTable.
+ */
+class SummaryProvider extends GeoServerDataProvider<Summary> {
+    private final List<Summary> summaries;
+
+    public SummaryProvider(List<Summary> summaries) {
+        this.summaries = summaries;
+    }
+
+    public SummaryProvider(FeatureCollection<SimpleFeatureType, SimpleFeature> data) {
+        this(Summary.summarize(data));
+    }
+
+    @Override
+    public List<Property<Summary>> getProperties() {
+        List<Property<Summary>> props = new ArrayList<Property<Summary>>();
+        props.add(
+            new AbstractProperty<Summary>("Name") {
+                public Object getPropertyValue(Summary sum) {
+                    return sum.getName();
+                }
+            });
+        
+        props.add(
+            new AbstractProperty<Summary>("Minimum") {
+                public Object getPropertyValue(Summary sum) {
+                    return sum.getMin();
+                }
+            });
+
+        props.add(
+            new AbstractProperty<Summary>("Maximum") {
+                public Object getPropertyValue(Summary sum) {
+                    return sum.getMax();
+                }
+            });
+
+        return props;
+    }
+
+    @Override
+    public List<Summary> getItems() {
+        List<Summary> list = new ArrayList<Summary>();
+        Collections.copy(summaries, list);
+        return list;
+    }
+}
--- src/community/css/src/main/scala/org/geoserver/community/css/web/SummaryTable.scala
+++ src/community/css/src/main/scala/org/geoserver/community/css/web/SummaryTable.scala
@@ -3,58 +3,14 @@ package org.geoserver.community.css.web
 import java.io.Serializable
 
 import scala.collection.JavaConverters._
-import scala.collection.mutable.LinkedHashMap
 
 import org.apache.wicket.Component
 import org.apache.wicket.markup.html.basic.Label
 import org.apache.wicket.model.Model
 import org.apache.wicket.model.IModel
 
-import com.vividsolutions.jts.geom.Geometry
-import org.geotools.feature.FeatureCollection
-import org.geotools.feature.FeatureIterator
-import org.opengis.feature.simple.SimpleFeature
-import org.opengis.feature.simple.SimpleFeatureType
-import org.opengis.feature.`type`.AttributeDescriptor
-
-import org.geoserver.web.wicket.GeoServerDataProvider
-import org.geoserver.web.wicket.GeoServerDataProvider.AbstractProperty
 import org.geoserver.web.wicket.GeoServerDataProvider.Property
 import org.geoserver.web.wicket.GeoServerTablePanel
-
-/**
- * A provider to make summaries available to the SummaryTable.
- */
-class SummaryProvider(summaries: Seq[Summary])
-extends GeoServerDataProvider[Summary] {
-
-  def this(data: FeatureCollection[SimpleFeatureType, SimpleFeature])
-    = this(Summary.summarize(data).asScala)
-
-  // override def newModel(a: Serializable): IModel[Serializable] = new Model(a)
-
-  override def getProperties(): java.util.List[Property[Summary]] = {
-    val list = new java.util.ArrayList[Property[Summary]]
-    list.add(new AbstractProperty[Summary]("Name"){
-      override def getPropertyValue(sum: Summary) = sum.getName
-    })
-    list.add(new AbstractProperty[Summary]("Minimum"){
-      override def getPropertyValue(sum: Summary) = sum.getMin
-    })
-    list.add(new AbstractProperty[Summary]("Maximum"){
-      override def getPropertyValue(sum: Summary) = sum.getMax
-    })
-    return list
-  }
-
-  override def getItems(): java.util.List[Summary] = {
-    val list = new java.util.ArrayList[Summary]
-    summaries.foreach { (x: Summary) =>
-      list.add(x)
-    }
-    return list
-  }
-}
 
 /**
  * A specialized GeoServerTablePanel that shows maximum and minimum data for a
@@ -68,7 +24,7 @@ extends GeoServerTablePanel[Summary](id, summary) {
   override def getComponentForProperty(
     id: String,
     value: IModel[_],
-    property: Property[Summary]): Component = {
+    property: Property[Summary]
+  ): Component =
     new Label(id, new Model(property.getPropertyValue(value.getObject.asInstanceOf[Summary]).toString))
-  }
 }

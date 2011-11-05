844d77800bc4c55d86294866489eae5858ce0635
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2011-11-05 00:45:36 +0000

    GEOS-4829: JSON output formatting of date/time to ISO 8601, including Calendar instances
    git-svn-id: https://svn.codehaus.org/geoserver/branches/2.1.x@16544 ef1d6e69-97e5-0310-af46-8a06194da32a

--- src/wfs/src/test/java/org/geoserver/wfs/response/GeoJSONBuilderTest.java
+++ src/wfs/src/test/java/org/geoserver/wfs/response/GeoJSONBuilderTest.java
@@ -6,6 +6,8 @@
 package org.geoserver.wfs.response;
 
 import java.io.StringWriter;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
@@ -45,5 +47,113 @@ public class GeoJSONBuilderTest extends TestCase {
         public MyPoint(double x, double y) {
             super(new Coordinate(x, y), new PrecisionModel(), -1);
         }
+    }
+    
+    public void testWriteDate() throws Exception{
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+        cal.clear();
+        cal.set(Calendar.YEAR, 2011);
+        cal.set(Calendar.MONTH, 9);
+        cal.set(Calendar.DAY_OF_MONTH, 25);
+        
+        java.sql.Date date = new java.sql.Date(cal.getTimeInMillis());
+        builder.object().key("date").value(date).endObject();
+        assertEquals("{\"date\":\"2011-10-25Z\"}", writer.toString());
+    }
+
+    public void testWriteTime() throws Exception{
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+        cal.clear();
+        cal.set(Calendar.HOUR, 15);
+        cal.set(Calendar.MINUTE, 48);
+        cal.set(Calendar.SECOND, 5);
+        
+        java.sql.Time date = new java.sql.Time(cal.getTimeInMillis());
+        builder.object().key("time").value(date).endObject();
+        assertEquals("{\"time\":\"15:48:05Z\"}", writer.toString());
+    }
+
+    public void testWriteDateTime() throws Exception{
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+        cal.clear();
+        cal.set(Calendar.YEAR, 2011);
+        cal.set(Calendar.MONTH, 9);
+        cal.set(Calendar.DAY_OF_MONTH, 25);
+        cal.set(Calendar.HOUR, 15);
+        cal.set(Calendar.MINUTE, 48);
+        cal.set(Calendar.SECOND, 5);
+        
+        java.sql.Timestamp date = new java.sql.Timestamp(cal.getTimeInMillis());
+        builder.object().key("timestamp").value(date).endObject();
+        assertEquals("{\"timestamp\":\"2011-10-25T15:48:05Z\"}", writer.toString());
+    }
+
+    public void testWriteDateTimeMillis() throws Exception{
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+        cal.clear();
+        cal.set(Calendar.YEAR, 2011);
+        cal.set(Calendar.MONTH, 9);
+        cal.set(Calendar.DAY_OF_MONTH, 25);
+        cal.set(Calendar.HOUR, 15);
+        cal.set(Calendar.MINUTE, 48);
+        cal.set(Calendar.SECOND, 5);
+        cal.set(Calendar.MILLISECOND, 223);
+        
+        java.sql.Timestamp date = new java.sql.Timestamp(cal.getTimeInMillis());
+        builder.object().key("timestamp").value(date).endObject();
+        assertEquals("{\"timestamp\":\"2011-10-25T15:48:05.223Z\"}", writer.toString());
+    }
+
+    public void testWriteJavaDate() throws Exception{
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+        cal.clear();
+        cal.set(Calendar.YEAR, 2011);
+        cal.set(Calendar.MONTH, 9);
+        cal.set(Calendar.DAY_OF_MONTH, 25);
+        cal.set(Calendar.HOUR, 15);
+        cal.set(Calendar.MINUTE, 48);
+        cal.set(Calendar.SECOND, 5);
+        cal.set(Calendar.MILLISECOND, 223);
+        
+        java.util.Date date = new java.util.Date(cal.getTimeInMillis());
+        builder.object().key("date").value(date).endObject();
+        assertEquals("{\"date\":\"2011-10-25T15:48:05.223Z\"}", writer.toString());
+    }
+
+    public void testWriteCalendar() throws Exception{
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+        cal.clear();
+        cal.set(Calendar.YEAR, 2011);
+        cal.set(Calendar.MONTH, 9);
+        cal.set(Calendar.DAY_OF_MONTH, 25);
+        
+        builder.object().key("cal").value(cal).endObject();
+        assertEquals("{\"cal\":\"2011-10-25T00:00:00Z\"}", writer.toString());
+    }
+
+    public void testWriteCalendarTZ() throws Exception{
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT-05:00"));
+        cal.clear();
+        cal.set(Calendar.YEAR, 2011);
+        cal.set(Calendar.MONTH, 9);
+        cal.set(Calendar.DAY_OF_MONTH, 25);
+        
+        builder.object().key("cal").value(cal).endObject();
+        assertEquals("{\"cal\":\"2011-10-25T00:00:00-05:00\"}", writer.toString());
+    }
+
+    public void testWriteCalendarFull() throws Exception{
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+        cal.clear();
+        cal.set(Calendar.YEAR, 2011);
+        cal.set(Calendar.MONTH, 9);
+        cal.set(Calendar.DAY_OF_MONTH, 25);
+        cal.set(Calendar.HOUR, 15);
+        cal.set(Calendar.MINUTE, 48);
+        cal.set(Calendar.SECOND, 5);
+        cal.set(Calendar.MILLISECOND, 223);
+        
+        builder.object().key("cal").value(cal).endObject();
+        assertEquals("{\"cal\":\"2011-10-25T15:48:05.223Z\"}", writer.toString());
     }
 }

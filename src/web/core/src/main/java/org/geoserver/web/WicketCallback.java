75952331317b7927d92167477756a3e30a77fe06
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2011-11-22 11:32:30 +0000

    [GEOS-4851] Safeguard catalog and configuration code from concurrent access
    git-svn-id: https://svn.codehaus.org/geoserver/branches/2.1.x@16593 ef1d6e69-97e5-0310-af46-8a06194da32a

---
+++ src/web/core/src/main/java/org/geoserver/web/WicketCallback.java
@@ -0,0 +1,50 @@
+/* Copyright (c) 2001 - 2011 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.web;
+
+import org.apache.wicket.IRequestTarget;
+import org.apache.wicket.Page;
+import org.apache.wicket.RequestCycle;
+
+/**
+ * Pluggable callback exposing the Wicket {@link RequestCycle} stages
+ * 
+ * @author Andrea Aime - GeoSolutions
+ * 
+ */
+public interface WicketCallback {
+
+    /**
+     * Called when the request cycle object is beginning its response
+     */
+    void onBeginRequest();
+
+    /**
+     * Called when the request cycle object has detached all request targets.
+     */
+    void onAfterTargetsDetached();
+
+    /**
+     * Called when the request cycle object has finished its response
+     */
+    void onEndRequest();
+
+    /**
+     * Called when a request target is set on the request cycle
+     * 
+     * @param requestTarget
+     */
+    void onRequestTargetSet(IRequestTarget requestTarget);
+
+    /**
+     * Called when a runtime exception is thrown, just before the actual handling of the runtime
+     * exception.
+     * 
+     * @param page Any page context where the exception was thrown
+     * @param e The exception
+     */
+    void onRuntimeException(Page page, RuntimeException e);
+
+}
---
+++ src/web/core/src/main/java/org/geoserver/web/WicketConfigurationLockCallback.java
@@ -0,0 +1,85 @@
+/* Copyright (c) 2001 - 2011 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.web;
+
+import org.apache.wicket.IRequestTarget;
+import org.apache.wicket.Page;
+import org.apache.wicket.request.target.component.IBookmarkablePageRequestTarget;
+import org.apache.wicket.request.target.component.IPageRequestTarget;
+import org.geoserver.GeoServerConfigurationLock;
+import org.geoserver.GeoServerConfigurationLock.LockType;
+
+/**
+ * Protects the catalog and configuration from concurrent access from the web GUI side (will stay
+ * here until the catalog and configution will become thread safe).
+ * <p>
+ * It locks in write mode all {@link GeoServerSecuredPage} subclasses, as those have some
+ * possibility to write on the configuration/catalog, all other pages are locked in read mode.
+ * 
+ * @author Andrea Aime - GeoSolutions
+ * 
+ */
+public class WicketConfigurationLockCallback implements WicketCallback {
+
+    GeoServerConfigurationLock locker;
+
+    static ThreadLocal<LockType> THREAD_LOCK = new ThreadLocal<GeoServerConfigurationLock.LockType>();
+
+    public WicketConfigurationLockCallback(GeoServerConfigurationLock locker) {
+        this.locker = locker;
+    }
+
+    public void onBeginRequest() {
+        // nothing to do here
+    }
+
+    public void onAfterTargetsDetached() {
+        // who cares?
+    }
+
+    public void onEndRequest() {
+        LockType type = THREAD_LOCK.get();
+        if (type != null) {
+            THREAD_LOCK.remove();
+            locker.unlock(type);
+        }
+    }
+
+    public void onRequestTargetSet(IRequestTarget target) {
+        // we can have many of these calls per http call, avoid locking multiple times,
+        // onEndRequest will be called just once
+        LockType type = THREAD_LOCK.get();
+        if (type != null) {
+            return;
+        }
+
+        // setup a write lock for secured pages, a read one for the others
+        if (target instanceof IPageRequestTarget) {
+            IPageRequestTarget pt = (IPageRequestTarget) target;
+            Page page = pt.getPage();
+            if (page instanceof GeoServerSecuredPage) {
+                type = LockType.WRITE;
+            }
+        } else if (target instanceof IBookmarkablePageRequestTarget) {
+            IBookmarkablePageRequestTarget bt = (IBookmarkablePageRequestTarget) target;
+
+            if (GeoServerSecuredPage.class.isAssignableFrom(bt.getPageClass())) {
+                type = LockType.WRITE;
+            }
+        }
+        if (type == null) {
+            type = LockType.READ;
+        }
+
+        // and lock
+        THREAD_LOCK.set(type);
+        locker.lock(type);
+    }
+
+    public void onRuntimeException(Page page, RuntimeException e) {
+        // nothing to do
+    }
+
+}

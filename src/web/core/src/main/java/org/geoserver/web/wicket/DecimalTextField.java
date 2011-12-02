3273444edd442b47ec94922e17b34175affb7504
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2011-12-02 16:23:25 +0000

    GEOS-4887: do not round up coordinates to three decimals in EnvelopePanel
    git-svn-id: https://svn.codehaus.org/geoserver/branches/2.1.x@16619 ef1d6e69-97e5-0310-af46-8a06194da32a

---
+++ src/web/core/src/main/java/org/geoserver/web/wicket/DecimalTextField.java
@@ -0,0 +1,67 @@
+/* Copyright (c) 2011 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.web.wicket;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.util.convert.IConverter;
+
+/**
+ * A TextField for {@code java.lang.Double} representations that allows for arbitrary number of
+ * decimal places, since the default TextField rounds up doubles to three decimals.
+ * 
+ * @author groldan
+ */
+public class DecimalTextField extends TextField<Double> {
+
+    private static final long serialVersionUID = 1L;
+
+    private NumberFormat format;
+
+    private IConverter decimalConverter;
+
+    public DecimalTextField(String id, IModel<Double> model) {
+        super(id, model, Double.class);
+        format = DecimalFormat.getInstance();
+        format.setMaximumFractionDigits(16);
+
+        decimalConverter = new IConverter() {
+            private static final long serialVersionUID = 1L;
+
+            // @Override
+            public String convertToString(Object value, Locale locale) {
+                return value == null? null : format.format(value);
+            }
+
+            // @Override
+            public Object convertToObject(String value, Locale locale) {
+                if (value == null || value.trim().length() == 0) {
+                    return null;
+                }
+                Number parsed;
+                try {
+                    parsed = format.parse(value);
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
+                }
+                return Double.valueOf(parsed.doubleValue());
+            }
+        };
+    }
+
+    public void setMaximumFractionDigits(int maximumFractionDigits) {
+        format.setMaximumFractionDigits(maximumFractionDigits);
+    }
+
+    @Override
+    public IConverter getConverter(Class<?> type) {
+        return decimalConverter;
+    }
+}

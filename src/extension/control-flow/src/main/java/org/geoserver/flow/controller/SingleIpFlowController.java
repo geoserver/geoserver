951183692b97132c04ea05a1032254ad27ff736c
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2012-04-05 22:29:23 +0000

    GEOS-4961: simplify SingleIpFlowController, all it needs to do is to extend SingleQueueFlowController and check the ip matches
    git-svn-id: https://svn.codehaus.org/geoserver/branches/2.1.x@16902 ef1d6e69-97e5-0310-af46-8a06194da32a

--- src/extension/control-flow/src/main/java/org/geoserver/flow/controller/SingleIpFlowController.java
+++ src/extension/control-flow/src/main/java/org/geoserver/flow/controller/SingleIpFlowController.java
@@ -5,61 +5,32 @@
 
 package org.geoserver.flow.controller;
 
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-
 import org.geoserver.ows.Request;
 
 /**
- * A flow controller that throttles concurrent requests made from the same ip (single ip, specified in configuration file)
+ * A flow controller that throttles concurrent requests made from the same ip (single ip, specified
+ * in configuration file)
  * 
  * @author Juan Marin, OpenGeo
  */
-public class SingleIpFlowController extends IpFlowController {
+public class SingleIpFlowController extends SingleQueueFlowController {
 
-    public SingleIpFlowController(int queueSize) {
-        super(queueSize);
-    }
+    private final String ip;
 
-    public SingleIpFlowController(int queueSize, String ip) {
+    public SingleIpFlowController(final int queueSize, final String ip) {
         super(queueSize);
-        ipAddresses.add(ip);
+        this.ip = ip;
     }
 
     @Override
-    public boolean requestIncoming(Request request, long timeout) {
-        boolean retval = true;
-        String incomingIp = getRemoteAddr(request.getHttpRequest());
-        if (incomingIp.equals(ipAddresses.get(0))) {
-            TimedBlockingQueue queue = null;
-            if (incomingIp != null && !incomingIp.equals("")) {
-                queue = queues.get(incomingIp);
-            }
+    boolean matchesRequest(Request request) {
+        final String incomingIp = IpFlowController.getRemoteAddr(request.getHttpRequest());
+        boolean matches = ip.equals(incomingIp);
+        return matches;
+    }
 
-            if (queue == null) {
-                queue = new TimedBlockingQueue(queueSize, true);
-                queues.put(incomingIp, queue);
-            }
-            QUEUE_ID.set(incomingIp);
-
-            try {
-                if (timeout > 0) {
-                    retval = queue.offer(request, timeout, TimeUnit.MILLISECONDS);
-                } else {
-                    queue.put(request);
-                }
-            } catch (InterruptedException e) {
-                LOGGER.log(Level.WARNING, "Unexpected interruption while "
-                        + "blocking on the request queue");
-            }
-            if (LOGGER.isLoggable(Level.FINE)) {
-                LOGGER.fine("SingleIpFlowController(" + queueSize + "," + incomingIp
-                        + ") queue size " + queue.size());
-                LOGGER.fine("SingleIpFlowController(" + queueSize + "," + incomingIp
-                        + ") total queues " + queues.size());
-            }
-        }
-        return retval;
+    public String getIp() {
+        return ip;
     }
 
 }

b6902dffaabc2c753b7d97d8f21f19970e7468e6
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2012-05-22 00:06:43 +0000

    updating version numbers and release notes for 2.2-beta2
    git-svn-id: https://svn.codehaus.org/geoserver/tags/2.2-beta2@17103 ef1d6e69-97e5-0310-af46-8a06194da32a

--- doc/en/docguide/source/conf.py
+++ doc/en/docguide/source/conf.py
@@ -47,7 +47,7 @@ copyright = u'2011 GeoServer'
 # The short X.Y version.
 version = '2.2'
 # The full version, including alpha/beta/rc tags.
-release = '2.2-SNAPSHOT'
+release = '2.2-beta2'
 # Users don't need to see the "SNAPSHOT" notation when it's there
 if release.find('SNAPSHOT') != -1:
    release = '2.2.x'

973f5e3005ea3756f00c0f3acc123571cddaade2
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2012-05-27 17:35:41 +0000

    [GEOS-5126] Saving a layer whose native SRS has no native EPSG code takes several seconds
    git-svn-id: https://svn.codehaus.org/geoserver/trunk@17118 ef1d6e69-97e5-0310-af46-8a06194da32a

--- src/main/src/main/java/org/geoserver/config/util/XStreamPersister.java
+++ src/main/src/main/java/org/geoserver/config/util/XStreamPersister.java
@@ -973,7 +973,7 @@ public class XStreamPersister {
         public String toString(Object obj) {
             CoordinateReferenceSystem crs = (CoordinateReferenceSystem) obj;
             try {
-                Integer epsg = CRS.lookupEpsgCode(crs, true);
+                Integer epsg = CRS.lookupEpsgCode(crs, false);
                 if (epsg != null) {
                     return "EPSG:" + epsg;
                 }

11d28b483a8de569963ab21b271ff891212e4cac
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2012-06-10 17:43:31 +0000

    [GEOS-5013] WFS request containing viewparams parameter causes an IllegalArgumentException
    git-svn-id: https://svn.codehaus.org/geoserver/trunk@17185 ef1d6e69-97e5-0310-af46-8a06194da32a

---
+++ src/wfs/src/test/java/org/geoserver/wfs/SQLViewTest.java
@@ -0,0 +1,112 @@
+package org.geoserver.wfs;
+
+import java.io.File;
+import java.util.Map;
+
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.CatalogBuilder;
+import org.geoserver.catalog.DataStoreInfo;
+import org.geoserver.catalog.FeatureTypeInfo;
+import org.geoserver.catalog.WorkspaceInfo;
+import org.geoserver.data.test.MockData;
+import org.geotools.data.DataStore;
+import org.geotools.data.Query;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.data.simple.SimpleFeatureStore;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotools.jdbc.JDBCDataStore;
+import org.geotools.jdbc.VirtualTable;
+import org.geotools.jdbc.VirtualTableParameter;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.w3c.dom.Document;
+
+import com.vividsolutions.jts.geom.Point;
+
+import static org.custommonkey.xmlunit.XMLAssert.*;
+
+public class SQLViewTest extends WFSTestSupport {
+
+    String tableTypeName;
+    String viewTypeName;
+    
+    @Override
+    protected void populateDataDirectory(MockData dataDirectory) throws Exception {
+        // just populate with the minimum
+        dataDirectory.addWellKnownType(MockData.PRIMITIVEGEOFEATURE, null);
+    }
+
+    @Override
+    protected void setUpInternal() throws Exception {
+        // run all the tests against a store that can do sql views 
+        Catalog cat = getCatalog();
+        DataStoreInfo ds = cat.getFactory().createDataStore();
+        ds.setName("sqlviews");
+        WorkspaceInfo ws = cat.getDefaultWorkspace();
+        ds.setWorkspace(ws);
+        
+        Map params = ds.getConnectionParameters(); 
+        params.put("dbtype", "h2");
+        File dbFile = new File(getTestData().getDataDirectoryRoot().getAbsolutePath(), "data/h2test");
+        params.put("database", dbFile.getAbsolutePath());
+        cat.add(ds);
+        
+        SimpleFeatureSource fsp = getFeatureSource(MockData.PRIMITIVEGEOFEATURE);
+        
+        DataStore store = (DataStore) ds.getDataStore(null);
+        SimpleFeatureTypeBuilder tb = new SimpleFeatureTypeBuilder();
+        
+        tb.init(fsp.getSchema());
+        tb.remove("surfaceProperty"); // the store cannot create multi-geom tables it seems
+        tb.remove("curveProperty"); // the store cannot create multi-geom tables it seems
+        tb.remove("uriProperty"); // this would render the store read only
+        tb.setName("pgeo");
+        SimpleFeatureType schema = tb.buildFeatureType();
+        store.createSchema(schema);
+        SimpleFeatureStore featureStore = (SimpleFeatureStore) store.getFeatureSource("pgeo");
+        featureStore.addFeatures(fsp.getFeatures());
+        
+        CatalogBuilder cb = new CatalogBuilder(cat);
+        cb.setStore(ds);
+        FeatureTypeInfo tft = cb.buildFeatureType(featureStore);
+        cat.add(tft);
+        
+        tableTypeName = tft.prefixedName();
+        
+        // create the sql view
+        JDBCDataStore jds = (JDBCDataStore) ds.getDataStore(null);
+        VirtualTable vt = new VirtualTable("pgeo_view", "select \"name\", \"pointProperty\" from \"pgeo\" where \"booleanProperty\" = %bool% and \"name\" = '%name%'");
+        vt.addParameter(new VirtualTableParameter("bool", "true"));
+        vt.addParameter(new VirtualTableParameter("name", "name-f001"));
+        vt.addGeometryMetadatata("pointProperty", Point.class, 4326);
+        jds.addVirtualTable(vt);
+        
+        FeatureTypeInfo vft = cb.buildFeatureType(jds.getFeatureSource(vt.getName()));
+        vft.getMetadata().put(FeatureTypeInfo.JDBC_VIRTUAL_TABLE, vt);
+        cat.add(vft);
+        viewTypeName = vft.prefixedName();
+    }
+    
+    /**
+     * Checks the setup did the expected job
+     * @throws Exception
+     */
+    public void testStoreSetup() throws Exception {
+        FeatureTypeInfo tableTypeInfo = getCatalog().getFeatureTypeByName(tableTypeName);
+        assertNotNull(tableTypeInfo);
+        assertEquals(5, tableTypeInfo.getFeatureSource(null, null).getCount(Query.ALL));
+        
+        FeatureTypeInfo viewTypeInfo = getCatalog().getFeatureTypeByName(viewTypeName);
+        assertNotNull(viewTypeInfo);
+        assertEquals(1, viewTypeInfo.getFeatureSource(null, null).getCount(Query.ALL));
+    }
+    
+    public void testViewParams() throws Exception {
+        Document dom = getAsDOM("wfs?request=GetFeature&typename=" + viewTypeName + "&version=1.1&viewparams=bool:true;name:name-f003");
+        print(dom);
+        
+        assertXpathEvaluatesTo("name-f003", "//gs:pgeo_view/gml:name", dom);
+        assertXpathEvaluatesTo("1", "count(//gs:pgeo_view)", dom);
+    }
+
+    
+}

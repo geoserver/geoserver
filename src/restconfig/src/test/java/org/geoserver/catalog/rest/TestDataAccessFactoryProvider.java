b9f947912bd64c236c8fe84d1ecda47243bd4e07
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2012-06-18 22:13:03 +0000

    GEOS-5171, having file upload endpoint support loading FileDataStoreFactorySpi interfaces as well as built in defaults, patch contributed by Rob Marianski
    git-svn-id: https://svn.codehaus.org/geoserver/trunk@17213 ef1d6e69-97e5-0310-af46-8a06194da32a

---
+++ src/restconfig/src/test/java/org/geoserver/catalog/rest/TestDataAccessFactoryProvider.java
@@ -0,0 +1,22 @@
+package org.geoserver.catalog.rest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.geoserver.data.DataAccessFactoryProducer;
+import org.geotools.data.DataAccessFactory;
+
+public class TestDataAccessFactoryProvider implements DataAccessFactoryProducer {
+
+    private List<? extends DataAccessFactory> factories = new ArrayList<DataAccessFactory>();
+
+    public void setDataStoreFactories(List<? extends DataAccessFactory> factories) {
+        this.factories = factories;
+    }
+
+    @Override
+    public List<DataAccessFactory> getDataStoreFactories() {
+        return Collections.unmodifiableList(this.factories);
+    }
+}

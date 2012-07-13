7fff8aa33c7fbcde79090188e2cc749bf58b1d4c
Andrea Aime <andrea.aime@gmail.com>
2012-07-13 15:09:33 +0200

    [GEOS-5208] WPS chaining may result in a NPE if the inner process attempts to report progress

--- src/extension/wps/wps-core/src/test/java/org/geoserver/wps/ExecuteTest.java
+++ src/extension/wps/wps-core/src/test/java/org/geoserver/wps/ExecuteTest.java
@@ -835,6 +835,53 @@ public class ExecuteTest extends WPSTestSupport {
         assertXpathExists("//wps:ProcessSucceeded", dom);
     }
     
+    /**
+     * http://jira.codehaus.org/browse/GEOS-5208
+     * @throws Exception
+     */
+    public void testChainedProgress() throws Exception {
+        String request = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + 
+        		"<wps:Execute version=\"1.0.0\" service=\"WPS\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://www.opengis.net/wps/1.0.0\" xmlns:wfs=\"http://www.opengis.net/wfs\" xmlns:wps=\"http://www.opengis.net/wps/1.0.0\" xmlns:ows=\"http://www.opengis.net/ows/1.1\" xmlns:gml=\"http://www.opengis.net/gml\" xmlns:ogc=\"http://www.opengis.net/ogc\" xmlns:wcs=\"http://www.opengis.net/wcs/1.1.1\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xsi:schemaLocation=\"http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd\">\n" + 
+        		"  <ows:Identifier>gs:CollectGeometries</ows:Identifier>\n" + 
+        		"  <wps:DataInputs>\n" + 
+        		"    <wps:Input>\n" + 
+        		"      <ows:Identifier>features</ows:Identifier>\n" + 
+        		"      <wps:Reference mimeType=\"text/xml; subtype=wfs-collection/1.0\" xlink:href=\"http://geoserver/wps\" method=\"POST\">\n" + 
+        		"        <wps:Body>\n" + 
+        		"          <wps:Execute version=\"1.0.0\" service=\"WPS\">\n" + 
+        		"            <ows:Identifier>gs:Monkey</ows:Identifier>\n" + 
+        		"            <wps:DataInputs>\n" + 
+        		"              <wps:Input>\n" + 
+        		"                <ows:Identifier>id</ows:Identifier>\n" + 
+        		"                <wps:Data>\n" + 
+        		"                  <wps:LiteralData>chained-monkey</wps:LiteralData>\n" + 
+        		"                </wps:Data>\n" + 
+        		"              </wps:Input>\n" + 
+        		"            </wps:DataInputs>\n" + 
+        		"            <wps:ResponseForm>\n" + 
+        		"              <wps:RawDataOutput mimeType=\"text/xml; subtype=gml/3.1.1\">\n" + 
+        		"                <ows:Identifier>result</ows:Identifier>\n" + 
+        		"              </wps:RawDataOutput>\n" + 
+        		"            </wps:ResponseForm>\n" + 
+        		"          </wps:Execute>\n" + 
+        		"        </wps:Body>\n" + 
+        		"      </wps:Reference>\n" + 
+        		"    </wps:Input>\n" + 
+        		"  </wps:DataInputs>\n" + 
+        		"  <wps:ResponseForm>\n" + 
+        		"    <wps:RawDataOutput mimeType=\"application/wkt\">\n" + 
+        		"      <ows:Identifier>result</ows:Identifier>\n" + 
+        		"    </wps:RawDataOutput>\n" + 
+        		"  </wps:ResponseForm>\n" + 
+        		"</wps:Execute>";
+        
+        MonkeyProcess.progress("chained-monkey", 0.1f, false);
+        MonkeyProcess.exit("chained-monkey", collectionOfThings(), false);
+        MockHttpServletResponse response = postAsServletResponse("wfs", request);
+        assertEquals("application/wkt", response.getContentType());
+        assertEquals("GEOMETRYCOLLECTION EMPTY", response.getOutputStreamContent());
+    }
+    
     public void testAsynchFailEncode() throws Exception {
         // submit asynch request with no updates
         String statusLocation = submitMonkey("x5");

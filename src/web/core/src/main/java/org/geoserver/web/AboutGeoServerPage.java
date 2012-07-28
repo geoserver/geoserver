3a91bf39223d3eaf6b6beeaec321914793b2bb57
jdeolive <jdeolive@opengeo.org>
2012-07-28 09:17:30 -0600

    added gwc revision info to build

--- src/web/core/src/main/java/org/geoserver/web/AboutGeoServerPage.java
+++ src/web/core/src/main/java/org/geoserver/web/AboutGeoServerPage.java
@@ -1,5 +1,7 @@
 package org.geoserver.web;
 
+import java.util.logging.Level;
+
 import org.apache.wicket.markup.html.basic.Label;
 import org.geotools.factory.GeoTools;
 
@@ -14,5 +16,28 @@ public class AboutGeoServerPage extends GeoServerBasePage {
     public AboutGeoServerPage() {
         add(new Label("geotoolsVersion", GeoTools.getVersion().toString()));
         add(new Label("geotoolsRevision", GeoTools.getBuildRevision().toString()));
+        add(new Label("geowebcacheVersion", getGwcVersion()));
+        add(new Label("geowebcacheRevision", getGwcRevision()));
+    }
+
+    public String getGwcVersion() {
+        Package p = lookupGwcPackage();
+        return p != null ? p.getSpecificationVersion() : null;
+    }
+
+    public String getGwcRevision() {
+        Package p = lookupGwcPackage();
+        return p != null ? p.getImplementationVersion() : null;
+    }
+
+    Package lookupGwcPackage() {
+        try {
+            return Package.getPackage("org.geowebcache");
+        }
+        catch(Exception e) {
+            //be safe
+            LOGGER.log(Level.FINE, "Error looking up org.geowebcache package", e);
+        }
+        return null;
     }
 }

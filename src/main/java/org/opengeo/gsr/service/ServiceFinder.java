af8e1d01f733f7ac367c39351adb672882ea9e8f
Juan Marin Otero <juan.marin.otero@gmail.com>
2012-08-02 17:05:59 -0400

    Add Service Finder stub

---
+++ src/main/java/org/opengeo/gsr/service/ServiceFinder.java
@@ -0,0 +1,34 @@
+package org.opengeo.gsr.service;
+
+import org.geoserver.catalog.rest.AbstractCatalogFinder;
+import org.geoserver.config.GeoServer;
+import org.geoserver.ows.Dispatcher;
+import org.geoserver.wms.WMS;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.resource.Resource;
+
+public class ServiceFinder extends AbstractCatalogFinder {
+
+    private GeoServer geoServer;
+
+    private WMS wms;
+
+    private Dispatcher dispatcher;
+
+    protected ServiceFinder(GeoServer geoServer, WMS wms, Dispatcher dispatcher) {
+        super(geoServer.getCatalog());
+        this.geoServer = geoServer;
+        this.wms = wms;
+        this.dispatcher = dispatcher;
+    }
+    
+    public Resource findTarget(Request request, Response response) {
+        Resource resource = null;
+        
+        
+        return resource;
+        
+    }
+    
+}

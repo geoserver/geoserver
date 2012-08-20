9f9a0e76699d565cd7ba5b3a0f98b70df6806ec2
Juan Marin Otero <juan.marin.otero@gmail.com>
2012-08-20 15:41:06 -0400

    Add geometry representations

---
+++ src/main/java/org/opengeo/gsr/core/geometry/Point.java
@@ -0,0 +1,47 @@
+package org.opengeo.gsr.core.geometry;
+
+/**
+ * 
+ * @author Juan Marin - OpenGeo
+ * 
+ */
+public class Point extends Geometry {
+
+    private double x;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    private double y;
+
+    private SpatialReference spatialReference;
+
+    public SpatialReference getSpatialReference() {
+        return spatialReference;
+    }
+
+    public void setSpatialReference(SpatialReference spatialReference) {
+        this.spatialReference = spatialReference;
+    }
+
+    public Point(double x, double y, SpatialReference spatialRef) {
+        this.x = x;
+        this.y = y;
+        this.spatialReference = spatialRef;
+        this.geometryType = GeometryType.POINT;
+    }
+
+}

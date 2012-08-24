ac8c26a5f6c79097537913071812ffa08a87f77d
David Winslow <dwinslow@opengeo.org>
2012-08-24 08:12:02 -0400

    Re-introduce reduced accuracy time parsing in WMS and WCS
    This reverts commit 638f1238b3ed60993dd5e17f68afef6a0dcf6216.

--- src/ows/src/test/java/org/geoserver/ows/kvp/TimeKvpParserTest.java
+++ src/ows/src/test/java/org/geoserver/ows/kvp/TimeKvpParserTest.java
@@ -38,12 +38,114 @@ public class TimeKvpParserTest extends TestCase {
      * Format of dates.
      */
     private final static DateFormat format;
-    static{
+    static {
     	 format = new SimpleDateFormat("yyyy-MM-dd'T'HH'Z'");
     	 format.setTimeZone(TimeKvpParser.UTC_TZ);
-
     }
-
+    
+    public void testReducedAccuracyYear() throws Exception {
+    	Calendar c = new GregorianCalendar();
+    	c.setTimeZone(TimeKvpParser.UTC_TZ);
+    	
+    	DateRange year = (DateRange) TimeKvpParser.getFuzzyDate("2000");
+    	c.clear();
+    	c.set(Calendar.YEAR, 2000);
+    	assertRangeStarts(year, c.getTime());
+    	c.set(Calendar.YEAR, 2001);
+    	c.add(Calendar.MILLISECOND, -1);
+    	assertRangeEnds(year, c.getTime());
+    	
+    	year = (DateRange) TimeKvpParser.getFuzzyDate("2001");
+    	c.clear();
+    	c.set(Calendar.YEAR, 2001);
+    	assertRangeStarts(year, c.getTime());
+    	c.set(Calendar.YEAR, 2002);
+    	c.add(Calendar.MILLISECOND, -1);
+    	assertRangeEnds(year, c.getTime());
+    	
+    	year = (DateRange) TimeKvpParser.getFuzzyDate("-6052");
+    	c.clear();
+    	c.set(Calendar.ERA, GregorianCalendar.BC);
+    	c.set(Calendar.YEAR, 6053);
+    	assertRangeStarts(year, c.getTime());
+    	c.set(Calendar.YEAR, 6052);
+    	c.add(Calendar.MILLISECOND, -1);
+    	assertRangeEnds(year, c.getTime());
+    }
+    
+    public void testReducedAccuracyHour() throws Exception {
+        Calendar c = new GregorianCalendar();
+        c.setTimeZone(TimeKvpParser.UTC_TZ);
+        c.clear();
+        
+        DateRange hour = (DateRange) TimeKvpParser.getFuzzyDate("2000-04-04T12Z");
+        c.set(Calendar.YEAR, 2000);
+        c.set(Calendar.MONTH, 3); // 0-indexed
+        c.set(Calendar.DAY_OF_MONTH, 4);
+        c.set(Calendar.HOUR_OF_DAY, 12);
+        assertRangeStarts(hour, c.getTime());
+        c.add(Calendar.HOUR_OF_DAY, 1);
+        c.add(Calendar.MILLISECOND, -1);
+        assertRangeEnds(hour, c.getTime());
+        
+        hour = (DateRange) TimeKvpParser.getFuzzyDate("2005-12-31T23Z"); // selected due to leapsecond at 23:59:60 UTC
+        c.clear();
+        c.set(Calendar.YEAR, 2005);
+        c.set(Calendar.MONTH, 11);
+        c.set(Calendar.DAY_OF_MONTH, 31);
+        c.set(Calendar.HOUR_OF_DAY, 23);
+        assertRangeStarts(hour, c.getTime());
+        c.add(Calendar.HOUR_OF_DAY, 1);
+        c.add(Calendar.MILLISECOND, -1);
+        assertRangeEnds(hour, c.getTime());
+        
+        hour = (DateRange) TimeKvpParser.getFuzzyDate("-25-06-08T17Z");
+        c.clear();
+        c.set(Calendar.ERA, GregorianCalendar.BC);
+        c.set(Calendar.YEAR, 26);
+        c.set(Calendar.MONTH, 5);
+        c.set(Calendar.DAY_OF_MONTH, 8);
+        c.set(Calendar.HOUR_OF_DAY, 17);
+        assertRangeStarts(hour, c.getTime());
+        c.add(Calendar.HOUR_OF_DAY, 1);
+        c.add(Calendar.MILLISECOND, -1);
+        assertRangeEnds(hour, c.getTime());
+    }
+    
+    public void testReducedAccuracyMilliseconds() throws Exception {
+        Calendar c = new GregorianCalendar();
+        c.setTimeZone(TimeKvpParser.UTC_TZ);
+        c.clear();
+        
+        Date instant = (Date) TimeKvpParser.getFuzzyDate("2000-04-04T12:00:00.000Z");
+        c.set(Calendar.YEAR, 2000);
+        c.set(Calendar.MONTH, 3); // 0-indexed
+        c.set(Calendar.DAY_OF_MONTH, 4);
+        c.set(Calendar.HOUR_OF_DAY, 12);
+        assertEquals(instant, c.getTime());
+        
+        instant = (Date) TimeKvpParser.getFuzzyDate("2005-12-31T23:59:60.000Z"); // selected due to leapsecond at 23:59:60 UTC
+        c.clear();
+        c.set(Calendar.YEAR, 2005);
+        c.set(Calendar.MONTH, 11);
+        c.set(Calendar.DAY_OF_MONTH, 31);
+        c.set(Calendar.HOUR_OF_DAY, 23);
+        c.set(Calendar.MINUTE, 59);
+        c.set(Calendar.SECOND, 60);
+        assertEquals(instant, c.getTime());
+        
+        instant = (Date) TimeKvpParser.getFuzzyDate("-25-06-08T17:15:00.123Z");
+        c.clear();
+        c.set(Calendar.ERA, GregorianCalendar.BC);
+        c.set(Calendar.YEAR, 26);
+        c.set(Calendar.MONTH, 5);
+        c.set(Calendar.DAY_OF_MONTH, 8);
+        c.set(Calendar.HOUR_OF_DAY, 17);
+        c.set(Calendar.MINUTE, 15);
+        c.set(Calendar.MILLISECOND, 123);
+        assertEquals(instant, c.getTime());
+    }
+    
     /**
      * Tests only the increment part of the time parameter.
      *
@@ -69,23 +171,23 @@ public class TimeKvpParserTest extends TestCase {
         List l = new ArrayList((Collection) timeKvpParser.parse(PERIOD));
         // Verify that the list contains at least one element.
         assertFalse(l.isEmpty());
-        assertEquals(format.parse("2007-01-01T12Z"), l.get(0));
-        assertEquals(format.parse("2007-01-03T00Z"), l.get(1));
-        assertEquals(format.parse("2007-01-04T12Z"), l.get(2));
-        assertEquals(format.parse("2007-01-06T00Z"), l.get(3));
-        assertEquals(format.parse("2007-01-07T12Z"), l.get(4));
-        assertEquals(format.parse("2007-01-09T00Z"), l.get(5));
-        assertEquals(format.parse("2007-01-10T12Z"), l.get(6));
-        assertEquals(format.parse("2007-01-12T00Z"), l.get(7));
+        assertInstant(format.parse("2007-01-01T12Z"), l.get(0));
+        assertInstant(format.parse("2007-01-03T00Z"), l.get(1));
+        assertInstant(format.parse("2007-01-04T12Z"), l.get(2));
+        assertInstant(format.parse("2007-01-06T00Z"), l.get(3));
+        assertInstant(format.parse("2007-01-07T12Z"), l.get(4));
+        assertInstant(format.parse("2007-01-09T00Z"), l.get(5));
+        assertInstant(format.parse("2007-01-10T12Z"), l.get(6));
+        assertInstant(format.parse("2007-01-12T00Z"), l.get(7));
         
         l = new ArrayList((Collection) timeKvpParser.parse("2007-01-01T12Z/2007-01-31T12Z/PT01S"));
         // Verify that the list contains at least one element.
         assertFalse(l.isEmpty());
         assertTrue(l.size()==100);
-        assertEquals(format.parse("2007-01-01T12Z"), l.get(0));
+        assertInstant(format.parse("2007-01-01T12Z"), l.get(0));
     }
     
-    public void testContinuousInterval() throws ParseException {
+	public void testContinuousInterval() throws ParseException {
         TimeKvpParser timeKvpParser = new TimeKvpParser("TIME");
         List l = new ArrayList((Collection)  timeKvpParser.parse(CONTINUOUS_PERIOD));
         // Verify that the list contains at least one element.
@@ -93,7 +195,9 @@ public class TimeKvpParserTest extends TestCase {
         assertTrue(l.get(0) instanceof DateRange);
         final DateRange range=(DateRange) l.get(0);
         assertEquals(format.parse("2007-01-01T12Z"), range.getMinValue());
-        assertEquals(format.parse("2007-01-31T12Z"), range.getMaxValue());
+        Date end = format.parse("2007-01-31T13Z");
+        end.setTime(end.getTime() - 1);
+        assertEquals(end, range.getMaxValue());
     }
     
     public void testMixedValues() throws ParseException {
@@ -104,11 +208,11 @@ public class TimeKvpParserTest extends TestCase {
         assertTrue(l.get(0) instanceof DateRange);
         final DateRange range=(DateRange) l.get(0);
         assertEquals(format.parse("2007-01-01T12Z"), range.getMinValue());
-        assertEquals(format.parse("2007-01-31T12Z"), range.getMaxValue());
+        Date end = format.parse("2007-01-31T13Z");
+        end.setTime(end.getTime() - 1);
+        assertEquals(end, range.getMaxValue());
         
-        assertTrue(l.get(1) instanceof Date);
-        final Date date=(Date) l.get(1);
-        assertEquals(format.parse("2007-02-01T12Z"), date);
+        assertRange((DateRange)l.get(1), format.parse("2007-02-01T12Z"), format.parse("2007-02-01T13Z"));
     }
     
     public void testInclusions() throws ParseException {
@@ -120,9 +224,7 @@ public class TimeKvpParserTest extends TestCase {
         assertTrue(l.size()==1);
         assertTrue(l.get(0) instanceof DateRange);
         final DateRange range=(DateRange) l.get(0);
-        assertEquals(format.parse("2007-01-01T12Z"), range.getMinValue());
-        assertEquals(format.parse("2007-01-31T12Z"), range.getMaxValue());
-        
+        assertRange(range, format.parse("2007-01-01T12Z"), format.parse("2007-01-31T13Z"));
     }
     
     public void testOrderedValues() throws Exception {
@@ -132,13 +234,11 @@ public class TimeKvpParserTest extends TestCase {
         // Verify that the list contains at least one element.
         assertFalse(l.isEmpty());
         assertTrue(l.size()==5);
-        assertEquals(format.parse("2007-01-01T12Z"),(Date) l.get(0));
-        assertEquals(format.parse("2007-01-05T12Z"),(Date) l.get(1));
-        assertEquals(format.parse("2007-01-12T12Z"),(Date) l.get(2));
-        assertEquals(format.parse("2007-01-17T12Z"),(Date) l.get(3));
-        assertEquals(format.parse("2007-01-29T12Z"),(Date) l.get(4));
-        
-        
+        assertRange((DateRange) l.get(0), format.parse("2007-01-01T12Z"), format.parse("2007-01-01T13Z"));
+        assertRange((DateRange) l.get(1), format.parse("2007-01-05T12Z"), format.parse("2007-01-05T13Z"));
+        assertRange((DateRange) l.get(2), format.parse("2007-01-12T12Z"), format.parse("2007-01-12T13Z"));
+        assertRange((DateRange) l.get(3), format.parse("2007-01-17T12Z"), format.parse("2007-01-17T13Z"));
+        assertRange((DateRange) l.get(4), format.parse("2007-01-29T12Z"), format.parse("2007-01-29T13Z"));
     }
     
     public void testNegativeYearCompliance() throws Exception {
@@ -147,29 +247,67 @@ public class TimeKvpParserTest extends TestCase {
         cal.setTimeZone(TimeZone.getTimeZone("GMT"));
         
         // base assertion - 0001 is year 1
-        Date date = (Date) ((List)timeKvpParser.parse("0001-06-01")).get(0);
-        cal.setTime(date);
+        DateRange date = (DateRange) ((List)timeKvpParser.parse("01-06-01")).get(0);
+        cal.setTime(date.getMinValue());
         assertEquals(1, cal.get(Calendar.YEAR));
         assertEquals(GregorianCalendar.AD, cal.get(Calendar.ERA));
         
-        date = (Date) ((List)timeKvpParser.parse("0000-06-01")).get(0);
-        cal.setTime(date);
+        date = (DateRange) ((List)timeKvpParser.parse("00-06-01")).get(0);
+        cal.setTime(date.getMinValue());
         // calendar calls it year 1, ISO spec means it's year 0
         // but we're just parsing here...
         assertEquals(1, cal.get(Calendar.YEAR));
         assertEquals(GregorianCalendar.BC, cal.get(Calendar.ERA));
         
         // so, the next year should be 2
-        date = (Date) ((List)timeKvpParser.parse("-0001-06-01")).get(0);
-        cal.setTime(date);
+        date = (DateRange) ((List)timeKvpParser.parse("-01-06-01")).get(0);
+        cal.setTime(date.getMinValue());
         assertEquals(2, cal.get(Calendar.YEAR));
         assertEquals(GregorianCalendar.BC, cal.get(Calendar.ERA));
         
         // now, big negative year compliance (see the spec, appendix D 2.2, pp 57-58)
-        date = (Date) ((List)timeKvpParser.parse("-18000-06-01")).get(0);
-        cal.setTime(date);
+        date = (DateRange) ((List)timeKvpParser.parse("-18000-06-01")).get(0);
+        cal.setTime(date.getMinValue());
         assertEquals(18001, cal.get(Calendar.YEAR));
         assertEquals(GregorianCalendar.BC, cal.get(Calendar.ERA));
+    }
+
+    private static void assertInstant(Date expected, Object object) {
+    	if (object instanceof DateRange) { 
+	    	assertEquals(object + " Should start at", expected, ((DateRange)object).getMinValue());
+	    	assertEquals(object + " Should end at", expected, ((DateRange)object).getMaxValue());
+    	} else if (object instanceof Date) {
+    		assertEquals(expected, object);
+    	} else {
+    		fail("Should have a DateRange: " + object);
+    	}
+	}
+    
+    private static void assertRange(DateRange range, Date start, Date end) {
+    	assertRangeStarts(range, start);
+    	assertRangeEnds(range, new Date(end.getTime() - 1));
+    }
+
+    public static void assertRangeLength(DateRange range, long expectedLength) {
+    	if (range.getMinValue() == null)
+    		fail("Expected finite range, saw: " + range);
+    	if (range.getMaxValue() == null)
+    		fail("Expected finite range, saw: " + range);
+    	long min = range.getMinValue().getTime();
+    	long max = range.getMaxValue().getTime();
+    	assertEquals("Range " + range + " should have length", expectedLength, max - min);
+    }
+    
+    public static void assertRangeStarts(DateRange range, Date expectedStart) {
+    	if (range.getMinValue() == null) 
+    		fail("Expected valid start date in range " + range);
+    	assertEquals("Range " + range + " should have start", expectedStart, range.getMinValue());
+    }
+    
+    public static void assertRangeEnds(DateRange range, Date expectedEnd) {
+    	if (range.getMaxValue() == null)
+    		fail("Expected valid end date in range " + range);
+    	assertEquals("Range " + range + " should have end", expectedEnd, range.getMaxValue());
     }
 }
 

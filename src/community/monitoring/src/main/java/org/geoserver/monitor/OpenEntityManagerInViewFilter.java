aeb2034f2c97b2fb63b193f71c8040492530c7dd
Mark Paxton <mark@innasys.com>
2012-08-24 13:26:23 +0100

    Adding a filter to provide open entity manager in view support for JPA persistance setups, equivalent to the open session in view filter for hibernate.

---
+++ src/community/monitoring/src/main/java/org/geoserver/monitor/OpenEntityManagerInViewFilter.java
@@ -0,0 +1,8 @@
+package org.geoserver.monitor;
+
+import org.geoserver.filters.GeoServerFilter;
+
+public class OpenEntityManagerInViewFilter extends
+	org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter implements GeoServerFilter {
+
+}

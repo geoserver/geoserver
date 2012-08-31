29d6d02e05e39e21e40860d40d22f4e69a21951b
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2012-08-31 14:05:47 +0200

    GEOS-5292: fixing sldService

--- src/community/sldService/src/main/java/org/geoserver/sldservice/rest/finder/LayerAttributesFinder.java
+++ src/community/sldService/src/main/java/org/geoserver/sldservice/rest/finder/LayerAttributesFinder.java
@@ -20,30 +20,37 @@
 package org.geoserver.sldservice.rest.finder;
 
 import org.geoserver.catalog.Catalog;
-import org.geoserver.catalog.rest.AbstractCatalogFinder;
 import org.geoserver.rest.RestletException;
 import org.geoserver.sldservice.rest.resource.ListAttributesResource;
+import org.restlet.Finder;
 import org.restlet.data.Method;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
 import org.restlet.data.Status;
 import org.restlet.resource.Resource;
 
-public class LayerAttributesFinder extends AbstractCatalogFinder {
+public class LayerAttributesFinder extends Finder {
 
-    public LayerAttributesFinder(Catalog catalog) {
-        super(catalog);
+    /**
+     * reference to the catalog
+     */
+    protected Catalog catalog;
+    
+    protected LayerAttributesFinder(Catalog catalog) {
+        this.catalog = catalog;
     }
     
     @Override
     public Resource findTarget(Request request, Response response) {
-        String layer = (String) request.getAttributes().get( "layer" );
-        
-        if ( layer != null && request.getMethod() == Method.GET ) {
-            return new ListAttributesResource(getContext(),request,response,catalog);
+        String layer = (String) request.getAttributes().get("layer");
+    
+        if (layer != null && request.getMethod() == Method.GET) {
+            return new ListAttributesResource(getContext(), request, response,
+                    catalog);
         }
-        
-        throw new RestletException( "No such layer: " + layer, Status.CLIENT_ERROR_NOT_FOUND );
+    
+        throw new RestletException("No such layer: " + layer,
+                Status.CLIENT_ERROR_NOT_FOUND);
     }
 
 }

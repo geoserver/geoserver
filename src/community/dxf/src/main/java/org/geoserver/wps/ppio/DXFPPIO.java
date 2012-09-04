45e2df698e087371965849e9e396764875209656
Peter Hopfgartner <peter.hopfgartner@r3-gis.com>
2012-09-04 18:30:32 +0200

    corrected comments on code authoring

--- src/community/dxf/src/main/java/org/geoserver/wps/ppio/DXFPPIO.java
+++ src/community/dxf/src/main/java/org/geoserver/wps/ppio/DXFPPIO.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org, 2012 - R3 GIS. All rights reserved.
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
  */
@@ -19,9 +19,9 @@ import org.geoserver.wfs.response.dxf.DXFWriter;
 import org.geoserver.wfs.response.dxf.DXFWriterFinder;
 
 /**
- * Inputs and outputs feature collections in GeoJSON format using gt-geojson
+ * Outputs feature collections in DXF format
  * 
- * @author Andrea Aime - OpenGeo
+ * @author Andrea Aime - OpenGeo, Peter Hopfgartner - R3 GIS
  * 
  */
 public class DXFPPIO extends CDataPPIO {
@@ -32,7 +32,6 @@ public class DXFPPIO extends CDataPPIO {
 
     @Override
     public void encode(Object value, OutputStream os) throws IOException {
-    	// the following code resembles org.geoserver.wfs.response.DXFOutputFormat
         BufferedWriter w = new BufferedWriter(new OutputStreamWriter(os));
         DXFWriter dxfWriter = DXFWriterFinder.getWriter("14", w);
         String[] names = {"wps_result"};

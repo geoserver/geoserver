6a217e43e7913f6a202c16447d105213169d8d4f
Juan Marin Otero <juan.marin.otero@gmail.com>
2012-09-11 11:19:24 -0400

    Add LabelInfo representation

---
+++ src/main/java/org/opengeo/gsr/core/symbol/LabelInfo.java
@@ -0,0 +1,30 @@
+/* Copyright (c) 2001 - 2012 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.opengeo.gsr.core.symbol;
+
+import java.util.List;
+
+/**
+ * 
+ * @author Juan Marin, OpenGeo
+ * 
+ */
+public class LabelInfo {
+
+    private List<Label> type;
+
+    public List<Label> getType() {
+        return type;
+    }
+
+    public void setType(List<Label> labels) {
+        this.type = labels;
+    }
+
+    public LabelInfo(List<Label> labels) {
+        super();
+        this.type = labels;
+    }
+}
--- src/test/java/org/opengeo/gsr/core/symbol/LabelSchemaTest.java
+++ src/test/java/org/opengeo/gsr/core/symbol/LabelSchemaTest.java
@@ -2,6 +2,9 @@ package org.opengeo.gsr.core.symbol;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.opengeo.gsr.JsonSchemaTest;
 
@@ -16,9 +19,9 @@ public class LabelSchemaTest extends JsonSchemaTest {
                 FontDecorationEnum.NONE);
         TextSymbol textSymbol = new TextSymbol(0, 0, 0, color, backgroundColor, borderLineColor,
                 VerticalAlignmentEnum.BOTTOM, HorizontalAlignmentEnum.LEFT, false, font);
-        Label label = new PointLabel(PointLabelPlacementEnum.ABOVE_RIGHT, "[NAME]", false,
+        Label pointLabel = new PointLabel(PointLabelPlacementEnum.ABOVE_RIGHT, "[NAME]", false,
                 textSymbol, 0, 0);
-        String json = getJson(label);
+        String json = getJson(pointLabel);
         assertTrue(validateJSON(json, "gsr/1.0/label.json"));
     }
 
@@ -31,9 +34,9 @@ public class LabelSchemaTest extends JsonSchemaTest {
                 FontDecorationEnum.NONE);
         TextSymbol textSymbol = new TextSymbol(0, 0, 0, color, backgroundColor, borderLineColor,
                 VerticalAlignmentEnum.BOTTOM, HorizontalAlignmentEnum.LEFT, false, font);
-        Label label = new LineLabel(LineLabelPlacementEnum.ABOVE_BEFORE, "[NAME]", false,
+        Label lineLabel = new LineLabel(LineLabelPlacementEnum.ABOVE_BEFORE, "[NAME]", false,
                 textSymbol, 0, 0);
-        String json = getJson(label);
+        String json = getJson(lineLabel);
         assertTrue(validateJSON(json, "gsr/1.0/label.json"));
     }
 
@@ -46,10 +49,46 @@ public class LabelSchemaTest extends JsonSchemaTest {
                 FontDecorationEnum.NONE);
         TextSymbol textSymbol = new TextSymbol(0, 0, 0, color, backgroundColor, borderLineColor,
                 VerticalAlignmentEnum.BOTTOM, HorizontalAlignmentEnum.LEFT, false, font);
-        Label label = new PolygonLabel(PolygonLabelPlacementEnum.ALWAYS_HORIZONTAL, "[NAME]",
-                false, textSymbol, 0, 0);
-        String json = getJson(label);
+        Label polygonLabel = new PolygonLabel(PolygonLabelPlacementEnum.ALWAYS_HORIZONTAL,
+                "[NAME]", false, textSymbol, 0, 0);
+        String json = getJson(polygonLabel);
         assertTrue(validateJSON(json, "gsr/1.0/label.json"));
+    }
+
+    @Test
+    public void testLabelInfoJsonSchema() throws Exception {
+        List<Label> labels = new ArrayList<Label>();
+        int[] color = { 78, 78, 78, 255 };
+        int[] lineColor = { 78, 78, 78, 255 };
+        int[] polygonColor = { 78, 78, 78, 255 };
+        int[] backgroundColor = null;
+        int[] borderLineColor = null;
+        Font pointFont = new Font("Arial", 12, FontStyleEnum.NORMAL, FontWeightEnum.BOLD,
+                FontDecorationEnum.NONE);
+        Font lineFont = new Font("Arial", 12, FontStyleEnum.NORMAL, FontWeightEnum.BOLD,
+                FontDecorationEnum.NONE);
+        Font polygonFont = new Font("Arial", 12, FontStyleEnum.NORMAL, FontWeightEnum.BOLD,
+                FontDecorationEnum.NONE);
+        TextSymbol pointTextSymbol = new TextSymbol(0, 0, 0, color, backgroundColor,
+                borderLineColor, VerticalAlignmentEnum.BOTTOM, HorizontalAlignmentEnum.LEFT, false,
+                pointFont);
+        Label pointLabel = new PointLabel(PointLabelPlacementEnum.ABOVE_RIGHT, "[NAME]", false,
+                pointTextSymbol, 0, 0);
+        TextSymbol lineTextSymbol = new TextSymbol(0, 0, 0, lineColor, backgroundColor,
+                borderLineColor, VerticalAlignmentEnum.BOTTOM, HorizontalAlignmentEnum.LEFT, false,
+                lineFont);
+        Label lineLabel = new LineLabel(LineLabelPlacementEnum.ABOVE_BEFORE, "[NAME]", false,
+                lineTextSymbol, 0, 0);
+        TextSymbol polygonTextSymbol = new TextSymbol(0, 0, 0, polygonColor, backgroundColor,
+                borderLineColor, VerticalAlignmentEnum.BOTTOM, HorizontalAlignmentEnum.LEFT, false,
+                polygonFont);
+        Label polygonLabel = new PolygonLabel(PolygonLabelPlacementEnum.ALWAYS_HORIZONTAL,
+                "[TAG]", false, polygonTextSymbol, 0, 0);
+        labels.add(pointLabel);
+        labels.add(lineLabel);
+        labels.add(polygonLabel);
+        String json = getJson(labels);
+        assertTrue(validateJSON(json, "gsr/1.0/labelInfo.json"));
     }
 
 }

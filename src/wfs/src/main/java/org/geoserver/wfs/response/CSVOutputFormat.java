2d2563fb57df204f4d1fcdb910f1b7680c1f5305
Robert Coup <robert@coup.net.nz>
2012-09-17 14:51:54 +1200

    [GEOS-5312] Fix embedded Windows newlines in WFS CSV output. Use a pre-compiled, simpler regex and find() rather than needless work.

--- src/wfs/src/main/java/org/geoserver/wfs/response/CSVOutputFormat.java
+++ src/wfs/src/main/java/org/geoserver/wfs/response/CSVOutputFormat.java
@@ -11,6 +11,7 @@ import java.io.OutputStreamWriter;
 import java.text.NumberFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.regex.Pattern;
 
 import javax.xml.namespace.QName;
 
@@ -157,20 +158,22 @@ public class CSVOutputFormat extends WFSGetFeatureOutputFormat {
      * delimited by double quotes, and also that double quotes within fields
      * must be escaped.  This method takes a field and returns one that
      * obeys the CSV spec.
-     */    
+     */
+    Pattern CSV_ESCAPES = Pattern.compile("[\"\n,\r]");
+
     private String prepCSVField(String field){
-    	// "embedded double-quote characters must be represented by a pair of double-quote characters."
-    	String mod = field.replaceAll("\"", "\"\"");
-    	
-    	/*
-    	 * Enclose string in double quotes if it contains double quotes, commas, or newlines
-    	 */
-    	if(mod.matches(".*(\"|\n|,).*")){
-    		mod = "\"" + mod + "\"";
-    	}
-    	
-		return mod;
-    	
+        // "embedded double-quote characters must be represented by a pair of double-quote characters."
+        String mod = field.replaceAll("\"", "\"\"");
+
+        /*
+         * Enclose string in double quotes if it contains double quotes, commas, or newlines
+         */
+        if(CSV_ESCAPES.matcher(mod).find()){
+            mod = "\"" + mod + "\"";
+        }
+
+        return mod;
+
     }
     
     @Override

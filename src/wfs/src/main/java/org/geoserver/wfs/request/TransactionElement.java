b220310ff3a4d08a5c0bb48b8132915a711d3e5b
Jody Garnett <jody.garnett@gmail.com>
2012-11-17 23:06:51 +1000

    Fill in missing headers, and provide a correct header for Browser (GEOS-5456)

--- src/wfs/src/main/java/org/geoserver/wfs/request/TransactionElement.java
+++ src/wfs/src/main/java/org/geoserver/wfs/request/TransactionElement.java
@@ -1,3 +1,7 @@
+/* Copyright (c) 2012 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.wfs.request;
 
 import org.eclipse.emf.ecore.EObject;

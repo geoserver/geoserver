524c90d1b9e6128b6ad5ce4c632eefd515b96659
Jody Garnett <jody.garnett@gmail.com>
2012-11-18 17:21:38 +1000

    Header for component builder

--- src/web/core/src/test/java/org/geoserver/web/ComponentBuilder.java
+++ src/web/core/src/test/java/org/geoserver/web/ComponentBuilder.java
@@ -1,5 +1,6 @@
-/**
- * 
+/* Copyright (c) 2001 - 2012 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
  */
 package org.geoserver.web;
 

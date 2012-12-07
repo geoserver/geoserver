eb2a80e0400c8f5cc171328f3abd64f6c87c3a9c
Andrea Aime <andrea.aime@gmail.com>
2012-12-07 10:40:33 +0100

    First bits of support for GetCoverage

--- src/community/wcs2_0/src/main/java/org/geoserver/wcs2_0/kvp/WCS20GetCapabilitiesRequestReader.java
+++ src/community/wcs2_0/src/main/java/org/geoserver/wcs2_0/kvp/WCS20GetCapabilitiesRequestReader.java
@@ -5,13 +5,12 @@
 package org.geoserver.wcs2_0.kvp;
 
 import java.util.Map;
-import net.opengis.ows20.AcceptVersionsType;
 
+import net.opengis.ows20.AcceptVersionsType;
 import net.opengis.wcs20.GetCapabilitiesType;
 import net.opengis.wcs20.Wcs20Factory;
-import org.eclipse.emf.ecore.EObject;
-import org.geoserver.ows.kvp.AcceptVersionsKvpParser;
 
+import org.geoserver.ows.kvp.AcceptVersionsKvpParser;
 import org.geoserver.ows.kvp.EMFKvpRequestReader;
 
 /**

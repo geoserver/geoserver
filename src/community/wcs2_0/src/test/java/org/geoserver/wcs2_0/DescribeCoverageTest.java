9a69ac7934ad30eac37b3c76b90348eb2033f518
Andrea Aime <andrea.aime@gmail.com>
2012-12-07 19:30:21 +0100

    Add test for DescribeCoveage XML parsing

--- src/community/wcs2_0/src/test/java/org/geoserver/wcs2_0/DescribeCoverageTest.java
+++ src/community/wcs2_0/src/test/java/org/geoserver/wcs2_0/DescribeCoverageTest.java
@@ -35,4 +35,22 @@ public class DescribeCoverageTest extends WCSTestSupport {
         
         checkValidationErrors(dom, WCS20_SCHEMA);
     }
+    
+    @Test
+    public void testDescribeCoveragePOST() throws Exception {
+        String request = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + 
+        		"<wcs:DescribeCoverage xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'\n" + 
+        		"  xmlns:wcs='http://www.opengis.net/wcs/2.0' xmlns:gml='http://www.opengis.net/gml/3.2'\n" + 
+        		"  xsi:schemaLocation='http://www.opengis.net/wcs/2.0 http://schemas.opengis.net/wcs/2.0/wcsAll.xsd'\n" + 
+        		"  service=\"WCS\" version=\"2.0.1\">\n" + 
+        		"  <wcs:CoverageId>wcs__BlueMarble</wcs:CoverageId>\n" + 
+        		"</wcs:DescribeCoverage>";
+        
+        Document dom = postAsDOM("wcs", request);
+        assertNotNull(dom);
+        print(dom, System.out);
+        
+        checkValidationErrors(dom, WCS20_SCHEMA);
+        
+    }
 }

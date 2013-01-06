1619729fbb5d7ee3b47346d011c544ce6cd2ec02
Davide Savazzi <davide@davidesavazzi.net>
2013-01-06 18:18:27 +0100

    GEOS-5533: fixed critical bug in FreemarkerTemplateResource

--- src/restconfig/src/main/java/org/geoserver/catalog/rest/FreemarkerTemplateResource.java
+++ src/restconfig/src/main/java/org/geoserver/catalog/rest/FreemarkerTemplateResource.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2001 - 2009 TOPP - www.openplans.org.  All rights reserved.
+/* Copyright (c) 2012 - 2013 TOPP - www.openplans.org.  All rights reserved.
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
  */
@@ -83,7 +83,7 @@ public class FreemarkerTemplateResource extends StoreFileResource {
                 LOGGER.info("PUT file: mimetype=" + mediaType + ", path=" + directory.getAbsolutePath());
             }
             
-            return RESTUtils.handleBinUpload(getAttribute("template") + "." + MEDIATYPE_FTL_EXTENSION, directory, getRequest());
+            return RESTUtils.handleBinUpload(getAttribute("template") + "." + MEDIATYPE_FTL_EXTENSION, directory, false, getRequest());
         } catch (IOException e) {
             throw new RestletException(e.getMessage(), Status.SERVER_ERROR_INTERNAL, e);
         }

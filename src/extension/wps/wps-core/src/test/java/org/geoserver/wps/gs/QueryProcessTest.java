a8c8a0620f48e66221102fecb1ad6e7842539f33
Andrea Aime <andrea.aime@gmail.com>
2013-01-18 09:01:48 +0100

    Fixing the build

--- src/extension/wps/wps-core/src/test/java/org/geoserver/wps/gs/QueryProcessTest.java
+++ src/extension/wps/wps-core/src/test/java/org/geoserver/wps/gs/QueryProcessTest.java
@@ -115,7 +115,6 @@ public class QueryProcessTest extends WPSTestSupport {
         assertXpathEvaluatesTo("1", "count(" + base + "/feature:ADDRESS)", d);
     }
     
-    @Test
     public void testFilterOGC() throws Exception {
         String request = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
                 + "<wps:Execute version=\"1.0.0\" service=\"WPS\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://www.opengis.net/wps/1.0.0\" xmlns:wfs=\"http://www.opengis.net/wfs\" xmlns:wps=\"http://www.opengis.net/wps/1.0.0\" xmlns:ows=\"http://www.opengis.net/ows/1.1\" xmlns:gml=\"http://www.opengis.net/gml\" xmlns:ogc=\"http://www.opengis.net/ogc\" xmlns:wcs=\"http://www.opengis.net/wcs/1.1.1\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xsi:schemaLocation=\"http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd\">\n"
@@ -170,7 +169,6 @@ public class QueryProcessTest extends WPSTestSupport {
     }
 
     
-    @Test
     public void testFilterOGCCData() throws Exception {
         String request = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
                 + "<wps:Execute version=\"1.0.0\" service=\"WPS\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://www.opengis.net/wps/1.0.0\" xmlns:wfs=\"http://www.opengis.net/wfs\" xmlns:wps=\"http://www.opengis.net/wps/1.0.0\" xmlns:ows=\"http://www.opengis.net/ows/1.1\" xmlns:gml=\"http://www.opengis.net/gml\" xmlns:ogc=\"http://www.opengis.net/ogc\" xmlns:wcs=\"http://www.opengis.net/wcs/1.1.1\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xsi:schemaLocation=\"http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd\">\n"

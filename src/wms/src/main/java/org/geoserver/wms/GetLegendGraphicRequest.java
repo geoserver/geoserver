619598a578b5c87910d96977200564bc5c456618
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2013-01-22 12:18:03 +0100

    Added symbols size handling in WMS GetLegendGraphic

--- src/wms/src/main/java/org/geoserver/wms/GetLegendGraphicRequest.java
+++ src/wms/src/main/java/org/geoserver/wms/GetLegendGraphicRequest.java
@@ -269,9 +269,10 @@ public class GetLegendGraphicRequest extends WMSRequest {
      * rendering. Anything of the following works: "yes", "true", "1". Anything else means false.
      * <li><code>forceLabels</code>: "on" means labels will always be drawn, even if only one rule
      * is available. "off" means labels will never be drawn, even if multiple rules are available.
-     * <li><code>forceTitles</code>: "on" means layer titles for layergroups will always be drawn, 
-     * even if only one layer is available. "off" means titles will never be drawn, even if multiple
-     * layers are available.
+     * <li><code>forceTitles</code>: "off" means titles will never be drawn, even if multiple layers
+     * are available.
+     * <li><code>minSymbolSize</code>: a number defining the minimum size to be rendered for a 
+     * symbol (defaults to 3).
      * </ul>
      * </p>
      * 

bd5d6f5eecd1dd9d367f5628fa388c8ac25cf08a
jdeolive <jdeolive@opengeo.org>
2013-01-22 17:45:44 -0700

    updating copyright header, added openplans copyright, GEOS-5460

--- src/main/src/main/java/org/geoserver/filters/GZIPResponseStream.java
+++ src/main/src/main/java/org/geoserver/filters/GZIPResponseStream.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2007 - 2013 OpenPlans - www.openplans.org. All rights reserved.
  * Copyright 2003 Jayson Falkner (jayson@jspinsider.com)
  * This code is from "Servlets and JavaServer pages; the J2EE Web Tier",
  * http://www.jspbook.com. You may freely use the code both commercially
@@ -6,7 +7,7 @@
  * the book and help support the authors, development of more free code,
  * and the JSP/Servlet/J2EE community.
  *
- * Modified by David Winslow <dwinslow@openplans.org>
+ * Modified by David Winslow <dwinslow@openplans.org> on 2007-12-13.
  */
 package org.geoserver.filters;
 

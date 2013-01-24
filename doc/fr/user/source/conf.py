e5753005a2ad4c3c72338dcc1152b323c192ce79
jive <jody.garnett@gmail.com>
2013-01-24 09:42:23 +0000

    updating version numbers and release notes for 2.3-beta1

--- doc/fr/user/source/conf.py
+++ doc/fr/user/source/conf.py
@@ -47,7 +47,7 @@ copyright = u'2012 TOPP'
 # The short X.Y version.
 version = '2.3'
 # The full version, including alpha/beta/rc tags.
-release = '2.3-SNAPSHOT'
+release = '2.3-beta1'
 # Users don't need to see the "SNAPSHOT" notation when it's there
 if release.find('SNAPSHOT') != -1:
    release = '2.3.x'

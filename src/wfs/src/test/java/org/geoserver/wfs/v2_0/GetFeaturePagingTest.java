a78a0ea64f70fd2778038be25ed71347ec8ea945
Andrea Aime <andrea.aime@gmail.com>
2013-01-26 19:32:09 +0100

    [GEOS-5556] count=0 - WFS GetFeature

--- src/wfs/src/test/java/org/geoserver/wfs/v2_0/GetFeaturePagingTest.java
+++ src/wfs/src/test/java/org/geoserver/wfs/v2_0/GetFeaturePagingTest.java
@@ -552,5 +552,14 @@ public class GetFeaturePagingTest extends WFS20TestSupport {
         assertStartIndexCount(doc, "previous", 0, 15);
         assertFalse(doc.getDocumentElement().hasAttribute("next"));
     }
+    
+    @Test
+    public void testCountZero() throws Exception {
+        Document doc = getAsDOM("/wfs?request=GetFeature&version=2.0.0&service=wfs&" +
+                "typename=gs:Fifteen&count=0");
+        XMLAssert.assertXpathExists("/wfs:FeatureCollection", doc);
+        XMLAssert.assertXpathEvaluatesTo("0", "/wfs:FeatureCollection/@numberMatched", doc);
+        XMLAssert.assertXpathEvaluatesTo("0", "/wfs:FeatureCollection/@numberReturned", doc);
+    }
 
 }

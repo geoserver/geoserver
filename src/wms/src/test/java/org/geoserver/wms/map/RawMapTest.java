8f4e2a842f16f97d5b23a180cff8e17c5d90fa54
jdeolive <jdeolive@opengeo.org>
2013-02-04 11:01:24 -0700

    added constructor to RawMap taking input stream

---
+++ src/wms/src/test/java/org/geoserver/wms/map/RawMapTest.java
@@ -0,0 +1,24 @@
+package org.geoserver.wms.map;
+
+import static org.easymock.classextension.EasyMock.*;
+import java.io.InputStream;
+
+
+import org.geoserver.wms.WMSMapContent;
+import org.junit.Test;
+
+public class RawMapTest {
+
+    @Test
+    public void testInputStream() throws Exception {
+        InputStream stream = createMock(InputStream.class);
+        expect(stream.read((byte[])anyObject())).andReturn(-1).once();
+        replay(stream);
+
+        WMSMapContent map = createNiceMock(WMSMapContent.class);
+        replay(map);
+
+        new RawMap(map, stream, "text/plain").writeTo(null);
+        verify(stream);
+    }
+}

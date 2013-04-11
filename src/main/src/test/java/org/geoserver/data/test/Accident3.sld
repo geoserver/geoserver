942b4febacd66c4141c15ed345a082a5fc79cdbf
Mauricio Pazos <mauricio.pazos@gmail.com>
2013-04-11 18:38:17 +0200

    it was improved the default scale denominator management

---
+++ src/main/src/test/java/org/geoserver/data/test/Accident3.sld
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<StyledLayerDescriptor version="1.0.0" 
+    xsi:schemaLocation="http://www.opengis.net/sld StyledLayerDescriptor.xsd" 
+    xmlns="http://www.opengis.net/sld" 
+    xmlns:ogc="http://www.opengis.net/ogc" 
+    xmlns:xlink="http://www.w3.org/1999/xlink" 
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <NamedLayer>
+    <Name>Zoom-based point</Name>
+    <UserStyle>
+      <Title>GeoServer SLD Cook Book: Zoom-based point</Title>
+      <FeatureTypeStyle>
+        <Rule>
+          <Name>Large</Name>
+          <MinScaleDenominator>320000000</MinScaleDenominator>
+          <PointSymbolizer>
+            <Graphic>
+              <Mark>
+                <WellKnownName>circle</WellKnownName>
+                <Fill>
+                  <CssParameter name="fill">#CC3300</CssParameter>
+                </Fill>
+              </Mark>
+              <Size>12</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+        <Rule>
+          <Name>Small</Name>
+          <MinScaleDenominator>640000000</MinScaleDenominator>
+          <PointSymbolizer>
+            <Graphic>
+              <Mark>
+                <WellKnownName>circle</WellKnownName>
+                <Fill>
+                  <CssParameter name="fill">#CC3300</CssParameter>
+                </Fill>
+              </Mark>
+              <Size>4</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+      </FeatureTypeStyle>
+    </UserStyle>
+  </NamedLayer>
+</StyledLayerDescriptor> 
--- src/wms/src/test/java/org/geoserver/wms/capabilities/GetCapabilitiesScaleHintTest.java
+++ src/wms/src/test/java/org/geoserver/wms/capabilities/GetCapabilitiesScaleHintTest.java
@@ -4,6 +4,7 @@
 package org.geoserver.wms.capabilities;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -19,6 +20,7 @@ import org.custommonkey.xmlunit.exceptions.XpathException;
 import org.geoserver.catalog.Catalog;
 import org.geoserver.catalog.LayerInfo;
 import org.geoserver.catalog.StyleInfo;
+import org.geoserver.config.GeoServerInfo;
 import org.geoserver.data.test.MockData;
 import org.geoserver.data.test.SystemTestData;
 import org.geoserver.test.GeoServerSystemTestSupport;
@@ -51,6 +53,8 @@ public class GetCapabilitiesScaleHintTest extends GeoServerSystemTestSupport {
     /** Test layers */
     public static final QName REGIONATED = new QName(MockData.SF_URI, "Regionated", MockData.SF_PREFIX);
     public static final QName ACCIDENT = new QName(MockData.SF_URI, "Accident", MockData.SF_PREFIX);
+    public static final QName ACCIDENT2 = new QName(MockData.SF_URI, "Accident2", MockData.SF_PREFIX);
+    public static final QName ACCIDENT3 = new QName(MockData.SF_URI, "Accident3", MockData.SF_PREFIX);
     
 
     private Catalog catalog;
@@ -111,14 +115,63 @@ public class GetCapabilitiesScaleHintTest extends GeoServerSystemTestSupport {
 			LayerInfo layerInfo =  this.catalog.getLayerByName(layerId);
 			layerInfo.setDefaultStyle(defaultStyle);
 		}
+		// creates the Accident2 layer and adds it to the geoserver catalog. The minimum scale denominator was not set in the style file. 
+		{
+			testData.addVectorLayer(
+	                ACCIDENT2,
+	                null, 
+	                ACCIDENT2.getLocalPart()+".properties",
+	                getClass(), 
+	                this.catalog             
+            );
+	
+			final String styleName = "Accident2";
+			testData.addStyle(styleName, this.catalog);
+			
+			StyleInfo defaultStyle = this.catalog.getStyleByName(styleName);
+			
+			String layerId = getLayerId(ACCIDENT2);
+			LayerInfo layerInfo =  this.catalog.getLayerByName(layerId);
+			layerInfo.setDefaultStyle(defaultStyle);
+		}
+		
+        WMS wms = new WMS(getGeoServer());
+        GeoServerInfo geoserverInfo = wms.getGeoServer().getGlobal();
+		
+		// creates the Accident3 layer and adds it to the geoserver catalog. The maximum scale denominator was not set in the style file. 
+		{
+			testData.addVectorLayer(
+	                ACCIDENT3,
+	                null, 
+	                ACCIDENT3.getLocalPart()+".properties",
+	                getClass(), 
+	                this.catalog             
+            );
+	
+			final String styleName = "Accident3";
+			testData.addStyle(styleName, this.catalog);
+			
+			StyleInfo defaultStyle = this.catalog.getStyleByName(styleName);
+			
+			String layerId = getLayerId(ACCIDENT3);
+			LayerInfo layerInfo =  this.catalog.getLayerByName(layerId);
+			layerInfo.setDefaultStyle(defaultStyle);
+		}
+		
 	}
 
 
     /**
      * Default values for ScaleHint should be set.
+     * 
      * <pre>
-     * Min: 0.0
-     * Max: infinity
+     * The computation of Min and Max values return: 
+     * 		Min: 0.0
+     * 		Max: infinity
+     * 
+     * Capabilities document Expected:
+     *  
+     * 		ScaleHint element shouldn't be generated.
      * </pre>
      * 
      * @throws Exception
@@ -131,12 +184,73 @@ public class GetCapabilitiesScaleHintTest extends GeoServerSystemTestSupport {
 		Element layerElement= searchLayerElement(getLayerId(ACCIDENT), dom);
 		
 		NodeList scaleNode = layerElement.getElementsByTagName("ScaleHint");
+		
 		Element scaleElement = (Element)scaleNode.item(0);
-            
-        assertEquals(0.0, Double.valueOf(scaleElement.getAttribute("min")));
-        assertEquals(Double.POSITIVE_INFINITY, Double.valueOf(scaleElement.getAttribute("max")));
+
+		assertTrue(scaleElement == null); // scale hint is not generated
     }
 
+    /**
+     * Default values for ScaleHint should be set.
+     * 
+     * <pre>
+     * The computation of Min and Max values return: 
+     * 		Min: 0.0
+     * 		Max: a value
+     * 
+     * Capabilities document Expected:
+     *  
+     *   <ScaleHint min=0 max=value/>     
+     * </pre>
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void scaleHintDefaultMinValue()throws Exception{
+        
+    	Document dom = findCapabilities();
+
+		Element layerElement= searchLayerElement(getLayerId(ACCIDENT2), dom);
+		
+		NodeList scaleNode = layerElement.getElementsByTagName("ScaleHint");
+		
+		Element scaleElement = (Element)scaleNode.item(0);
+
+        assertEquals(0.0, Double.valueOf(scaleElement.getAttribute("min")));
+	    assertEquals(Double.valueOf(640000000), Double.valueOf(scaleElement.getAttribute("max")));
+
+    }
+
+    /**
+     * Default values for ScaleHint should be set.
+     * 
+     * <pre>
+     * The computation of Min and Max values return: 
+     * 		Min: a value
+     * 		Max: Infinity
+     * 
+     * Capabilities document Expected:
+     *  
+     *   <ScaleHint min=value max=infinity/>     
+     * </pre>
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void scaleHintDefaultMaxValue()throws Exception{
+        
+    	Document dom = findCapabilities();
+
+		Element layerElement= searchLayerElement(getLayerId(ACCIDENT3), dom);
+		
+		NodeList scaleNode = layerElement.getElementsByTagName("ScaleHint");
+		
+		Element scaleElement = (Element)scaleNode.item(0);
+
+        assertEquals(Double.valueOf(320000000), Double.valueOf(scaleElement.getAttribute("min")));
+	    assertEquals(Double.POSITIVE_INFINITY, Double.valueOf(scaleElement.getAttribute("max")));
+
+    }
 	
     /**
      * <pre>

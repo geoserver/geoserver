fcf99487312e8763a87d1c75b5fcb016112ab847
Simone Giannecchini <simone.giannecchini@geo-solutions.it>
2013-05-17 00:23:04 +0200

    GEOS-5816

--- src/wcs1_0/src/main/java/org/geoserver/wcs/kvp/InterpolationMethodKvpParser.java
+++ src/wcs1_0/src/main/java/org/geoserver/wcs/kvp/InterpolationMethodKvpParser.java
@@ -23,12 +23,16 @@ public class InterpolationMethodKvpParser extends KvpParser {
     }
 
     @Override
-    public Object parse(String value) throws Exception {
-        if (InterpolationMethodType.get(value) == null)
-            throw new WcsException("Could not find interpolationMethod '" + value + "'",
+    public Object parse(String interpolation) throws Exception {
+
+        if (interpolation.startsWith("nearest")) {
+            interpolation = "nearest neighbor";
+        }        
+        if (InterpolationMethodType.get(interpolation) == null)
+            throw new WcsException("Could not find interpolationMethod '" + interpolation + "'",
                     InvalidParameterValue, "interpolationMethod");
 
-        return InterpolationMethodType.get(value);
+        return InterpolationMethodType.get(interpolation);
     }
 
 }

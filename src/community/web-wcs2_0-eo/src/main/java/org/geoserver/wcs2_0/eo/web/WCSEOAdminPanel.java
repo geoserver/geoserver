8a7f0a49525f294f9ac7e6d5f1e65ac2dac561b9
Andrea Aime <andrea.aime@gmail.com>
2013-07-03 16:00:57 +0200

    The wcs 2.0 EO and web wcs 2.0 EO modules

---
+++ src/community/web-wcs2_0-eo/src/main/java/org/geoserver/wcs2_0/eo/web/WCSEOAdminPanel.java
@@ -0,0 +1,31 @@
+package org.geoserver.wcs2_0.eo.web;
+
+import org.apache.wicket.markup.html.form.CheckBox;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.validation.validator.MinimumValidator;
+import org.geoserver.catalog.MetadataMap;
+import org.geoserver.wcs2_0.eo.WCSEOMetadata;
+import org.geoserver.web.services.AdminPagePanel;
+import org.geoserver.web.util.MapModel;
+
+public class WCSEOAdminPanel extends AdminPagePanel {
+    private static final long serialVersionUID = 1302234327415740649L;
+
+    public WCSEOAdminPanel(String id, IModel<?> model) {
+        super(id, model);
+
+        PropertyModel<MetadataMap> metadata = new PropertyModel<MetadataMap>(model, "metadata");
+
+        CheckBox enabled = new CheckBox("enabled", new MapModel(metadata,
+                WCSEOMetadata.ENABLED.key));
+        add(enabled);
+        
+        TextField<Integer> defaultCount = new TextField<Integer>("defaultCount", new MapModel(metadata,
+                WCSEOMetadata.COUNT_DEFAULT.key), Integer.class);
+        defaultCount.add(new MinimumValidator<Integer>(1));
+        add(defaultCount);
+    }
+
+}
---
+++ src/community/web-wcs2_0-eo/src/test/java/org/geoserver/wcs2_0/eo/web/WCSEOLayerConfigTest.java
@@ -0,0 +1,80 @@
+package org.geoserver.wcs2_0.eo.web;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import javax.xml.namespace.QName;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.util.tester.FormTester;
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.LayerInfo;
+import org.geoserver.data.test.MockData;
+import org.geoserver.data.test.SystemTestData;
+import org.geoserver.wcs2_0.eo.WCSEOMetadata;
+import org.geoserver.web.ComponentBuilder;
+import org.geoserver.web.FormTestPage;
+import org.geoserver.web.GeoServerWicketTestSupport;
+import org.junit.Test;
+
+public class WCSEOLayerConfigTest extends GeoServerWicketTestSupport {
+
+    protected static QName WATTEMP = new QName(MockData.SF_URI, "watertemp", MockData.SF_PREFIX);
+
+    @Override
+    protected void onSetUp(SystemTestData testData) throws Exception {
+        super.onSetUp(testData);
+        Catalog catalog = getCatalog();
+        testData.addRasterLayer(WATTEMP, "watertemp.zip", null, null, SystemTestData.class, catalog);
+    }
+    
+    @Override
+    protected void setUpTestData(SystemTestData testData) throws Exception {
+        testData.setUpWcs11RasterLayers();
+    }
+    
+    @Test
+    public void testEditPlainTiff() {
+        final LayerInfo layer = getCatalog().getLayerByName(getLayerId(MockData.TASMANIA_DEM));
+        tester.startPage(new FormTestPage(new ComponentBuilder() {
+
+            public Component buildComponent(String id) {
+                return new WCSEOLayerConfig(id, new Model(layer));
+            }
+        }));
+        
+        // print(tester.getLastRenderedPage(), true, true);
+        Component panel = tester.getLastRenderedPage().get("form:panel");
+        // the panel must not be visible for this layer
+        assertFalse(panel.isVisible());
+    }
+    
+    @Test
+    public void testEditMosaic() {
+        // setup the panel with a mosaic
+        final LayerInfo layer = getCatalog().getLayerByName(getLayerId(WATTEMP));
+        tester.startPage(new FormTestPage(new ComponentBuilder() {
+
+            public Component buildComponent(String id) {
+                return new WCSEOLayerConfig(id, new Model(layer));
+            }
+        }));
+        
+        // print(tester.getLastRenderedPage(), true, true);
+        Component panel = tester.getLastRenderedPage().get("form:panel");
+        // the panel must be visible for this layer, it's a ,mosaic
+        assertTrue(panel.isVisible());
+        
+        FormTester ft = tester.newFormTester("form");
+        ft.setValue("panel:dataset", true);
+        ft.submit();
+
+        // print(tester.getLastRenderedPage(), true, true);
+
+        tester.assertModelValue("form:panel:dataset", true);
+        assertTrue((boolean) layer.getResource().getMetadata().get(WCSEOMetadata.DATASET.key, Boolean.class));
+    }
+
+
+}

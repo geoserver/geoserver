054b3b505259ceaad356722dd2affaa12689c610
David Winslow <dwinslow@opengeo.org>
2013-07-09 14:05:05 -0400

    Revert "Refactor docs/sources to reflect css change to extension status"
    This reverts commit adcf9fff6bf8c31a780aae19ee9e4cd3f4470f35.

---
+++ src/extension/css/src/test/java/org/geoserver/community/css/web/CssDemoTest.java
@@ -0,0 +1,65 @@
+package org.geoserver.community.css.web;
+
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.extensions.ajax.markup.html.tabs.AjaxTabbedPanel;
+import org.apache.wicket.markup.html.panel.EmptyPanel;
+import org.geoserver.web.GeoServerWicketTestSupport;
+import org.geoserver.web.wicket.GeoServerTablePanel;
+import org.junit.Test;
+
+public class CssDemoTest extends GeoServerWicketTestSupport {
+    @Test
+    public void testBasicLayout() {
+        login();
+        tester.startPage(CssDemoPage.class);
+        tester.assertRenderedPage(CssDemoPage.class);
+        tester.assertComponent("main-content:context", AjaxTabbedPanel.class);
+        tester.assertComponent("main-content:context:panel", EmptyPanel.class);
+        tester.assertComponent("main-content:change.style", AjaxLink.class);
+        tester.assertComponent("main-content:change.layer", AjaxLink.class);
+        tester.assertComponent("main-content:create.style", AjaxLink.class);
+        tester.assertComponent("main-content:associate.styles" , AjaxLink.class);
+        tester.assertComponent("main-content:style.editing" , StylePanel.class);
+    }
+
+    @Test
+    public void testOpenLayersMapPanel() {
+        login();
+        tester.startPage(CssDemoPage.class);
+        tester.clickLink("main-content:context:tabs-container:tabs:1:link");
+        tester.assertComponent("main-content:context:panel", OpenLayersMapPanel.class);
+    }
+
+    @Test
+    public void testSLDPreviewPanel() {
+        login();
+        tester.startPage(CssDemoPage.class);
+        tester.clickLink("main-content:context:tabs-container:tabs:3:link");
+        tester.assertComponent("main-content:context:panel", SLDPreviewPanel.class);
+    }
+
+    @Test 
+    public void testStyleChooser() {
+        login();
+        tester.startPage(CssDemoPage.class);
+        tester.clickLink("main-content:change.style");
+        tester.assertComponent("main-content:popup:content:style.table", GeoServerTablePanel.class);
+    }
+
+
+    @Test
+    public void testLayerChooser() {
+        login();
+        tester.startPage(CssDemoPage.class);
+        tester.clickLink("main-content:change.layer");
+        tester.assertComponent("main-content:popup:content:layer.table", GeoServerTablePanel.class);
+    }
+
+    @Test
+    public void testDocsPanel() {
+        login();
+        tester.startPage(CssDemoPage.class);
+        tester.clickLink("main-content:context:tabs-container:tabs:4:link");
+        tester.assertComponent("main-content:context:panel", DocsPanel.class);
+    }
+}

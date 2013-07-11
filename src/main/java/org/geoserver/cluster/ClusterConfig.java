c237486544ac11527c33b8905a5d76e94efaf086
Kevin Smith <ksmith@opengeo.org>
2013-07-11 15:13:29 -0700

    Tidying up in clustering module

--- src/main/java/org/geoserver/cluster/ClusterConfig.java
+++ src/main/java/org/geoserver/cluster/ClusterConfig.java
@@ -2,16 +2,29 @@ package org.geoserver.cluster;
 
 import java.util.Properties;
 
+/**
+ * Configuration properties for clustered configuration/catalog.
+ *
+ */
 public class ClusterConfig extends Properties {
 
+    /**
+     * @return is clustering enabled
+     */
     public boolean isEnabled() {
         return Boolean.valueOf(getProperty("enabled", "true"));
     }
 
+    /**
+     * @return what method should be used for synchronization
+     */
     public String getSyncMethod() {
         return getProperty("sync_method", "reload");
     }
 
+    /**
+     * @return how long to wait and accumulate changes before synchronizing
+     */
     public int getSyncDelay() {
         return Integer.parseInt(getProperty("sync_delay", "5"));
     }
--- src/main/java/org/geoserver/cluster/Event.java
+++ src/main/java/org/geoserver/cluster/Event.java
@@ -2,6 +2,10 @@ package org.geoserver.cluster;
 
 import java.io.Serializable;
 
+/**
+ * Base class for events to be signalled across the cluster.  Carries an identifier for the 
+ * originating node which will be implementation specific.
+ */
 public class Event implements Serializable {
 
     /** serialVersionUID */
@@ -9,10 +13,18 @@ public class Event implements Serializable {
 
     Serializable source;
 
+    /**
+     * Set an identifier for the node on which the event originates.
+     * @param source
+     */
     public void setSource(Serializable source) {
         this.source = source;
     }
 
+    /**
+     * Get an identifier of the node on which the event originated.
+     * @param source
+     */
     public Serializable getSource() {
         return source;
     }

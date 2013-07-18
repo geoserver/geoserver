dcca0326eb8a5fff7b1a6bd410ba558edda631e5
Niels Charlier <niels@scitus.be>
2013-07-18 11:45:59 +0200

    Make CSW compatible with jdbcconfig

--- src/community/csw/core/src/main/java/org/geoserver/csw/store/internal/CSWUnmappingFilterVisitor.java
+++ src/community/csw/core/src/main/java/org/geoserver/csw/store/internal/CSWUnmappingFilterVisitor.java
@@ -4,14 +4,20 @@
  */
 package org.geoserver.csw.store.internal;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.geoserver.csw.records.RecordDescriptor;
 import org.geoserver.csw.store.internal.CatalogStoreMapping.CatalogStoreMappingElement;
 import org.geoserver.csw.util.QNameResolver;
 import org.geotools.data.complex.filter.XPathUtil;
 import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.filter.visitor.DuplicatingFilterVisitor;
+import org.opengis.filter.Filter;
 import org.opengis.filter.FilterFactory2;
 import org.opengis.filter.Id;
+import org.opengis.filter.expression.Expression;
 import org.opengis.filter.expression.PropertyName;
 
 /**
@@ -46,9 +52,17 @@ public class CSWUnmappingFilterVisitor extends DuplicatingFilterVisitor {
     public Object visit(PropertyName expression, Object extraData) {
         
         XPathUtil.StepList steps = XPathUtil.steps( rd.getFeatureDescriptor() , expression.getPropertyName(), rd.getNamespaceSupport());
-        
+                
         if (steps.size()==1 && steps.get(0).getName().getLocalPart().equalsIgnoreCase("AnyText")) {
-            return new RecordTextFunction(mapping);
+            
+            Expression result = ff.literal(" ");
+             
+            for (CatalogStoreMappingElement element : mapping.elements()) {
+                Expression fieldIgnoreNull = ff.function( "if_then_else", ff.function("isNull", element.getContent()), ff.literal(""), element.getContent());
+                result = ff.function("strConcat", result, ff.function("strConcat", ff.literal(" "),  fieldIgnoreNull ) );
+            }
+                        
+            return result;                    
         } 
                 
         String path = CatalogStoreMapping.toDotPath(steps);
@@ -66,6 +80,19 @@ public class CSWUnmappingFilterVisitor extends DuplicatingFilterVisitor {
     
     @Override
     public Object visit(Id filter, Object extraData) {
-        return getFactory(extraData).equal(mapping.getIdentifierElement().getContent(), getFactory(extraData).literal(filter.getIDs()), true);
+        
+        Iterator<Object> it = filter.getIDs().iterator();
+        
+        if (!it.hasNext()) {
+            return Filter.EXCLUDE;
+        }
+        
+        Filter newFilter = getFactory(extraData).equal(mapping.getIdentifierElement().getContent(), getFactory(extraData).literal(it.next()), true);
+               
+        while (it.hasNext()) {
+            newFilter = getFactory(extraData).or(newFilter, getFactory(extraData).equal(mapping.getIdentifierElement().getContent(), getFactory(extraData).literal(it.next()), true));
+        }
+        
+        return newFilter;
     }
 }

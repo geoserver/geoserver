97f616dbf0bb07f3459c52c43b60bb6f7cf3928f
Roy Braam <braam.roy@gmail.com>
2013-08-21 09:23:47 +0200

    Completed the unit tests with tests for 'Scalehint in units per diagonal pixel' and as denominators.

--- src/wms/src/test/java/org/geoserver/wms/capabilities/GetCapabilitiesScaleHintTest.java
+++ src/wms/src/test/java/org/geoserver/wms/capabilities/GetCapabilitiesScaleHintTest.java
@@ -20,12 +20,15 @@ import org.custommonkey.xmlunit.XpathEngine;
 import org.custommonkey.xmlunit.exceptions.XpathException;
 import org.geoserver.catalog.Catalog;
 import org.geoserver.catalog.LayerInfo;
+import org.geoserver.catalog.MetadataMap;
 import org.geoserver.catalog.StyleInfo;
+import org.geoserver.config.GeoServer;
 import org.geoserver.data.test.MockData;
 import org.geoserver.data.test.SystemTestData;
 import org.geoserver.test.GeoServerSystemTestSupport;
 import org.geoserver.wms.GetCapabilitiesRequest;
 import org.geoserver.wms.WMS;
+import org.geoserver.wms.WMSInfo;
 import org.geoserver.wms.WMSTestSupport;
 import org.junit.Assert;
 import org.junit.Test;
@@ -40,7 +43,7 @@ import org.w3c.dom.NodeList;
  * @author Mauricio Pazos
  *
  */
-public class GetCapabilitiesScaleHintTest extends GeoServerSystemTestSupport {
+public class GetCapabilitiesScaleHintTest extends WMSTestSupport {
 
     private final XpathEngine xpath;
 
@@ -120,7 +123,7 @@ public class GetCapabilitiesScaleHintTest extends GeoServerSystemTestSupport {
     @Test
     public void scaleHintDefaultValues()throws Exception{
         
-    	Document dom = findCapabilities();
+    	Document dom = findCapabilities(false);
 
 		Element layerElement= searchLayerElement(getLayerId(ACCIDENT), dom);
 		
@@ -149,7 +152,39 @@ public class GetCapabilitiesScaleHintTest extends GeoServerSystemTestSupport {
     @Test
     public void scaleHintDefaultMinValue()throws Exception{
         
-    	Document dom = findCapabilities();
+    	Document dom = findCapabilities(false);
+
+		Element layerElement= searchLayerElement(getLayerId(ACCIDENT2), dom);
+		
+		NodeList scaleNode = layerElement.getElementsByTagName("ScaleHint");
+		
+		Element scaleElement = (Element)scaleNode.item(0);
+
+        assertEquals(0.0, Double.valueOf(scaleElement.getAttribute("min")));
+        
+	    assertEquals(Double.valueOf(640000000), Double.valueOf(scaleElement.getAttribute("max")));
+
+    }
+    
+    /**
+     * Default values for ScaleHint should be set.
+     * 
+     * <pre>
+     * The computation of Min and Max values return: 
+     * 		Min: 0.0
+     * 		Max: a value
+     * 
+     * Capabilities document Expected:
+     *  
+     *   <ScaleHint min=0 max=value/>     
+     * </pre>
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void scaleHintUnitsPerPixelDefaultMinValue()throws Exception{
+        
+    	Document dom = findCapabilities(true);
 
 		Element layerElement= searchLayerElement(getLayerId(ACCIDENT2), dom);
 		
@@ -181,7 +216,38 @@ public class GetCapabilitiesScaleHintTest extends GeoServerSystemTestSupport {
     @Test
     public void scaleHintDefaultMaxValue()throws Exception{
         
-    	Document dom = findCapabilities();
+    	Document dom = findCapabilities(false);
+
+		Element layerElement= searchLayerElement(getLayerId(ACCIDENT3), dom);
+		
+		NodeList scaleNode = layerElement.getElementsByTagName("ScaleHint");
+		
+		Element scaleElement = (Element)scaleNode.item(0);
+
+        assertEquals(Double.valueOf(320000000), Double.valueOf(scaleElement.getAttribute("min")));
+	    assertEquals(Double.POSITIVE_INFINITY, Double.valueOf(scaleElement.getAttribute("max")));
+
+    }
+    
+	/**
+     * Default values for ScaleHint should be set.
+     * 
+     * <pre>
+     * The computation of Min and Max values return: 
+     * 		Min: a value
+     * 		Max: Infinity
+     * 
+     * Capabilities document Expected:
+     *  
+     *   <ScaleHint min=value max=infinity/>     
+     * </pre>
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void scaleHintUnitsPerPixelDefaultMaxValue()throws Exception{
+        
+    	Document dom = findCapabilities(true);
 
 		Element layerElement= searchLayerElement(getLayerId(ACCIDENT3), dom);
 		
@@ -193,7 +259,7 @@ public class GetCapabilitiesScaleHintTest extends GeoServerSystemTestSupport {
 	    assertEquals(Double.POSITIVE_INFINITY, Double.valueOf(scaleElement.getAttribute("max")));
 
     }
-	
+    
     /**
      * <pre>
      * Max is the maximum value found in the set of rules 
@@ -205,7 +271,30 @@ public class GetCapabilitiesScaleHintTest extends GeoServerSystemTestSupport {
     @Test
     public void scaleHintFoundMaxMinDenominators()throws Exception{
 
-    	Document dom = findCapabilities();
+    	Document dom = findCapabilities(false);
+
+		final String layerName = getLayerId(REGIONATED);
+        Element layerElement= searchLayerElement(layerName, dom);
+        
+		NodeList scaleNode = layerElement.getElementsByTagName("ScaleHint");
+		Element scaleElement = (Element)scaleNode.item(0);
+            
+        assertEquals(Double.valueOf(80000000), Double.valueOf(scaleElement.getAttribute("min")));
+	    assertEquals(Double.valueOf(640000000), Double.valueOf(scaleElement.getAttribute("max")));
+    }
+    
+    /**
+     * <pre>
+     * Max is the maximum value found in the set of rules 
+     * Min is the minimum value found in the set of rules
+     * </pre>
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void scaleHintUnitsPerPixelFoundMaxMinDenominators()throws Exception{
+
+    	Document dom = findCapabilities(true);
 
 		final String layerName = getLayerId(REGIONATED);
         Element layerElement= searchLayerElement(layerName, dom);
@@ -216,19 +305,23 @@ public class GetCapabilitiesScaleHintTest extends GeoServerSystemTestSupport {
         assertEquals(Double.valueOf(31678.383797157323), Double.valueOf(scaleElement.getAttribute("min")));
 	    assertEquals(Double.valueOf(253427.07037725858), Double.valueOf(scaleElement.getAttribute("max")));
     }
-    
     /**
      * Retrieves the WMS's capabilities document.
      * 
+     * @param scaleHintUnitsPerDiaPixel true if the scalehint must be in units per diagonal of a pixel
      * @return Capabilities as {@link Document}
      * 
      * @throws Exception
      */
-    private Document findCapabilities() throws Exception{
-    	
-    	WMS wms = (WMS) applicationContext.getBean("wms");
-    	
-    	GetCapabilitiesTransformer tr = new GetCapabilitiesTransformer(wms, BASE_URL, FORMATS, LEGEND_FORMAT, null);
+    private Document findCapabilities(Boolean scaleHintUnitsPerDiaPixel) throws Exception{
+    	//set the Scalehint units per diagonal pixel setting.
+        WMS wms = getWMS();
+        WMSInfo info=wms.getServiceInfo();
+        MetadataMap mm= info.getMetadata();
+        mm.put(WMS.SCALEHINT_MAPUNITS_PIXEL, scaleHintUnitsPerDiaPixel);
+        info.getGeoServer().save(info);
+        
+        GetCapabilitiesTransformer tr = new GetCapabilitiesTransformer(wms, BASE_URL, FORMATS, LEGEND_FORMAT, null);
     	GetCapabilitiesRequest req = new GetCapabilitiesRequest();
         req.setBaseUrl(BASE_URL);
         req.setVersion(WMS.VERSION_1_1_1.toString());

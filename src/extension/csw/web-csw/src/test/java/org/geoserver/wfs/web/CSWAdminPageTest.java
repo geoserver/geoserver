6f4a4ae3890fad1db3191b8360ce4451fa87739a
Niels Charlier <niels@scitus.be>
2013-09-22 14:46:09 +0200

    GEOS-5938: CSW admin page

---
+++ src/extension/csw/web-csw/src/test/java/org/geoserver/wfs/web/CSWAdminPageTest.java
@@ -0,0 +1,27 @@
+/* Copyright (c) 2001 - 2013 OpenPlans - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.wfs.web;
+
+import org.geoserver.csw.CSWInfo;
+import org.geoserver.csw.web.CSWAdminPage;
+import org.geoserver.web.GeoServerWicketTestSupport;
+import org.geoserver.web.wicket.KeywordsEditor;
+import org.junit.Test;
+
+public class CSWAdminPageTest extends GeoServerWicketTestSupport {
+    @Test
+    public void testValues() throws Exception {
+        CSWInfo csw = getGeoServerApplication().getGeoServer().getService(CSWInfo.class);
+
+        login();
+        tester.startPage(CSWAdminPage.class);
+
+        tester.assertRenderedPage(CSWAdminPage.class);
+        
+        // test that components have been filled as expected
+        tester.assertComponent("form:keywords", KeywordsEditor.class);
+        tester.assertModelValue("form:keywords", csw.getKeywords());
+    }
+}

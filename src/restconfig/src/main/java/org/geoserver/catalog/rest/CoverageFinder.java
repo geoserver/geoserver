6ad55a1f80605c9f3031ccf851327eea26b3dc80
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2013-10-14 11:27:30 +0200

    [GEOS-6243]:EmptyMosaic: REST APIs support

--- src/restconfig/src/main/java/org/geoserver/catalog/rest/CoverageFinder.java
+++ src/restconfig/src/main/java/org/geoserver/catalog/rest/CoverageFinder.java
@@ -7,6 +7,7 @@ package org.geoserver.catalog.rest;
 import org.geoserver.catalog.Catalog;
 import org.geoserver.catalog.NamespaceInfo;
 import org.geoserver.rest.RestletException;
+import org.restlet.data.Form;
 import org.restlet.data.Method;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
@@ -24,7 +25,7 @@ public class CoverageFinder extends AbstractCatalogFinder {
         String ws = getAttribute(request, "workspace");
         String cs = getAttribute(request, "coveragestore");
         String c = getAttribute(request, "coverage");
-        
+
         //ensure referenced resources exist
         if ( ws != null && catalog.getWorkspaceByName( ws ) == null ) {
             throw new RestletException( "No such workspace: " + ws, Status.CLIENT_ERROR_NOT_FOUND );
@@ -32,17 +33,22 @@ public class CoverageFinder extends AbstractCatalogFinder {
         if ( cs != null && catalog.getCoverageStoreByName(ws, cs) == null ) {
             throw new RestletException( "No such coveragestore: " + ws + "," + cs, Status.CLIENT_ERROR_NOT_FOUND );
         }
-        
-        if ( c == null && request.getMethod() == Method.GET ) {
-            return new CoverageListResource( getContext(), request, response, catalog );
-        }
-        
-        if ( c != null ) {
-            if ( cs != null &&
-                    catalog.getCoverageByCoverageStore(catalog.getCoverageStoreByName(ws, cs), c) == null) {
-                throw new RestletException( "No such coverage: "+ws+","+cs+","+c, Status.CLIENT_ERROR_NOT_FOUND );
+
+        if (c == null && request.getMethod() == Method.GET) {
+            Form form = request.getResourceRef().getQueryAsForm();
+            String list = form.getFirstValue("list");
+            if ("all".equalsIgnoreCase(list)) {
+                return new HarvestedCoveragesResource(getContext(), request, response, catalog);
+            } else {
+                return new CoverageListResource(getContext(), request, response, catalog);
             }
-            else {
+
+        }
+
+        if ( c != null ) {
+            if ( cs != null && catalog.getCoverageByCoverageStore(catalog.getCoverageStoreByName(ws, cs), c) == null) {
+                throw new RestletException( "No such coverage: "+ws+","+cs+","+c, Status.CLIENT_ERROR_NOT_FOUND );
+            } else {
                 //look up by workspace/namespace
                 NamespaceInfo ns = catalog.getNamespaceByPrefix( ws );
                 if ( ns == null || catalog.getCoverageByName( ns, c ) == null ) {
@@ -50,7 +56,6 @@ public class CoverageFinder extends AbstractCatalogFinder {
                 }
             }
         }
-            
         return new CoverageResource(null,request,response,catalog);
     }
 

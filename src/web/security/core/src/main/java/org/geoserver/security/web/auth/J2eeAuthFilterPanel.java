d6f7d27902955bb3f05f858cc4c036d8de792590
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2013-10-18 13:11:19 +0200

    GEOS-6080: added standard RoleSource support (Header, RoleService, UserGroupService) to J2EEAuthenticationFilter

--- src/web/security/core/src/main/java/org/geoserver/security/web/auth/J2eeAuthFilterPanel.java
+++ src/web/security/core/src/main/java/org/geoserver/security/web/auth/J2eeAuthFilterPanel.java
@@ -4,9 +4,23 @@
  */
 package org.geoserver.security.web.auth;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.form.ChoiceRenderer;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
+import org.geoserver.security.config.J2eeAuthenticationBaseFilterConfig.J2EERoleSource;
+import org.geoserver.security.config.J2eeAuthenticationBaseFilterConfig;
 import org.geoserver.security.config.J2eeAuthenticationFilterConfig;
+import org.geoserver.security.config.RoleSource;
+import org.geoserver.security.config.PreAuthenticatedUserNameFilterConfig.PreAuthenticatedUserNameRoleSource;
 import org.geoserver.security.filter.GeoServerJ2eeAuthenticationFilter;
+import org.geoserver.security.web.auth.PreAuthenticatedUserNameFilterPanel.HeaderPanel;
+import org.geoserver.security.web.auth.PreAuthenticatedUserNameFilterPanel.RoleServicePanel;
+import org.geoserver.security.web.auth.PreAuthenticatedUserNameFilterPanel.UserGroupServicePanel;
 import org.geoserver.security.web.role.RoleServiceChoice;
 
 /**
@@ -14,10 +28,14 @@ import org.geoserver.security.web.role.RoleServiceChoice;
  * 
  * @author Justin Deoliveira, OpenGeo
  */
-public class J2eeAuthFilterPanel extends AuthenticationFilterPanel<J2eeAuthenticationFilterConfig> {
+public class J2eeAuthFilterPanel extends J2eeBaseAuthFilterPanel<J2eeAuthenticationFilterConfig> {
+
+    /** serialVersionUID */
+    private static final long serialVersionUID = 1L;
 
     public J2eeAuthFilterPanel(String id, IModel<J2eeAuthenticationFilterConfig> model) {
         super(id, model);
-        add(new RoleServiceChoice("roleServiceName"));
     }
+
+   
 }

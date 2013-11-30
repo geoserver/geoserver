4cf98997a0a9bd025d65d764a54594eea278c495
Andrea Aime <andrea.aime@gmail.com>
2013-11-30 19:31:51 +0100

    [GEOS-6176] Create a new wosrkspace specific style breaks CSS extension user interface

--- src/extension/css/src/main/java/org/geoserver/community/css/web/StyleNameInput.java
+++ src/extension/css/src/main/java/org/geoserver/community/css/web/StyleNameInput.java
@@ -4,33 +4,37 @@
  */
 package org.geoserver.community.css.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.IChoiceRenderer;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
-import org.apache.wicket.validation.IValidator;
 import org.apache.wicket.validation.IValidatable;
+import org.apache.wicket.validation.IValidator;
 import org.apache.wicket.validation.ValidationError;
-
+import org.geoserver.catalog.LayerInfo;
 import org.geoserver.catalog.WorkspaceInfo;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class LayerNameInput extends Panel {
+public class StyleNameInput extends Panel {
     String workspace = null;
     String name = "";
 
-    public LayerNameInput(String id, final CssDemoPage demo) {
+    public StyleNameInput(String id, final CssDemoPage demo) {
         super(id);
 
-        Form form = new Form("layer.name.form");
+        final Form form = new Form("layer.name.form");
 
         IValidator<String> nameValidator =
             new IValidator<String>() {
@@ -58,11 +62,36 @@ public class LayerNameInput extends Panel {
                    return value == null ? "No workspace" : value;
                }
            };
-         DropDownChoice workspaceChooser = new DropDownChoice(
+         final WebMarkupContainer warningContainer = new WebMarkupContainer("warningContainer");
+         warningContainer.setOutputMarkupId(true);
+         form.add(warningContainer);
+         final Label warning = new Label("workspaceWarning");
+         warning.setOutputMarkupId(true);
+         warning.setVisible(false);
+         warningContainer.add(warning);
+         final DropDownChoice<String> workspaceChooser = new DropDownChoice<String>(
                  "layer.workspace.field",
                  new PropertyModel<String>(this, "workspace"),
                  workspaces,
                  workspaceRenderer);
+         workspaceChooser.add(new AjaxFormComponentUpdatingBehavior("onchange") {
+            
+            @Override
+            protected void onUpdate(AjaxRequestTarget target) {
+                LayerInfo layer = demo.getLayer();
+                WorkspaceInfo layerWorkspace = layer.getResource().getStore().getWorkspace();
+                String selected = workspaceChooser.getModelObject();
+                if(selected == null || selected.equals(layerWorkspace.getName())) {
+                    warning.setVisible(false);
+                } else {
+                    warning.setDefaultModel(new Model("Warning, the layer " 
+                            + layer.getName() + " is in workspace " + layerWorkspace.getName() 
+                            + ",  cannot be used with a style in workspace " + selected));
+                    warning.setVisible(true);
+                }
+                target.addComponent(warningContainer);
+            }
+        });
          form.add(workspaceChooser);
 
          TextField nameField =
@@ -82,7 +111,7 @@ public class LayerNameInput extends Panel {
                          );
                      }
                      
-                     demo.createCssTemplate(name);
+                     demo.createCssTemplate(workspace, name);
 
                      PageParameters params = new PageParameters();
                      params.put("layer", demo.getLayer().prefixedName());

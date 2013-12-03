c345753d7780a8440d235f1d85739fd92d5809b5
jdeolive <jdeolive@boundlessgeo.com>
2013-12-03 10:48:53 -0700

    Ensuring http response info set properly.
    - Factored out common string constants to utility class
    - Added content-disposition and mime type to wfs output format response
    - Allowing wfs output format to referred to by common names

--- src/community/geopkg/src/main/java/org/geoserver/geopkg/GeoPackageGetFeatureOutputFormat.java
+++ src/community/geopkg/src/main/java/org/geoserver/geopkg/GeoPackageGetFeatureOutputFormat.java
@@ -4,10 +4,13 @@
  */
 package org.geoserver.geopkg;
 
+import static org.geoserver.geopkg.GeoPkg.*;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.geoserver.config.GeoServer;
@@ -15,10 +18,18 @@ import org.geoserver.platform.Operation;
 import org.geoserver.platform.ServiceException;
 import org.geoserver.wfs.WFSGetFeatureOutputFormat;
 import org.geoserver.wfs.request.FeatureCollectionResponse;
+import org.geoserver.wfs.request.GetFeatureRequest;
+import org.geoserver.wfs.request.Query;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.geopkg.FeatureEntry;
 import org.geotools.geopkg.GeoPackage;
+
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
 /**
  * 
@@ -30,9 +41,42 @@ import org.geotools.geopkg.GeoPackage;
 public class GeoPackageGetFeatureOutputFormat extends WFSGetFeatureOutputFormat {
 
     public GeoPackageGetFeatureOutputFormat(GeoServer gs) {
-        super(gs, "application/x-sqlite3");
+        super(gs, Sets.union(Sets.newHashSet(MIME_TYPE), Sets.newHashSet(NAMES)));
     }
-    
+
+    @Override
+    public String getMimeType(Object value, Operation operation)
+            throws ServiceException {
+        return MIME_TYPE;
+    }
+
+    @Override
+    public String getCapabilitiesElementName() {
+        return NAMES.iterator().next();
+    }
+
+    @Override
+    public List<String> getCapabilitiesElementNames() {
+        return Lists.newArrayList(NAMES);
+    }
+
+    @Override
+    public String getPreferredDisposition(Object value, Operation operation) {
+        return DISPOSITION_ATTACH;
+    }
+
+    @Override
+    public String getAttachmentFileName(Object value, Operation operation) {
+        GetFeatureRequest req = GetFeatureRequest.adapt(operation.getParameters()[0]);
+
+        return Joiner.on("_").join(Iterables.transform(req.getQueries(), new Function<Query,String>() {
+            @Override
+            public String apply(Query input) {
+                return input.getTypeNames().get(0).getLocalPart();
+            }
+        })) + "." + EXTENSION;
+    }
+
     @Override
     protected void write(FeatureCollectionResponse featureCollection, OutputStream output,
             Operation getFeature) throws IOException, ServiceException {

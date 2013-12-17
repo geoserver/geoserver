bf4bfd04a834d6499e73f79d7be78878a6f0d292
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2013-12-17 16:02:41 +0100

    [Supporting delete stores from REST]

--- src/main/src/main/java/org/geoserver/catalog/StructuredSingleGridCoverage2DReader.java
+++ src/main/src/main/java/org/geoserver/catalog/StructuredSingleGridCoverage2DReader.java
@@ -53,12 +53,6 @@ public class StructuredSingleGridCoverage2DReader extends SingleGridCoverage2DRe
     }
 
     @Override
-    public boolean removeCoverage(String coverageName) throws IOException,
-            UnsupportedOperationException {
-        return structuredDelegate.removeCoverage(coverageName);
-    }
-
-    @Override
     public boolean removeCoverage(String coverageName, boolean delete) throws IOException,
             UnsupportedOperationException {
         return structuredDelegate.removeCoverage(coverageName, delete);

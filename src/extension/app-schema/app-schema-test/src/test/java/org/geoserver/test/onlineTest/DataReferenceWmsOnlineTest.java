cdf2c27a8237920aeb0c6eae2b3edfeefab51200
Ben Caradoc-Davies <Ben.Caradoc-Davies@csiro.au>
2014-01-13 16:34:11 +0800

    Migrated app-schema WMS tests to PNG

--- src/extension/app-schema/app-schema-test/src/test/java/org/geoserver/test/onlineTest/DataReferenceWmsOnlineTest.java
+++ src/extension/app-schema/app-schema-test/src/test/java/org/geoserver/test/onlineTest/DataReferenceWmsOnlineTest.java
@@ -20,50 +20,45 @@ import org.w3c.dom.Document;
  * 
  * @author Niels Charlier
  * 
- * Base class for wms online tests
+ *         Base class for wms online tests
  * 
  */
 public abstract class DataReferenceWmsOnlineTest extends AbstractDataReferenceWfsTest {
 
     public DataReferenceWmsOnlineTest() throws Exception {
         super();
-    }    
-      
-    @Test
-    public void testGetMapSimpleLithology() throws Exception
-    {
-        InputStream is = getBinary("wms?request=GetMap&SRS=EPSG:4326&layers=gsml:MappedFeature&styles=simplelithology&BBOX=140,-38,145,-35&width=500&height=500&FORMAT=image/jpeg");
-        BufferedImage imageBuffer = ImageIO.read(is);
-        
-        assertNotBlank("app-schema test getmap simple lithology", imageBuffer, Color.WHITE);
-        
-        ImageAssert.assertEquals(DataUtilities.urlToFile(getClass().getResource("/test-data/img/datareference_simplelithology.tiff")), imageBuffer, 250);       
-    }  
-    
-    @Test
-    public void testGetMapStratChart() throws Exception
-    {
-        InputStream is = getBinary("wms?request=GetMap&SRS=EPSG:4326&layers=gsml:MappedFeature&styles=stratchart&BBOX=140,-38,150,-35&width=500&height=500&FORMAT=image/jpeg");
-        BufferedImage imageBuffer = ImageIO.read(is);
-        
-        assertNotBlank("app-schema test getmap stratchart", imageBuffer, Color.WHITE);
-        
-        ImageAssert.assertEquals(DataUtilities.urlToFile(getClass().getResource("/test-data/img/datareference_stratchart.tiff")), imageBuffer, 250);
-    }  
-
-    @Test
-    public void testGetMapAfterWFS() throws Exception
-    {
-        Document doc = getAsDOM("wfs?request=getFeature&version=1.1.0&typeName=gsml:MappedFeature&maxFeatures=1");
-        LOGGER.info(prettyString(doc));
-        
-        InputStream is = getBinary("wms?request=GetMap&SRS=EPSG:4326&layers=gsml:MappedFeature&styles=stratchart&BBOX=140,-38,150,-35&width=500&height=500&FORMAT=image/jpeg");
-        BufferedImage imageBuffer = ImageIO.read(is);
-                
-        assertNotBlank("app-schema test getmap outcrop character", imageBuffer, Color.WHITE);   
-        ImageAssert.assertEquals(DataUtilities.urlToFile(getClass().getResource("/test-data/img/datareference_stratchart.tiff")), imageBuffer, 250);
-
     }
 
-    
+    @Test
+    public void testGetMapSimpleLithology() throws Exception {
+        InputStream is = getBinary("wms?request=GetMap&SRS=EPSG:4326&layers=gsml:MappedFeature&styles=simplelithology&BBOX=140,-38,145,-35&width=500&height=500&FORMAT=image/png");
+        BufferedImage imageBuffer = ImageIO.read(is);
+        assertNotBlank("app-schema test getmap simple lithology", imageBuffer, Color.WHITE);
+        ImageAssert.assertEquals(
+                DataUtilities.urlToFile(getClass().getResource(
+                        "/test-data/img/datareference_simplelithology.png")), imageBuffer, 250);
+    }
+
+    @Test
+    public void testGetMapStratChart() throws Exception {
+        InputStream is = getBinary("wms?request=GetMap&SRS=EPSG:4326&layers=gsml:MappedFeature&styles=stratchart&BBOX=140,-38,150,-35&width=500&height=500&FORMAT=image/png");
+        BufferedImage imageBuffer = ImageIO.read(is);
+        assertNotBlank("app-schema test getmap stratchart", imageBuffer, Color.WHITE);
+        ImageAssert.assertEquals(
+                DataUtilities.urlToFile(getClass().getResource(
+                        "/test-data/img/datareference_stratchart.png")), imageBuffer, 250);
+    }
+
+    @Test
+    public void testGetMapAfterWFS() throws Exception {
+        Document doc = getAsDOM("wfs?request=getFeature&version=1.1.0&typeName=gsml:MappedFeature&maxFeatures=1");
+        LOGGER.info(prettyString(doc));
+        InputStream is = getBinary("wms?request=GetMap&SRS=EPSG:4326&layers=gsml:MappedFeature&styles=stratchart&BBOX=140,-38,150,-35&width=500&height=500&FORMAT=image/png");
+        BufferedImage imageBuffer = ImageIO.read(is);
+        assertNotBlank("app-schema test getmap outcrop character", imageBuffer, Color.WHITE);
+        ImageAssert.assertEquals(
+                DataUtilities.urlToFile(getClass().getResource(
+                        "/test-data/img/datareference_stratchart.png")), imageBuffer, 250);
+    }
+
 }

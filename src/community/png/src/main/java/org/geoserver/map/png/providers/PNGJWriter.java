4851c57216e2c92e0508bc55a6e1a6011de7ec8a
Andrea Aime <andrea.aime@gmail.com>
2014-02-12 12:32:39 +0100

    [GEOS-6344] PNGJWriter logs exceptions at warning level even if the client closed the connection

--- src/community/png/src/main/java/org/geoserver/map/png/providers/PNGJWriter.java
+++ src/community/png/src/main/java/org/geoserver/map/png/providers/PNGJWriter.java
@@ -99,7 +99,6 @@ public class PNGJWriter {
             }
             pw.end();
         } catch (Exception e) {
-            LOGGER.log(Level.SEVERE, "Failed to encode the PNG", e);
             throw new ServiceException(e);
         } finally {
             pw.close();

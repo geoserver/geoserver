419f867e8700a1938c290bc864202059f8ec317f
surzhin.konstantin <surzhin.konstantin@L403LABSURZHIN.vniizht.lan>
2014-02-18 15:09:52 +0400

    I've organised import declarations.

--- src/platform/src/test/java/org/geoserver/platform/ClassExclusionFilterTest.java
+++ src/platform/src/test/java/org/geoserver/platform/ClassExclusionFilterTest.java
@@ -8,10 +8,11 @@ package org.geoserver.platform;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *

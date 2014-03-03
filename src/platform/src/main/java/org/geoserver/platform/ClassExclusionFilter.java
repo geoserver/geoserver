22b76f13695f9b0fa4e1aef7d901eacd7fc6806c
Konstantin-Surzhin <the_pleiades@comtv.ru>
2014-03-03 14:34:05 +0400

    fixed warnings and errors related to unused import, scope of variables, need bracers, parameters should be final, only one return, method is not designed for extension.

--- src/platform/src/main/java/org/geoserver/platform/ClassExclusionFilter.java
+++ src/platform/src/main/java/org/geoserver/platform/ClassExclusionFilter.java
@@ -12,23 +12,23 @@ package org.geoserver.platform;
  * 
  */
 public class ClassExclusionFilter implements ExtensionFilter {
-    Class beanClass;
+    private Class beanClass;
 
-    boolean matchSubclasses;
+    private boolean matchSubclasses;
 
-    public Class getBeanClass() {
+    public final Class getBeanClass() {
         return beanClass;
     }
 
     /**
-     * Specifies which class to be filtered away
+     * Specifies which class to be filtered away.
      * @param beanClass
      */
-    public void setBeanClass(Class beanClass) {
+    public final void setBeanClass(final Class beanClass) {
         this.beanClass = beanClass;
     }
 
-    public boolean isMatchSubclasses() {
+    public final boolean isMatchSubclasses() {
         return matchSubclasses;
     }
 
@@ -36,18 +36,20 @@ public class ClassExclusionFilter implements ExtensionFilter {
      * When true enables hierarchy matching. Defaults to false
      * @param matchSubclasses
      */
-    public void setMatchSubclasses(boolean matchSubclasses) {
+    public final void setMatchSubclasses(final boolean matchSubclasses) {
         this.matchSubclasses = matchSubclasses;
     }
 
-    public boolean exclude(String beanId, Object bean) {
+    @Override
+    public final boolean exclude(final String beanId, final Object bean) {
+        boolean ret = false;
         if (this.beanClass != null && bean != null) {
-            if(matchSubclasses)
-                return this.beanClass.isAssignableFrom(bean.getClass());
-            else
-                return this.beanClass.equals(bean.getClass());
+            if(matchSubclasses){
+                ret = this.beanClass.isAssignableFrom(bean.getClass());}
+            else{
+                ret = this.beanClass.equals(bean.getClass());}
         }
 
-        return false;
+        return ret;
     }
 }

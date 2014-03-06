688e72fad6019b2c94e4506731def7216492bcbc
Konstantin-Surzhin <the_pleiades@comtv.ru>
2014-03-06 17:04:37 +0400

    fixed warnings and errors related to scope of variables, need bracers, parameters should be final, method is not designed for extension, constant name must match pattern '^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$'  and I have changed some tests.

--- src/platform/src/test/java/org/geoserver/platform/GeoServerExtensionsTest.java
+++ src/platform/src/test/java/org/geoserver/platform/GeoServerExtensionsTest.java
@@ -4,12 +4,7 @@
  */
 package org.geoserver.platform;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertNull;
-import static junit.framework.Assert.assertSame;
-import static junit.framework.Assert.assertTrue;
-import static junit.framework.Assert.fail;
+import java.lang.reflect.Field;
 import static org.easymock.EasyMock.anyObject;
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.createNiceMock;
@@ -19,10 +14,20 @@ import static org.easymock.EasyMock.verify;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletContext;
+import org.geotools.util.SoftValueHashMap;
 
 import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -47,91 +52,166 @@ public class GeoServerExtensionsTest {
 
     @Test
     public void testSetApplicationContext() {
-        ApplicationContext appContext1 = createMock(ApplicationContext.class);
-        ApplicationContext appContext2 = createMock(ApplicationContext.class);
+            ApplicationContext appContext1 = createMock(ApplicationContext.class);
+            
+            GeoServerExtensions gse = new GeoServerExtensions();
+            
+            gse.setApplicationContext(appContext1);
+                      
+        try {
+            Field context = gse.getClass().getDeclaredField("context");
+            context.setAccessible(true);
 
-        GeoServerExtensions gse = new GeoServerExtensions();
-        gse.setApplicationContext(appContext1);
-        gse.extensionsCache.put(GeoServerExtensionsTest.class, new String[] { "fake" });
+            ApplicationContext applicationContext = createMock(ApplicationContext.class);
+            applicationContext = (ApplicationContext) context.get(applicationContext);
 
-        assertSame(appContext1, gse.context);
+            assertSame(appContext1, applicationContext);
 
-        gse.setApplicationContext(appContext2);
-        assertSame(appContext2, gse.context);
-        assertEquals(0, gse.extensionsCache.size());
+            Field f1 = gse.getClass().getDeclaredField("EXTENSIONS_CACHE");
+            Field f2 = gse.getClass().getDeclaredField("SINGLETON_BEAN_CHACHE");
+
+            f1.setAccessible(true);
+            f2.setAccessible(true);
+
+            SoftValueHashMap<Class, String[]> obj1 = new SoftValueHashMap<Class, String[]>(40);
+            ConcurrentHashMap<String, Object> obj2 = new ConcurrentHashMap<String, Object>();
+
+            obj1 = (SoftValueHashMap<Class, String[]>) f1.get(obj1);
+            obj2 = (ConcurrentHashMap<String, Object>) f2.get(obj2);
+
+            obj1.put(GeoServerExtensionsTest.class, new String[]{"fake"});
+            obj2.put("fake", new Object());
+
+            assertEquals("Test first side effect", 1, obj1.size());
+            assertEquals("Test second side effect", 1, obj2.size());
+            gse.setApplicationContext(appContext1);
+            assertEquals("Test first side effect", 0, obj1.size());
+            assertEquals("Test second side effect", 0, obj2.size());
+
+        } catch (NoSuchFieldException ex) {
+            Logger.getLogger(GeoServerExtensionsTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("NoSuchFieldException");
+        } catch (SecurityException ex) {
+            Logger.getLogger(GeoServerExtensionsTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("SecurityException");
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(GeoServerExtensionsTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("IllegalArgumentException");
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(GeoServerExtensionsTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("IllegalArgumentException");
+        }
     }
 
     @Test
     public void testExtensions() {
-        ApplicationContext appContext = createMock(ApplicationContext.class);
-        GeoServerExtensions gse = new GeoServerExtensions();
-        gse.setApplicationContext(appContext);
-
-        assertEquals(0, gse.extensionsCache.size());
-        expect(appContext.getBeanNamesForType(ExtensionFilter.class)).andReturn(new String[0]);
-        expect(appContext.getBeanNamesForType(GeoServerExtensionsTest.class)).andReturn(
-                new String[] { "testKey", "fakeKey" });
-        expect(appContext.getBeanNamesForType(ExtensionProvider.class)).andReturn(new String[0]);
-        expect(appContext.getBean("testKey")).andReturn(this);
-        // note I'm testing null is a valid value. If that's not the case, it
-        // should be reflected in the code, but I'm writing the test after the
-        // code so that's what it does
-        expect(appContext.isSingleton((String) anyObject())).andReturn(true).anyTimes();
-        expect(appContext.getBean("fakeKey")).andReturn(null);
-        replay(appContext);
-
-        List<GeoServerExtensionsTest> extensions = gse.extensions(GeoServerExtensionsTest.class);
-        assertNotNull(extensions);
-        assertEquals(2, extensions.size());
-        assertTrue(extensions.contains(this));
-        assertTrue(extensions.contains(null));
-
-        assertEquals(3, gse.extensionsCache.size());
-        assertTrue(gse.extensionsCache.containsKey(GeoServerExtensionsTest.class));
-        assertNotNull(gse.extensionsCache.get(GeoServerExtensionsTest.class));
-        assertEquals(2, gse.extensionsCache.get(GeoServerExtensionsTest.class).length);
-
-        verify(appContext);
+        try {
+            ApplicationContext appContext = createMock(ApplicationContext.class);
+            GeoServerExtensions gse = new GeoServerExtensions();
+            gse.setApplicationContext(appContext);
+            
+            Field f = gse.getClass().getDeclaredField("EXTENSIONS_CACHE");
+            f.setAccessible(true);
+            SoftValueHashMap<Class, String[]> obj = new SoftValueHashMap<Class, String[]>(40);
+            obj = (SoftValueHashMap<Class, String[]>) f.get(obj);
+            
+            assertEquals(0, obj.size());
+            expect(appContext.getBeanNamesForType(ExtensionFilter.class)).andReturn(new String[0]);
+            expect(appContext.getBeanNamesForType(GeoServerExtensionsTest.class)).andReturn(
+                    new String[] { "testKey", "fakeKey" });
+            expect(appContext.getBeanNamesForType(ExtensionProvider.class)).andReturn(new String[0]);
+            expect(appContext.getBean("testKey")).andReturn(this);
+            // note I'm testing null is a valid value. If that's not the case, it
+            // should be reflected in the code, but I'm writing the test after the
+            // code so that's what it does
+            expect(appContext.isSingleton((String) anyObject())).andReturn(true).anyTimes();
+            expect(appContext.getBean("fakeKey")).andReturn(null);
+            replay(appContext);
+            
+            List<GeoServerExtensionsTest> extensions = gse.extensions(GeoServerExtensionsTest.class);
+            assertNotNull(extensions);
+            assertEquals(2, extensions.size());
+            assertTrue(extensions.contains(this));
+            assertTrue(extensions.contains(null));
+            
+            assertEquals(3, obj.size());
+            assertTrue(obj.containsKey(GeoServerExtensionsTest.class));
+            assertNotNull(obj.get(GeoServerExtensionsTest.class));
+            assertEquals(2, obj.get(GeoServerExtensionsTest.class).length);
+            
+            verify(appContext);
+        } catch (NoSuchFieldException ex) {
+            Logger.getLogger(GeoServerExtensionsTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("NoSuchFieldException");
+        } catch (SecurityException ex) {
+            Logger.getLogger(GeoServerExtensionsTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("SecurityException");
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(GeoServerExtensionsTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("IllegalArgumentException");
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(GeoServerExtensionsTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("IllegalArgumentException");
+        }
     }
 
     /**
-     * If a context is explicitly provided that is not the one set through
-     * setApplicationContext(), the extensions() method shall look into it and
-     * bypass the cache
+     * If a CONTEXT is explicitly provided that is not the one set through
+ setApplicationContext(), the extensions() method shall look into it and
+ bypass the cache
      */
     @Test
     public void testExtensionsApplicationContext() {
-        ApplicationContext appContext = createMock(ApplicationContext.class);
-        ApplicationContext customAppContext = createMock(ApplicationContext.class);
-
-        GeoServerExtensions gse = new GeoServerExtensions();
-        gse.setApplicationContext(appContext);
-
-        // setApplicationContext cleared the static cache
-        assertEquals(0, GeoServerExtensions.extensionsCache.size());
-        // set the expectation over the app context used as argument
-        expect(customAppContext.getBeanNamesForType(ExtensionFilter.class)).andReturn(new String[0]);
-        expect(customAppContext.getBeanNamesForType(GeoServerExtensionsTest.class)).andReturn(
-                new String[] { "itDoesntMatterForThePurpose" });
-        expect(customAppContext.getBeanNamesForType(ExtensionProvider.class)).andReturn(new String[0]);
-        expect(customAppContext.getBeanNamesForType(ExtensionFilter.class)).andReturn(new String[0]);
-        expect(customAppContext.getBean("itDoesntMatterForThePurpose")).andReturn(this);
-        expect(appContext.isSingleton((String) anyObject())).andReturn(true).anyTimes();
-        expect(customAppContext.isSingleton((String) anyObject())).andReturn(true).anyTimes();
-        replay(customAppContext);
-        replay(appContext);
-
-        List<GeoServerExtensionsTest> extensions = GeoServerExtensions.extensions(
-                GeoServerExtensionsTest.class, customAppContext);
-
-        assertNotNull(extensions);
-        assertEquals(1, extensions.size());
-        assertSame(this, extensions.get(0));
-        // cache should be untouched after this since our own context were used
-        assertEquals(0, GeoServerExtensions.extensionsCache.size());
-
-        verify(appContext);
-        verify(customAppContext);
+        try {
+            ApplicationContext appContext = createMock(ApplicationContext.class);
+            ApplicationContext customAppContext = createMock(ApplicationContext.class);
+            
+            GeoServerExtensions gse = new GeoServerExtensions();
+            gse.setApplicationContext(appContext);
+            
+            Field f = gse.getClass().getDeclaredField("EXTENSIONS_CACHE");
+            f.setAccessible(true);
+            SoftValueHashMap<Class, String[]> obj = new SoftValueHashMap<Class, String[]>(40);
+            obj = (SoftValueHashMap<Class, String[]>) f.get(obj);
+            
+            // setApplicationContext cleared the static cache
+            assertEquals(0, obj.size());
+            // set the expectation over the app CONTEXT used as argument
+            expect(customAppContext.getBeanNamesForType(ExtensionFilter.class)).andReturn(new String[0]);
+            expect(customAppContext.getBeanNamesForType(GeoServerExtensionsTest.class)).andReturn(
+                    new String[] { "itDoesntMatterForThePurpose" });
+            expect(customAppContext.getBeanNamesForType(ExtensionProvider.class)).andReturn(new String[0]);
+            expect(customAppContext.getBeanNamesForType(ExtensionFilter.class)).andReturn(new String[0]);
+            expect(customAppContext.getBean("itDoesntMatterForThePurpose")).andReturn(this);
+            expect(appContext.isSingleton((String) anyObject())).andReturn(true).anyTimes();
+            expect(customAppContext.isSingleton((String) anyObject())).andReturn(true).anyTimes();
+            replay(customAppContext);
+            replay(appContext);
+            
+            List<GeoServerExtensionsTest> extensions = GeoServerExtensions.extensions(
+                    GeoServerExtensionsTest.class, customAppContext);
+            
+            assertNotNull(extensions);
+            assertEquals(1, extensions.size());
+            assertSame(this, extensions.get(0));
+            // cache should be untouched after this since our own CONTEXT were used
+            assertEquals(0, obj.size());
+            
+            verify(appContext);
+            verify(customAppContext);
+        } catch (NoSuchFieldException ex) {
+            Logger.getLogger(GeoServerExtensionsTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("NoSuchFieldException");
+        } catch (SecurityException ex) {
+            Logger.getLogger(GeoServerExtensionsTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("SecurityException");
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(GeoServerExtensionsTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("IllegalArgumentException");
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(GeoServerExtensionsTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("IllegalArgumentException");
+        }
     }
     
     @Test
@@ -139,7 +219,7 @@ public class GeoServerExtensionsTest {
         ApplicationContext appContext = createNiceMock(ApplicationContext.class);
 
         // setApplicationContext cleared the static cache
-        // set the expectation over the app context used as argument
+        // set the expectation over the app CONTEXT used as argument
         NameExclusionFilter filter = new NameExclusionFilter();
         filter.setBeanId("testId");
         expect(appContext.getBeanNamesForType(ExtensionFilter.class)).andReturn(new String[] { "filter" }).anyTimes();
@@ -168,7 +248,7 @@ public class GeoServerExtensionsTest {
         ApplicationContext appContext = createNiceMock(ApplicationContext.class);
 
         // setApplicationContext cleared the static cache
-        // set the expectation over the app context used as argument
+        // set the expectation over the app CONTEXT used as argument
         ClassExclusionFilter filter = new ClassExclusionFilter();
         filter.setBeanClass(GeoServerExtensionsTest.class);
         expect(appContext.getBeanNamesForType(ExtensionFilter.class)).andReturn(new String[] { "filter" }).anyTimes();

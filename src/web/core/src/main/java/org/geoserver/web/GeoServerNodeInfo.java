c6ec74bc3906774607793191dbe86af6dc3cc3e0
Andrea Aime <andrea.aime@gmail.com>
2014-03-12 19:10:41 +0100

    [GEOS-5964] Make GeoServer nodes in a cluster identifiable by label and color in the UI

---
+++ src/web/core/src/main/java/org/geoserver/web/GeoServerNodeInfo.java
@@ -0,0 +1,36 @@
+/* Copyright (c) 2014 OpenPlans - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.web;
+
+import org.apache.wicket.markup.html.WebMarkupContainer;
+
+/**
+ * Provides identification and styling for the GeoServer id element showing right below the
+ * GeoServer logo in the GeoServer GUI. A system variable based implementation is provided by
+ * default, just register another one in the web-app context in order to override it
+ * 
+ * @author Andrea Aime - GeoSolutions
+ */
+interface GeoServerNodeInfo {
+
+    /**
+     * The node id, displayed as a label in the GUI. If null is returned, the element will be
+     * hidden.
+     * 
+     * @return
+     */
+    String getId();
+
+    /**
+     * Allows customization of the label container, in particular, its style and visibility, but
+     * also any other attribute. Implementors are suggested to hide the id element unless an
+     * administrator is logged in, but specific implementations can use different policies. See
+     * {@link DefaultGeoServerNodeInfo} for the default implementation of the visibility logic
+     * 
+     * @param parent
+     * @return
+     */
+    void customize(WebMarkupContainer nodeInfoContainer);
+}

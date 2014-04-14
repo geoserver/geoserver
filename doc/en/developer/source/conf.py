cecad0aadf6c35c4fc22de6713b421bd4c523d2e
bencaradocdavies <Ben.Caradoc-Davies@csiro.au>
2014-04-14 08:19:12 +0000

    updating version numbers and release notes for 2.4.6

--- doc/en/developer/source/conf.py
+++ doc/en/developer/source/conf.py
@@ -47,7 +47,7 @@ copyright = u'2013, OpenPlans'
 # The short X.Y version.
 version = '2.4'
 # The full version, including alpha/beta/rc tags.
-release = '2.4-SNAPSHOT'
+release = '2.4.6'
 # Users don't need to see the "SNAPSHOT" notation when it's there
 if release.find('SNAPSHOT') != -1:
    release = '2.4.x'

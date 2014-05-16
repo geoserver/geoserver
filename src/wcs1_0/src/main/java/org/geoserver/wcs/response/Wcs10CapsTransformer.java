af91d620b3e7f529ccbcfb8637ca871e9432b4c2
Andrea Aime <andrea.aime@gmail.com>
2014-05-16 20:14:13 +0200

    [GEOS-6486] WCS 1.0 capabilities generation fails when using multi-coverage readers (no test because we'd need netcdf data to do it, and netcdf is not in supported land yet)

--- src/wcs1_0/src/main/java/org/geoserver/wcs/response/Wcs10CapsTransformer.java
+++ src/wcs1_0/src/main/java/org/geoserver/wcs/response/Wcs10CapsTransformer.java
@@ -34,8 +34,8 @@ import org.geoserver.catalog.ResourceInfo;
 import org.geoserver.catalog.util.ReaderDimensionsAccessor;
 import org.geoserver.config.ContactInfo;
 import org.geoserver.config.GeoServer;
-import org.geoserver.config.SettingsInfo;
 import org.geoserver.config.ResourceErrorHandling;
+import org.geoserver.config.SettingsInfo;
 import org.geoserver.ows.URLMangler.URLType;
 import org.geoserver.ows.util.ResponseUtils;
 import org.geoserver.wcs.WCSInfo;
@@ -741,7 +741,8 @@ public class Wcs10CapsTransformer extends TransformerBase {
                 
                 GridCoverage2DReader reader = null;
                 try {
-                    reader = (GridCoverage2DReader) catalog.getResourcePool().getGridCoverageReader(csinfo, GeoTools.getDefaultHints());
+                    reader = (GridCoverage2DReader) cv.getGridCoverageReader(null,
+                            GeoTools.getDefaultHints());
                 } catch (IOException e) {
                     LOGGER.severe("Unable to acquire a reader for this coverage with format: " + csinfo.getFormat().getName());
                 }

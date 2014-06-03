9e9e9395acfb40cc2502d2aa0379f328abff55ec
D. J. Hagberg-DHG <donald.hagberg@vaisala.com>
2014-06-03 15:44:50 -0600

    Fix handling of NPE in AdvancedDispatchFilter, seen when geoserver-monitor-plugin is added to geoserver 2.5.1 and additional application-specific plugin servlets are added into geoserver.

--- src/platform/src/main/java/org/geoserver/platform/AdvancedDispatchFilter.java
+++ src/platform/src/main/java/org/geoserver/platform/AdvancedDispatchFilter.java
@@ -26,10 +26,10 @@ import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpSession;
 
 /**
- * A servlet filter that allows for advanced dispatching. 
+ * A servlet filter that allows for advanced dispatching.
  * <p>
- * This fiter allows for a single mapping from web.xml for all requests to the spring dispatcher. 
- * It creates a wrapper around the servlet request object that "fakes" the serveltPath property to 
+ * This fiter allows for a single mapping from web.xml for all requests to the spring dispatcher.
+ * It creates a wrapper around the servlet request object that "fakes" the serveltPath property to
  * make it look like the mapping was created in web.xml when in actuality it was created in spring.
  * </p>
  * @author Justin Deoliveira, OpenGeo
@@ -42,7 +42,7 @@ public class AdvancedDispatchFilter implements Filter {
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        
+
         if (request instanceof HttpServletRequest) {
             request = new AdvancedDispatchHttpRequest((HttpServletRequest) request);
         }
@@ -51,24 +51,24 @@ public class AdvancedDispatchFilter implements Filter {
 
     public void init(FilterConfig filterConfig) throws ServletException {
     }
-    
+
     static class AdvancedDispatchHttpRequest extends HttpServletRequestWrapper {
-        
+
         String servletPath = null;
-        
+
         public AdvancedDispatchHttpRequest(HttpServletRequest delegate) {
             super(delegate);
-            
+
             if (delegate.getClass().getSimpleName().endsWith("MockHttpServletRequest")) {
                 return;
             }
-            
+
             String path = delegate.getPathInfo();
-            
+
             if (path == null) {
                 return;
             }
-            
+
             int slash = path.indexOf('/', 1);
             if (slash > -1 ) {
                 this.servletPath = path.substring(0, slash);
@@ -76,24 +76,26 @@ public class AdvancedDispatchFilter implements Filter {
             else {
                 this.servletPath = path;
             }
-            
+
             int question = this.servletPath.indexOf('?');
             if (question > -1 ) {
                 this.servletPath = this.servletPath.substring(0, question);
             }
-            
+
         }
-        
+
         public String getPathInfo() {
             HttpServletRequest delegate = (HttpServletRequest) getRequest();
-            if(delegate.getPathInfo().startsWith(servletPath))
+            if (servletPath != null &&
+                    delegate.getPathInfo() != null &&
+                    delegate.getPathInfo().startsWith(servletPath))
                 return delegate.getPathInfo().substring(servletPath.length());
             else
                 return delegate.getPathInfo();
         }
 
         public String getServletPath() {
-            return servletPath != null ? 
+            return servletPath != null ?
                 servletPath : ((HttpServletRequest)getRequest()).getServletPath();
         }
     }
--- src/platform/src/test/java/org/geoserver/platform/AdvancedDispatchFilterTest.java
+++ src/platform/src/test/java/org/geoserver/platform/AdvancedDispatchFilterTest.java
@@ -11,10 +11,10 @@ import static org.junit.Assert.*;
  * @author tw
  */
 public class AdvancedDispatchFilterTest {
-    
+
     public AdvancedDispatchFilterTest() {
     }
-  
+
     /**
      * Test of destroy method, of class AdvancedDispatchFilter.
      */
@@ -25,20 +25,46 @@ public class AdvancedDispatchFilterTest {
         request.setRequestURL("/test?name=0");
         MockHttpServletResponse response = new MockHttpServletResponse();
         MockFilterChain filterChain = new MockFilterChain();
-        
+
         AdvancedDispatchFilter instance = new AdvancedDispatchFilter();
         try {
             instance.doFilter(request, response, filterChain);
         } catch (Exception ex) {
             fail("should work");
-        } 
+        }
     }
-    
+
+    /**
+     * Need to handle a null result from getPathInfo.
+     */
+    @Test
+    public void testHandlePathInfoNull() {
+        // Ensure null request succeeds
+        MockHttpServletRequest mockRequest = new MyMockRequest();
+        mockRequest.setPathInfo(null);
+        assertNull(mockRequest.getPathInfo());
+
+        AdvancedDispatchFilter.AdvancedDispatchHttpRequest advRequest
+                = new AdvancedDispatchFilter.AdvancedDispatchHttpRequest(mockRequest);
+        assertNull(advRequest.getPathInfo());
+
+        // Ensure non-null servlet path is handled
+        advRequest.servletPath = "/bar";
+        mockRequest.setPathInfo("/bar/foo");
+        assertEquals("/bar/foo", mockRequest.getPathInfo());
+        assertEquals("/foo", advRequest.getPathInfo());
+
+        // Ensure non-null request succeeds
+        mockRequest.setPathInfo("/foo");
+        assertEquals("/foo", mockRequest.getPathInfo());
+        assertEquals("/foo", advRequest.getPathInfo());
+    }
+
     /**
      * Necessary due to special filtering out delegates with
      * name MockHttpServletRequest.
      */
     class MyMockRequest extends MockHttpServletRequest {
-        
+
     }
 }

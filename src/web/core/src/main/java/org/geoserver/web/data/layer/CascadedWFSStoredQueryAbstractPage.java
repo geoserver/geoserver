c0a7a76bab537549924ce661af68c9f798bb647f
Sampo Savolainen <sampo.savolainen@spatineo.com>
2014-06-09 16:39:55 +0300

    Add missing copyright clauses

--- src/web/core/src/main/java/org/geoserver/web/data/layer/CascadedWFSStoredQueryAbstractPage.java
+++ src/web/core/src/main/java/org/geoserver/web/data/layer/CascadedWFSStoredQueryAbstractPage.java
@@ -1,3 +1,8 @@
+/* Copyright (c) 2014 OpenPlans - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+
 package org.geoserver.web.data.layer;
 
 import java.io.IOException;
@@ -28,8 +33,6 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.StringResourceModel;
 import org.geoserver.catalog.DataStoreInfo;
-import org.geoserver.catalog.FeatureTypeInfo;
-import org.geoserver.feature.retype.RetypingDataStore;
 import org.geoserver.web.GeoServerSecuredPage;
 import org.geoserver.web.wicket.GeoServerDataProvider;
 import org.geoserver.web.wicket.GeoServerTablePanel;
--- src/web/core/src/main/java/org/geoserver/web/data/layer/CascadedWFSStoredQueryEditPage.java
+++ src/web/core/src/main/java/org/geoserver/web/data/layer/CascadedWFSStoredQueryEditPage.java
@@ -1,3 +1,8 @@
+/* Copyright (c) 2014 OpenPlans - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+
 package org.geoserver.web.data.layer;
 
 import java.io.IOException;

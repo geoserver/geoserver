12be7bc2f8af1c5a9f5199b3c7e7ecc317d17379
Christian Mueller <christian.mueller@os-solutions.at>
2014-06-15 16:47:16 +0200

    Adjust copyright headers

--- src/web/wms/src/main/java/org/geoserver/wms/web/MimeTypesFormComponent.java
+++ src/web/wms/src/main/java/org/geoserver/wms/web/MimeTypesFormComponent.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2001 - 2013 OpenPlans - www.openplans.org. All rights reserved.
+/* Copyright (c) 2014 OpenPlans - www.openplans.org. All rights reserved.
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
  */

1877c10b76777aece0a29526ec96c9d68bf0dc02
jdeolive <jdeolive@boundlessgeo.com>
2014-07-11 10:50:12 -0400

    reorganized module into geotools and geoserver components

---
+++ src/community/ysld/src/main/java/org/geoserver/ysld/YsldHandler.java
@@ -0,0 +1,61 @@
+package org.geoserver.ysld;
+
+import org.geoserver.catalog.StyleHandler;
+import org.geotools.styling.ResourceLocator;
+import org.geotools.styling.StyledLayerDescriptor;
+import org.geotools.util.Version;
+import org.geotools.ysld.Ysld;
+import org.xml.sax.EntityResolver;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class YsldHandler extends StyleHandler {
+
+    public static final String FORMAT = "ysld";
+    public static final String MIMETYPE = "application/vnd.ysld+yaml";
+
+    public YsldHandler() {
+        super("Ysld", FORMAT);
+    }
+
+    @Override
+    public String getFileExtension() {
+        return "yaml";
+    }
+
+    @Override
+    public String getCodeMirrorEditMode() {
+        return "yaml";
+    }
+
+    @Override
+    public StyledLayerDescriptor parse(Object input, Version version, ResourceLocator resourceLocator,
+        EntityResolver entityResolver) throws IOException {
+        return Ysld.parse(toReader(input));
+    }
+
+    @Override
+    public void encode(StyledLayerDescriptor sld, Version version, boolean pretty, OutputStream output) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<Exception> validate(Object input, Version version, EntityResolver entityResolver) throws IOException {
+        List<Exception> errors = new ArrayList();
+        try {
+            Ysld.parse(toReader(input));
+        }
+        catch(Exception e) {
+            errors.add(e);
+        }
+        return errors;
+    }
+
+    @Override
+    public String mimeType(Version version) {
+        return MIMETYPE;
+    }
+}

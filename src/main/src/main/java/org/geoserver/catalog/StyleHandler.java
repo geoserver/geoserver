be33e1e50a74bf5ed84964b608e0c4c638db0705
jdeolive <jdeolive@boundlessgeo.com>
2014-07-14 16:58:09 -0600

    Added copyright header

--- src/main/src/main/java/org/geoserver/catalog/StyleHandler.java
+++ src/main/src/main/java/org/geoserver/catalog/StyleHandler.java
@@ -1,3 +1,7 @@
+/* Copyright (c) 2001 - 2014 OpenPlans - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.catalog;
 
 import java.io.File;

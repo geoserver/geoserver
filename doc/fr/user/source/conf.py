b171935b227f1c0bd18ab1098ac4a7d8de03782f
jodygarnett <jody.garnett@gmail.com>
2014-07-23 00:45:05 +0000

    updating version numbers and release notes for 2.6-beta

--- doc/fr/user/source/conf.py
+++ doc/fr/user/source/conf.py
@@ -47,7 +47,7 @@ copyright = u'2014 OpenPlans'
 # The short X.Y version.
 version = '2.6'
 # The full version, including alpha/beta/rc tags.
-release = '2.6-SNAPSHOT'
+release = '2.6-beta'
 # Users don't need to see the "SNAPSHOT" notation when it's there
 if release.find('SNAPSHOT') != -1:
    release = '2.6.x'

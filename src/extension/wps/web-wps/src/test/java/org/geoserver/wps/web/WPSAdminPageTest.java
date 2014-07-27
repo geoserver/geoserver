049eeea094013f21a14235f07ba79e290a93de8a
Andrea Aime <andrea.aime@gmail.com>
2014-07-27 17:19:27 +0200

    [GEOS-6519] WPS not enabling in workspace

--- src/extension/wps/web-wps/src/test/java/org/geoserver/wps/web/WPSAdminPageTest.java
+++ src/extension/wps/web-wps/src/test/java/org/geoserver/wps/web/WPSAdminPageTest.java
@@ -4,6 +4,13 @@
  */
 package org.geoserver.wps.web;
 
+import java.util.Collections;
+
+import org.apache.wicket.PageParameters;
+import org.geoserver.catalog.WorkspaceInfo;
+import org.geoserver.config.GeoServer;
+import org.geoserver.config.impl.ServiceInfoImpl;
+import org.geoserver.ows.util.OwsUtils;
 import org.geoserver.web.GeoServerWicketTestSupport;
 import org.geoserver.web.wicket.KeywordsEditor;
 import org.geoserver.wps.WPSInfo;
@@ -22,5 +29,28 @@ public class WPSAdminPageTest extends GeoServerWicketTestSupport {
         // test that components have been filled as expected
         tester.assertComponent("form:keywords", KeywordsEditor.class);
         tester.assertModelValue("form:keywords", wps.getKeywords());
+    }
+
+    @Test
+    public void testWorkspace() throws Exception {
+        GeoServer geoServer = getGeoServerApplication().getGeoServer();
+        WPSInfo wps = geoServer.getService(WPSInfo.class);
+        WorkspaceInfo defaultWs = getCatalog().getDefaultWorkspace();
+        WPSInfo wps2 = geoServer.getFactory().create(WPSInfo.class);
+        OwsUtils.copy(wps, wps2, WPSInfo.class);
+        ((ServiceInfoImpl) wps2).setId(null);
+        wps2.setWorkspace(defaultWs);
+        wps2.setMaintainer("TestMaintainer");
+        geoServer.add(wps2);
+
+        // start the page with the custom workspace
+        login();
+        tester.startPage(WPSAdminPage.class,
+                new PageParameters(Collections.singletonMap("workspace", defaultWs.getName())));
+        // print(tester.getLastRenderedPage(), true, true, true);
+
+        // test that components have been filled as expected
+        tester.assertModelValue("form:maintainer", "TestMaintainer");
+
     }
 }

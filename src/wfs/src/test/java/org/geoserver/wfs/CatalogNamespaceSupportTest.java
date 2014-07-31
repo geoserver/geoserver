2b68f28c430aa8de16cf070ced15183f610fa37f
jdeolive <jdeolive@boundlessgeo.com>
2014-07-31 17:56:06 -0600

    Avoiding full namespace scan on GetFeature request, GEOS-6597.
    Backport of #671.

--- src/wfs/src/test/java/org/geoserver/wfs/CatalogNamespaceSupportTest.java
+++ src/wfs/src/test/java/org/geoserver/wfs/CatalogNamespaceSupportTest.java
@@ -8,9 +8,8 @@ import org.junit.Test;
 import static org.easymock.EasyMock.*;
 import static org.junit.Assert.assertEquals;
 
-/**
- * Justin Deoliveira, Boundless
- */
+import static org.junit.Assert.assertNull;
+
 public class CatalogNamespaceSupportTest {
 
     @Test
@@ -43,5 +42,51 @@ public class CatalogNamespaceSupportTest {
         assertEquals("http://foo.org", nsSupport.getURI(""));
 
         verify(ns, cat);
+    }
+
+    @Test
+    public void testGetPrefix() {
+        NamespaceInfo ns = createMock(NamespaceInfo.class);
+        expect(ns.getPrefix()).andReturn("foo");
+
+        Catalog cat = createMock(Catalog.class);
+        expect(cat.getNamespaceByURI("http://foo.org")).andReturn(ns);
+
+        replay(ns, cat);
+
+        CatalogNamespaceSupport nsSupport = new CatalogNamespaceSupport(cat);
+        assertEquals("foo", nsSupport.getPrefix("http://foo.org"));
+
+        verify(ns, cat);
+    }
+
+    @Test
+    public void testGetDefaultPrefix() {
+        NamespaceInfo ns = createMock(NamespaceInfo.class);
+        expect(ns.getPrefix()).andReturn("foo");
+
+        Catalog cat = createMock(Catalog.class);
+        expect(cat.getDefaultNamespace()).andReturn(ns);
+
+        replay(ns, cat);
+
+        CatalogNamespaceSupport nsSupport = new CatalogNamespaceSupport(cat);
+        assertEquals("foo", nsSupport.getPrefix(""));
+
+        verify(ns, cat);
+    }
+
+    @Test
+    public void testNulls() {
+        Catalog cat = createMock(Catalog.class);
+        expect(cat.getNamespaceByURI(null)).andReturn(null);
+        expect(cat.getNamespaceByPrefix(null)).andReturn(null);
+        replay(cat);
+
+        CatalogNamespaceSupport nsSupport = new CatalogNamespaceSupport(cat);
+        assertNull(nsSupport.getPrefix(null));
+        assertNull(nsSupport.getURI(null));
+
+        verify(cat);
     }
 }

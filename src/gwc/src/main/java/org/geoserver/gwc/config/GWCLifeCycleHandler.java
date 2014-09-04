87ecb035dd45e5a56d051f704c1af47421b46bbf
Gabriel Roldan <gabriel.roldan@gmail.com>
2014-09-04 01:26:50 -0300

    Revert "Make GWCLifeCycleHandler behave correctly on application shutdown."
    This reverts commit 88bb8b98 until figuring out why it breaks the build.

--- src/gwc/src/main/java/org/geoserver/gwc/config/GWCLifeCycleHandler.java
+++ src/gwc/src/main/java/org/geoserver/gwc/config/GWCLifeCycleHandler.java
@@ -4,16 +4,30 @@
  */
 package org.geoserver.gwc.config;
 
+import java.util.logging.Logger;
+
 import org.geoserver.config.impl.GeoServerLifecycleHandler;
 import org.geoserver.gwc.GWC;
+import org.geotools.util.logging.Logging;
+import org.springframework.beans.factory.InitializingBean;
 
 /**
  * GeoServer life cycle listener that reloads the gwc configuration
+ * <p>
+ * Note: this class implements {@link InitializingBean} to force spring to create it once its
+ * {@link GWC} dependency is ready. Otherwise it may not be created until the application is in the
+ * process of shutting down, which causes an exception as the GWC instance cannot be provided by
+ * then (see GEOS-6468).
  */
-public class GWCLifeCycleHandler implements GeoServerLifecycleHandler {
+public class GWCLifeCycleHandler implements GeoServerLifecycleHandler, InitializingBean {
 
-    public GWCLifeCycleHandler() {
-        // nothing to do
+    private static final Logger LOGGER = Logging.getLogger(GWCLifeCycleHandler.class);
+
+    private final GWC mediator;
+
+    public GWCLifeCycleHandler(GWC mediator) {
+        this.mediator = mediator;
+        LOGGER.fine("GWC life cycle handler created.");
     }
 
     public void beforeReload() {
@@ -22,16 +36,22 @@ public class GWCLifeCycleHandler implements GeoServerLifecycleHandler {
 
     @Override
     public void onReload() {
-        GWC.tryReload();
+        mediator.reload();
     }
 
     @Override
     public void onReset() {
-        GWC.tryReset();
+        mediator.reset();
     }
 
     @Override
     public void onDispose() {
         onReset();
     }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // Do nothing, just forcing the bean to be created
+    }
+
 }

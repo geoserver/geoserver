4defaaeea7d67adf4b2c82673c0e81c868ad746b
Jody Garnett <jody.garnett@gmail.com>
2014-09-04 12:30:50 -0700

    Missed a few (GSIP-118)

--- src/extension/app-schema/app-schema-test/src/test/java/org/geoserver/test/XLink32MockData.java
+++ src/extension/app-schema/app-schema-test/src/test/java/org/geoserver/test/XLink32MockData.java
@@ -1,5 +1,5 @@
-/* 
- * Copyright (c) 2001 - 2009 OpenPlans
+/* (c) 2014 Open Source Geospatial Foundation - all rights reserved
+ * (c) 2001 - 2009 OpenPlans
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
  */

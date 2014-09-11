5e848d24af775857233382598c9fa0767b22057d
Kevin Michael Smith <smithkm@draconic.ca>
2014-09-11 16:06:54 -0700

    Unit tests for cluster sync ack

--- src/test/java/org/geoserver/cluster/hazelcast/EventHzSynchronizerRecvTest.java
+++ src/test/java/org/geoserver/cluster/hazelcast/EventHzSynchronizerRecvTest.java
@@ -25,6 +25,7 @@ import org.geoserver.catalog.event.CatalogRemoveEvent;
 import org.geoserver.catalog.event.impl.CatalogAddEventImpl;
 import org.geoserver.catalog.event.impl.CatalogPostModifyEventImpl;
 import org.geoserver.catalog.event.impl.CatalogRemoveEventImpl;
+import org.geoserver.catalog.impl.DataStoreInfoImpl;
 import org.geoserver.cluster.ConfigChangeEvent;
 import org.geoserver.cluster.ConfigChangeEvent.Type;
 import org.geoserver.config.ConfigurationListener;
@@ -34,6 +35,7 @@ import org.geoserver.config.ServiceInfo;
 import org.geoserver.config.SettingsInfo;
 import org.hamcrest.integration.EasyMock2Adapter;
 import org.junit.Before;
+import org.junit.Test;
 
 public class EventHzSynchronizerRecvTest extends HzSynchronizerRecvTest {
 
@@ -65,6 +67,45 @@ public class EventHzSynchronizerRecvTest extends HzSynchronizerRecvTest {
                 return true;
             }
         };
+    }
+    
+    @Test
+    public void testPublishAck() throws Exception {
+    	// This is just testStoreDelete with an extra assert to test if the ack was sent 
+        DataStoreInfo info;
+        WorkspaceInfo wsInfo;
+        final String storeName = "testStore";
+        final String storeId = "Store-TEST";
+        final String storeWorkspace = "Workspace-TEST";
+        
+        {
+            info = createMock(DataStoreInfo.class);
+            wsInfo = createMock(WorkspaceInfo.class);
+    
+            expect(info.getName()).andStubReturn(storeName);
+            expect(info.getId()).andStubReturn(storeId);
+            expect(info.getWorkspace()).andStubReturn(wsInfo);
+            
+            expect(wsInfo.getId()).andStubReturn(storeWorkspace);
+            
+            expectationTestStoreDelete(info, storeName, storeId, DataStoreInfo.class);
+        }
+        replay(info, wsInfo);
+        ConfigChangeEvent evt = new ConfigChangeEvent(storeId, storeName, DataStoreInfoImpl.class, Type.REMOVE);
+        {
+            sync = getSynchronizer();
+            sync.initialize(configWatcher);
+            evt.setWorkspaceId(storeWorkspace);
+            
+            // Mock a message coming in from the cluster
+            
+            mockMessage(evt);
+        }
+        waitForSync();
+        
+        // Did the ack get sent
+        this.assertAcked(evt.getUUID());
+        verify(info, wsInfo);
     }
     
     @Override

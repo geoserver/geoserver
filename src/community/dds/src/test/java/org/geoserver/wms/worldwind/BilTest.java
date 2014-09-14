0449b402087822eb468058958dbb65554e7c6c00
Parker Abercrombie <parker@pabercrombie.com>
2014-09-13 17:21:21 -0700

    Changed how the request envelope is determined in BilMapResponse to correctly handle WMS 1.3.0 GetMap requests. (New method modeled after GeoTiffMapResponse.) Added unit test for WMS 1.3 requests. This change fixes issue GEOS-6662.

--- src/community/dds/src/test/java/org/geoserver/wms/worldwind/BilTest.java
+++ src/community/dds/src/test/java/org/geoserver/wms/worldwind/BilTest.java
@@ -15,7 +15,6 @@ import org.apache.commons.io.FileUtils;
 import org.geoserver.data.test.SystemTestData;
 import org.geoserver.wms.WMSInfo;
 import org.geoserver.wms.WMSTestSupport;
-import org.geoserver.wms.map.RenderedImageMapOutputFormat;
 import org.junit.Test;
 
 import com.mockrunner.mock.web.MockHttpServletResponse;
@@ -104,7 +103,27 @@ public class BilTest extends WMSTestSupport {
 	    String exceptstr  = getAsString(request);
 	    assertTrue("testLargeRequest",exceptstr.contains("512x512"));
 	}
-	
+
+    @Test
+    public void testWms13Request() throws Exception {
+        String layer = getLayerId(AUS_DEM);
+
+        String request = "wms?service=wms&request=GetMap&version=1.3.0" +
+        "&layers=" + layer + "&styles=&bbox=-46.3,108.3,-4.2,160.3&width=64&height=64" +
+        "&format=application/bil&crs=EPSG:4326";
+
+        MockHttpServletResponse servletResponse = getAsServletResponse(request);
+        byte[] bytes1_3_0 = getBinary(servletResponse);
+
+        // Check response length in bytes
+        int expected = width * height * 2; // 2 bytes/pixel
+        assertEquals("testStandardRequest", expected, bytes1_3_0.length);
+
+        // Check that the WMS 1.3.0 response is equivalent to the WMS 1.1.1 response.
+        byte[] bytes1_1_1 = getStandardRequest("application/bil16");
+        assertArrayEquals("WMS 1.3.0 response different from WMS 1.1.1", bytes1_1_1, bytes1_3_0);
+    }
+
 	/**
 	 * Need to override since we are in the community folder
 	 */

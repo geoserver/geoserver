092b561d69a5a9f70d36572280dbb821c3382d7e
Andrea Aime <andrea.aime@gmail.com>
2014-10-01 19:58:56 +0200

    [GEOS-6700] Add GUI integration for the SOLR data store

---
+++ src/community/solr/src/main/java/org/geoserver/solr/SolrXStreamInitializer.java
@@ -0,0 +1,28 @@
+/* Copyright (c) 2014 OpenPlans - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+
+package org.geoserver.solr;
+
+import org.geoserver.config.util.XStreamPersister;
+import org.geoserver.config.util.XStreamPersisterInitializer;
+import org.geotools.data.solr.SolrAttribute;
+import org.geotools.data.solr.SolrLayerConfiguration;
+
+import com.thoughtworks.xstream.XStream;
+
+/**
+ * 
+ * Implementation of XStreamPersisterInitializer extension point to serialize SolrLayerConfiguration
+ *
+ */
+public class SolrXStreamInitializer implements XStreamPersisterInitializer {
+
+    @Override
+    public void init(XStreamPersister persister) {
+        persister.registerBreifMapComplexType("solrLayerConfiguration",SolrLayerConfiguration.class);
+        XStream xs = persister.getXStream();
+        xs.alias("solrAttribute", SolrAttribute.class);
+    }
+}

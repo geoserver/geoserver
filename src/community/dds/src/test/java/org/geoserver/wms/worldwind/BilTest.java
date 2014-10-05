38dc5232bf6158acbac64603e75f29ae26bb9c47
Parker Abercrombie <parker@pabercrombie.com>
2014-10-05 10:16:57 -0700

    Changed expected content length for application/bil test to match expected length for application/bil16. Expected behavior is for the plugin to maintain the native byte encoding when application/bil is requested, which is application/bil16 for the test file.

--- src/community/dds/src/test/java/org/geoserver/wms/worldwind/BilTest.java
+++ src/community/dds/src/test/java/org/geoserver/wms/worldwind/BilTest.java
@@ -32,21 +32,21 @@ public class BilTest extends WMSTestSupport {
 	/**
      * This is a READ ONLY TEST so we can use one time setup
      */
-	
+
 	public static String WCS_PREFIX = "wcs";
     public static String WCS_URI = "http://www.opengis.net/wcs/1.1.1";
     public static QName AUS_DEM = new QName(WCS_URI, "Ausdem", WCS_PREFIX);
-    
+
     private final int width = 64;
     private final int height = 64;
-    
+
     @Override
     protected void onSetUp(SystemTestData testData) throws Exception {
         super.onSetUp(testData);
 
         testData.addStyle("raster", "raster.sld", BilTest.class, getCatalog());
         testData.addRasterLayer(AUS_DEM, "aus_dem.tiff", "tiff", null, BilTest.class, getCatalog());
-        
+
         WMSInfo wmsInfo = getGeoServer().getService(WMSInfo.class);
         wmsInfo.setMaxBuffer(50);
         getGeoServer().save(wmsInfo);
@@ -56,8 +56,8 @@ public class BilTest extends WMSTestSupport {
 	public void testBil() throws Exception {
         byte[] response = getStandardRequest("application/bil");
 
-        // TODO why is expected content here different than for bil16 below?
-        assertEquals("testStandardRequest", 9377, response.length);
+        int expected = width * height * 2; // Native encoding, 2 bytes/pixel
+        assertEquals("testStandardRequest", expected, response.length);
     }
 
     @Test
@@ -100,11 +100,11 @@ public class BilTest extends WMSTestSupport {
 	    String request = "wms?service=wms&request=GetMap&version=1.1.1" +
 	    		"&layers=" + layer + "&styles=&bbox=108.3,-46.3,160.3,-4.2&width=600&height=600" + 
 	    		"&format=image/bil&srs=EPSG:4326";
-	    
+
 	    String exceptstr  = getAsString(request);
 	    assertTrue("testLargeRequest",exceptstr.contains("512x512"));
 	}
-	
+
 	/**
 	 * Need to override since we are in the community folder
 	 */

34b1d3dcfe1d370e4a39d5499b5df5c9110bbd2a
Andrea Aime <andrea.aime@gmail.com>
2014-10-26 11:32:53 +0100

    [GEOS-6735] CSSHandler leaves file handles open

--- src/extension/css/src/main/java/org/geoserver/community/css/web/CssHandler.java
+++ src/extension/css/src/main/java/org/geoserver/community/css/web/CssHandler.java
@@ -5,6 +5,14 @@
  */
 package org.geoserver.community.css.web;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
 import org.geoscript.geocss.compat.CSS2SLD;
 import org.geoserver.catalog.StyleHandler;
 import org.geoserver.catalog.Styles;
@@ -14,20 +22,13 @@ import org.geotools.styling.StyledLayerDescriptor;
 import org.geotools.util.Version;
 import org.xml.sax.EntityResolver;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 /**
- * Style handler for geocss.
- *
- * Justin Deoliveira, Boundless
+ * Style handler for geocss. Justin Deoliveira, Boundless
  */
 public class CssHandler extends StyleHandler {
 
     public static final String FORMAT = "css";
+
     public static final String MIME_TYPE = "application/vnd.geoserver.geocss+css";
 
     protected CssHandler() {
@@ -40,24 +41,37 @@ public class CssHandler extends StyleHandler {
     }
 
     @Override
-    public StyledLayerDescriptor parse(Object input, Version version, ResourceLocator resourceLocator, EntityResolver entityResolver) throws IOException {
-        Style style = CSS2SLD.convert(toReader(input));
-        return Styles.sld(style);
+    public StyledLayerDescriptor parse(Object input, Version version,
+            ResourceLocator resourceLocator, EntityResolver entityResolver) throws IOException {
+        Reader reader = null;
+        try {
+            reader = toReader(input);
+            Style style = CSS2SLD.convert(reader);
+            return Styles.sld(style);
+        } finally {
+            IOUtils.closeQuietly(reader);
+        }
     }
 
     @Override
-    public void encode(StyledLayerDescriptor sld, Version version, boolean pretty, OutputStream output) throws IOException {
+    public void encode(StyledLayerDescriptor sld, Version version, boolean pretty,
+            OutputStream output) throws IOException {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public List<Exception> validate(Object input, Version version, EntityResolver entityResolver) throws IOException {
+    public List<Exception> validate(Object input, Version version, EntityResolver entityResolver)
+            throws IOException {
+        Reader reader = null;
         try {
-            CSS2SLD.convert(toReader(input));
+            reader = toReader(input);
+            Style style = CSS2SLD.convert(reader);
+            Styles.sld(style);
             return Collections.emptyList();
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             return Arrays.asList(e);
+        } finally {
+            IOUtils.closeQuietly(reader);
         }
     }
 }

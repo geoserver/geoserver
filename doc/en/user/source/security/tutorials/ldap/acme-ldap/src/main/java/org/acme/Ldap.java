e28dd6289c62c57a1985c9275e3a469a19bbb7db
Brad Hards <bradh@frogmouth.net>
2014-11-14 21:00:42 +1100

    GEOS-6761: Switch to OSGEO copyright in XML files.
    Also updates documentation configuration and some stray Java files.

--- doc/en/user/source/security/tutorials/ldap/acme-ldap/src/main/java/org/acme/Ldap.java
+++ doc/en/user/source/security/tutorials/ldap/acme-ldap/src/main/java/org/acme/Ldap.java
@@ -1,7 +1,8 @@
-/* Copyright (c) 2001 - 2013 OpenPlans - www.openplans.org. All rights reserved.
+/* Copyright (c) 2014 Open Source Geospatial Foundation - all rights reserved
+  (c) 2001 - 2013 OpenPlans
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
- * 
+ *
  * This code was derived from 
  * http://svn.apache.org/repos/asf/directory/documentation/samples/trunk/embedded-sample/. Which
  * falls under the following license. 
--- src/wms/src/main/java/org/geoserver/wms/featureinfo/XML311FeatureInfoOutputFormat.java
+++ src/wms/src/main/java/org/geoserver/wms/featureinfo/XML311FeatureInfoOutputFormat.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014 OpenPlans - www.openplans.org. All rights reserved.
+/* (c) 2014 Open Source Geospatial Foundation - all rights reserved
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
  */

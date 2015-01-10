d73dcac107f17e0cb727302f46fee08044be9a7f
Jody Garnett <jody.garnett@gmail.com>
2015-01-09 18:38:51 -0800

    Fix app schema test that was using PropertyFeatureReader directly
    (and was not closing its reader)

--- src/extension/app-schema/app-schema-test/src/test/java/org/geoserver/test/onlineTest/setup/AppSchemaTestOracleSetup.java
+++ src/extension/app-schema/app-schema-test/src/test/java/org/geoserver/test/onlineTest/setup/AppSchemaTestOracleSetup.java
@@ -163,128 +163,129 @@ public class AppSchemaTestOracleSetup extends ReferenceDataOracleSetup {
                 .append("END DROP_TABLE_OR_VIEW;\n"); 
 
         for (String fileName : propertyFiles.keySet()) {
-            PropertyFeatureReader reader = new PropertyFeatureReader(propertyFiles.get(fileName),
-            // remove .properties as it will be added in PropertyFeatureReader constructor
-                    fileName.substring(0, fileName.lastIndexOf(".properties")));
-            SimpleFeatureType schema = reader.getFeatureType();
-            String tableName = schema.getName().getLocalPart().toUpperCase();
-            // drop table if exists
-            buf.append("CALL DROP_TABLE_OR_VIEW('").append(tableName).append("')\n");
-            // create the table
-            buf.append("CREATE TABLE ").append(tableName).append("(");
-            // + pkey
-            int size = schema.getAttributeCount() + 1;
-            String[] fieldNames = new String[size];
-            List<String> createParams = new ArrayList<String>();
-            int j = 0;
-            String type;
-            String field;
-            int spatialIndexCounter = 0;
-            for (PropertyDescriptor desc : schema.getDescriptors()) {
-                field = desc.getName().toString().toUpperCase();
-                fieldNames[j] = field;
-                if (desc instanceof GeometryDescriptor) {
-                    type = "SDO_GEOMETRY";
-                    // Update spatial index
-                    int srid = getSrid(((GeometryType) desc.getType()));
-                    
-                    spatialIndex.append("DELETE FROM user_sdo_geom_metadata WHERE table_name = '")
-                            .append(tableName).append("'\n");
-		
-                    spatialIndex
-                            .append("Insert into user_sdo_geom_metadata ")
-                            .append("(TABLE_NAME,COLUMN_NAME,DIMINFO,SRID)")
-                            .append("values ('")
-                            .append(tableName)
-                            .append("','")
-                            .append(field)
-                            .append(
-                                    "',MDSYS.SDO_DIM_ARRAY(MDSYS.SDO_DIM_ELEMENT('X',140.962,144.909,0.00001),")
-                            .append("MDSYS.SDO_DIM_ELEMENT('Y',-38.858,-33.98,0.00001)")
-                            .append( //support 3d index
-                            		((GeometryDescriptor) desc).getCoordinateReferenceSystem() != null
-                            		&& ((GeometryDescriptor) desc).getCoordinateReferenceSystem().getCoordinateSystem().getDimension() == 3 ?
-                            		", MDSYS.SDO_DIM_ELEMENT('Z',-100000, 100000, 1) )," : "),")
-                            .append(srid).append(")\n");
-
-                    // ensure it's <= 30 characters to avoid Oracle exception
-                    String indexName = (tableName.length() <= 26 ? tableName : tableName.substring(
-                            0, 26)) + "_IDX";
-                    if (spatialIndexCounter > 0) {
-                        // to avoid duplicate index name when there are > 1 geometry in the same table
-                        indexName += spatialIndexCounter;
-                    }
-
-                    spatialIndex.append("CREATE INDEX \"").append(indexName).append("\" ON \"")
-                            .append(tableName).append("\"(\"").append(field).append("\") ").append(
-                                    "INDEXTYPE IS \"MDSYS\".\"SPATIAL_INDEX\"\n");
-                    spatialIndexCounter++;
-                } else {
-                    type = Classes.getShortName(desc.getType().getBinding());
-                    if (type.equalsIgnoreCase("String")) {
-                        type = "NVARCHAR2(250)";
-                    } else if (type.equalsIgnoreCase("Double")) {
-                        type = "NUMBER";
-                    }
-                    // etc. assign as required
-                }
-                createParams.add(field + " " + type);
-                j++;
-            }
-            // Add numeric PK for sorting
-            fieldNames[j] = "PKEY";
-            createParams.add("PKEY VARCHAR2(30)");
-            buf.append(StringUtils.join(createParams.iterator(), ", "));
-            buf.append(")\n");
-            buf.append("ALTER TABLE " + tableName + " ADD CONSTRAINT " + tableName + " PRIMARY KEY (PKEY)\n");
-            // then insert rows
-            SimpleFeature feature;
-            FeatureId id;
-            while (reader.hasNext()) {
-                buf.append("INSERT INTO ").append(tableName).append("(");
-                feature = reader.next();
-                buf.append(StringUtils.join(fieldNames, ", "));
-                buf.append(") ");
-                buf.append("VALUES (");
-                Collection<Property> properties = feature.getProperties();
-                String[] values = new String[size];
-                int valueIndex = 0;
-                for (Property prop : properties) {
-                    Object value = prop.getValue();
-                    if (value instanceof Geometry) {
-                    	//use wkt writer to convert geometry to string, so third dimension can be supported if present.
-                    	Geometry geom = (Geometry) value;
-                    	value = new WKTWriter(geom.getCoordinate().z == Double.NaN? 2 : 3).write(geom);
-                    }
-                    if (value == null || value.toString().equalsIgnoreCase("null")) {
-                        values[valueIndex] = "null";
-                    } else if (prop.getType() instanceof GeometryType) {
-                        int srid = getSrid(((GeometryType) prop.getType()));
-                        StringBuffer geomValue = new StringBuffer(parser + "('");
-                        geomValue.append(value).append("'");
-                        if (srid > -1) {
-                            // attach srid
-                            geomValue.append(", ").append(srid);
+            File file = new File(fileName);
+            
+            try ( PropertyFeatureReader reader = new PropertyFeatureReader("test", file ) ){            
+                SimpleFeatureType schema = reader.getFeatureType();
+                String tableName = schema.getName().getLocalPart().toUpperCase();
+                // drop table if exists
+                buf.append("CALL DROP_TABLE_OR_VIEW('").append(tableName).append("')\n");
+                // create the table
+                buf.append("CREATE TABLE ").append(tableName).append("(");
+                // + pkey
+                int size = schema.getAttributeCount() + 1;
+                String[] fieldNames = new String[size];
+                List<String> createParams = new ArrayList<String>();
+                int j = 0;
+                String type;
+                String field;
+                int spatialIndexCounter = 0;
+                for (PropertyDescriptor desc : schema.getDescriptors()) {
+                    field = desc.getName().toString().toUpperCase();
+                    fieldNames[j] = field;
+                    if (desc instanceof GeometryDescriptor) {
+                        type = "SDO_GEOMETRY";
+                        // Update spatial index
+                        int srid = getSrid(((GeometryType) desc.getType()));
+                        
+                        spatialIndex.append("DELETE FROM user_sdo_geom_metadata WHERE table_name = '")
+                                .append(tableName).append("'\n");
+    		
+                        spatialIndex
+                                .append("Insert into user_sdo_geom_metadata ")
+                                .append("(TABLE_NAME,COLUMN_NAME,DIMINFO,SRID)")
+                                .append("values ('")
+                                .append(tableName)
+                                .append("','")
+                                .append(field)
+                                .append(
+                                        "',MDSYS.SDO_DIM_ARRAY(MDSYS.SDO_DIM_ELEMENT('X',140.962,144.909,0.00001),")
+                                .append("MDSYS.SDO_DIM_ELEMENT('Y',-38.858,-33.98,0.00001)")
+                                .append( //support 3d index
+                                		((GeometryDescriptor) desc).getCoordinateReferenceSystem() != null
+                                		&& ((GeometryDescriptor) desc).getCoordinateReferenceSystem().getCoordinateSystem().getDimension() == 3 ?
+                                		", MDSYS.SDO_DIM_ELEMENT('Z',-100000, 100000, 1) )," : "),")
+                                .append(srid).append(")\n");
+    
+                        // ensure it's <= 30 characters to avoid Oracle exception
+                        String indexName = (tableName.length() <= 26 ? tableName : tableName.substring(
+                                0, 26)) + "_IDX";
+                        if (spatialIndexCounter > 0) {
+                            // to avoid duplicate index name when there are > 1 geometry in the same table
+                            indexName += spatialIndexCounter;
                         }
-                        geomValue.append(")");
-                        values[valueIndex] = geomValue.toString();
-                    } else if (prop.getType().getBinding().getSimpleName().equalsIgnoreCase("DATE")) {
-                        values[valueIndex] = "TO_DATE('" + value + "', 'yyyy-MM-dd')";
+    
+                        spatialIndex.append("CREATE INDEX \"").append(indexName).append("\" ON \"")
+                                .append(tableName).append("\"(\"").append(field).append("\") ").append(
+                                        "INDEXTYPE IS \"MDSYS\".\"SPATIAL_INDEX\"\n");
+                        spatialIndexCounter++;
                     } else {
-                        values[valueIndex] = "'" + value + "'";
+                        type = Classes.getShortName(desc.getType().getBinding());
+                        if (type.equalsIgnoreCase("String")) {
+                            type = "NVARCHAR2(250)";
+                        } else if (type.equalsIgnoreCase("Double")) {
+                            type = "NUMBER";
+                        }
+                        // etc. assign as required
                     }
-                    valueIndex++;
+                    createParams.add(field + " " + type);
+                    j++;
                 }
-                id = feature.getIdentifier();                
-                // insert primary key
-                values[valueIndex] = "'" + id.toString() + "'";
-                buf.append(StringUtils.join(values, ","));
+                // Add numeric PK for sorting
+                fieldNames[j] = "PKEY";
+                createParams.add("PKEY VARCHAR2(30)");
+                buf.append(StringUtils.join(createParams.iterator(), ", "));
                 buf.append(")\n");
+                buf.append("ALTER TABLE " + tableName + " ADD CONSTRAINT " + tableName + " PRIMARY KEY (PKEY)\n");
+                // then insert rows
+                SimpleFeature feature;
+                FeatureId id;
+                while (reader.hasNext()) {
+                    buf.append("INSERT INTO ").append(tableName).append("(");
+                    feature = reader.next();
+                    buf.append(StringUtils.join(fieldNames, ", "));
+                    buf.append(") ");
+                    buf.append("VALUES (");
+                    Collection<Property> properties = feature.getProperties();
+                    String[] values = new String[size];
+                    int valueIndex = 0;
+                    for (Property prop : properties) {
+                        Object value = prop.getValue();
+                        if (value instanceof Geometry) {
+                        	//use wkt writer to convert geometry to string, so third dimension can be supported if present.
+                        	Geometry geom = (Geometry) value;
+                        	value = new WKTWriter(geom.getCoordinate().z == Double.NaN? 2 : 3).write(geom);
+                        }
+                        if (value == null || value.toString().equalsIgnoreCase("null")) {
+                            values[valueIndex] = "null";
+                        } else if (prop.getType() instanceof GeometryType) {
+                            int srid = getSrid(((GeometryType) prop.getType()));
+                            StringBuffer geomValue = new StringBuffer(parser + "('");
+                            geomValue.append(value).append("'");
+                            if (srid > -1) {
+                                // attach srid
+                                geomValue.append(", ").append(srid);
+                            }
+                            geomValue.append(")");
+                            values[valueIndex] = geomValue.toString();
+                        } else if (prop.getType().getBinding().getSimpleName().equalsIgnoreCase("DATE")) {
+                            values[valueIndex] = "TO_DATE('" + value + "', 'yyyy-MM-dd')";
+                        } else {
+                            values[valueIndex] = "'" + value + "'";
+                        }
+                        valueIndex++;
+                    }
+                    id = feature.getIdentifier();                
+                    // insert primary key
+                    values[valueIndex] = "'" + id.toString() + "'";
+                    buf.append(StringUtils.join(values, ","));
+                    buf.append(")\n");
+                }
             }
-        }
-        buf.append(spatialIndex.toString());
-        if (buf.length() > 0) {
-            this.sql = buf.toString();
+            buf.append(spatialIndex.toString());
+            if (buf.length() > 0) {
+                this.sql = buf.toString();
+            }
         }
     }
 

012b9498ddf0b7025b64bf32c5875c287b828a14
Andrea Aime <andrea.aime@gmail.com>
2015-01-15 09:57:54 +0100

    [GEOS-2420], headers and javadoc fixes following Jody's review

--- src/extension/wps/wps-core/src/main/java/org/geoserver/wps/process/GeoServerProcessors.java
+++ src/extension/wps/wps-core/src/main/java/org/geoserver/wps/process/GeoServerProcessors.java
@@ -74,7 +74,10 @@ public class GeoServerProcessors implements ApplicationContextAware {
      * Look up a Factory by name of a process it supports.
      * 
      * @param name Name of the Process you wish to work with
-     * @param applyFilters TODO
+     * @param applyFilters Whether to apply the available {@link ProcessFilter} to the returned
+     *        factory, or not (if the code needs to check the original process factory by class name
+     *        for example, better not to apply the filters, which often wrap the factories to add
+     *        extra functionality)
      * @return ProcessFactory capable of creating an instanceof the named process
      */
     public static ProcessFactory createProcessFactory(Name name, boolean applyFilters) {

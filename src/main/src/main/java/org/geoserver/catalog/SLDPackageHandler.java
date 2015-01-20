3ef9bbc0e63f0cf17b77d4b9ae5e728c4d862ed4
Jose Garcia <jose.garcia@geocat.net>
2015-01-20 09:41:28 +0100

    Move SLD package extension in StyleResource. Removed unrequired import and update header

--- src/main/src/main/java/org/geoserver/catalog/SLDPackageHandler.java
+++ src/main/src/main/java/org/geoserver/catalog/SLDPackageHandler.java
@@ -1,13 +1,10 @@
-/* (c) 2014 Open Source Geospatial Foundation - all rights reserved
- * (c) 2001 - 2014 OpenPlans
- * This code is licensed under the GPL 2.0 license, available at the root
+/* This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
  */
 package org.geoserver.catalog;
 
 import com.google.common.io.Files;
 import org.apache.commons.io.FileUtils;
-import org.geotools.map.StyleLayer;
 import org.geotools.styling.*;
 import org.geotools.util.Version;
 import org.geotools.util.logging.Logging;

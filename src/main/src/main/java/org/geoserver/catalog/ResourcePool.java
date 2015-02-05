1f486eda40bdd978f4e33944ace7e65e005db2dc
Andrea Aime <andrea.aime@gmail.com>
2015-02-05 16:40:00 +0100

    [GEOS-6873] GeoServer leaking commons http-client connection pools in ResourcePool

--- src/main/src/main/java/org/geoserver/catalog/ResourcePool.java
+++ src/main/src/main/java/org/geoserver/catalog/ResourcePool.java
@@ -9,6 +9,7 @@ import java.awt.RenderingHints;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -34,7 +35,6 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.eclipse.xsd.XSDElementDeclaration;
 import org.eclipse.xsd.XSDParticle;
@@ -675,10 +675,10 @@ public class ResourcePool {
         }
         
         //check the cache
-        List<AttributeTypeInfo> atts = (List<AttributeTypeInfo>) featureTypeAttributeCache.get(info.getId());
+        List<AttributeTypeInfo> atts = featureTypeAttributeCache.get(info.getId());
         if (atts == null) {
             synchronized (featureTypeAttributeCache) {
-                atts = (List<AttributeTypeInfo>) featureTypeAttributeCache.get(info.getId());
+                atts = featureTypeAttributeCache.get(info.getId());
                 if (atts == null) {
                     //load from feature type
                     atts = loadAttributes(info);
@@ -715,7 +715,7 @@ public class ResourcePool {
             att.setMaxOccurs(pd.getMaxOccurs());
             att.setNillable(pd.isNillable());
             att.setBinding(pd.getType().getBinding());
-            int length = FeatureTypes.getFieldLength((AttributeDescriptor) pd);
+            int length = FeatureTypes.getFieldLength(pd);
             if(length > 0) {
                 att.setLength(length);
             }
@@ -839,10 +839,10 @@ public class ResourcePool {
     
     FeatureType getCacheableFeatureType( FeatureTypeInfo info, boolean handleProjectionPolicy ) throws IOException {
         String key = getFeatureTypeInfoKey(info, handleProjectionPolicy);
-        FeatureType ft = (FeatureType) featureTypeCache.get( key );
+        FeatureType ft = featureTypeCache.get( key );
         if ( ft == null ) {
             synchronized ( featureTypeCache ) {
-                ft = (FeatureType) featureTypeCache.get( key );
+                ft = featureTypeCache.get( key );
                 if ( ft == null ) {
                     
                     //grab the underlying feature type
@@ -966,7 +966,7 @@ public class ResourcePool {
                 
                     AttributeDescriptor ad = (AttributeDescriptor) pd;
                     ad = handleDescriptor(ad, info);
-                    tb.add( (AttributeDescriptor) ad );
+                    tb.add( ad );
                 }
             }
             ft = tb.buildFeatureType();
@@ -1277,11 +1277,11 @@ public class ResourcePool {
             }
             
             key = new CoverageHintReaderKey(info.getId(), hints);
-            reader = (GridCoverage2DReader) hintCoverageReaderCache.get( key );
+            reader = hintCoverageReaderCache.get( key );
         } else {
             key = info.getId();
             if(key != null) {
-                reader = (GridCoverageReader) coverageReaderCache.get( key );
+                reader = coverageReaderCache.get( key );
             }
         }
         
@@ -1290,9 +1290,9 @@ public class ResourcePool {
             synchronized ( hints != null ? hintCoverageReaderCache : coverageReaderCache ) {
                 if (key != null) {
                     if (hints != null) {
-                        reader = (GridCoverageReader) hintCoverageReaderCache.get(key);
+                        reader = hintCoverageReaderCache.get(key);
                     } else {
-                        reader = (GridCoverageReader) coverageReaderCache.get(key);
+                        reader = coverageReaderCache.get(key);
                     }
                 }
                 if (reader == null) {
@@ -1342,7 +1342,7 @@ public class ResourcePool {
             }
             // Avoid dimensions wrapping since we have a multi-coverage reader 
             // but no coveragename have been specified
-            return (GridCoverage2DReader) reader;
+            return reader;
 
             
         }
@@ -1513,10 +1513,10 @@ public class ResourcePool {
     public WebMapServer getWebMapServer(WMSStoreInfo info) throws IOException {
         try {
             String id = info.getId();
-            WebMapServer wms = (WebMapServer) wmsCache.get(id);
+            WebMapServer wms = wmsCache.get(id);
             if (wms == null) {
                 synchronized (wmsCache) {
-                    wms = (WebMapServer) wmsCache.get(id);
+                    wms = wmsCache.get(id);
                     if (wms == null) {
                         HTTPClient client = getHTTPClient(info);
                         String capabilitiesURL = info.getCapabilitiesURL();
@@ -1809,7 +1809,7 @@ public class ResourcePool {
         public V remove(Object key) {
             V object = super.remove(key);
             if (object != null) {
-                dispose((K) key, (V) object);
+                dispose((K) key, object);
             }
             return object;
         }
@@ -1989,8 +1989,20 @@ public class ResourcePool {
     class WMSCache extends CatalogResourceCache<String, WebMapServer> {
 
         @Override
-        protected void dispose(String key, WebMapServer object) {
-            // nothing to do
+        protected void dispose(String key, WebMapServer server) {
+            HTTPClient client = server.getHTTPClient();
+            if (client instanceof Closeable) {
+                // dispose the client, and the connection pool hosted into it as a consequence
+                // the connection pool additionally holds a few threads that are also getting
+                // disposed with this call
+                MultithreadedHttpClient mt = (MultithreadedHttpClient) client;
+                try {
+                    mt.close();
+                } catch (Exception e) {
+                    LOGGER.log(Level.FINE,
+                            "Failure while disposing the http client for a WMS store", e);
+                }
+            }
         }
 
     }

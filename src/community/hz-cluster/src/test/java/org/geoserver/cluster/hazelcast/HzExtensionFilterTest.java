dd07e1808b587ae5b398c22f6ea1e292b85974c4
Kevin Smith <ksmith@boundlessgeo.com>
2015-04-24 14:33:25 -0700

    Unit tests for HzExtensionFilter

---
+++ src/community/hz-cluster/src/test/java/org/geoserver/cluster/hazelcast/HzExtensionFilterTest.java
@@ -0,0 +1,73 @@
+/* (c) 2015 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+
+package org.geoserver.cluster.hazelcast;
+
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.Assert.*;
+
+import org.easymock.EasyMock;
+import org.geoserver.platform.ExtensionFilter;
+import org.geoserver.platform.GeoServerExtensionsHelper;
+import org.geoserver.util.CacheProvider;
+import org.geoserver.util.DefaultCacheProvider;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class HzExtensionFilterTest {
+    
+    @Rule
+    public GeoServerExtensionsHelper.ExtensionsHelperRule extensions =
+        new GeoServerExtensionsHelper.ExtensionsHelperRule();
+    
+    @Test
+    public void testActive() {
+        CacheProvider rivalProvider = EasyMock.createMock("rivalProvider", CacheProvider.class);
+        CacheProvider hzProvider = EasyMock.createMock("hzProvider", HzCacheProvider.class);
+        HzCluster cluster = EasyMock.createMock("cluster", HzCluster.class);
+        
+        EasyMock.expect(cluster.isEnabled()).andStubReturn(true); // Cluster is enabled
+        
+        EasyMock.replay(rivalProvider, hzProvider, cluster);
+        
+        ExtensionFilter filter = new HzExtensionFilter(cluster);
+        
+        extensions.singleton("filter", filter, ExtensionFilter.class);
+        
+        extensions.singleton("rivalProvider", rivalProvider, CacheProvider.class);
+        extensions.singleton("hzProvider", hzProvider, CacheProvider.class, HzCacheProvider.class);
+        
+        CacheProvider result = DefaultCacheProvider.findProvider();
+        
+        assertThat(result, sameInstance(hzProvider)); // Clustered provider used
+        
+        EasyMock.verify(rivalProvider, hzProvider, cluster);
+    }
+    
+    @Test
+    public void testInactive() {
+        CacheProvider rivalProvider = EasyMock.createMock("rivalProvider", CacheProvider.class);
+        CacheProvider hzProvider = EasyMock.createMock("hzProvider", HzCacheProvider.class);
+        HzCluster cluster = EasyMock.createMock("cluster", HzCluster.class);
+        
+        EasyMock.expect(cluster.isEnabled()).andStubReturn(false); // Cluster is disabled
+        
+        EasyMock.replay(rivalProvider, hzProvider, cluster);
+        
+        ExtensionFilter filter = new HzExtensionFilter(cluster);
+        
+        extensions.singleton("filter", filter, ExtensionFilter.class);
+        
+        extensions.singleton("rivalProvider", rivalProvider, CacheProvider.class);
+        extensions.singleton("hzProvider", hzProvider, CacheProvider.class, HzCacheProvider.class);
+        
+        CacheProvider result = DefaultCacheProvider.findProvider();
+        
+        assertThat(result, sameInstance(rivalProvider)); // Other provider used
+        
+        EasyMock.verify(rivalProvider, hzProvider, cluster);
+    }
+    
+}

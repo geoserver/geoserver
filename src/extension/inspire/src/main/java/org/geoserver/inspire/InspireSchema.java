2ea0bc4db04a982014780a4c11f1fe089489efa1
Marcus Sen <mase@bgs.ac.uk>
2015-05-30 21:46:55 +0100

    Updated copyright headers

--- src/extension/inspire/src/main/java/org/geoserver/inspire/InspireSchema.java
+++ src/extension/inspire/src/main/java/org/geoserver/inspire/InspireSchema.java
@@ -1,3 +1,7 @@
+/* (c) 2014 - 2015 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.inspire;
 
 public class InspireSchema {

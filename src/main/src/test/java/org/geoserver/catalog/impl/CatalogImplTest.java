8df48c9c2f59d57af6de4bd0e236bed831067729
Torben Barsbsballe <tbarsballe@boundlessgeo.com>
2015-07-03 14:47:37 -0700

    [GEOS-7098] Improve getStoreByName null handling

--- src/main/src/test/java/org/geoserver/catalog/impl/CatalogImplTest.java
+++ src/main/src/test/java/org/geoserver/catalog/impl/CatalogImplTest.java
@@ -35,6 +35,7 @@ import org.geoserver.catalog.MetadataMap;
 import org.geoserver.catalog.NamespaceInfo;
 import org.geoserver.catalog.Predicates;
 import org.geoserver.catalog.ResourceInfo;
+import org.geoserver.catalog.StoreInfo;
 import org.geoserver.catalog.StyleInfo;
 import org.geoserver.catalog.WMSLayerInfo;
 import org.geoserver.catalog.WMSStoreInfo;
@@ -712,6 +713,41 @@ public class CatalogImplTest {
         assertNotNull(ds5);
         assertFalse( ds == ds5 );
         assertEquals( ds, ds5 );
+    }
+    
+    @Test
+    public void testGetStoreByName() {
+        addDataStore();
+        
+        StoreInfo ds2 = catalog.getStoreByName(ds.getName(), StoreInfo.class);
+        assertNotNull(ds2);
+        assertFalse( ds == ds2 );
+        assertEquals( ds, ds2 );
+        
+        StoreInfo ds3 = catalog.getStoreByName(ws, null, StoreInfo.class);
+        assertNotNull(ds3);
+        assertFalse( ds == ds3 );
+        assertEquals( ds, ds3 );
+        
+        StoreInfo ds4 = catalog.getStoreByName(ws, Catalog.DEFAULT, StoreInfo.class);
+        assertNotNull(ds4);
+        assertFalse( ds == ds4 );
+        assertEquals( ds, ds4 );
+        
+        StoreInfo ds5 = catalog.getStoreByName(Catalog.DEFAULT, Catalog.DEFAULT, StoreInfo.class);
+        assertNotNull(ds5);
+        assertFalse( ds == ds5 );
+        assertEquals( ds, ds5 );
+        
+        StoreInfo ds6 = catalog.getStoreByName((String)null, null, StoreInfo.class);
+        assertNotNull(ds6);
+        assertFalse( ds == ds6 );
+        assertEquals( ds, ds3 );
+        
+        StoreInfo ds7 = catalog.getStoreByName(Catalog.DEFAULT, Catalog.DEFAULT, StoreInfo.class);
+        assertNotNull(ds7);
+        assertFalse( ds == ds7 );
+        assertEquals( ds6, ds7 );
     }
     
     @Test

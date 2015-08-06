9cb926d0cb6b1f9a088749bdb0c43601d7264791
Andrea Aime <andrea.aime@gmail.com>
2015-08-06 10:45:27 +0200

    [GEOS-7079] Allow importer to harvest a granule against a empty mosaic

--- src/extension/importer/core/src/main/java/org/geoserver/importer/Importer.java
+++ src/extension/importer/core/src/main/java/org/geoserver/importer/Importer.java
@@ -1048,7 +1048,7 @@ public class Importer implements DisposableBean, ApplicationListener {
             throws IOException {
         if (data instanceof SpatialFile) {
             SpatialFile sf = (SpatialFile) data;
-            List<HarvestedSource> harvests = sr.harvest(sr.getGridCoverageNames()[0], sf.getFile(),
+            List<HarvestedSource> harvests = sr.harvest(null, sf.getFile(),
                     null);
             checkSingleHarvest(harvests);
         } else if (data instanceof Directory) {

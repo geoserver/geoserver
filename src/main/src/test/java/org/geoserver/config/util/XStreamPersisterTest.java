393e5c9f1338346bc6db18ebc418c9604f70c0ec
Andrea Aime <andrea.aime@gmail.com>
2015-08-07 14:32:45 +0200

    Adding linked hash set in the xstream white list, it's used in the layer secondary styles storage

--- src/main/src/test/java/org/geoserver/config/util/XStreamPersisterTest.java
+++ src/main/src/test/java/org/geoserver/config/util/XStreamPersisterTest.java
@@ -640,6 +640,7 @@ public class XStreamPersisterTest {
         // l.setName( "layer" );
         l.setResource( ft );
         l.setDefaultStyle( s );
+        l.getStyles().add(s);
         catalog.add( l );
         
         ByteArrayOutputStream out = out();
@@ -651,8 +652,9 @@ public class XStreamPersisterTest {
         assertEquals( l.getResource().getName(), l.getName() );
         assertEquals( ft, l.getResource() );
         assertEquals( s, l.getDefaultStyle() );
-        //assertNotNull( l.getStyles() );
-        
+        assertNotNull(l.getStyles());
+        assertEquals(1, l.getStyles().size());
+        assertTrue(l.getStyles().contains(s));
     }
     
     @Test

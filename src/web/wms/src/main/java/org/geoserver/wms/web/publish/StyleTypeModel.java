7de3057279ef9474e1af928048e7abe5480cdb7a
Torben Barsbsballe <tbarsballe@boundlessgeo.com>
2015-08-11 10:08:08 -0700

    Add style generation to New/Edit Style page

---
+++ src/web/wms/src/main/java/org/geoserver/wms/web/publish/StyleTypeModel.java
@@ -0,0 +1,30 @@
+/* (c) 2015 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.wms.web.publish;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.geoserver.catalog.StyleInfo;
+import org.geoserver.catalog.StyleType;
+import org.geoserver.web.GeoServerApplication;
+
+/**
+ * A loadable model for the registered style list that does sort the styles too 
+ */
+@SuppressWarnings("serial")
+public class StyleTypeModel extends LoadableDetachableModel {
+
+    @Override
+    protected Object load() {
+        List<StyleType> styleTypes = Arrays.asList(StyleType.values());
+        Collections.sort(styleTypes);
+        return styleTypes;
+    }
+
+}

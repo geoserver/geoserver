dc0af829750ff5caf40f61f11d91b7a5b840cf76
Andrea Aime <andrea.aime@gmail.com>
2015-08-23 12:12:17 +0200

    [GEOS-6026] WMS capabilities should report workspace qualified styles as ... qualified

--- src/wms/src/main/java/org/geoserver/wms/capabilities/GetCapabilitiesTransformer.java
+++ src/wms/src/main/java/org/geoserver/wms/capabilities/GetCapabilitiesTransformer.java
@@ -75,6 +75,7 @@ import org.geotools.xml.transform.Translator;
 import org.opengis.feature.type.Name;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.operation.TransformException;
+import org.opengis.util.InternationalString;
 import org.springframework.util.Assert;
 import org.vfny.geoserver.util.ResponseUtils;
 import org.xml.sax.Attributes;
@@ -944,9 +945,11 @@ public class GetCapabilitiesTransformer extends TransformerBase {
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
-                element("Name", defaultStyle.getName());
-                element("Title", ftStyle.getTitle());
-                element("Abstract", ftStyle.getAbstract());
+                element("Name", defaultStyle.prefixedName());
+                if (ftStyle.getDescription() != null) {
+                    element("Title", ftStyle.getDescription().getTitle());
+                    element("Abstract", ftStyle.getDescription().getAbstract());
+                }
                 handleLegendURL(layer, layer.getLegend(), null ,layer.getDefaultStyle());
                 end("Style");
 
@@ -959,9 +962,11 @@ public class GetCapabilitiesTransformer extends TransformerBase {
                         throw new RuntimeException(e);
                     }
                     start("Style");
-                    element("Name", styleInfo.getName());
-                    element("Title", ftStyle.getTitle());
-                    element("Abstract", ftStyle.getAbstract());
+                    element("Name", styleInfo.prefixedName());
+                    if (ftStyle.getDescription() != null) {
+                        element("Title", ftStyle.getDescription().getTitle());
+                        element("Abstract", ftStyle.getDescription().getAbstract());
+                    }
                     handleLegendURL(layer, null, styleInfo, styleInfo);
                     end("Style");
                 }
@@ -973,6 +978,12 @@ public class GetCapabilitiesTransformer extends TransformerBase {
         
 
         
+        private void element(String element, InternationalString is) {
+            if (is != null) {
+                element(element, is.toString());
+            }
+        }
+
         /**
          * Inserts the ScaleHint element in the layer information. 
          * <p>

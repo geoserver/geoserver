ef168f3560a0afaed1f1aa3420ecf2a8d1362072
Andreas Watermeyer <Andreas.Watermeyer@its-telco.de>
2015-09-03 09:55:53 +0200

    GEOS-1235, GEOS-6777, GEOS-3651, GEOS-7053: Avoid date wrapping due to timezone change

--- src/wfs/src/main/java/org/geoserver/wfs/xml/xs/DateBinding.java
+++ src/wfs/src/main/java/org/geoserver/wfs/xml/xs/DateBinding.java
@@ -17,13 +17,24 @@ import org.geotools.xs.bindings.XSDateBinding;
  * timezone.
  *
  * @author Justin Deoliveira, The Open Planning Project
- *
+ * @author awaterme
  */
 public class DateBinding extends XSDateBinding {
     public String encode(Object object, String value) throws Exception {
+    
+		/*
+		 * GEOS-1235, GEOS-6777, GEOS-3651, GEOS-7053:
+		 * The string conversion is implementation by
+		 * org.geotools.xml.impl.XsDateTimeFormat#format(Object, StringBuffer,
+		 * java.text.FieldPosition): Apparently the CITE tests require a UTC
+		 * Timezone. To avoid flipping into the previous or next day, depending
+		 * on the system default time zone, consider the offset.
+		 */
         Date date = (Date) object;
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
+        long sourceMs = date.getTime();
+        int offsetMs = calendar.getTimeZone().getOffset(date.getTime());
+        calendar.setTimeInMillis(sourceMs+offsetMs);
         calendar.setTimeZone(TimeZone.getTimeZone("UTC"));
 
         return DatatypeConverterImpl.getInstance().printDate(calendar);

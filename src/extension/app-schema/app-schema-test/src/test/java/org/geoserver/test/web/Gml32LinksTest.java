1041ceba472160ed8620bc33ed108030d5dd0955
Stefano Costa <ridethepenguin@gmail.com>
2015-10-01 18:13:24 +0200

    [GEOS-7226] Default GML links in Layer Preview page do not work for app-schema layers

---
+++ src/extension/app-schema/app-schema-test/src/test/java/org/geoserver/test/web/Gml32LinksTest.java
@@ -0,0 +1,24 @@
+/* (c) 2015 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+
+package org.geoserver.test.web;
+
+import java.util.Arrays;
+
+import org.geoserver.data.test.SystemTestData;
+import org.geoserver.test.Gsml30MockData;
+
+public class Gml32LinksTest extends AbstractMapPreviewPageTest {
+
+    public Gml32LinksTest() {
+        super(Arrays.asList("../ows?service=WFS&amp;version=1.1.0&amp;request=GetFeature&amp;typeName=gsml:MappedFeature&amp;outputFormat=gml32&amp;maxFeatures=50"));
+    }
+
+    @Override
+    protected SystemTestData createTestData() throws Exception {
+        return new Gsml30MockData();
+    }
+
+}

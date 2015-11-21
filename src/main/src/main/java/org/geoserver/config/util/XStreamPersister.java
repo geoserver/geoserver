e17c9ec1998f32c37eab68550ca21ab2e2e2bd84
Andrea Aime <andrea.aime@gmail.com>
2015-11-21 14:50:19 +0100

    [GEOS-7317] com.thoughtworks.xstream.mapper.DynamicProxyMapper is not part of XStream class whitelist

--- src/main/src/main/java/org/geoserver/config/util/XStreamPersister.java
+++ src/main/src/main/java/org/geoserver/config/util/XStreamPersister.java
@@ -143,6 +143,7 @@ import com.thoughtworks.xstream.io.HierarchicalStreamDriver;
 import com.thoughtworks.xstream.io.HierarchicalStreamReader;
 import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 import com.thoughtworks.xstream.mapper.ClassAliasingMapper;
+import com.thoughtworks.xstream.mapper.DynamicProxyMapper;
 import com.thoughtworks.xstream.mapper.Mapper;
 import com.vividsolutions.jts.geom.Geometry;
 import org.geoserver.catalog.DataLinkInfo;
@@ -462,6 +463,7 @@ public class XStreamPersister {
         xs.allowTypeHierarchy(Info.class);
         xs.allowTypeHierarchy(Multimap.class);
         xs.allowTypeHierarchy(JAIInfo.class);
+        xs.allowTypes(new Class[] {DynamicProxyMapper.DynamicProxy.class});
         xs.allowTypes(new String[] { "java.util.Collections$SingletonList" });
         xs.allowTypesByWildcard(new String[] { "org.geoserver.catalog.**" });
         xs.allowTypesByWildcard(new String[] { "org.geoserver.security.**" });

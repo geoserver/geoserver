425e76e30395ac2288eee49546ba2bbbd06d946b
Niels Charlier <niels@scitus.be>
2015-11-26 15:35:25 +0100

    Refactor layer/layergroup UI: update headers

--- src/web/core/src/main/java/org/geoserver/web/data/layergroup/LayerGroupEditPage.java
+++ src/web/core/src/main/java/org/geoserver/web/data/layergroup/LayerGroupEditPage.java
@@ -1,4 +1,4 @@
-/* (c) 2014 Open Source Geospatial Foundation - all rights reserved
+/* (c) 2014 - 2015 Open Source Geospatial Foundation - all rights reserved
  * (c) 2001 - 2013 OpenPlans
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.

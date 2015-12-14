c61a6b93aeb4e7bd9ca96cfbd9daacac4405d71d
Dean Povey <dean.povey@gmail.com>
2015-12-14 18:14:57 +1000

    [GEOS-7297] Fix issue where stored queries fail under load.

--- src/wfs/src/main/java/org/geoserver/wfs/StoredQueryProvider.java
+++ src/wfs/src/main/java/org/geoserver/wfs/StoredQueryProvider.java
@@ -1,4 +1,4 @@
-/* (c) 2014 Open Source Geospatial Foundation - all rights reserved
+/* (c) 2014 - 2015 Open Source Geospatial Foundation - all rights reserved
  * (c) 2001 - 2013 OpenPlans
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
@@ -225,7 +225,7 @@ public class StoredQueryProvider {
         try {
             StoredQueryDescriptionType q = 
                 (StoredQueryDescriptionType) p.parse(new BufferedInputStream(fin));
-            return createStoredQuery(q);
+            return createStoredQuery(q, false);
         } 
         finally {
             fin.close();

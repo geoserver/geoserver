396c648cb2a4bef6481290f170d9c2a97810800f
Andrea Aime <andrea.aime@gmail.com>
2015-12-29 10:52:08 +0100

    Adding file forgot in the initial migration

---
+++ src/web/core/src/main/java/org/geoserver/web/GeoServerWicketServlet.java
@@ -0,0 +1,21 @@
+/* (c) 2014 - 2015 Open Source Geospatial Foundation - all rights reserved
+ * (c) 2001 - 2013 OpenPlans
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.web;
+
+import org.apache.wicket.protocol.http.WicketFilter;
+import org.apache.wicket.protocol.http.WicketServlet;
+import org.geoserver.platform.GeoServerExtensions;
+
+@SuppressWarnings("serial")
+public class GeoServerWicketServlet extends WicketServlet {
+
+    @Override
+    protected WicketFilter newWicketFilter() {
+        WicketFilter filter = new WicketFilter(GeoServerExtensions.bean(GeoServerApplication.class));
+        filter.setFilterPath("/web");
+        return filter;
+    }
+}

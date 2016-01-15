87fb015dc1dc1db9ab01292023cff1ffd6841edf
Alvaro Huarte <ahuarte47@yahoo.es>
2016-01-15 08:30:29 +0100

    Avoid log about non existing memory leak

--- src/wms/src/main/java/org/geoserver/wms/GetMap.java
+++ src/wms/src/main/java/org/geoserver/wms/GetMap.java
@@ -1,4 +1,4 @@
-/* (c) 2014 Open Source Geospatial Foundation - all rights reserved
+/* (c) 2014 - 2016 Open Source Geospatial Foundation - all rights reserved
  * (c) 2001 - 2014 OpenPlans
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
@@ -120,6 +120,7 @@ public class GetMap {
             map = fireFinished(map);
             return map;
         } catch (Throwable t) {
+            mapContent.dispose();
             fireFailed(t);
             if(t instanceof RuntimeException) {
                 throw (RuntimeException) t;

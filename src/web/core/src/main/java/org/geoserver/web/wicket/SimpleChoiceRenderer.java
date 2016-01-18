ebf34e4c9e505598b0c63f9b4ff0896d458767a2
Andrea Aime <andrea.aime@gmail.com>
2016-01-18 11:23:32 -0800

    SimpleChoiceRenderer compiles

--- src/web/core/src/main/java/org/geoserver/web/wicket/SimpleChoiceRenderer.java
+++ src/web/core/src/main/java/org/geoserver/web/wicket/SimpleChoiceRenderer.java
@@ -1,23 +1,23 @@
-/* (c) 2014 Open Source Geospatial Foundation - all rights reserved
+/* (c) 2014 - 2016 Open Source Geospatial Foundation - all rights reserved
  * (c) 2001 - 2013 OpenPlans
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
  */
 package org.geoserver.web.wicket;
 
-import org.apache.wicket.markup.html.form.IChoiceRenderer;
+import org.apache.wicket.markup.html.form.ChoiceRenderer;
 
 /**
  * A choice renderer in which each displayed value is also the id
  */
 @SuppressWarnings("serial")
-public class SimpleChoiceRenderer implements IChoiceRenderer { 
+public class SimpleChoiceRenderer<T> extends ChoiceRenderer<T> { 
 
-    public Object getDisplayValue(Object object) {
+    public Object getDisplayValue(T object) {
         return object;
     }
 
-    public String getIdValue(Object object, int index) {
+    public String getIdValue(T object, int index) {
         return object.toString();
     }
 

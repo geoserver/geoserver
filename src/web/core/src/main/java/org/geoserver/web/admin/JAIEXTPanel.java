e5aa4ed061fb22da923ce631a708da554bc24878
Jody Garnett <jody.garnett@gmail.com>
2016-01-18 12:29:07 -0800

    update admin package to wicket 7

--- src/web/core/src/main/java/org/geoserver/web/admin/JAIEXTPanel.java
+++ src/web/core/src/main/java/org/geoserver/web/admin/JAIEXTPanel.java
@@ -7,7 +7,7 @@ package org.geoserver.web.admin;
 import org.apache.wicket.Component;
 import org.apache.wicket.extensions.markup.html.form.palette.Palette;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.IChoiceRenderer;
+import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
@@ -18,16 +18,21 @@ import org.geoserver.web.wicket.LiveCollectionModel;
 import org.geoserver.web.wicket.ParamResourceModel;
 
 public class JAIEXTPanel extends Panel {
+    private static final long serialVersionUID = -4274061927074052166L;
+
 
     public JAIEXTPanel(String id, IModel<JAIInfo> model) {
         super(id, model);
 
         PropertyModel<JAIEXTInfo> jaiextModel = new PropertyModel<JAIEXTInfo>(model, "JAIEXTInfo");
 
-        Palette jaiextSelector = new Palette("jaiextOps",
+        @SuppressWarnings({ "rawtypes", "unchecked" })
+        Palette jaiextSelector = new Palette<String>("jaiextOps",
                 LiveCollectionModel.set(new PropertyModel(jaiextModel, "JAIOperations")),
                 LiveCollectionModel.set(new PropertyModel(jaiextModel, "JAIEXTOperations")),
-                new ChoiceRenderer(), 7, false) {
+                new JAIChoiceRenderer(), 7, false) {
+                    private static final long serialVersionUID = -4665147378650094510L;
+
             /**
              * Override otherwise the header is not i18n'ized
              */
@@ -48,12 +53,13 @@ public class JAIEXTPanel extends Panel {
     }
 
     
-    static class ChoiceRenderer implements IChoiceRenderer<String>{
-
+    static class JAIChoiceRenderer extends ChoiceRenderer<String> {
+        private static final long serialVersionUID = -1978519626641784908L;
+        
         private static final String ALGEBRIC = "algebric";
         private static final String OPERATION_CONST = "operationConst";
         private static final String STATS = "Stats";
-
+        
         @Override
         public Object getDisplayValue(String object) {
             if(object.equalsIgnoreCase(STATS)){
@@ -71,6 +77,8 @@ public class JAIEXTPanel extends Panel {
         public String getIdValue(String object, int index) {
             return object;
         }
+
+
         
     }
 }

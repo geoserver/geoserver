2f2faade5381eb083918f0ccc793d756dca88e3c
Kevin Smith <ksmith@boundlessgeo.com>
2016-01-18 16:23:47 -0800

    Fix compiler error in gwc-s3

--- src/community/gwc-s3/src/test/java/org/geoserver/gwc/web/blob/S3BlobStoresPageTest.java
+++ src/community/gwc-s3/src/test/java/org/geoserver/gwc/web/blob/S3BlobStoresPageTest.java
@@ -15,7 +15,7 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.AjaxRequestHandler;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 import org.apache.wicket.markup.html.form.CheckBox;
@@ -131,7 +131,7 @@ public class S3BlobStoresPageTest extends GeoServerWicketTestSupport {
         
         //select
         CheckBox selector = ((CheckBox) tester.getComponentFromLastRenderedPage("storesPanel:listContainer:items:1:selectItemContainer:selectItem"));
-        tester.getServletRequest().setParameter(selector.getInputName(), "true");
+        tester.getRequest().setParameter(selector.getInputName(), "true");
         tester.executeAjaxEvent(selector, "onclick");
                 
         assertEquals(1, table.getSelection().size());        
@@ -158,7 +158,7 @@ public class S3BlobStoresPageTest extends GeoServerWicketTestSupport {
         //select
         //super.print(page, false, false, true);
         selector = ((CheckBox) tester.getComponentFromLastRenderedPage("storesPanel:listContainer:items:2:selectItemContainer:selectItem"));
-        tester.getServletRequest().setParameter(selector.getInputName(), "true");
+        tester.getRequest().setParameter(selector.getInputName(), "true");
         tester.executeAjaxEvent(selector, "onclick");
         
         //click delete
@@ -172,7 +172,7 @@ public class S3BlobStoresPageTest extends GeoServerWicketTestSupport {
         
         //confirm      
         GeoServerDialog dialog = (GeoServerDialog) tester.getComponentFromLastRenderedPage("confirmDeleteDialog");
-        dialog.submit(new AjaxRequestTarget(tester.getLastRenderedPage()));       
+        dialog.submit(new AjaxRequestHandler(tester.getLastRenderedPage()));
         
         assertFalse(GWC.get().getBlobStores().contains(dummy1));
         layer = GWC.get().getTileLayerByName("cite:Lakes");

3a19e423c436e252ed5999df959eccb71b55f6a0
NielsCharlier <niels@scitus.be>
2016-01-18 17:21:35 -0800

    printing compiling

--- src/extension/printing/src/main/java/org/geoserver/printing/PrintDemoPage.java
+++ src/extension/printing/src/main/java/org/geoserver/printing/PrintDemoPage.java
@@ -5,31 +5,24 @@
  */
 package org.geoserver.printing;
 
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.OnLoadHeaderItem;
 import org.apache.wicket.markup.html.IHeaderContributor;
-import org.apache.wicket.markup.html.IHeaderResponse;
-import org.apache.wicket.markup.html.resources.JavascriptPackageResourceReference;
+//import org.apache.wicket.markup.html.resources.JavascriptPackageResourceReference;
 
 import org.geoserver.web.GeoServerBasePage;
 
 public class PrintDemoPage extends GeoServerBasePage implements IHeaderContributor {
-  public void renderHead(IHeaderResponse response) {
-    response.renderCSSReference(
-        "http://extjs.cachefly.net/ext-2.2.1/resources/css/ext-all.css"
-    );
-    response.renderCSSReference(
-        "http://extjs.cachefly.net/ext-2.2.1/examples/shared/examples.css"
-    );
-    response.renderJavascriptReference("http://extjs.cachefly.net/builds/ext-cdn-771.js");
-    response.renderJavascriptReference("http://openlayers.org/api/2.8/OpenLayers.js");
-    
-    response.renderJavascriptReference(
-    	new JavascriptPackageResourceReference(PrintDemoPage.class, "GeoExt.js")
-    );
-    response.renderJavascriptReference(
-        new JavascriptPackageResourceReference(PrintDemoPage.class, "GeoExtPrinting.js")
-    );
-    response.renderJavascriptReference(
-        new JavascriptPackageResourceReference(PrintDemoPage.class, "Printing.js")
-    );
-  }
+	private static final long serialVersionUID = 2904825847695306563L;
+
+	public void renderHead(IHeaderResponse response) {
+	    response.render(CssHeaderItem.forCSS("http://extjs.cachefly.net/ext-2.2.1/resources/css/ext-all.css", null));
+	    response.render(CssHeaderItem.forCSS("http://extjs.cachefly.net/ext-2.2.1/examples/shared/examples.css", null));
+	    response.render(OnLoadHeaderItem.forScript("http://extjs.cachefly.net/builds/ext-cdn-771.js"));
+	    response.render(OnLoadHeaderItem.forScript("http://openlayers.org/api/2.8/OpenLayers.js"));
+	    response.render(OnLoadHeaderItem.forScript(PrintDemoPage.class.getResource("GeoExt.js").toString()));
+	    response.render(OnLoadHeaderItem.forScript(PrintDemoPage.class.getResource("GeoExtPrinting.js").toString()));
+	    response.render(OnLoadHeaderItem.forScript(PrintDemoPage.class.getResource("GeoExtPrinting.js").toString()));
+	  }
 }

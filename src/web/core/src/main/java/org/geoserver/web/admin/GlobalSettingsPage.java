478e4f34b97e834469f13389060f38f93b5e2f72
Torben Barsbsballe <tbarsballe@boundlessgeo.com>
2016-01-19 11:17:55 -0800

    Fix id validation on GlobalSettingsPage for web-rest tests

--- src/web/core/src/main/java/org/geoserver/web/admin/GlobalSettingsPage.java
+++ src/web/core/src/main/java/org/geoserver/web/admin/GlobalSettingsPage.java
@@ -1,4 +1,4 @@
-/* (c) 2014 - 2015 Open Source Geospatial Foundation - all rights reserved
+/* (c) 2014 - 2016 Open Source Geospatial Foundation - all rights reserved
  * (c) 2001 - 2013 OpenPlans
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
@@ -176,7 +176,7 @@ public class GlobalSettingsPage extends ServerAdminPage {
         @Override
         public ResourceErrorHandling getObject(String id,
                 IModel<? extends List<? extends ResourceErrorHandling>> choices) {
-            return ResourceErrorHandling.valueOf(id);
+            return id == null ? null : ResourceErrorHandling.valueOf(id);
         }
     }
 }

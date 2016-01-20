a28fffd72265dfdf355b1467eed3ec84bda2f322
Andrea Aime <andrea.aime@gmail.com>
2016-01-20 10:38:45 -0800

    The crypt must be created just once

--- src/web/core/src/main/java/org/geoserver/web/GeoServerCryptProvider.java
+++ src/web/core/src/main/java/org/geoserver/web/GeoServerCryptProvider.java
@@ -1,4 +1,4 @@
-/* (c) 2015 Open Source Geospatial Foundation - all rights reserved
+/* (c) 2015 - 2016 Open Source Geospatial Foundation - all rights reserved
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
  */
@@ -18,6 +18,8 @@ import org.geoserver.security.GeoServerSecurityManager;
 class GeoServerCryptProvider implements IProvider<ICrypt> {
 
     GeoServerSecurityManager manager;
+    volatile ICrypt theCrypt;
+
     
     public GeoServerCryptProvider(GeoServerSecurityManager manager) {
         this.manager = manager;
@@ -28,11 +30,24 @@ class GeoServerCryptProvider implements IProvider<ICrypt> {
         // fully dynamic lookup as I'm not sure if this method gets called
         // before or after the application gets its app contxt
         if (manager.isEncryptingUrlParams()) {
-            GeoServerApplication application = GeoServerApplication.get();
-            return application.getSecuritySettings().getCryptFactory().newCrypt();
+            return getCrypt();
         } else {
             return new NoCrypt();
         }
+    }
+
+    private ICrypt getCrypt() {
+        // lazy init via double checked locking (with volatile, should be safe)
+        // because we cannot get to the settings untile after the whole app startup is done
+        if(theCrypt == null) {
+            synchronized (this) {
+                if(theCrypt == null) {
+                    GeoServerApplication application = GeoServerApplication.get();
+                    theCrypt = application.getSecuritySettings().getCryptFactory().newCrypt();
+                }
+            }
+        }
+        return theCrypt;
     }
 
 }

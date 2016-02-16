831a15fd2dd1b4856f819bea6303e13e558396f4
afabiani <alessio.fabiani@geo-solutions.it>
2016-02-16 14:29:14 +0100

    - Revert spurious change on Importer

--- src/extension/importer/core/src/main/java/org/geoserver/importer/FeatureDataConverter.java
+++ src/extension/importer/core/src/main/java/org/geoserver/importer/FeatureDataConverter.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.URLDecoder;
 import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.HashSet;

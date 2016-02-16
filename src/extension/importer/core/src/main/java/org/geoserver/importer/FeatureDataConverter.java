bbb15dd09bfa6762c5c3c149a7462bbbcd715cc7
afabiani <alessio.fabiani@geo-solutions.it>
2016-02-16 13:58:02 +0100

    Related to GEOS-7427

--- src/extension/importer/core/src/main/java/org/geoserver/importer/FeatureDataConverter.java
+++ src/extension/importer/core/src/main/java/org/geoserver/importer/FeatureDataConverter.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.URLDecoder;
 import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.HashSet;

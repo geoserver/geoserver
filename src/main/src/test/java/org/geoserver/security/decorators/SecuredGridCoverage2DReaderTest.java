75a30c9451db932f7360aeb70ade567a27083b35
afabiani <alessio.fabiani@geo-solutions.it>
2016-02-16 13:58:02 +0100

    [GEOS-7427] Make SecuredGridCoverage2DReader using a cached Crop operation instance

--- src/main/src/test/java/org/geoserver/security/decorators/SecuredGridCoverage2DReaderTest.java
+++ src/main/src/test/java/org/geoserver/security/decorators/SecuredGridCoverage2DReaderTest.java
@@ -1,3 +1,8 @@
+/* (c) 2014 - 2016 Open Source Geospatial Foundation - all rights reserved
+ * (c) 2001 - 2013 OpenPlans
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.security.decorators;
 
 import static org.easymock.EasyMock.expect;

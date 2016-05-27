6413e6d63eed7437a7cdf89a9280a115a2d6ff3e
Andrea Aime <andrea.aime@gmail.com>
2016-05-27 11:39:46 +0200

    [GEOS-7558] WMSValidator should use attributes() instead of getAttributes()

--- src/wms/src/main/java/org/geoserver/wms/WMSValidator.java
+++ src/wms/src/main/java/org/geoserver/wms/WMSValidator.java
@@ -5,6 +5,8 @@
  */
 package org.geoserver.wms;
 
+import java.io.IOException;
+
 import org.geoserver.catalog.AttributeTypeInfo;
 import org.geoserver.catalog.CoverageInfo;
 import org.geoserver.catalog.FeatureTypeInfo;
@@ -21,6 +23,7 @@ import com.vividsolutions.jts.geom.Geometry;
  * @author David Winslow, OpenGeo
  */
 public class WMSValidator extends AbstractCatalogValidator {
+    
     public void validate(LayerInfo lyr, boolean isNew) {
         if (lyr.isEnabled() == false) {
             // short-circuit - for disabled layers we don't need to validate
@@ -28,42 +31,46 @@ public class WMSValidator extends AbstractCatalogValidator {
             return;
         }
 
-        if (
-            lyr.getResource() == null ||
-            (hasGeometry(lyr) && 
-                (lyr.getResource().getSRS() == null ||
-                 lyr.getResource().getLatLonBoundingBox() == null))
-        ) throw new RuntimeException( "Layer's resource is not fully configured");
-      
-        // Resource-dependent checks
-        if (lyr.getType() == PublishedType.RASTER) {
-            if (!(lyr.getResource() instanceof CoverageInfo))
-                throw new RuntimeException("Layer with type RASTER doesn't have a coverage associated");
-            CoverageInfo cvinfo = (CoverageInfo) lyr.getResource();
-            try {
-                cvinfo.getCatalog().getResourcePool()
-                        .getGridCoverageReader(cvinfo, GeoTools.getDefaultHints());
-            } catch (Throwable t) {
-                throw new RuntimeException("Couldn't connect to raster layer's resource");
-            }
-        } else if (lyr.getType() == PublishedType.VECTOR) {
-            if (!(lyr.getResource() instanceof FeatureTypeInfo))
-                throw new RuntimeException("Layer with type VECTOR doesn't have a featuretype associated");
-            FeatureTypeInfo ftinfo = (FeatureTypeInfo) lyr.getResource();
-        } else throw new RuntimeException("Layer is neither RASTER nor VECTOR type");
-
-        // Style-dependent checks
-        if (hasGeometry(lyr) &&
-           (lyr.getDefaultStyle() == null || lyr.getStyles().contains(null))
-        ) throw new RuntimeException("Layer has null styles!");
+        try {
+            if (
+                lyr.getResource() == null ||
+                (hasGeometry(lyr) && 
+                    (lyr.getResource().getSRS() == null ||
+                     lyr.getResource().getLatLonBoundingBox() == null))
+            ) throw new RuntimeException( "Layer's resource is not fully configured");
+          
+            // Resource-dependent checks
+            if (lyr.getType() == PublishedType.RASTER) {
+                if (!(lyr.getResource() instanceof CoverageInfo))
+                    throw new RuntimeException("Layer with type RASTER doesn't have a coverage associated");
+                CoverageInfo cvinfo = (CoverageInfo) lyr.getResource();
+                try {
+                    cvinfo.getCatalog().getResourcePool()
+                            .getGridCoverageReader(cvinfo, GeoTools.getDefaultHints());
+                } catch (Throwable t) {
+                    throw new RuntimeException("Couldn't connect to raster layer's resource");
+                }
+            } else if (lyr.getType() == PublishedType.VECTOR) {
+                if (!(lyr.getResource() instanceof FeatureTypeInfo))
+                    throw new RuntimeException("Layer with type VECTOR doesn't have a featuretype associated");
+                FeatureTypeInfo ftinfo = (FeatureTypeInfo) lyr.getResource();
+            } else throw new RuntimeException("Layer is neither RASTER nor VECTOR type");
+    
+            // Style-dependent checks
+            if (hasGeometry(lyr) &&
+               (lyr.getDefaultStyle() == null || lyr.getStyles().contains(null))
+            ) throw new RuntimeException("Layer has null styles!");
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private static boolean hasGeometry(LayerInfo lyr) {
+    private static boolean hasGeometry(LayerInfo lyr) throws IOException {
         if (lyr.getResource() instanceof CoverageInfo) return true;
 
         if (lyr.getResource() instanceof FeatureTypeInfo) {
-            for (AttributeTypeInfo att : ((FeatureTypeInfo)lyr.getResource()).getAttributes()) {
-                if (Geometry.class.isAssignableFrom(att.getBinding())) {
+            for (AttributeTypeInfo att : ((FeatureTypeInfo)lyr.getResource()).attributes()) {
+                if (att.getBinding() != null && Geometry.class.isAssignableFrom(att.getBinding())) {
                     return true;
                 }
             }

750591f493a76f1161cfc8a41a59c6f1a833b61b
Nuno Oliveira <nuno.oliveira@geo-solutions.it>
2016-06-16 17:28:25 +0100

    [GEOS-7591] Allow WMTS service to be invoked per workspace

--- src/gwc/src/test/java/org/geoserver/gwc/GWCTestHelpers.java
+++ src/gwc/src/test/java/org/geoserver/gwc/GWCTestHelpers.java
@@ -1,4 +1,4 @@
-/* (c) 2014 Open Source Geospatial Foundation - all rights reserved
+/* (c) 2014 - 2016 Open Source Geospatial Foundation - all rights reserved
  * (c) 2001 - 2013 OpenPlans
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
@@ -11,12 +11,14 @@ import java.util.Set;
 import org.geoserver.catalog.PublishedInfo;
 import org.geoserver.catalog.StoreInfo;
 import org.geoserver.catalog.PublishedType;
+import org.geoserver.catalog.WorkspaceInfo;
 import org.geoserver.catalog.impl.DataStoreInfoImpl;
 import org.geoserver.catalog.impl.FeatureTypeInfoImpl;
 import org.geoserver.catalog.impl.LayerGroupInfoImpl;
 import org.geoserver.catalog.impl.LayerInfoImpl;
 import org.geoserver.catalog.impl.NamespaceInfoImpl;
 import org.geoserver.catalog.impl.StyleInfoImpl;
+import org.geoserver.catalog.impl.WorkspaceInfoImpl;
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
 
@@ -24,11 +26,21 @@ import com.google.common.collect.Lists;
 
 public class GWCTestHelpers {
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
     public static LayerInfoImpl mockLayer(String resourceName, String[] extraStyles, PublishedType type) {
+        return mockLayer(resourceName, null, extraStyles, type);
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public static LayerInfoImpl mockLayer(String resourceName, String workSpaceName, String[] extraStyles, PublishedType type) {
         StoreInfo store = new DataStoreInfoImpl(null);
         store.setName(resourceName + "-store");
         store.setEnabled(true);
+
+        if(workSpaceName != null) {
+            WorkspaceInfo workspace = new WorkspaceInfoImpl();
+            workspace.setName(workSpaceName);
+            store.setWorkspace(workspace);
+        }
 
         NamespaceInfoImpl ns = new NamespaceInfoImpl();
         ns.setPrefix("test");

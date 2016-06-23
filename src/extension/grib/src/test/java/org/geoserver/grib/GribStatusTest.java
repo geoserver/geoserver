31a7b0577e59cbf0ba9afdcd214821ba88adbdac
DBlasby <dblasby@boundlessgeo.com>
2016-06-23 11:57:16 -0700

    Add copyright header

--- src/extension/grib/src/test/java/org/geoserver/grib/GribStatusTest.java
+++ src/extension/grib/src/test/java/org/geoserver/grib/GribStatusTest.java
@@ -1,3 +1,8 @@
+/* (c) 2016 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+
 package org.geoserver.grib;
 
 import static org.junit.Assert.assertTrue;

254c078fec976e712a2a31c58368d209821967df
DBlasby <dblasby@boundlessgeo.com>
2016-06-29 11:28:47 -0700

    GEOS-7567 GSIP-143 netcdf-out status page - add native lib version (if available)

--- src/extension/netcdf-out/src/main/java/org/geoserver/netcdfout/NetCDFOutStatus.java
+++ src/extension/netcdf-out/src/main/java/org/geoserver/netcdfout/NetCDFOutStatus.java
@@ -5,6 +5,7 @@
 
 package org.geoserver.netcdfout;
 
+import java.lang.reflect.Method;
 import java.util.Optional;
 
 import org.geoserver.platform.ModuleStatus;
@@ -50,10 +51,33 @@ public class NetCDFOutStatus implements ModuleStatus {
         return true;
     }
 
+    public String Nc4Version() {
+        try {
+            // This reflection code is to deal with JNA being an optional jar.
+            // Its the same as;
+            // Nc4prototypes nc4 = (Nc4prototypes) Native.loadLibrary("netcdf", Nc4prototypes.class);
+            // return nc4.nc_inq_libvers(); 
+            Class jnaNativeClass = Class.forName("com.sun.jna.Native");
+            Method loadLibraryMethod = jnaNativeClass.getMethod("loadLibrary", String.class,
+                    Class.class);
+            Object nc4 = loadLibraryMethod.invoke(null, "netcdf", Nc4prototypes.class);
+
+            Method nc_inq_libversMethod = Nc4prototypes.class.getMethod("nc_inq_libvers");
+            String version = (String) nc_inq_libversMethod.invoke(nc4);
+
+            return version;
+        } catch (Exception e) {
+            return "unavailable (" + e.getClass() + ":" + e.getMessage() + ")";
+        }
+    }
+
     @Override
     public Optional<String> getMessage() {
         String message = "NETCDF-4 Binary Available: " + NetCDFUtilities.isNC4CAvailable();
         message += "\nNc4prototypes Version: " + GeoTools.getVersion(Nc4prototypes.class);
+        if (NetCDFUtilities.isNC4CAvailable()) {
+            message += "\nc_inq_libvers: " + Nc4Version();
+        }
         return Optional.ofNullable(message);
     }
 

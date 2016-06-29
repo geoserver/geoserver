a4679aeebb08567807cc231561a8bf8985484b5e
DBlasby <dblasby@boundlessgeo.com>
2016-06-29 14:32:24 -0700

    GSIP-143 add status page for system properties and environment

---
+++ src/platform/src/main/java/org/geoserver/platform/SystemEnvironmentStatus.java
@@ -0,0 +1,60 @@
+/* (c) 2016 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+
+package org.geoserver.platform;
+
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.Map.Entry;
+
+public class SystemEnvironmentStatus implements ModuleStatus {
+
+    @Override
+    public String getModule() {
+        return "system-environment";
+    }
+
+    @Override
+    public Optional<String> getComponent() {
+        return Optional.ofNullable("system-environment");
+    }
+
+    @Override
+    public String getName() {
+        return "system-environment";
+    }
+
+    @Override
+    public Optional<String> getVersion() {
+        return Optional.ofNullable(null);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Optional<String> getMessage() {
+        StringBuffer result = new StringBuffer();
+        for (Iterator<Entry<String, String>> it = System.getenv().entrySet().iterator(); it
+                .hasNext();) {
+            Entry<String, String> entry = it.next();
+            result.append(entry.getKey().toString() + "=" + entry.getValue().toString() + "\n");
+        }
+        return Optional.ofNullable(result.toString());
+    }
+
+    @Override
+    public Optional<String> getDocumentation() {
+        return Optional.empty();
+    }
+
+}

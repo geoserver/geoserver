63e6e671a3c0f3ba3da3c7810f845878308e7b89
afabiani <alessio.fabiani@geo-solutions.it>
2016-07-05 10:35:03 +0200

    - Backup&Restore community module GSIP 145 - JIRA GEOS-7623

---
+++ src/community/backup-restore/web/src/main/java/org/geoserver/backuprestore/web/BackupRestoreExecutionModel.java
@@ -0,0 +1,56 @@
+/* (c) 2016 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.backuprestore.web;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.geoserver.backuprestore.AbstractExecutionAdapter;
+import org.geoserver.backuprestore.BackupExecutionAdapter;
+import org.geoserver.backuprestore.RestoreExecutionAdapter;
+import org.geotools.util.logging.Logging;
+
+/**
+ * @author Alessio Fabiani, GeoSolutions
+ *
+ */
+public class BackupRestoreExecutionModel<T extends AbstractExecutionAdapter> extends LoadableDetachableModel<AbstractExecutionAdapter> {
+
+    static Logger LOGGER = Logging.getLogger(BackupRestoreExecutionModel.class);
+
+    long id;
+
+    private Class<T> clazz;
+    
+    public BackupRestoreExecutionModel(AbstractExecutionAdapter exec, Class<T> clazz) {
+        this(exec.getId(), clazz);
+    }
+
+    public BackupRestoreExecutionModel(long id, Class<T> clazz) {
+        this.id = id;
+        this.clazz = clazz;
+    }
+    
+    public Class<T> getType() {
+        return this.clazz;
+    }
+    
+    @Override
+    protected AbstractExecutionAdapter load() {
+        try {
+            if (getType() == BackupExecutionAdapter.class) {
+                return BackupRestoreWebUtils.backupFacade().getBackupExecutions().get(id);
+            } else if (getType() == RestoreExecutionAdapter.class) {
+                return BackupRestoreWebUtils.backupFacade().getRestoreExecutions().get(id);
+            }
+            return null;
+        }
+        catch(Exception e) {
+            LOGGER.log(Level.WARNING, "Unable to load execution " + id, e);
+            return null;
+        }
+    }
+}
---
+++ src/community/backup-restore/web/src/main/java/org/geoserver/backuprestore/web/ResourceFilePanel.java
@@ -0,0 +1,149 @@
+/* (c) 2016 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.backuprestore.web;
+
+import java.io.File;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.OnChangeAjaxBehavior;
+import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.SubmitLink;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.Model;
+import org.geoserver.platform.resource.Files;
+import org.geoserver.platform.resource.Resource;
+import org.geoserver.web.wicket.GeoServerDialog;
+import org.geoserver.web.wicket.ParamResourceModel;
+import org.geoserver.web.wicket.browser.ExtensionFileFilter;
+import org.geoserver.web.wicket.browser.GeoServerFileChooser;
+
+@SuppressWarnings("serial")
+public class ResourceFilePanel extends Panel {
+
+    private static final String[] FILE_EXTENSIONS = new String[] {".zip", ".gz", ".tar", ".tgz", ".bz"};
+
+    String file;
+    
+    TextField fileField;
+    GeoServerDialog dialog;
+
+    public ResourceFilePanel(String id) {
+        super(id);
+        
+        add(dialog = new GeoServerDialog("dialog"));
+
+        Form form = new Form("form", new CompoundPropertyModel(this));
+        add(form);
+
+        fileField = new TextField("file");
+        fileField.setRequired(true);
+        fileField.setOutputMarkupId(true);
+        fileField.add(new OnChangeAjaxBehavior(){
+            
+            @Override
+            protected void onUpdate(final AjaxRequestTarget target){
+                // Access the updated model value:
+                final String valueAsString =
+                    ((TextField<String>) getComponent()).getModelObject();
+                
+                // use what the user currently typed
+                File file = null;
+                if (!valueAsString.trim().equals("")) {
+                    file = new File(valueAsString);
+                    if (!file.exists())
+                        file = null;
+                }
+            }
+        });
+        
+        form.add(fileField);
+        form.add(chooserButton(form));
+    }
+
+    public Resource getResource() {
+        return Files.asResource(new File(this.file));
+    };
+    
+    Component chooserButton(Form form) {
+        AjaxSubmitLink link = new AjaxSubmitLink("chooser") {
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form form) {
+                dialog.setTitle(new ParamResourceModel("chooseFile", this));
+                dialog.showOkCancel(target, new GeoServerDialog.DialogDelegate() {
+
+                    @Override
+                    protected Component getContents(String id) {
+                        // use what the user currently typed
+                        File file = null;
+                        if (!fileField.getInput().trim().equals("")) {
+                            file = new File(fileField.getInput());
+                            if (!file.exists())
+                                file = null;
+                        }
+
+                        GeoServerFileChooser chooser = new GeoServerFileChooser(id, new Model(file)) {
+                            @Override
+                            protected void fileClicked(File file, AjaxRequestTarget target) {
+                                ResourceFilePanel.this.file = file.getAbsolutePath();
+                                
+                                fileField.clearInput();
+                                fileField.setModelObject(file.getAbsolutePath());
+
+                                target.add(fileField);
+                                dialog.close(target);
+                            }
+                        };
+
+                        initFileChooser(chooser);
+                        return chooser;
+                    }
+
+                    @Override
+                    protected boolean onSubmit(AjaxRequestTarget target, Component contents) {
+                        GeoServerFileChooser chooser = (GeoServerFileChooser) contents;
+                        file = ((File) chooser.getDefaultModelObject()).getAbsolutePath();
+
+                        // clear the raw input of the field won't show the new model value
+                        fileField.clearInput();
+                        //fileField.setModelObject(file);
+                        
+                        target.add(fileField);
+                        return true;
+                    }
+
+                    @Override
+                    public void onClose(AjaxRequestTarget target) {
+                        // update the field with the user chosen value
+                        target.add(fileField);
+                    }
+
+                });
+
+            }
+
+        };
+        // otherwise the link won't trigger when the form contents are not valid
+        link.setDefaultFormProcessing(false);
+        return link;
+    }
+
+    SubmitLink submitLink() {
+        return new SubmitLink("submit") {
+
+            @Override
+            public void onSubmit() {
+            }
+        };
+    }
+
+    protected void initFileChooser(GeoServerFileChooser fileChooser) {
+        fileChooser.setFilter(new Model(new ExtensionFileFilter(FILE_EXTENSIONS)));
+        // fileChooser.setFilter(new Model((Serializable)FileFileFilter.FILE));
+    }
+}

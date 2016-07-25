811cf6a9632df7d01c1b5b64c1c1920dcd562138
Erik Merkle <emerkle@boundlessgeo.com>
2016-07-25 16:40:56 -0500

    Align plugin for GeoGig REST implementation with GeoGig Web API
     - Remove automatic DataStore creation during INIT via plugin
     - Return a 400 Bad Request if Content-Type is not supported,
       instead of creating a GeoGig repository with default config.

--- src/community/geogig/src/main/java/org/geogig/geoserver/rest/InitCommandResource.java
+++ src/community/geogig/src/main/java/org/geogig/geoserver/rest/InitCommandResource.java
@@ -10,28 +10,17 @@ import static org.locationtech.geogig.porcelain.ConfigOp.ConfigAction.CONFIG_SET
 import static org.locationtech.geogig.porcelain.ConfigOp.ConfigScope.LOCAL;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.Serializable;
 import java.net.URI;
 import java.util.Map;
 
-import org.geogig.geoserver.config.GeoGigInitializer;
 import org.geogig.geoserver.config.RepositoryInfo;
 import org.geogig.geoserver.config.RepositoryManager;
-import org.geoserver.catalog.Catalog;
-import org.geoserver.catalog.DataStoreInfo;
-import org.geoserver.catalog.NamespaceInfo;
-import org.geoserver.catalog.WorkspaceInfo;
-import org.locationtech.geogig.geotools.data.GeoGigDataStoreFactory;
-import org.locationtech.geogig.plumbing.ResolveRepositoryName;
 import org.locationtech.geogig.porcelain.ConfigOp;
 import org.locationtech.geogig.rest.repository.CommandResource;
 import org.restlet.data.Request;
 import org.restlet.data.Status;
 import org.restlet.resource.Representation;
 import org.restlet.resource.Variant;
-
-import com.google.common.base.Throwables;
 
 public class InitCommandResource extends CommandResource {
 
@@ -47,12 +36,8 @@ public class InitCommandResource extends CommandResource {
         if (getResponse().getStatus() == Status.SUCCESS_CREATED) {
             // set the Author name and email from the Init request
             setAuthor(request);
-            // get the repository name
-            String repositoryName = geogig.get().command(ResolveRepositoryName.class).call();
             // save the repo in the Manager
             RepositoryInfo repoInfo = saveRepository();
-            Catalog catalog = RepositoryManager.get().getCatalog();
-            setUpDataStore(catalog, repositoryName, repoInfo);
         }
         return representation;
     }
@@ -88,33 +73,5 @@ public class InitCommandResource extends CommandResource {
         repoInfo.setLocation(location);
         // save the repo, this will set a UUID
         return RepositoryManager.get().save(repoInfo);
-    }
-
-    public DataStoreInfo setUpDataStore(Catalog catalog, String storeName, RepositoryInfo repoInfo) {
-        NamespaceInfo ns = catalog.getDefaultNamespace();
-        WorkspaceInfo ws = catalog.getDefaultWorkspace();
-        DataStoreInfo ds = catalog.getFactory().createDataStore();
-        ds.setEnabled(true);
-        ds.setDescription("GeoGIG repository");
-        ds.setName(storeName);
-        ds.setType(GeoGigDataStoreFactory.DISPLAY_NAME);
-        ds.setWorkspace(ws);
-        Map<String, Serializable> connParams = ds.getConnectionParameters();
-
-        connParams.put(GeoGigDataStoreFactory.REPOSITORY.key, repoInfo.getId());
-        connParams.put(GeoGigDataStoreFactory.DEFAULT_NAMESPACE.key, ns.getURI());
-        connParams.put(GeoGigDataStoreFactory.RESOLVER_CLASS_NAME.key,
-                GeoGigInitializer.REPO_RESOLVER_CLASSNAME);
-        catalog.add(ds);
-
-        try {
-            DataStoreInfo dsInfo = catalog.getDataStoreByName(ws, storeName);
-            String repoId = (String) dsInfo.getConnectionParameters()
-                    .get(GeoGigDataStoreFactory.REPOSITORY.key);
-            RepositoryManager.get().get(repoId);
-        } catch (IOException e) {
-            Throwables.propagate(e);
-        }
-        return ds;
     }
 }

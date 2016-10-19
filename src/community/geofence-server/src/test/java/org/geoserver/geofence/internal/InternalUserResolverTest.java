7f8fe6a5d73ed05557aa32feb8fd82560d65cf4e
afabiani <alessio.fabiani@geo-solutions.it>
2016-10-19 13:12:37 +0200

    [GEOS-7803] GeoFence Embedder does not check for plain Rule Services
     - (GEOS-7803) improve code

--- src/community/geofence-server/src/test/java/org/geoserver/geofence/internal/InternalUserResolverTest.java
+++ src/community/geofence-server/src/test/java/org/geoserver/geofence/internal/InternalUserResolverTest.java
@@ -12,6 +12,14 @@ import java.util.Arrays;
 import java.util.Set;
 
 import org.geoserver.data.test.SystemTestData;
+import org.geoserver.security.GeoServerRoleService;
+import org.geoserver.security.GeoServerRoleStore;
+import org.geoserver.security.GeoServerSecurityManager;
+import org.geoserver.security.GeoServerUserGroupService;
+import org.geoserver.security.GeoServerUserGroupStore;
+import org.geoserver.security.impl.GeoServerRole;
+import org.geoserver.security.impl.GeoServerUser;
+import org.geoserver.security.impl.GeoServerUserGroup;
 import org.geoserver.test.GeoServerSystemTestSupport;
 import org.junit.Test;
 
@@ -28,6 +36,15 @@ public class InternalUserResolverTest extends GeoServerSystemTestSupport {
         addUser("pipo", "clown", null, Arrays.asList("CIRCUS", "KLINIEK", "ZEVER"));
         addUser("jantje", "jantje", null, Arrays.asList("MOPJES", "ZEVER"));
         
+        GeoServerSecurityManager secMgr = getSecurityManager();
+        GeoServerRoleService roleService = secMgr.loadRoleService("default");
+        
+        GeoServerRoleStore rolesStore = roleService.createStore();
+        
+        GeoServerRole roleTest = rolesStore.createRoleObject("ROLE_TEST");
+        rolesStore.addRole(roleTest);
+        rolesStore.associateRoleToUser(roleTest, "role_user_test");
+        rolesStore.store();
     }
 
     @Test
@@ -36,18 +53,29 @@ public class InternalUserResolverTest extends GeoServerSystemTestSupport {
         
         assertTrue(resolver.existsUser("pipo"));
         assertTrue(resolver.existsUser("jantje"));
+        
+        assertTrue(resolver.existsUser("role_user_test"));
+        
         assertTrue(resolver.existsRole("ZEVER"));
         assertTrue(resolver.existsRole("CIRCUS"));
         assertTrue(resolver.existsRole("MOPJES"));
         assertTrue(resolver.existsRole("KLINIEK"));
+        
+        assertTrue(resolver.existsRole("ROLE_TEST"));
+        
         Set<String> roles = resolver.getRoles("pipo");
         assertEquals(3, roles.size());
         assertTrue(roles.contains("CIRCUS"));
         assertTrue(roles.contains("ZEVER"));
         assertTrue(roles.contains("KLINIEK"));
+        
         roles = resolver.getRoles("jantje");
         assertEquals(2, roles.size());
         assertTrue(roles.contains("ZEVER"));
         assertTrue(roles.contains("MOPJES"));
+        
+        roles = resolver.getRoles("role_user_test");
+        assertEquals(1, roles.size());
+        assertTrue(roles.contains("ROLE_TEST"));
     }
 }

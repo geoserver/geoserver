b27eddb19ef135f6b1e485e6d1ac2372f904377e
afabiani <alessio.fabiani@geo-solutions.it>
2016-10-28 19:38:43 +0200

    - (GEOS-7825) Implement GitHub OAuth2 Security Plugin

---
+++ src/community/security/oauth2-github/src/main/java/org/geoserver/security/oauth2/services/GitHubUserAuthenticationConverter.java
@@ -0,0 +1,49 @@
+/* (c) 2016 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.security.oauth2.services;
+
+import java.util.Map;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.provider.token.DefaultUserAuthenticationConverter;
+
+/**
+ * User Authentication Converter for GitHub token details.
+ * 
+ * @author Alessio Fabiani, GeoSolutions S.A.S.
+ */
+public class GitHubUserAuthenticationConverter extends DefaultUserAuthenticationConverter {
+
+    private static Object USERNAME_KEY = USERNAME;
+
+    /**
+     * Default Constructor.
+     * 
+     * @param username_key
+     */
+    public GitHubUserAuthenticationConverter() {
+        super();
+    }
+    
+    /**
+     * Default Constructor.
+     * 
+     * @param username_key
+     */
+    public GitHubUserAuthenticationConverter(String username_key) {
+        super();
+
+        USERNAME_KEY = username_key;
+    }
+
+    @Override
+    public Authentication extractAuthentication(Map<String, ?> map) {
+        if (map.containsKey(USERNAME_KEY)) {
+            return new UsernamePasswordAuthenticationToken(map.get(USERNAME_KEY), "N/A", null);
+        }
+        return null;
+    }
+}

f06e8ea9ea999441f3e9519218013e0fe0006006
DBlasby <dblasby@boundlessgeo.com>
2016-11-09 14:06:50 -0800

    add (c) header

--- src/extension/importer/core/src/main/java/org/geoserver/importer/TransformingCancelableInteratingFC.java
+++ src/extension/importer/core/src/main/java/org/geoserver/importer/TransformingCancelableInteratingFC.java
@@ -1,3 +1,7 @@
+/* (c) 2016 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.importer;
 
 import java.util.NoSuchElementException;

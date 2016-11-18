93be5bc46838db20740390c2bae44457e257bf45
Andrea Aime <andrea.aime@gmail.com>
2016-11-18 19:43:51 +0100

    [GEOS-7848] Proxy vulnerability

--- src/wfs/src/test/java/org/geoserver/wfs/servlets/TestWfsPostTest.java
+++ src/wfs/src/test/java/org/geoserver/wfs/servlets/TestWfsPostTest.java
@@ -7,26 +7,25 @@ package org.geoserver.wfs.servlets;
 import static org.junit.Assert.assertTrue;
 
 import org.geoserver.ows.util.ResponseUtils;
+import org.geoserver.platform.GeoServerExtensions;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.vfny.geoserver.wfs.servlets.TestWfsPost;
 
 public class TestWfsPostTest {
-
+    
+    /**
+     * The proxy base url variable
+     */
+    static final String PROXY_BASE_URL = "PROXY_BASE_URL";
+    
     @Test
     public void testEscapeXMLReservedChars() throws Exception {
         TestWfsPost servlet = new TestWfsPost();
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        request.setScheme("http");
-        request.setServerName("localhost");
-        request.setServerPort(8080);
-        request.setContextPath("/geoserver/TestWfsPost");
-        request.setRequestURI(ResponseUtils.stripQueryString(ResponseUtils.appendPath(
-                    "/geoserver/TestWfsPost")));
-        request.setQueryString(ResponseUtils.getQueryString("form_hf_0=&url=vjoce<>:garbage"));
-        request.setRemoteAddr("127.0.0.1");
+        MockHttpServletRequest request = buildMockRequest();
         request.addHeader("Host", "localhost:8080");
+        request.setQueryString(ResponseUtils.getQueryString("form_hf_0=&url=vjoce<>:garbage"));
         request.setParameter("url", "vjoce<>:garbage");
         request.setMethod("GET");
         MockHttpServletResponse response = new MockHttpServletResponse();
@@ -34,5 +33,49 @@ public class TestWfsPostTest {
         // System.out.println(response.getContentAsString());
         // check xml chars have been escaped
         assertTrue(response.getContentAsString().contains("java.net.MalformedURLException: no protocol: vjoce&lt;&gt;:garbage"));
+    }
+    
+    @Test
+    public void testDisallowOpenProxy() throws Exception {
+        TestWfsPost servlet = new TestWfsPost();
+        MockHttpServletRequest request = buildMockRequest();
+        request.setParameter("url", "http://www.google.com");
+        request.setMethod("GET");
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        servlet.service(request, response);
+        // System.out.println(response.getContentAsString());
+        // check xml chars have been escaped
+        assertTrue(response.getContentAsString().contains("Invalid url requested, the demo requests should be hitting: http://localhost:8080/geoserver"));
+    }
+    
+    @Test
+    public void testDisallowOpenProxyWithProxyBase() throws Exception {
+        System.setProperty(PROXY_BASE_URL, "http://geoserver.org/geoserver");
+        try {
+            TestWfsPost servlet = new TestWfsPost();
+            MockHttpServletRequest request = buildMockRequest();
+            request.setParameter("url", "http://localhost:1234/internalApp");
+            request.setMethod("GET");
+            MockHttpServletResponse response = new MockHttpServletResponse();
+            servlet.service(request, response);
+            // System.out.println(response.getContentAsString());
+            // check xml chars have been escaped
+            assertTrue(response.getContentAsString().contains("Invalid url requested, the demo requests should be hitting: http://geoserver.org/geoserver"));
+        } finally {
+            System.clearProperty(PROXY_BASE_URL);
+        }
+    }
+    
+    private MockHttpServletRequest buildMockRequest() {
+        
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setScheme("http");
+        request.setServerName("localhost");
+        request.setServerPort(8080);
+        request.setContextPath("/geoserver/TestWfsPost");
+        request.setRequestURI(ResponseUtils.stripQueryString(ResponseUtils.appendPath(
+                    "/geoserver/TestWfsPost")));
+        request.setRemoteAddr("127.0.0.1");
+        return request;
     }
 }

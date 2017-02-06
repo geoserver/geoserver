fd3852f2e4c5b933aef445b1d7aa792df7347ac0
afabiani <alessio.fabiani@geo-solutions.it>
2017-02-06 13:28:49 +0100

    [Backport] - Notification Community Plugin Module

---
+++ src/community/notification/src/main/java/org/geoserver/notification/INotificationTransactionListener.java
@@ -0,0 +1,16 @@
+/* (c) 2017 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+
+package org.geoserver.notification;
+
+import org.geoserver.wfs.TransactionPlugin;
+
+public interface INotificationTransactionListener extends TransactionPlugin {
+
+    void setMessageMultiplexer(MessageMultiplexer messageMultiplexer);
+
+    MessageMultiplexer getMessageMultiplexer();
+
+}

1395237e8191ab45c6e21c84a9dbed9ac6d7f5e2
Andrea Aime <andrea.aime@gmail.com>
2017-02-15 11:42:44 +0100

    Adding indexing to back opensearch searches

--- src/community/opensearch-eo/src/test/resources/postgis.sql
+++ src/community/opensearch-eo/src/test/resources/postgis.sql
@@ -3,6 +3,7 @@ create extension if not exists postgis;
 create extension if not exists hstore;
 
 -- cleanup
+drop table if exists granule; 
 drop table if exists collection_ogclink;
 drop table if exists product_ogclink;
 drop table if exists ogclink;
@@ -37,6 +38,29 @@ create table collection (
   "eoAcquisitionStation" varchar,
   "customAttributes" hstore
 );
+-- index all (really, this is a search engine)
+-- manually generated indexes
+create index "idx_collection_footprint" on collection using GIST(footprint);
+-- the following indexes have been generated adding
+-- SELECT 'CREATE INDEX "idx_' || table_name || '_' || column_name || '" ON "' || table_name || '" ("' || column_name || '");'   FROM information_schema.columns WHERE table_schema = current_schema() and table_name = 'collection' and (column_name like 'eo%' or column_name like 'opt%' or column_name like 'sar%' or column_name like 'time%');
+CREATE INDEX "idx_collection_timeStart" ON "collection" ("timeStart");
+CREATE INDEX "idx_collection_timeEnd" ON "collection" ("timeEnd");
+CREATE INDEX "idx_collection_eoIdentifier" ON "collection" ("eoIdentifier");
+CREATE INDEX "idx_collection_eoProductType" ON "collection" ("eoProductType");
+CREATE INDEX "idx_collection_eoPlatform" ON "collection" ("eoPlatform");
+CREATE INDEX "idx_collection_eoPlatformSerialIdentifier" ON "collection" ("eoPlatformSerialIdentifier");
+CREATE INDEX "idx_collection_eoInstrument" ON "collection" ("eoInstrument");
+CREATE INDEX "idx_collection_eoSensorType" ON "collection" ("eoSensorType");
+CREATE INDEX "idx_collection_eoCompositeType" ON "collection" ("eoCompositeType");
+CREATE INDEX "idx_collection_eoProcessingLevel" ON "collection" ("eoProcessingLevel");
+CREATE INDEX "idx_collection_eoOrbitType" ON "collection" ("eoOrbitType");
+CREATE INDEX "idx_collection_eoSpectralRange" ON "collection" ("eoSpectralRange");
+CREATE INDEX "idx_collection_eoWavelenght" ON "collection" ("eoWavelenght");
+CREATE INDEX "idx_collection_eoSecurityConstraints" ON "collection" ("eoSecurityConstraints");
+CREATE INDEX "idx_collection_eoDissemination" ON "collection" ("eoDissemination");
+CREATE INDEX "idx_collection_eoAcquisitionStation" ON "collection" ("eoAcquisitionStation");
+
+
 
 -- the products and attributes describing them
 create table product (
@@ -44,7 +68,7 @@ create table product (
   name varchar,
   "htmlDescription" text,
   "omEoMetadata" text,
-  location geometry(Polygon, 4326),
+  footprint geometry(Polygon, 4326),
   "timeStart" timestamp,
   "timeEnd" timestamp,
   "originalPackageLocation" varchar,
@@ -88,6 +112,49 @@ create table product (
   "eoResolution" float,
   "customAttributeValues" hstore
 );
+-- index all (really, this is a search engine)
+-- manually generated indexes
+create index "idx_product_footprint" on product using GIST(footprint);
+-- the following indexes have been generated adding
+-- SELECT 'CREATE INDEX "idx_' || table_name || '_' || column_name || '" ON "' || table_name || '" ("' || column_name || '");'   FROM information_schema.columns WHERE table_name = 'product' and column_name like 'eo%' or column_name like 'opt%' or column_name like 'sar%' or column_name like 'time%';
+ CREATE INDEX "idx_product_timeStart" ON "product" ("timeStart");
+ CREATE INDEX "idx_product_timeEnd" ON "product" ("timeEnd");
+ CREATE INDEX "idx_product_eoParentIdentifier" ON "product" ("eoParentIdentifier");
+ CREATE INDEX "idx_product_eoProductionStatus" ON "product" ("eoProductionStatus");
+ CREATE INDEX "idx_product_eoAcquisitionType" ON "product" ("eoAcquisitionType");
+ CREATE INDEX "idx_product_eoOrbitNumber" ON "product" ("eoOrbitNumber");
+ CREATE INDEX "idx_product_eoOrbitDirection" ON "product" ("eoOrbitDirection");
+ CREATE INDEX "idx_product_eoTrack" ON "product" ("eoTrack");
+ CREATE INDEX "idx_product_eoFrame" ON "product" ("eoFrame");
+ CREATE INDEX "idx_product_eoSwathIdentifier" ON "product" ("eoSwathIdentifier");
+ CREATE INDEX "idx_product_optCloudCover" ON "product" ("optCloudCover");
+ CREATE INDEX "idx_product_optSnowCover" ON "product" ("optSnowCover");
+ CREATE INDEX "idx_product_eoProductQualityStatus" ON "product" ("eoProductQualityStatus");
+ CREATE INDEX "idx_product_eoProductQualityDegradationStatus" ON "product" ("eoProductQualityDegradationStatus");
+ CREATE INDEX "idx_product_eoProcessorName" ON "product" ("eoProcessorName");
+ CREATE INDEX "idx_product_eoProcessingCenter" ON "product" ("eoProcessingCenter");
+ CREATE INDEX "idx_product_eoCreationDate" ON "product" ("eoCreationDate");
+ CREATE INDEX "idx_product_eoModificationDate" ON "product" ("eoModificationDate");
+ CREATE INDEX "idx_product_eoProcessingDate" ON "product" ("eoProcessingDate");
+ CREATE INDEX "idx_product_eoSensorMode" ON "product" ("eoSensorMode");
+ CREATE INDEX "idx_product_eoArchivingCenter" ON "product" ("eoArchivingCenter");
+ CREATE INDEX "idx_product_eoProcessingMode" ON "product" ("eoProcessingMode");
+ CREATE INDEX "idx_product_eoAvailabilityTime" ON "product" ("eoAvailabilityTime");
+ CREATE INDEX "idx_product_eoAcquisitionStation" ON "product" ("eoAcquisitionStation");
+ CREATE INDEX "idx_product_eoAcquisitionSubtype" ON "product" ("eoAcquisitionSubtype");
+ CREATE INDEX "idx_product_eoStartTimeFromAscendingNode" ON "product" ("eoStartTimeFromAscendingNode");
+ CREATE INDEX "idx_product_eoCompletionTimeFromAscendingNode" ON "product" ("eoCompletionTimeFromAscendingNode");
+ CREATE INDEX "idx_product_eoIlluminationAzimuthAngle" ON "product" ("eoIlluminationAzimuthAngle");
+ CREATE INDEX "idx_product_eoIlluminationZenithAngle" ON "product" ("eoIlluminationZenithAngle");
+ CREATE INDEX "idx_product_eoIlluminationElevationAngle" ON "product" ("eoIlluminationElevationAngle");
+ CREATE INDEX "idx_product_sarPolarisationMode" ON "product" ("sarPolarisationMode");
+ CREATE INDEX "idx_product_sarPolarisationChannels" ON "product" ("sarPolarisationChannels");
+ CREATE INDEX "idx_product_sarAntennaLookDirection" ON "product" ("sarAntennaLookDirection");
+ CREATE INDEX "idx_product_sarMinimumIncidenceAngle" ON "product" ("sarMinimumIncidenceAngle");
+ CREATE INDEX "idx_product_sarMaximumIncidenceAngle" ON "product" ("sarMaximumIncidenceAngle");
+ CREATE INDEX "idx_product_sarDopplerFrequency" ON "product" ("sarDopplerFrequency");
+ CREATE INDEX "idx_product_sarIncidenceAngleVariation" ON "product" ("sarIncidenceAngleVariation");
+ CREATE INDEX "idx_product_eoResolution" ON "product" ("eoResolution");
 
 -- ogc links (abstract)
 create table ogclink (
@@ -108,3 +175,11 @@ create table collection_ogclink (
 create table product_ogclink (
   product_id int references product(id)
 ) inherits(ogclink);
+
+-- the granules table
+create table granule (
+  id serial primary key,
+  product_id int references product(id),
+  location varchar,
+  the_geom geometry(Polygon, 4326)
+)

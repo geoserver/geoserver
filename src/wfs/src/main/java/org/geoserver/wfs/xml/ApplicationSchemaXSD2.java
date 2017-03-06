45aab5a0bde03efde449bcb36afa465b70812080
Nuno Oliveira <nuno.oliveira@geo-solutions.it>
2017-03-06 14:03:50 +0000

    [GEOS-8017] Stop using a global cached schema in WFS 1.1.0

--- src/wfs/src/main/java/org/geoserver/wfs/xml/ApplicationSchemaXSD2.java
+++ src/wfs/src/main/java/org/geoserver/wfs/xml/ApplicationSchemaXSD2.java
@@ -6,65 +6,23 @@
 package org.geoserver.wfs.xml;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.xsd.XSDSchema;
 import org.geoserver.catalog.FeatureTypeInfo;
-import org.geoserver.ows.URLMangler.URLType;
-import org.geoserver.ows.util.ResponseUtils;
-import org.geotools.xml.XSD;
 
-public class ApplicationSchemaXSD2 extends XSD {
+public class ApplicationSchemaXSD2 extends ApplicationSchemaXSD1 {
 
-    FeatureTypeSchemaBuilder schemaBuilder;
-    Map<String,Set<FeatureTypeInfo>> featureTypes;
-    String baseURL;
-    
-    public ApplicationSchemaXSD2(FeatureTypeSchemaBuilder schemaBuilder, 
-        Map<String,Set<FeatureTypeInfo>> featureTypes) {
-        
-        this.schemaBuilder = schemaBuilder;
-        this.featureTypes = featureTypes;
-    }
-    
-    @Override
-    public String getNamespaceURI() {
-        if (featureTypes.size() == 1) {
-            return featureTypes.keySet().iterator().next();
-        }
-        
-        //TODO: return xsd namespace?
-        return null;
+    public ApplicationSchemaXSD2(FeatureTypeSchemaBuilder schemaBuilder) {
+        super(schemaBuilder);
     }
 
-    public void setBaseURL(String baseURL) {
-        this.baseURL = baseURL;
+    public ApplicationSchemaXSD2(FeatureTypeSchemaBuilder schemaBuilder, Map<String, Set<FeatureTypeInfo>> featureTypes) {
+        super(schemaBuilder, featureTypes);
     }
-    
-    public String getBaseURL() {
-        return baseURL;
-    }
-    
-    @Override
-    public String getSchemaLocation() {
-        StringBuilder sb = new StringBuilder();
-        for (Set<FeatureTypeInfo> fts : featureTypes.values()) {
-            for (FeatureTypeInfo ft : fts) {
-                sb.append(ft.getPrefixedName()).append(",");
-            }
-        }
-        sb.setLength(sb.length()-1);
-        
-        HashMap kvp = new HashMap();
-        kvp.putAll(schemaBuilder.getDescribeFeatureTypeParams());
-        kvp.put("typename", sb.toString());
-        
-        return ResponseUtils.buildURL(baseURL, "wfs", kvp, URLType.SERVICE);
-    }
-    
+
     @Override
     protected XSDSchema buildSchema() throws IOException {
         Set<FeatureTypeInfo> types = new HashSet();
@@ -73,5 +31,4 @@ public class ApplicationSchemaXSD2 extends XSD {
         }
         return schemaBuilder.build(types.toArray(new FeatureTypeInfo[types.size()]), baseURL, 1);
     }
-
 }

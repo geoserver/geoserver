d48d77291f4e32e4372b048a4b2012b6d9e89dd1
Ian Turton <ijturton@gmail.com>
2017-03-28 19:32:01 +0100

    Namespace XMl & JSON endpoints

---
+++ src/restconfig-ng/src/main/java/org/geoserver/catalog/rest/NamespaceController.java
@@ -0,0 +1,232 @@
+package org.geoserver.catalog.rest;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.CatalogBuilder;
+import org.geoserver.catalog.CatalogInfo;
+import org.geoserver.catalog.CoverageStoreInfo;
+import org.geoserver.catalog.DataStoreInfo;
+import org.geoserver.catalog.NamespaceInfo;
+import org.geoserver.catalog.ResourceInfo;
+import org.geoserver.catalog.WMSStoreInfo;
+import org.geoserver.catalog.WorkspaceInfo;
+import org.geoserver.config.util.XStreamPersister;
+import org.geoserver.rest.ResourceNotFoundException;
+import org.geoserver.rest.RestException;
+import org.geoserver.rest.RestletException;
+import org.geoserver.rest.converters.FreemarkerHTMLMessageConverter;
+import org.geoserver.rest.converters.XStreamMessageConverter;
+import org.geoserver.rest.wrapper.RestWrapper;
+import org.geotools.util.logging.Logging;
+import org.restlet.data.Status;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+
+import freemarker.template.SimpleHash;
+import freemarker.template.Template;
+import freemarker.template.TemplateModelException;
+
+@RestController
+@RequestMapping(path = "/restng", produces = { MediaType.APPLICATION_JSON_VALUE,
+        MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_HTML_VALUE })
+public class NamespaceController extends CatalogController {
+    private static final String NAMESPACE_NAME = "namespaceName";
+
+    private static final Logger LOGGER = Logging.getLogger(NamespaceController.class);
+
+    @Autowired
+    public NamespaceController(Catalog catalog) {
+        super(catalog);
+    }
+
+    @GetMapping(value = "/namespaces/{namespaceName}", produces = {
+            MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_HTML_VALUE,
+            MediaType.APPLICATION_XML_VALUE })
+    public RestWrapper<NamespaceInfo> getWorkspace(@PathVariable String namespaceName) {
+
+        NamespaceInfo namespace = catalog.getNamespaceByPrefix(namespaceName);
+        if (namespace == null) {
+            throw new ResourceNotFoundException("No such namespace: '" + namespaceName + "' found");
+        }
+
+        LOGGER.info("GET " + namespaceName);
+        LOGGER.info("got " + namespace.getName());
+
+        return wrapObject(namespace, NamespaceInfo.class);
+    }
+
+    @GetMapping(value = "/namespaces", produces = { MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_HTML_VALUE })
+    public RestWrapper getNamespaces() {
+
+        List<NamespaceInfo> wkspaces = catalog.getNamespaces();
+        return wrapList(wkspaces, NamespaceInfo.class);
+    }
+
+    @PostMapping(value = "/namespaces", produces = { MediaType.APPLICATION_JSON_VALUE,
+            MediaType.TEXT_HTML_VALUE, MediaType.APPLICATION_XML_VALUE }, consumes = { "text/xml",
+                    MediaType.APPLICATION_XML_VALUE, TEXT_JSON, MediaType.APPLICATION_JSON_VALUE })
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<String> postNamespace(@RequestBody NamespaceInfo namepace,
+            UriComponentsBuilder builder) {
+        catalog.add(namepace);
+        String name = namepace.getName();
+        LOGGER.info("Added namespace " + name);
+        // build the new path
+        UriComponents uriComponents = getUriComponents(name, builder);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(uriComponents.toUri());
+        return new ResponseEntity<String>(name, headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/namespaces/{prefix}", produces = { MediaType.APPLICATION_JSON_VALUE,
+            MediaType.TEXT_HTML_VALUE, MediaType.APPLICATION_XML_VALUE }, consumes = { "text/xml",
+                    MediaType.APPLICATION_XML_VALUE, TEXT_JSON, MediaType.APPLICATION_JSON_VALUE })
+    public void putNamespace(@RequestBody NamespaceInfo namespace, @PathVariable String prefix,
+            UriComponentsBuilder builder) {
+        if ("default".equals(prefix)) {
+            catalog.setDefaultNamespace(namespace);
+        }
+        // name must exist
+        NamespaceInfo nsi = catalog.getNamespaceByPrefix(prefix);
+        if (nsi == null) {
+            throw new RestException("Can't change a non existant namespace (" + prefix + ")",
+                    HttpStatus.NOT_FOUND);
+        }
+
+        String infoName = namespace.getName();
+        if (infoName != null && !prefix.equals(infoName)) {
+            throw new RestException("Can't change name of workspace", HttpStatus.FORBIDDEN);
+        }
+
+        new CatalogBuilder(catalog).updateNamespace(nsi, namespace);
+        catalog.save(nsi);
+    }
+
+    @DeleteMapping(path = "/namespaces/{prefix}")
+    protected void deleteStyle(@PathVariable String prefix) {
+
+        NamespaceInfo ns = catalog.getNamespaceByPrefix(prefix);
+        if (prefix.equals("default")) {
+            throw new RestException("Can't delete the default namespace",
+                    HttpStatus.METHOD_NOT_ALLOWED);
+        }
+        if (ns == null) {
+            throw new RestException("Namespace '" + prefix + "' not found", HttpStatus.NOT_FOUND);
+        }
+        if ( !catalog.getResourcesByNamespace(ns, ResourceInfo.class).isEmpty() ) {
+            throw new RestException( "Namespace not empty", HttpStatus.UNAUTHORIZED );
+        }
+        catalog.remove(ns);
+    }
+
+    private UriComponents getUriComponents(String name, UriComponentsBuilder builder) {
+        UriComponents uriComponents;
+
+        uriComponents = builder.path("/namespaces/{id}").buildAndExpand(name);
+
+        return uriComponents;
+    }
+
+    @Override
+    public void configureFreemarker(FreemarkerHTMLMessageConverter converter, Template template) {
+
+        template.getConfiguration().setObjectWrapper(new ObjectToMapWrapper(NamespaceInfo.class) {
+
+            @Override
+            protected void wrapInternal(Map properties, SimpleHash model, Object object) {
+                if (properties == null) {
+                    try {
+                        properties = model.toMap();
+                    } catch (TemplateModelException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+                List<Map<String, Map<String, String>>> dsProps = new ArrayList<>();
+
+                NamespaceInfo wkspace = (NamespaceInfo) object;
+
+            }
+
+            @Override
+            protected void wrapInternal(SimpleHash model,
+                    @SuppressWarnings("rawtypes") Collection object) {
+
+                for (Object w : object) {
+                    NamespaceInfo ns = (NamespaceInfo) w;
+                    wrapInternal(null, model, ns);
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public void configurePersister(XStreamPersister persister, XStreamMessageConverter converter) {
+        persister.setCallback(new XStreamPersister.Callback() {
+            @Override
+            protected Class<NamespaceInfo> getObjectClass() {
+                return NamespaceInfo.class;
+            }
+
+            @Override
+            protected CatalogInfo getCatalogObject() {
+                Map<String, String> uriTemplateVars = (Map<String, String>) RequestContextHolder
+                        .getRequestAttributes()
+                        .getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE,
+                                RequestAttributes.SCOPE_REQUEST);
+                String prefix = uriTemplateVars.get(NAMESPACE_NAME);
+
+                if (prefix == null) {
+                    return null;
+                }
+                return catalog.getNamespaceByPrefix(prefix);
+            }
+
+            @Override
+            protected void postEncodeNamespace(NamespaceInfo cs, HierarchicalStreamWriter writer,
+                    MarshallingContext context) {
+
+            }
+
+            @Override
+            protected void postEncodeReference(Object obj, String ref, String prefix,
+                    HierarchicalStreamWriter writer, MarshallingContext context) {
+                if (obj instanceof NamespaceInfo) {
+                    converter.encodeLink("/namespaces/" + converter.encode(ref), writer);
+                }
+            }
+        });
+    }
+
+}

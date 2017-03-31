029fb03bcd9e911e5d4b01a291b81bc1e44304d0
Torben Barsballe <tbarsballe@boundlessgeo.com>
2017-03-31 23:25:28 +0200

    Add Import Data DELETE

--- src/extension/importer/rest-ng/src/main/java/org/geoserver/importer/rest/ImportDataController.java
+++ src/extension/importer/rest-ng/src/main/java/org/geoserver/importer/rest/ImportDataController.java
@@ -4,6 +4,7 @@
  */
 package org.geoserver.importer.rest;
 
+import java.io.File;
 import java.util.NoSuchElementException;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,13 +20,8 @@ import org.geoserver.importer.Importer;
 import org.geoserver.importer.rest.converters.ImportContextJSONConverterWriter;
 import org.geoserver.rest.RestBaseController;
 import org.geoserver.rest.RestException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterators;
@@ -41,8 +37,7 @@ public class ImportDataController extends ImportBaseController {
 
     protected ImportContextJSONConverterWriter converterWriter;
 
-    @GetMapping(value = { "/imports/{importId}/data",
-            "/imports/{importId}/tasks/{taskId}/data", }, produces = {
+    @GetMapping(value = { "/imports/{importId}/data", "/imports/{importId}/tasks/{taskId}/data", }, produces = {
                     MediaType.APPLICATION_JSON_VALUE, CatalogController.TEXT_JSON,
                     MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_HTML_VALUE })
     public ImportData getData(@PathVariable(required = true, name = "importId") Long importId,
@@ -67,20 +62,38 @@ public class ImportDataController extends ImportBaseController {
 
     }
 
-    
+    //We need to force spring to ignore the .shp here (we don't want a .shp encoded response!
+    @GetMapping(value = {"/imports/{importId}/data/files",
+            "/imports/{importId}/tasks/{taskId}/data/files",
+            "/imports/{importId}/data/files/{fileName:.+}",
+            "/imports/{importId}/tasks/{taskId}/data/files/{fileName:\\.+}" },
+            produces = {MediaType.APPLICATION_JSON_VALUE, CatalogController.TEXT_JSON})
+    public ImportData getDirectory(@PathVariable(required = true, name = "importId") Long importId,
+                                   @PathVariable(required = false, name = "taskId") Integer taskId,
+                                   @PathVariable(required = false, name = "fileName") String fileName) throws Exception {
+
+
+        return getDataImport(importId, fileName);
+    }
             
     
     //We need to force spring to ignore the .shp here (we don't want a .shp encoded response!
-    @GetMapping(value = {"/imports/{importId}/data/files/{fileName:.+}", "/imports/{importId}/data/files/{fileName:.+}",
-            "/imports/{importId}/data/files", "/imports/{importId}/tasks/{taskId}/data/files",
-            "/imports/{importId}/tasks/{taskId}/data/files/{fileName:\\.+}" }, 
-            produces = {MediaType.APPLICATION_JSON_VALUE, CatalogController.TEXT_JSON})
-    public ImportData getDirectory(@PathVariable(required = true, name = "importId") Long importId,
+    @DeleteMapping(value = {"/imports/{importId}/data/files",
+            "/imports/{importId}/tasks/{taskId}/data/files",
+            "/imports/{importId}/data/files/{fileName:.+}",
+            "/imports/{importId}/tasks/{taskId}/data/files/{fileName:\\.+}" })
+    public ResponseEntity deleteDirectory(@PathVariable(required = true, name = "importId") Long importId,
             @PathVariable(required = false, name = "taskId") Integer taskId,
             @PathVariable(required = false, name = "fileName") String fileName) throws Exception {
         
-        
-        return getDataImport(importId, fileName);
+        Directory dir = lookupDirectory(importId);
+        ImportData file = lookupFile(fileName, dir);
+
+        if (dir.getFiles().remove(file)) {
+            return new ResponseEntity("", new HttpHeaders(), HttpStatus.NO_CONTENT);
+        } else {
+            throw new RestException("Unable to remove file: " + file.getName(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     private ImportData getDataImport(Long importId, String fileName) {

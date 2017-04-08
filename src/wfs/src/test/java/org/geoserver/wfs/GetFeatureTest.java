5b0672a9813029064a565cb19cdbf53bb6435394
Steve Ikeoka <Steve.Ikeoka@GDIT.com>
2017-04-08 15:57:35 -0700

    [GEOS-8023] Fixed a unit test that relied on an unqualified layer name lookup.

--- src/wfs/src/test/java/org/geoserver/wfs/GetFeatureTest.java
+++ src/wfs/src/test/java/org/geoserver/wfs/GetFeatureTest.java
@@ -314,7 +314,7 @@ public class GetFeatureTest extends WFSTestSupport {
         testGetFifteenAll("cdf/Fifteen/wfs?request=GetFeature&typename=cdf:Fifteen&version=1.0.0&service=wfs");
         testGetFifteenAll("cdf/Fifteen/wfs?request=GetFeature&typename=Fifteen&version=1.0.0&service=wfs");
     
-        Document doc = getAsDOM("sf/Fifteen/wfs?request=GetFeature&typename=cdf:Seven&version=1.0.0&service=wfs");
+        Document doc = getAsDOM("cdf/Fifteen/wfs?request=GetFeature&typename=cdf:Seven&version=1.0.0&service=wfs");
         XMLAssert.assertXpathEvaluatesTo("1", "count(//ogc:ServiceException)", doc);
     }
     

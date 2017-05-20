5a723f6f6b4ce2aefbee70fdd59baf25c1480eeb
Nuno Oliveira <nuno.oliveira@geo-solutions.it>
2017-05-20 18:58:57 +0100

    [GEOS-2749] Reproject geometries in WMS GetFeatureInfo responses when info_format is GML

--- src/wms/src/test/java/org/geoserver/wms/wms_1_3/WMSCascadeTest.java
+++ src/wms/src/test/java/org/geoserver/wms/wms_1_3/WMSCascadeTest.java
@@ -13,8 +13,6 @@ import java.util.Map;
 
 import org.custommonkey.xmlunit.NamespaceContext;
 import org.custommonkey.xmlunit.SimpleNamespaceContext;
-import org.custommonkey.xmlunit.XMLUnit;
-import org.custommonkey.xmlunit.XpathEngine;
 import org.geoserver.config.GeoServer;
 import org.geoserver.data.test.SystemTestData;
 import org.geoserver.test.http.MockHttpResponse;
@@ -31,9 +29,14 @@ import org.w3c.dom.Document;
 
 import org.springframework.mock.web.MockHttpServletResponse;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
 @RunWith(Parameterized.class)
 public class WMSCascadeTest extends WMSCascadeTestSupport {
-    
+
     private final boolean aphEnabled;
 
     @Parameters(name = "{index} APH enabled: {0}")
@@ -50,6 +53,8 @@ public class WMSCascadeTest extends WMSCascadeTestSupport {
         GeoServer gs = getGeoServer();
         WMSInfo wms = gs.getService(WMSInfo.class);
         wms.getMetadata().put(WMS.ADVANCED_PROJECTION_KEY, aphEnabled);
+        // make sure GetFeatureInfo is not deactivated (this will only update the global service)
+        wms.setFeaturesReprojectionDisabled(false);
         gs.save(wms);
     }
     
@@ -65,6 +70,15 @@ public class WMSCascadeTest extends WMSCascadeTestSupport {
                 + "&styles&bbox=-110.0,-200.0,110.0,200.0&crs=EPSG:4326&bgcolor=0xFFFFFF&transparent=FALSE&format=image/png&width=190&height=100"), new MockHttpResponse(pngImage, "image/png"));
         wms11Client.expectGet(new URL(wms11BaseURL + "?service=WMS&version=1.1.1&request=GetMap&layers=world4326" 
                 + "&styles&bbox=-200.0,-110.0,200.0,110.0&srs=EPSG:4326&bgcolor=0xFFFFFF&transparent=FALSE&format=image/png&width=190&height=100"), new MockHttpResponse(pngImage, "image/png"));
+
+        // setup mocked get feature info (the return features use EPSG:3857)
+        URL featureInfo = WMSTestSupport.class.getResource("wms-features.xml");
+        wms13Client.expectGet(new URL(wms13BaseURL + "?SERVICE=WMS&INFO_FORMAT=application/vnd.ogc.gml&LAYERS=world4326" +
+                        "&CRS=EPSG:4326&FEATURE_COUNT=50&FORMAT=image%2Fpng&HEIGHT=101&TRANSPARENT=TRUE&J=-609621&REQUEST=GetFeatureInfo" +
+                        "&I=-875268&WIDTH=101&BBOX=-103.829117187,44.3898919295,-103.804563429,44.4069939679&STYLES=&QUERY_LAYERS=world4326&VERSION=1.3.0"),
+                new MockHttpResponse(featureInfo, "application/vnd.ogc.gml"));
+
+
     }
     
     @Test
@@ -100,7 +114,53 @@ public class WMSCascadeTest extends WMSCascadeTestSupport {
         
         xpath.evaluate("//wms:Layer[name='" + WORLD4326_110_NFI + "']", dom);
     }
-    
-   
-    
+
+    @Test
+    public void testGetFeatureInfoReprojection() throws Exception {
+        // do the get feature request using EPSG:4326
+        String url = "wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&FORMAT=image/png&TRANSPARENT=true" +
+                "&QUERY_LAYERS=" + WORLD4326_130 + "&STYLES&LAYERS=" + WORLD4326_130 + "&INFO_FORMAT=text/xml; subtype=gml/3.1.1" +
+                "&FEATURE_COUNT=50&X=50&Y=50&SRS=EPSG:4326&WIDTH=101&HEIGHT=101&BBOX=-103.829117187,44.3898919295,-103.804563429,44.4069939679";
+        Document result = getAsDOM(url);
+        // setup XPATH engine namespaces
+        Map<String, String> namespaces = new HashMap<>();
+        namespaces.put("gml", "http://www.opengis.net/gml" );
+        namespaces.put("gs", "http://geoserver.org" );
+        namespaces.put("ogc", "http://www.opengis.net/ogc" );
+        namespaces.put("ows", "http://www.opengis.net/ows" );
+        namespaces.put("wfs", "http://www.opengis.net/wfs" );
+        namespaces.put("xlink", "http://www.w3.org/1999/xlink" );
+        namespaces.put("xs", "http://www.w3.org/2001/XMLSchema" );
+        namespaces.put("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+        xpath.setNamespaceContext(new SimpleNamespaceContext(namespaces));
+        // check the response content, the features should have been reproject from EPSG:3857 to EPSG:4326
+        String srs = xpath.evaluate("//wfs:FeatureCollection/gml:featureMembers/" +
+                "gs:world4326_130[@gml:id='bugsites.55']/gs:the_geom/gml:Point/@srsName", result);
+        assertThat(srs, notNullValue());
+        assertThat(srs.contains("4326"), is(true));
+        String rawCoordinates = xpath.evaluate("//wfs:FeatureCollection/gml:featureMembers/" +
+                "gs:world4326_130[@gml:id='bugsites.55']/gs:the_geom/gml:Point/gml:pos/text()", result);
+        assertThat(rawCoordinates, notNullValue());
+        String[] coordinates = rawCoordinates.split(" ");
+        assertThat(coordinates.length, is(2));
+        checkNumberSimilar(coordinates[0], 44.39832008, 0.0001);
+        checkNumberSimilar(coordinates[1], -103.81711048, 0.0001);
+        // deactivate features reprojection
+        WMSInfo wms = getGeoServer().getService(WMSInfo.class);
+        wms.setFeaturesReprojectionDisabled(true);
+        getGeoServer().save(wms);
+        // execute the get feature info request
+        result = getAsDOM(url);
+         srs = xpath.evaluate("//wfs:FeatureCollection/gml:featureMembers/" +
+                "gs:world4326_130[@gml:id='bugsites.55']/gs:the_geom/gml:Point/@srsName", result);
+        assertThat(srs, notNullValue());
+        assertThat(srs.contains("3857"), is(true));
+         rawCoordinates = xpath.evaluate("//wfs:FeatureCollection/gml:featureMembers/" +
+                "gs:world4326_130[@gml:id='bugsites.55']/gs:the_geom/gml:Point/gml:pos/text()", result);
+        assertThat(rawCoordinates, notNullValue());
+        coordinates = rawCoordinates.split(" ");
+        assertThat(coordinates.length, is(2));
+        checkNumberSimilar(coordinates[0], -11556867.874, 0.0001);
+        checkNumberSimilar(coordinates[1], 5527291.47718493, 0.0001);
+    }
 }

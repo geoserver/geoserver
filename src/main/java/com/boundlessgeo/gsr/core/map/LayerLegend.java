a6ac1c6279010a16d5cb5da223e12e367f0decd5
Torben Barsballe <tbarsballe@boundlessgeo.com>
2017-06-05 12:27:50 -0700

    Add missing copyright headurs, update existing ones

--- src/main/java/com/boundlessgeo/gsr/core/map/LayerLegend.java
+++ src/main/java/com/boundlessgeo/gsr/core/map/LayerLegend.java
@@ -1,3 +1,7 @@
+/* Copyright (c) 2017 Boundless - http://boundlessgeo.com All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package com.boundlessgeo.gsr.core.map;
 
 import com.boundlessgeo.gsr.core.renderer.*;

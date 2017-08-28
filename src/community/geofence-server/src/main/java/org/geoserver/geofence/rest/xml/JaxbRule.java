00c38cf7f71a5832d71608b842802dff9c3b5000
Niels Charlier <niels@scitus.be>
2017-08-28 11:50:22 +0200

    GEOS-8239 Support Limits and LayerDetails in geofence internal server REST

--- src/community/geofence-server/src/main/java/org/geoserver/geofence/rest/xml/JaxbRule.java
+++ src/community/geofence-server/src/main/java/org/geoserver/geofence/rest/xml/JaxbRule.java
@@ -4,16 +4,251 @@
  */
 package org.geoserver.geofence.rest.xml;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import org.geoserver.geofence.core.model.IPAddressRange;
 import org.geoserver.geofence.core.model.Rule;
+import org.geoserver.geofence.core.model.RuleLimits;
+import org.geoserver.geofence.core.model.enums.AccessType;
+import org.geoserver.geofence.core.model.enums.CatalogMode;
 import org.geoserver.geofence.core.model.enums.GrantType;
+import org.geoserver.geofence.core.model.enums.LayerType;
+
+import com.vividsolutions.jts.geom.MultiPolygon;
 
 @XmlRootElement(name = "Rule")
 public class JaxbRule {
+    
+    /**
+     * Specification for "LIMIT" rules.
+     *
+     */
+    public static class Limits {
+        
+        private MultiPolygon allowedArea;
+        
+        private String catalogMode;
+
+        @XmlElement
+        @XmlJavaTypeAdapter(MultiPolygonAdapter.class)
+        public MultiPolygon getAllowedArea() {
+            return allowedArea;
+        }
+
+        public void setAllowedArea(MultiPolygon allowedArea) {
+            this.allowedArea = allowedArea;
+        }
+
+        @XmlElement
+        public String getCatalogMode() {
+            return catalogMode;
+        }
+
+        public void setCatalogMode(String catalogMode) {
+            this.catalogMode = catalogMode;
+        }
+        
+        public RuleLimits toRuleLimits(RuleLimits ruleLimits) {
+            if (ruleLimits == null) {
+                ruleLimits = new RuleLimits();
+            }
+            if (getAllowedArea() != null) {
+                ruleLimits.setAllowedArea(getAllowedArea());
+            }
+            if (getCatalogMode() != null) {
+                ruleLimits.setCatalogMode(CatalogMode.valueOf(getCatalogMode().toUpperCase()));
+            }
+            return ruleLimits;
+        }
+        
+    }
+    
+    /**
+     * Access specification for a Layer Attribute
+     *
+     */
+    public static class LayerAttribute {
+        private String name;
+        private String dataType;
+        private String accessType;
+        
+        public LayerAttribute() {
+            
+        }
+        
+        public LayerAttribute(org.geoserver.geofence.core.model.LayerAttribute att) {
+            this.name = att.getName();
+            this.dataType = att.getDatatype();
+            this.accessType = att.getAccess().toString();
+        }
+        
+        @XmlElement
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        
+        @XmlElement
+        public String getDataType() {
+            return dataType;
+        }
+        public void setDataType(String dataType) {
+            this.dataType = dataType;
+        }
+
+        @XmlElement
+        public String getAccessType() {
+            return accessType;
+        }
+        public void setAccessType(String accessType) {
+            this.accessType = accessType;
+        }
+        
+        public org.geoserver.geofence.core.model.LayerAttribute 
+            toLayerAttribute() {
+            org.geoserver.geofence.core.model.LayerAttribute att = new
+                    org.geoserver.geofence.core.model.LayerAttribute();
+            if (accessType != null) {
+                att.setAccess(AccessType.valueOf(accessType.toUpperCase()));
+            }            
+            att.setDatatype(dataType);
+            att.setName(name);
+            return att;
+        }
+        
+        
+    }
+    
+    /**
+     * Details for layer access.
+     *
+     */
+    public static class LayerDetails {
+        private String layerType;
+        private String defaultStyle;
+        private String cqlFilterRead;
+        private String cqlFilterWrite;
+        private MultiPolygon allowedArea;
+        private String catalogMode;
+        private Set<String> allowedStyles = new HashSet<String>();
+        private Set<LayerAttribute> layerAttributes = new HashSet<LayerAttribute>();
+        
+        @XmlElement
+        public String getLayerType() {
+            return layerType;
+        }
+        public void setLayerType(String layerType) {
+            this.layerType = layerType;
+        }
+
+        @XmlElement
+        public String getDefaultStyle() {
+            return defaultStyle;
+        }
+        public void setDefaultStyle(String defaultStyle) {
+            this.defaultStyle = defaultStyle;
+        }
+
+        @XmlElement
+        public String getCqlFilterRead() {
+            return cqlFilterRead;
+        }
+        public void setCqlFilterRead(String cqlFilterRead) {
+            this.cqlFilterRead = cqlFilterRead;
+        }
+
+        @XmlElement
+        public String getCqlFilterWrite() {
+            return cqlFilterWrite;
+        }
+        public void setCqlFilterWrite(String cqlFilterWrite) {
+            this.cqlFilterWrite = cqlFilterWrite;
+        }
+
+        @XmlElement
+        @XmlJavaTypeAdapter(MultiPolygonAdapter.class)
+        public MultiPolygon getAllowedArea() {
+            return allowedArea;
+        }
+        public void setAllowedArea(MultiPolygon allowedArea) {
+            this.allowedArea = allowedArea;
+        }
+
+        @XmlElement
+        public String getCatalogMode() {
+            return catalogMode;
+        }
+        public void setCatalogMode(String catalogMode) {
+            this.catalogMode = catalogMode;
+        }
+
+        @XmlElement(name = "allowedStyle")
+        public Set<String> getAllowedStyles() {
+            return allowedStyles;
+        }
+        public void setAllowedStyles(Set<String> allowedStyles) {
+            this.allowedStyles = allowedStyles;
+        }
+
+        @XmlElement(name = "attribute")
+        public Set<LayerAttribute> getAttributes() {
+            return layerAttributes;
+        }
+        public void setAttributes(Set<LayerAttribute> layerAttributes) {
+            this.layerAttributes = layerAttributes;
+        }
+        
+        public org.geoserver.geofence.core.model.LayerDetails 
+            toLayerDetails(org.geoserver.geofence.core.model.LayerDetails details) {
+            if (details == null) {
+                details = new org.geoserver.geofence.core.model.LayerDetails();
+            }
+            if (layerType != null) {
+                details.setType(LayerType.valueOf(layerType.toUpperCase()));
+            }
+            if (allowedStyles != null) {
+                details.getAllowedStyles().addAll(allowedStyles);
+            }
+            if (allowedArea != null) {
+                details.setArea(allowedArea);
+            }
+            if (layerAttributes != null) {
+                for (LayerAttribute att : layerAttributes) {
+                    Iterator<org.geoserver.geofence.core.model.LayerAttribute> it = 
+                            details.getAttributes().iterator();
+                    while (it.hasNext()) {
+                        if (it.next().getName().equals(att.getName())) {
+                            it.remove();
+                            break;
+                        }
+                    }
+                    details.getAttributes().add(att.toLayerAttribute());
+                }
+            }
+            if (catalogMode != null) {
+                details.setCatalogMode(CatalogMode.valueOf(catalogMode.toUpperCase()));
+            }
+            if (cqlFilterRead != null) {
+                details.setCqlFilterRead(cqlFilterRead);
+            }
+            if (cqlFilterWrite != null) {
+                details.setCqlFilterWrite(cqlFilterWrite);
+            }
+            if (defaultStyle != null) {
+                details.setDefaultStyle(defaultStyle);
+            }
+            return details;
+        }
+    }
 
     private Long id;
 
@@ -34,6 +269,10 @@ public class JaxbRule {
     private String request;
 
     private String access;
+    
+    private Limits limits;
+    
+    private LayerDetails layerDetails;
 
     public JaxbRule() {
 
@@ -50,6 +289,25 @@ public class JaxbRule {
         service = rule.getService() == null ? null : rule.getService().toUpperCase();
         request = rule.getRequest();
         access = rule.getAccess().toString();
+        if (rule.getRuleLimits() != null) {
+            limits = new Limits();
+            limits.setAllowedArea(rule.getRuleLimits().getAllowedArea());
+            limits.setCatalogMode(rule.getRuleLimits().getCatalogMode().toString());
+        }
+        if (rule.getLayerDetails() != null) {
+            layerDetails = new LayerDetails();
+            layerDetails.setAllowedArea(rule.getLayerDetails().getArea());
+            layerDetails.getAllowedStyles().addAll(rule.getLayerDetails().getAllowedStyles());
+            layerDetails.setCatalogMode(rule.getLayerDetails().getCatalogMode().toString());
+            layerDetails.setCqlFilterRead(rule.getLayerDetails().getCqlFilterRead());
+            layerDetails.setCqlFilterWrite(rule.getLayerDetails().getCqlFilterWrite());
+            layerDetails.setDefaultStyle(rule.getLayerDetails().getDefaultStyle());
+            layerDetails.setLayerType(rule.getLayerDetails().getType().toString());
+            for (org.geoserver.geofence.core.model.LayerAttribute att : 
+                rule.getLayerDetails().getAttributes()) {
+                layerDetails.getAttributes().add(new LayerAttribute(att));
+            }
+        }
     }
 
     @XmlAttribute
@@ -135,6 +393,23 @@ public class JaxbRule {
 
     public void setAccess(String access) {
         this.access = access;
+    }
+    
+    @XmlElement
+    public Limits getLimits() {
+        return limits;
+    }
+    
+    public void setLimits(Limits limits) {
+        this.limits = limits;
+    }      
+
+    public LayerDetails getLayerDetails() {
+        return layerDetails;
+    }
+
+    public void setLayerDetails(LayerDetails layerDetails) {
+        this.layerDetails = layerDetails;
     }
 
     public Rule toRule() {

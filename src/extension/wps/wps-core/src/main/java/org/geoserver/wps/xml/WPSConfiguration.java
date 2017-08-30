5bb2f17d83839deb93bdc89e7742cf4bdf22dbfd
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2017-08-30 11:25:48 +0200

    GEOS-8267: added support for viewParams in WPS Execute embedded WFS requests

--- src/extension/wps/wps-core/src/main/java/org/geoserver/wps/xml/WPSConfiguration.java
+++ src/extension/wps/wps-core/src/main/java/org/geoserver/wps/xml/WPSConfiguration.java
@@ -6,7 +6,13 @@
 package org.geoserver.wps.xml;
 
 import java.util.Map;
+
+import org.geoserver.wfs.xml.v1_0_0.GetFeatureTypeBinding;
+import org.geotools.wfs.WFSParserDelegate;
+import org.geotools.wfs.v1_0.WFS;
+import org.geotools.wfs.v1_1.WFSConfiguration;
 import org.geotools.wps.WPS;
+import org.geotools.xml.XSDParserDelegate;
 import org.picocontainer.MutablePicoContainer;
 
 public class WPSConfiguration extends org.geotools.wps.WPSConfiguration {
@@ -25,5 +31,18 @@ public class WPSConfiguration extends org.geotools.wps.WPSConfiguration {
         container.registerComponentInstance(new org.geoserver.wcs.xml.v1_1_1.WCSParserDelegate());
         container.registerComponentInstance(new org.geoserver.wcs.xml.v1_0_0.WCSParserDelegate());
         container.registerComponentInstance(new org.geoserver.wcs2_0.xml.WCSParserDelegate());
+        // replace WFSParserDelegate from GeoTools with a new one using GeoServer GetFeatureTypeBinding,
+        // able to parse viewParams attribute and enable usage of SQL views
+        Object wfs = container.getComponentInstanceOfType(WFSParserDelegate.class);
+        container.unregisterComponentByInstance(wfs);
+        container.registerComponentInstance(new XSDParserDelegate(new WFSConfiguration() {
+
+            @Override
+            protected void configureBindings(MutablePicoContainer container) {
+                super.configureBindings(container);
+                container.registerComponentImplementation(WFS.GetFeatureType, GetFeatureTypeBinding.class);
+            }
+            
+        }));
     }
 }

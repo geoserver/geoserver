19eee6809062f15fd767d55cffa9c2d369d88e4f
Erik Merkle <emerkle@boundlessgeo.com>
2017-09-26 22:15:53 -0500

    Fix POSTing for GeoGig Import
    Signed-off-by: Erik Merkle <emerkle@boundlessgeo.com>

--- src/community/geogig/src/main/java/org/geogig/geoserver/spring/config/GeogigSpringConfig.java
+++ src/community/geogig/src/main/java/org/geogig/geoserver/spring/config/GeogigSpringConfig.java
@@ -6,6 +6,8 @@ package org.geogig.geoserver.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 /**
  * Configure the geogig content negotiation strategy.
@@ -17,6 +19,10 @@ public class GeogigSpringConfig {
     GeogigContentNegotiationStrategy geogigContentNegotiationStrategy() {
         return new GeogigContentNegotiationStrategy();
     }
-    
+
+    @Bean
+    MultipartResolver multipartResolver() {
+        return new CommonsMultipartResolver();
+    }
 }
 

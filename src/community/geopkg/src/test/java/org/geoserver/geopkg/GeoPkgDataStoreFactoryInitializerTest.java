31b57d7e5382b01d0519c215bddbd70c1fec6d96
Brad Hards <bradh@frogmouth.net>
2017-09-30 19:07:03 +1000

    [GEOS-8257] Correct Y order for geopackage WPS tiles output.
    GeoPackage first tile is top left (flipped from mbtiles). This is already handled for
    Map output, just incorrect for WPS tiles.

--- src/community/geopkg/src/test/java/org/geoserver/geopkg/GeoPkgDataStoreFactoryInitializerTest.java
+++ src/community/geopkg/src/test/java/org/geoserver/geopkg/GeoPkgDataStoreFactoryInitializerTest.java
@@ -1,3 +1,7 @@
+/* (c) 2014-2017 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.geopkg;
 
 import org.geoserver.data.DataStoreFactoryInitializer;

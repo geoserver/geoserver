70712cf6d51d9f59cadac0cece23994db29e4b03
Torben Barsballe <tbarsballe@boundlessgeo.com>
2017-11-02 16:07:52 -0700

    [GEOS-6745] Copy style needs to copy content and format

--- src/web/wms/src/main/java/org/geoserver/wms/web/data/StyleAdminPanel.java
+++ src/web/wms/src/main/java/org/geoserver/wms/web/data/StyleAdminPanel.java
@@ -426,6 +426,7 @@ public class StyleAdminPanel extends StyleEditTabPanel {
                         // same here, force validation or the field won't be updated
                         stylePage.editor.reset();
                         stylePage.setRawStyle(stylePage.readFile(style));
+                        stylePage.getStyleInfo().setFormat(style.getFormat());
                         target.appendJavaScript(String.format(
                                 "if (document.gsEditors) { document.gsEditors.editor.setOption('mode', '%s'); }", 
                                 stylePage.styleHandler().getCodeMirrorEditMode()));

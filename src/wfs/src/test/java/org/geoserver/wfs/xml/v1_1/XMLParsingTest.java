4cc238e3b5fc6440be97b812f7a968f3fa684692
Ian Turton <ijturton@gmail.com>
2017-11-15 17:37:16 +0000

    [GEOS-8227] test for issue (#2638)
    * test to show GT fix of GEOS-8227 works

--- src/wfs/src/test/java/org/geoserver/wfs/xml/v1_1/XMLParsingTest.java
+++ src/wfs/src/test/java/org/geoserver/wfs/xml/v1_1/XMLParsingTest.java
@@ -5,18 +5,25 @@
  */
 package org.geoserver.wfs.xml.v1_1;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.io.ByteArrayInputStream;
 import java.math.BigInteger;
-
-import net.opengis.wfs.GetFeatureWithLockType;
-import net.opengis.wfs.NativeType;
+import java.util.Map;
 
 import org.geoserver.data.test.SystemTestData;
 import org.geoserver.wfs.WFSTestSupport;
+import org.geoserver.wfs.xml.v1_1_0.WFSConfiguration;
 import org.geotools.xml.Parser;
 import org.junit.Test;
+import org.opengis.feature.simple.SimpleFeature;
+
+import net.opengis.wfs.GetFeatureWithLockType;
+import net.opengis.wfs.InsertElementType;
+import net.opengis.wfs.NativeType;
+import net.opengis.wfs.PropertyType;
+import net.opengis.wfs.TransactionType;
+import net.opengis.wfs.UpdateElementType;
 
 public class XMLParsingTest extends WFSTestSupport {
 
@@ -57,4 +64,67 @@ public class XMLParsingTest extends WFSTestSupport {
         assertEquals("text/xml; subtype=gml/3.1.1", gf.getOutputFormat());
         assertEquals(new BigInteger("10"), gf.getExpiry());
     }
+    @Test
+    public void testUpdate() throws Exception {
+            WFSConfiguration xmlConfiguration11 = getXmlConfiguration11();
+            Parser p = new Parser(xmlConfiguration11);
+            p.setHandleMixedContent(true);
+
+            String request = "<wfs:Transaction service=\"WFS\" version=\"1.1.0\" " + //
+                            "xmlns:topp=\"http://www.openplans.org/topp\" " + //
+                            "xmlns:ogc=\"http://www.opengis.net/ogc\" " + //
+                            "xmlns:wfs=\"http://www.opengis.net/wfs\">" + //
+                            "<wfs:Update typeName=\"topp:tasmania_roads\">" + //
+                            "<wfs:Property>" + //
+                            "<wfs:Name>TYPE</wfs:Name>" + //
+                            "<wfs:Value><![CDATA[street\n" + //
+                            "line2\n" + //
+                            "line3\n]]></wfs:Value>" + //
+                            "</wfs:Property>" + //
+                            "<ogc:Filter>" + //
+                            "<ogc:FeatureId fid=\"tasmania_roads.1\"/>" + //
+                            "</ogc:Filter>" + //
+                            "</wfs:Update>" + //
+                            "</wfs:Transaction>";
+
+            TransactionType obj = (TransactionType) p.parse(new ByteArrayInputStream(request.getBytes()));
+            UpdateElementType update = (UpdateElementType) obj.getUpdate().get(0);
+            PropertyType property = (PropertyType) update.getProperty().get(0);
+            
+            assertEquals("street\nline2\nline3\n", property.getValue());
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+            WFSConfiguration xmlConfiguration11 = getXmlConfiguration11();
+            Parser p = new Parser(xmlConfiguration11);
+            p.setHandleMixedContent(true);
+
+            String request = "<wfs:Transaction xmlns:wfs=\"http://www.opengis.net/wfs\" "
+                            + "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" "
+                            + "service=\"WFS\" version=\"1.1.0\" xsi:schemaLocation=\"http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd\">"
+                            + "<wfs:Insert>" + //
+                            "<feature:t2 xmlns:feature=\"http://test\">" + //
+                            "<feature:geom>" + //
+                            "<gml:Point xmlns:gml=\"http://www.opengis.net/gml\">" + //
+                            "<gml:pos>0 0</gml:pos>" + //
+                            "</gml:Point>" + //
+                            "</feature:geom>" + //
+                            "<feature:descr><![CDATA[1\n" + //
+                            "2\n" + //
+                            "3\n" + //
+                            "]]></feature:descr>" + //
+                            "</feature:t2>" + //
+                            "</wfs:Insert>" + //
+                            "</wfs:Transaction>";
+
+            TransactionType obj = (TransactionType) p.parse(new ByteArrayInputStream(request.getBytes()));
+            InsertElementType insert = (InsertElementType) obj.getInsert().get(0);
+            SimpleFeature feature = (SimpleFeature) insert.getFeature().get(0);
+            Map<?, ?> attr = (Map<?, ?>) feature.getAttribute("descr");
+            String expected = "1\n2\n3\n";
+            
+            assertEquals("new lines eaten",expected,attr.get(null).toString());
+    }
+    
 }

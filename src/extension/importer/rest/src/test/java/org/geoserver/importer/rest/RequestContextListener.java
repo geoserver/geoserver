81a8856c9c75fad4aa1c43947b36b5f11764374e
Torben Barsballe <tbarsballe@boundlessgeo.com>
2017-12-01 15:07:48 -0800

    [GEOS-8460] Make importer share the request context when initializing a job asynchronously

--- src/extension/importer/rest/src/test/java/org/geoserver/importer/rest/RequestContextListener.java
+++ src/extension/importer/rest/src/test/java/org/geoserver/importer/rest/RequestContextListener.java
@@ -4,9 +4,13 @@
  */
 package org.geoserver.importer.rest;
 
+import org.geoserver.catalog.CatalogInfo;
 import org.geoserver.catalog.ResourceInfo;
+import org.geoserver.catalog.StoreInfo;
+import org.geoserver.catalog.WorkspaceInfo;
 import org.geoserver.security.AbstractResourceAccessManager;
 import org.geoserver.security.DataAccessLimits;
+import org.geoserver.security.WorkspaceAccessLimits;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -22,7 +26,7 @@ public final class RequestContextListener extends AbstractResourceAccessManager 
     private CallBack callBack;
 
     public interface CallBack {
-        void invoked(HttpServletRequest request, Authentication user, ResourceInfo resource);
+        void invoked(HttpServletRequest request, Authentication user, CatalogInfo info);
     }
 
     public void setCallBack(CallBack callBack) {
@@ -41,5 +45,19 @@ public final class RequestContextListener extends AbstractResourceAccessManager 
             }
         }
         return super.getAccessLimits(user, resource);
+    }
+
+    @Override
+    public WorkspaceAccessLimits getAccessLimits(Authentication user, WorkspaceInfo workspace) {
+        if (callBack != null) {
+            // let's see if we can access this request context
+            try {
+                HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+                callBack.invoked(request, user, workspace);
+            } catch (Exception exception) {
+                // the call back should have test if the request context was properly obtained
+            }
+        }
+        return super.getAccessLimits(user, workspace);
     }
 }

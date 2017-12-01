2c041f65db47ffa9f1c3b66fd25c4c44c4391459
Gabriel Roldan <groldan@boundlessgeo.com>
2017-12-01 18:39:37 -0300

    Add WFS Transaction to GT transaction test case
    Add a test case to ensure org.geoserver.wfs.Transaction
    creates a GeoTools Transaction with the provided extra
    data.
    
    For instance:
    - Make sure WFS transaction's extendedProperties map to GT
    transaction's properties.
    - Make sure WFS transaction "handle" is set as the GT
    transaction's VersioningCommitMessage property
    - Make sure the GT transaction's VersioningCommitAuthor property
    is correctly derived from the Spring SecurityContex's Authentication,
    whether it's Principal is an instanceo of String or UserDetails.

--- src/wfs/src/main/java/org/geoserver/wfs/Transaction.java
+++ src/wfs/src/main/java/org/geoserver/wfs/Transaction.java
@@ -572,7 +572,7 @@ public class Transaction {
             Object principal = authentication.getPrincipal();
             if(principal instanceof UserDetails) {
                 username = ((UserDetails) principal).getUsername(); 
-            } else if(principal instanceof String) {
+            } else if(principal instanceof String) { // OAuth
             	username = principal.toString();
             }
         }

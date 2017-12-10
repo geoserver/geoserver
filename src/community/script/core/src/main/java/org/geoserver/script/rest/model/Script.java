7076e763387f0f682d05959e5b2d10442be12cf2
Andrea Aime <andrea.aime@gmail.com>
2017-12-10 10:45:34 +0100

    Adding missing headers (fixes GEOS-5450 and GEOS-5444)

--- src/community/script/core/src/main/java/org/geoserver/script/rest/model/Script.java
+++ src/community/script/core/src/main/java/org/geoserver/script/rest/model/Script.java
@@ -1,3 +1,7 @@
+/* (c) 2017 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.script.rest.model;
 
 public class Script {
--- src/extension/wcs2_0-eo/web/src/test/java/org/geoserver/wcs2_0/eo/web/WCSEOLayerConfigTest.java
+++ src/extension/wcs2_0-eo/web/src/test/java/org/geoserver/wcs2_0/eo/web/WCSEOLayerConfigTest.java
@@ -1,3 +1,7 @@
+/* (c) 2017 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.wcs2_0.eo.web;
 
 import static org.junit.Assert.assertFalse;

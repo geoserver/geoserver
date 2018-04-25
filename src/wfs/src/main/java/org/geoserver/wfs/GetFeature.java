d27e0d42ad8797ab921a1ea040543adfe3507b07
Fernando Miño <fernandomino7@gmail.com>
2018-04-25 16:48:25 -0500

    WFS 2.0 tests failing fix

--- src/wfs/src/main/java/org/geoserver/wfs/GetFeature.java
+++ src/wfs/src/main/java/org/geoserver/wfs/GetFeature.java
@@ -261,7 +261,13 @@ public class GetFeature {
         //stored queries, preprocess compile any stored queries into actual query objects
         boolean getFeatureById = processStoredQueries(request);
         // resolve empty prefix QNnames to local workspace
-        queries = resolveLocalWorkspaceNS(request.getQueries());
+        if(WFSInfo.Version.V_20.compareTo(request.getVersion()) < 0){
+            queries = resolveLocalWorkspaceNS(request.getQueries());
+        }
+        else{
+            queries = request.getQueries();
+        }
+        
         
         if (request.isQueryTypeNamesUnset()) {
             expandTypeNames(request, queries, getFeatureById, getCatalog());

fb73595633b967277c78b25ef5f9a9c8afb56942
Torben Barsballe <tbarsballe@boundlessgeo.com>
2018-05-24 15:00:19 -0700

    Use GridSetBroker to remove GridSets, not TileLayerDispatcher

--- src/gwc/src/main/java/org/geoserver/gwc/GWC.java
+++ src/gwc/src/main/java/org/geoserver/gwc/GWC.java
@@ -2125,7 +2125,7 @@ public class GWC implements DisposableBean, InitializingBean, ApplicationContext
         }
 
         for (String gridSetId : gridsetIds) {
-            tld.removeGridset(gridSetId);
+            gridSetBroker.remove(gridSetId);
         }
     }
 

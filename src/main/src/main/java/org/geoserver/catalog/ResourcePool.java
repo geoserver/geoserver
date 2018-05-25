66a14230ac285a275a056a0c0b8dcb55612f9248
Andrea Aime <andrea.aime@gmail.com>
2018-05-25 16:40:51 +0200

    [GEOS-8755] CatalogRepository should return a null on store not found, instead it throws a RuntimeException

--- src/main/src/main/java/org/geoserver/catalog/ResourcePool.java
+++ src/main/src/main/java/org/geoserver/catalog/ResourcePool.java
@@ -2840,4 +2840,12 @@ public class ResourcePool {
         }
         return info;
     }
+
+    /**
+     * Package private on purpose, using it for testing
+     * @return
+     */
+    CatalogRepository getRepository() {
+        return repository;
+    }
 }

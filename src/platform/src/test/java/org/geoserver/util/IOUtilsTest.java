34257b306629c50d7e813bd008593be06bd5203b
Steve Ikeoka <Steve.Ikeoka@GDIT.com>
2018-06-12 08:40:40 -0700

    Check filenames inside of archives.

--- src/platform/src/test/java/org/geoserver/util/IOUtilsTest.java
+++ src/platform/src/test/java/org/geoserver/util/IOUtilsTest.java
@@ -6,12 +6,14 @@
 package org.geoserver.util;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import com.google.common.io.Files;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.util.zip.ZipOutputStream;
 import org.junit.Rule;
@@ -40,5 +42,30 @@ public class IOUtilsTest {
         IOUtils.decompress(bin, p2.toFile());
 
         assertTrue(p2.resolve("foo/bar/bar.txt").toFile().exists());
+    }
+
+    @Test
+    public void testDecompressStreamBadEntryName() throws IOException {
+        File destDir = temp.newFolder("d3").toPath().toFile();
+        destDir.mkdirs();
+        try (InputStream input = getClass().getResourceAsStream("/bad-zip-file.zip")) {
+            IOUtils.decompress(input, destDir);
+            fail("Expected decompression to fail");
+        } catch (IOException e) {
+            assertTrue(e.getMessage().startsWith("Entry is outside of the target directory"));
+        }
+    }
+
+    @Test
+    public void testDecompressFileBadEntryName() throws IOException {
+        File destDir = temp.newFolder("d4").toPath().toFile();
+        destDir.mkdirs();
+        File input = new File("src/test/resources/bad-zip-file.zip");
+        try {
+            IOUtils.decompress(input, destDir);
+            fail("Expected decompression to fail");
+        } catch (IOException e) {
+            assertTrue(e.getMessage().startsWith("Entry is outside of the target directory"));
+        }
     }
 }

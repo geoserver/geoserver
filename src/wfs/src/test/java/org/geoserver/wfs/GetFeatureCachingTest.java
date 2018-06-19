1fceace588aef29923f84046d89aeb68d6b80040
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2018-06-19 14:06:21 -0400

    GEOS-8779 Disable FeatureSizeFeatureCollection by default to avoid unoptimized counts
    * FeatureSizeFeatureCollection is disabled by default, to prevent unoptimized counts when a feature collection returns -1 for size()
    
    https://osgeo-org.atlassian.net/browse/GEOS-8790
    
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- src/wfs/src/test/java/org/geoserver/wfs/GetFeatureCachingTest.java
+++ src/wfs/src/test/java/org/geoserver/wfs/GetFeatureCachingTest.java
@@ -6,21 +6,23 @@ package org.geoserver.wfs;
 
 import org.geoserver.wfs.v2_0.GetFeatureTest;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 
 /**
  * Test making sure GetFeature still works when the caching machinery is off (since most test
  * datasets will actually be cached in memory)
  */
-public class GetFeatureNoCachingTest extends GetFeatureTest {
+public class GetFeatureCachingTest extends GetFeatureTest {
 
     @BeforeClass
-    public static void disableCaching() {
-        FeatureSizeFeatureCollection.setFeatureCacheLimit(0);
+    public static void enableCaching() {
+        Assert.assertEquals(0, FeatureSizeFeatureCollection.DEFAULT_CACHE_SIZE);
+        FeatureSizeFeatureCollection.setFeatureCacheLimit(16);
     }
 
     @AfterClass
-    public static void enableCaching() {
+    public static void disableCaching() {
         FeatureSizeFeatureCollection.setFeatureCacheLimit(
                 FeatureSizeFeatureCollection.DEFAULT_CACHE_SIZE);
     }

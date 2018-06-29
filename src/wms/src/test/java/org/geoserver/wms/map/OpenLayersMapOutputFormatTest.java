7071d510c60a4b505e5152096ade9657f57168ea
Fernando Miño <fernandomino7@gmail.com>
2018-06-29 14:52:16 -0500

    [GEOS-8805] Openlayers3 Freemarker template parameters fix

--- src/wms/src/test/java/org/geoserver/wms/map/OpenLayersMapOutputFormatTest.java
+++ src/wms/src/test/java/org/geoserver/wms/map/OpenLayersMapOutputFormatTest.java
@@ -94,6 +94,7 @@ public class OpenLayersMapOutputFormatTest extends WMSTestSupport {
 
         GetMapRequest request = createGetMapRequest(MockData.BASIC_POLYGONS);
         request.getRawKvp().put("</script><script>alert('x-scripted');</script><script>", "foo");
+        request.getRawKvp().put("25064;ALERT(1)//419", "1");
         final WMSMapContent map = new WMSMapContent();
         map.getViewport().setBounds(new ReferencedEnvelope(env, DefaultGeographicCRS.WGS84));
         map.setMapWidth(300);
@@ -115,6 +116,11 @@ public class OpenLayersMapOutputFormatTest extends WMSTestSupport {
                         .replace("\\r", "")
                         .indexOf(
                                 "\"</script\\><script\\>alert(\\'x-scripted\\');</script\\><script\\>\": 'foo'");
+        assertTrue(index > -1);
+        index =
+                htmlDoc.replace("\\n", "")
+                        .replace("\\r", "")
+                        .indexOf("\"25064;ALERT(1)//419\": '1'");
         assertTrue(index > -1);
     }
 
@@ -376,6 +382,15 @@ public class OpenLayersMapOutputFormatTest extends WMSTestSupport {
         return new String(bos.toByteArray(), "UTF-8");
     }
 
+    String getAsHTMLOL3(WMSMapContent map) throws IOException {
+        OpenLayers3MapOutputFormat mapProducer =
+                GeoServerExtensions.extensions(OpenLayers3MapOutputFormat.class).get(0);
+        RawMap rawMap = mapProducer.produceMap(map);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        rawMap.writeTo(bos);
+        return new String(bos.toByteArray(), "UTF-8");
+    }
+
     @Test
     public void testExceptionsInImage() throws Exception {
         // the base request
@@ -402,5 +417,52 @@ public class OpenLayersMapOutputFormatTest extends WMSTestSupport {
         String html = getAsString(path);
         assertThat(html, containsString("\"EXCEPTIONS\": 'application/vnd.ogc.se_xml'"));
         assertThat(html, not(containsString("\"exceptions\": 'application/vnd.ogc.se_inimage'")));
+    }
+
+    @Test
+    public void testXssOL3() throws Exception {
+
+        Catalog catalog = getCatalog();
+        final FeatureSource fs =
+                catalog.getFeatureTypeByName(
+                                MockData.BASIC_POLYGONS.getPrefix(),
+                                MockData.BASIC_POLYGONS.getLocalPart())
+                        .getFeatureSource(null, null);
+
+        final Envelope env = fs.getBounds();
+
+        LOGGER.info("about to create map ctx for BasicPolygons with bounds " + env);
+
+        GetMapRequest request = createGetMapRequest(MockData.BASIC_POLYGONS);
+        request.putHttpRequestHeader("USER-AGENT", "Firefox 40.1");
+        request.getRawKvp().put("</script><script>alert('x-scripted');</script><script>", "foo");
+        request.getRawKvp().put("25064;ALERT(1)//419", "1");
+        final WMSMapContent map = new WMSMapContent();
+        map.getViewport().setBounds(new ReferencedEnvelope(env, DefaultGeographicCRS.WGS84));
+        map.setMapWidth(300);
+        map.setMapHeight(300);
+        map.setBgColor(Color.red);
+        map.setTransparent(false);
+        map.setRequest(request);
+
+        StyleInfo styleByName = catalog.getStyleByName("Default");
+        Style basicStyle = styleByName.getStyle();
+        FeatureLayer layer = new FeatureLayer(fs, basicStyle);
+        layer.setTitle("Title");
+        map.addLayer(layer);
+        request.setFormat("application/openlayers3");
+        String htmlDoc = getAsHTMLOL3(map);
+        // check that weird param is correctly encoded to avoid js code execution
+        int index =
+                htmlDoc.replace("\\n", "")
+                        .replace("\\r", "")
+                        .indexOf(
+                                "\"</script\\><script\\>alert(\\'x-scripted\\');</script\\><script\\>\": 'foo'");
+        assertTrue(index > -1);
+        index =
+                htmlDoc.replace("\\n", "")
+                        .replace("\\r", "")
+                        .indexOf("\"25064;ALERT(1)//419\": '1'");
+        assertTrue(index > -1);
     }
 }

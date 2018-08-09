1413889eaa25fa37a9e84a48685345247b99b7a8
Joseph Miller <jmiller@boundlessgeo.com>
2018-08-10 00:02:22 +0800

    Crud delete (#9)
    * first pass at deleteFeatures controller
    
    * Switched to a featuretypeinfo method consistent with what the DAO does
    
    * Added addFeatures and unit test
    
    * Added feature delete unit test and fixed issue where the featureid was not a long
    
    * added delete by geometry query unit test
    
    * Added where clause test for the delete
    
    * Started on feature add error generation unit test
    
    * Added some documentation
    
    * Addressed pull request comments about objectid conversion and error handling

--- src/main/java/com/boundlessgeo/gsr/api/feature/FeatureLayerController.java
+++ src/main/java/com/boundlessgeo/gsr/api/feature/FeatureLayerController.java
@@ -1,25 +1,37 @@
 package com.boundlessgeo.gsr.api.feature;
 
 import java.io.IOException;
+
+
+
+import com.boundlessgeo.gsr.model.feature.*;
+
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+import com.boundlessgeo.gsr.translate.feature.FeatureDAO;
+import com.boundlessgeo.gsr.translate.feature.FeatureEncoder;
 
 import com.boundlessgeo.gsr.model.feature.EditResults;
 import com.boundlessgeo.gsr.model.feature.Feature;
 import com.boundlessgeo.gsr.model.feature.FeatureArray;
-import com.boundlessgeo.gsr.translate.feature.FeatureDAO;
+
 import com.boundlessgeo.gsr.translate.map.LayerDAO;
 import org.geoserver.catalog.FeatureTypeInfo;
 import org.geoserver.catalog.LayerInfo;
 import org.geoserver.config.GeoServer;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import com.boundlessgeo.gsr.api.AbstractGSRController;
-import com.boundlessgeo.gsr.model.feature.FeatureLayer;
 import com.boundlessgeo.gsr.model.map.LayerOrTable;
 
 /**
@@ -51,7 +63,57 @@ public class FeatureLayerController extends AbstractGSRController {
     }
 
     /**
-     * Update Feature enpoint
+     *
+     * @param workspaceName Workspace name
+     * @param layerId Layer id
+     * @param objectIdsText A comma delimited list of object ids of features to delete.  If present all other query parameters are ignored.
+     * @param geometryTypeName If using a geometry to select delete features, specifies the input geometry type.  Values:
+     *      *                    esriGeometryPoint | esriGeometryMultipoint | esriGeometryPolyline | esriGeometryPolygon |
+     *      *                    esriGeometryEnvelope
+     * @param whereClause If not using a list of object ids or a geometry query use this to pass in an attribute query.  Eg POP2000 > 350000
+     * @param geometryText A geometry representing a spatial filter to filter the features by.  See https://developers.arcgis.com/documentation/common-data-types/geometry-objects.htm
+     * @param inSRText The spatial reference of the input geometry. If the inSR is not specified, the geometry is
+     *      *                   assumed to be in the spatial reference of the map.
+     * @param spatialRelText The spatial relationship to be applied on the input geometry while performing the query.
+     *      *                   Values: esriSpatialRelIntersects | esriSpatialRelContains | esriSpatialRelCrosses |
+     *      *                   esriSpatialRelEnvelopeIntersects | esriSpatialRelIndexIntersects | esriSpatialRelOverlaps |
+     *      *                   esriSpatialRelTouches | esriSpatialRelWithin
+     * @return
+     */
+    @ResponseBody
+    @PostMapping(path = "/{layerId}/deleteFeatures")
+    public EditResults featureDelete(@PathVariable String workspaceName, @PathVariable Integer layerId,
+                                     @RequestParam(name = "objectIds", required = false) String objectIdsText,
+                                     @RequestParam(name = "geometryType", required = false) String geometryTypeName,
+                                     @RequestParam(name = "where", required = false) String whereClause,
+                                     @RequestParam(name = "geometry", required = false) String geometryText,
+                                     @RequestParam(name = "inSR", required = false) String inSRText,
+                                     @RequestParam(name = "spatialRel", required = false) String
+                                                 spatialRelText) throws IOException {
+        LayerOrTable entry;
+        List<EditResult> editResults;
+        Long id;
+
+        entry = LayerDAO.find(catalog, workspaceName, layerId);
+        if (entry == null) {
+            throw new NoSuchElementException("No table or layer in workspace \"" + workspaceName + "\" for id " + layerId);
+        }
+        LayerInfo l = entry.layer;
+        FeatureCollection features = FeatureDAO.getFeatureCollectionForLayer(workspaceName,
+                entry.getId(), geometryTypeName, geometryText, inSRText, null, spatialRelText,
+                objectIdsText, null, null, null, null, whereClause, false,
+                null, l);
+        long[] ids = FeatureEncoder.objectIds(features).getObjectIds();
+        List<Long> idsList = Arrays.stream(ids).boxed().collect(Collectors.toList());
+        FeatureTypeInfo featureTypeInfo = (FeatureTypeInfo) l.getResource();
+        editResults = FeatureDAO.deleteFeatures(featureTypeInfo, idsList);
+
+
+        return new EditResults(null, null, editResults);
+    }
+
+    /**
+     * Update Feature endpoint
      * 
      * @param featureArray Array of features to update
      * @param workspaceName Workspace name
@@ -67,6 +129,7 @@ public class FeatureLayerController extends AbstractGSRController {
      * @return Results of the update
      * @throws IOException
      */
+
     @PostMapping(path = "/{layerId}/updateFeatures")
     public EditResults updateFeatures(@RequestBody FeatureArray featureArray, @PathVariable String workspaceName, @PathVariable Integer layerId) throws IOException {
         List<Feature> features = featureArray == null ? null : featureArray.features;
@@ -83,5 +146,38 @@ public class FeatureLayerController extends AbstractGSRController {
         } else {
             throw new IllegalArgumentException("Layer is not a feature layer");
         }
+
+
+    }
+
+    /**
+     *
+     * @param featureArray Array of features to update
+     * @param workspaceName Workspace name
+     * @param layerId layer id
+     * @return Results of the update
+     * @throws IOException
+     */
+    @PostMapping(path = "/{layerId}/addFeatures")
+    public EditResults addFeatures(@RequestBody FeatureArray featureArray,
+                                   @PathVariable String workspaceName,
+                                   @PathVariable Integer layerId) throws IOException {
+
+        List<Feature> features = featureArray == null ? null : featureArray.features;
+        if (features == null || features.size() < 1) {
+            throw new IllegalArgumentException("No features provided");
+        }
+
+        LayerInfo layer = featureGet(workspaceName, layerId).layer;
+
+        if (layer.getResource() instanceof FeatureTypeInfo) {
+            FeatureTypeInfo fti = (FeatureTypeInfo) layer.getResource();
+
+            return new EditResults(FeatureDAO.createFeatures(fti, features),null, null);
+        } else {
+            throw new IllegalArgumentException("Layer is not a feature layer");
+        }
+
+
     }
 }
--- src/test/java/com/boundlessgeo/gsr/controller/feature/FeatureLayerControllerTest.java
+++ src/test/java/com/boundlessgeo/gsr/controller/feature/FeatureLayerControllerTest.java
@@ -362,4 +362,215 @@ public class FeatureLayerControllerTest extends ControllerTest {
         assertEquals(2, nativeGeom.getNumGeometries());
         assertEquals(nativeGeom, updatedGeom);
     }
+
+    @Test
+    public void testAddFeatures() throws Exception {
+        //Use 'cgf' workspace - 0 Lines, 1 MLines, 2 MPoints, 3 MPolygons, 4 Points, 5 Polygons,
+        Catalog catalog = getCatalog();
+        FeatureTypeInfo fti = catalog.getFeatureTypeByName("cgf","Lines");
+
+        //verify initial feature state
+        assertEquals(1, fti.getFeatureSource(null, null).getFeatures().size());
+        FeatureIterator iterator = fti.getFeatureSource(null, null).getFeatures().features();
+        org.opengis.feature.Feature nativeFeature = iterator.next();
+        //geom should be valid
+        LineString nativeGeom = (LineString) nativeFeature.getDefaultGeometryProperty().getValue();
+        assertTrue(Math.abs(nativeGeom.getGeometryN(0).getCoordinates()[0].x - 500050.0) >= 0.1);
+        assertTrue(Math.abs(nativeGeom.getGeometryN(0).getCoordinates()[0].y - 499950.0) >= 0.1);
+
+        String q = query("cgf", "0", "/addFeatures?f=json");
+
+        String body =
+                "[\n" +
+                        "  {\n" +
+                        "  \"geometry\" : {" +
+                        "      \"geometryType\":\"esriGeometryPolyline\", " +
+                        "      \"paths\" : [[[500050.0, 499950.0],[500150.0, 500050.0]]], " +
+                        "      \"spatialReference\" : {\"wkid\" : 32615}" +
+                        "    },\n" +
+                        "    \"attributes\" : {\n" +
+                        "      \"id\" : \"t0002\",\n" +
+                        "    }\n" +
+                        "  }\n" +
+                        "]";
+        JSON result = postAsJSON(q, body, "application/json");
+        assertTrue(String.valueOf(result) + " is a JSON object", result instanceof JSONObject);
+
+        JSONObject json = (JSONObject) result;
+
+        JSONObject resultObj = json.getJSONArray("addResults").getJSONObject(0);
+        assertNotSame( 0, resultObj.getInt("objectId"));
+        assertEquals( true, resultObj.getBoolean("success"));
+
+        //verify feature was added
+        assertEquals(2, fti.getFeatureSource(null, null).getFeatures().size());
+
+        iterator = fti.getFeatureSource(null, null).getFeatures().features();
+        nativeFeature = iterator.next();
+        org.opengis.feature.Feature nativeFeature2 = iterator.next();
+        //geom should be valid
+        nativeGeom = (LineString) nativeFeature2.getDefaultGeometryProperty().getValue();
+        assertEquals(nativeGeom.getGeometryN(0).getCoordinates()[0].x, 500050.0, 0.1);
+        assertEquals(nativeGeom.getGeometryN(0).getCoordinates()[0].y, 499950.0, 0.1);
+    }
+
+    @Test
+    public void testAddFeaturesErrors() throws Exception {
+        String q = query("cgf", "0", "/addFeatures?f=json");
+
+        //malformed geometry
+        String body =
+                "[\n" +
+                        "  {\n" +
+                        "  \"geometry\" : {},\n" +
+                        "    \"attributes\" : {\n" +
+                        "      \"objectid\" : 0,\n" +
+                        "      \"id\" : \"t0001\",\n" +
+                        "    }\n" +
+                        "  }\n" +
+                        "]";
+        JSON result = postAsJSON(q, body, "application/json");
+
+        assertTrue(String.valueOf(result) + " is a JSON object", result instanceof JSONObject);
+
+        JSONObject json = (JSONObject) result;
+
+        JSONObject resultObj = json.getJSONArray("addResults").getJSONObject(0);
+        assertEquals( false, resultObj.getBoolean("success"));
+        assertEquals(1000, resultObj.getJSONObject("error").getInt("code"));
+
+        //missing attribute
+        body =
+                "[\n" +
+                        "  {\n" +
+                        "  \"geometry\" : {" +
+                        "      \"geometryType\":\"esriGeometryPolyline\", " +
+                        "      \"paths\" : [[[500050.0, 499950.0],[500150.0, 500050.0]]], " +
+                        "      \"spatialReference\" : {\"wkid\" : 32615}" +
+                        "    },\n" +
+                        "    \"attributes\" : {\n" +
+                        "      \"objectid\" : 0\n" +
+                        "    }\n" +
+                        "  }\n" +
+                        "]";
+        result = postAsJSON(q, body, "application/json");
+        assertTrue(String.valueOf(result) + " is a JSON object", result instanceof JSONObject);
+
+        json = (JSONObject) result;
+
+        resultObj = json.getJSONArray("addResults").getJSONObject(0);
+        assertEquals( false, resultObj.getBoolean("success"));
+        assertEquals(1000, resultObj.getJSONObject("error").getInt("code"));
+
+        //duplicate objectid, should lead to a new feature with a new objectid
+        body =
+                "[\n" +
+                        "  {\n" +
+                        "  \"geometry\" : {" +
+                        "      \"geometryType\":\"esriGeometryPolyline\", " +
+                        "      \"paths\" : [[[500050.0, 499950.0],[500150.0, 500050.0]]], " +
+                        "      \"spatialReference\" : {\"wkid\" : 32615}" +
+                        "    },\n" +
+                        "    \"attributes\" : {\n" +
+                        "      \"objectid\" : 0,\n" +
+                        "      \"id\" : \"t0001\",\n" +
+                        "    }\n" +
+                        "  }\n" +
+                        "]";
+        result = postAsJSON(q, body, "application/json");
+        assertTrue(String.valueOf(result) + " is a JSON object", result instanceof JSONObject);
+
+        json = (JSONObject) result;
+
+        resultObj = json.getJSONArray("addResults").getJSONObject(0);
+        assertEquals( true, resultObj.getBoolean("success"));
+        assertNotSame(0,resultObj.get("objectId"));
+    }
+
+    @Test
+    public void testDeleteFeaturesByObjectId() throws Exception {
+        //Use 'cgf' workspace - 0 Lines, 1 MLines, 2 MPoints, 3 MPolygons, 4 Points, 5 Polygons,
+        Catalog catalog = getCatalog();
+        FeatureTypeInfo fti = catalog.getFeatureTypeByName("cgf","Lines");
+
+        //verify initial feature state
+        assertEquals(1, fti.getFeatureSource(null, null).getFeatures().size());
+
+        String q = query("cgf", "0", "/deleteFeatures?f=json&objectIds=0");
+
+        String body = "";
+
+        JSON result = postAsJSON(q, body, "application/json");
+        assertTrue(String.valueOf(result) + " is a JSON object", result instanceof JSONObject);
+
+        JSONObject json = (JSONObject) result;
+
+        JSONObject resultObj = json.getJSONArray("deleteResults").getJSONObject(0);
+        assertEquals( true, resultObj.getBoolean("success"));
+
+        //verify feature was delete
+        assertEquals(0, fti.getFeatureSource(null, null).getFeatures().size());
+
+
+    }
+
+    @Test
+    public void testDeleteFeaturesByGeometryQuery() throws Exception {
+        //Use 'cgf' workspace - 0 Lines, 1 MLines, 2 MPoints, 3 MPolygons, 4 Points, 5 Polygons,
+        Catalog catalog = getCatalog();
+        FeatureTypeInfo fti = catalog.getFeatureTypeByName("cgf","Lines");
+
+        //verify initial feature state
+        assertEquals(1, fti.getFeatureSource(null, null).getFeatures().size());
+
+        String q = query("cgf", "0", "/deleteFeatures?f=json" +
+                "&geometry=500124,500024,500176,500076" +
+                "&geometryType=esriGeometryEnvelope" +
+                "&inSR=32615" +
+                "&outSR=32615" +
+                "&spatialRel=esriSpatialRelEnvelopeIntersects");
+
+        String body = "";
+
+        JSON result = postAsJSON(q, body, "application/json");
+        assertTrue(String.valueOf(result) + " is a JSON object", result instanceof JSONObject);
+
+        JSONObject json = (JSONObject) result;
+
+        JSONObject resultObj = json.getJSONArray("deleteResults").getJSONObject(0);
+        assertEquals( true, resultObj.getBoolean("success"));
+
+        //verify feature was delete
+        assertEquals(0, fti.getFeatureSource(null, null).getFeatures().size());
+
+
+    }
+
+    @Test
+    public void testDeleteFeaturesByWhereQuery() throws Exception {
+        //Use 'cgf' workspace - 0 Lines, 1 MLines, 2 MPoints, 3 MPolygons, 4 Points, 5 Polygons,
+        Catalog catalog = getCatalog();
+        FeatureTypeInfo fti = catalog.getFeatureTypeByName("cgf","Lines");
+
+        //verify initial feature state
+        assertEquals(1, fti.getFeatureSource(null, null).getFeatures().size());
+
+        String q = query("cgf", "0", "/deleteFeatures?f=json" +
+                "&where=id in ('Lines.0')" );
+
+        String body = "";
+
+        JSON result = postAsJSON(q, body, "application/json");
+        assertTrue(String.valueOf(result) + " is a JSON object", result instanceof JSONObject);
+
+        JSONObject json = (JSONObject) result;
+
+        JSONObject resultObj = json.getJSONArray("deleteResults").getJSONObject(0);
+        assertEquals( true, resultObj.getBoolean("success"));
+
+        //verify feature was delete
+        assertEquals(0, fti.getFeatureSource(null, null).getFeatures().size());
+
+
+    }
 }

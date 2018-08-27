a9e12fc7a0f2ffdaef26a5202166b67c01307675
Jody Garnett <jody.garnett@gmail.com>
2018-08-27 12:13:09 +0300

    fix hello service repo reference

--- doc/en/developer/source/programming-guide/ows-services/hello/src/main/java/HelloWorld.java
+++ doc/en/developer/source/programming-guide/ows-services/hello/src/main/java/HelloWorld.java
@@ -3,16 +3,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloWorld
-{
+public class HelloWorld {
 
-    public HelloWorld()
-    {
+    public HelloWorld() {
         // Do nothing
     }
 
-    public void sayHello(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-        response.getOutputStream().write( "Hello World".getBytes() );
+    public void sayHello(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.getOutputStream().write("Hello World".getBytes());
     }
 }

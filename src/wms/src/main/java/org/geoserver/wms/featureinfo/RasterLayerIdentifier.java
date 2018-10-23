127afe98b9b61f74cd7f1364236524dac83736ee
Devon Tucker <dtucker@boundlessgeo.com>
2018-10-23 15:56:28 -0700

    Remove internal XMLChar reference, use xalan version instead.

--- src/wms/src/main/java/org/geoserver/wms/featureinfo/RasterLayerIdentifier.java
+++ src/wms/src/main/java/org/geoserver/wms/featureinfo/RasterLayerIdentifier.java
@@ -5,7 +5,6 @@
  */
 package org.geoserver.wms.featureinfo;
 
-import com.sun.org.apache.xml.internal.utils.XMLChar;
 import java.awt.Rectangle;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.RenderedImage;
@@ -17,7 +16,10 @@ import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
+
 import javax.media.jai.PlanarImage;
+
+import org.apache.xml.utils.XMLChar;
 import org.geoserver.catalog.CoverageInfo;
 import org.geoserver.catalog.ProjectionPolicy;
 import org.geoserver.wms.FeatureInfoRequestParameters;

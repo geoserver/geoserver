c95b86ad6a9773de6acecf43ff55328b82269bb4
Fernando Mino <fernandomino7@gmail.com>
2018-10-31 14:42:56 +0100

    Fix for LDAP testing

--- src/security/ldap/src/test/java/org/geoserver/security/ldap/LDAPRoleServiceTest.java
+++ src/security/ldap/src/test/java/org/geoserver/security/ldap/LDAPRoleServiceTest.java
@@ -12,13 +12,20 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.SortedSet;
+import org.apache.directory.server.annotations.CreateLdapServer;
+import org.apache.directory.server.annotations.CreateTransport;
+import org.apache.directory.server.core.annotations.ApplyLdifFiles;
+import org.apache.directory.server.core.annotations.CreateDS;
+import org.apache.directory.server.core.annotations.CreatePartition;
+import org.apache.directory.server.core.integ.FrameworkRunner;
 import org.geoserver.security.GeoServerRoleService;
 import org.geoserver.security.impl.GeoServerRole;
-import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 public class LDAPRoleServiceTest extends LDAPBaseTest {
+
     GeoServerRoleService service;
 
     public void createRoleService(boolean userFilter) throws IOException {
@@ -37,112 +44,7 @@ public class LDAPRoleServiceTest extends LDAPBaseTest {
         super.setUp();
     }
 
-    @Test
-    public void testGetRoles() throws Exception {
-        Assume.assumeTrue(LDAPTestUtils.initLdapServer(true, ldapServerUrl, basePath));
-
-        checkAllRoles();
-    }
-
-    @Test
-    public void testGetRolesAuthenticated() throws Exception {
-        Assume.assumeTrue(LDAPTestUtils.initLdapServer(false, ldapServerUrl, basePath));
-        configureAuthentication();
-        checkAllRoles();
-    }
-
-    @Test
-    public void testGetRolesCount() throws Exception {
-        Assume.assumeTrue(LDAPTestUtils.initLdapServer(true, ldapServerUrl, basePath));
-
-        checkRoleCount();
-    }
-
-    @Test
-    public void testGetRolesCountAuthenticated() throws Exception {
-        Assume.assumeTrue(LDAPTestUtils.initLdapServer(true, ldapServerUrl, basePath));
-        configureAuthentication();
-        checkRoleCount();
-    }
-
-    @Test
-    public void testGetRoleByName() throws Exception {
-        Assume.assumeTrue(LDAPTestUtils.initLdapServer(true, ldapServerUrl, basePath));
-
-        checkRoleByName();
-    }
-
-    @Test
-    public void testGetRoleByNameAuthenticated() throws Exception {
-        Assume.assumeTrue(LDAPTestUtils.initLdapServer(false, ldapServerUrl, basePath));
-        configureAuthentication();
-        checkRoleByName();
-    }
-
-    @Test
-    public void testGetAdminRoles() throws Exception {
-        Assume.assumeTrue(LDAPTestUtils.initLdapServer(true, ldapServerUrl, basePath));
-
-        checkAdminRoles();
-    }
-
-    @Test
-    public void testGetAdminRolesAuthenticated() throws Exception {
-        Assume.assumeTrue(LDAPTestUtils.initLdapServer(false, ldapServerUrl, basePath));
-        configureAuthentication();
-        checkAdminRoles();
-    }
-
-    @Test
-    public void testGetRolesForUser() throws Exception {
-        Assume.assumeTrue(LDAPTestUtils.initLdapServer(true, ldapServerUrl, basePath));
-
-        checkUserRoles("admin", false);
-    }
-
-    @Test
-    public void testGetRolesForUserAuthenticated() throws Exception {
-        Assume.assumeTrue(LDAPTestUtils.initLdapServer(false, ldapServerUrl, basePath));
-
-        configureAuthentication();
-        checkUserRoles("admin", false);
-    }
-
-    @Test
-    public void testGetRolesForUserUsingUserFilter() throws Exception {
-        Assume.assumeTrue(
-                LDAPTestUtils.initLdapServer(true, ldapServerUrl, basePath, "data2.ldif"));
-
-        checkUserRoles("admin", true);
-    }
-
-    @Test
-    public void testGetRolesForUserAuthenticatedUsingUserFilter() throws Exception {
-        Assume.assumeTrue(
-                LDAPTestUtils.initLdapServer(false, ldapServerUrl, basePath, "data2.ldif"));
-
-        configureAuthentication();
-        checkUserRoles("admin", true);
-    }
-
-    @Test
-    public void testGetUserNamesForRole() throws Exception {
-        Assume.assumeTrue(LDAPTestUtils.initLdapServer(true, ldapServerUrl, basePath));
-
-        checkUserNamesForRole("admin", 1, false);
-        checkUserNamesForRole("other", 2, false);
-    }
-
-    @Test
-    public void testGetUserNamesForRoleUsingUserFilter() throws Exception {
-        Assume.assumeTrue(
-                LDAPTestUtils.initLdapServer(true, ldapServerUrl, basePath, "data2.ldif"));
-
-        checkUserNamesForRole("admin", 1, true);
-        checkUserNamesForRole("other", 2, true);
-    }
-
-    private void configureAuthentication() {
+    protected void configureAuthentication() {
         ((LDAPRoleServiceConfig) config)
                 .setUser(
                         "uid=admin,ou=People,dc=example,dc=com"); // ("uid=admin,ou=People,dc=example,dc=com");
@@ -150,7 +52,7 @@ public class LDAPRoleServiceTest extends LDAPBaseTest {
         config.setBindBeforeGroupSearch(true);
     }
 
-    private void checkAdminRoles() throws IOException {
+    protected void checkAdminRoles() throws IOException {
         config.setAdminGroup("admin");
         config.setGroupAdminGroup("other");
         createRoleService(false);
@@ -166,7 +68,7 @@ public class LDAPRoleServiceTest extends LDAPBaseTest {
         assertNull(service.getGroupAdminRole());
     }
 
-    private void checkUserNamesForRole(String roleName, int expected, boolean userFilter)
+    protected void checkUserNamesForRole(String roleName, int expected, boolean userFilter)
             throws IOException {
         createRoleService(userFilter);
 
@@ -175,20 +77,20 @@ public class LDAPRoleServiceTest extends LDAPBaseTest {
         assertEquals(expected, userNames.size());
     }
 
-    private void checkRoleByName() throws IOException {
+    protected void checkRoleByName() throws IOException {
         createRoleService(false);
 
         assertNotNull(service.getRoleByName("admin"));
         assertNull(service.getRoleByName("dummy"));
     }
 
-    private void checkRoleCount() throws IOException {
+    protected void checkRoleCount() throws IOException {
         createRoleService(false);
 
         assertTrue(service.getRoleCount() > 0);
     }
 
-    private void checkAllRoles() throws IOException {
+    protected void checkAllRoles() throws IOException {
         createRoleService(false);
 
         SortedSet<GeoServerRole> roles = service.getRoles();
@@ -199,7 +101,7 @@ public class LDAPRoleServiceTest extends LDAPBaseTest {
         assertEquals(role.toString().toUpperCase(), role.toString());
     }
 
-    private void checkUserRoles(String username, boolean userFilter) throws IOException {
+    protected void checkUserRoles(String username, boolean userFilter) throws IOException {
         createRoleService(userFilter);
         SortedSet<GeoServerRole> allRoles = service.getRoles();
         SortedSet<GeoServerRole> roles = service.getRolesForUser(username);
@@ -214,5 +116,143 @@ public class LDAPRoleServiceTest extends LDAPBaseTest {
     @Override
     protected void createConfig() {
         config = new LDAPRoleServiceConfig();
+    }
+
+    @RunWith(FrameworkRunner.class)
+    @CreateLdapServer(
+        transports = {
+            @CreateTransport(
+                protocol = "LDAP",
+                address = "localhost",
+                port = LDAPTestUtils.LDAP_SERVER_PORT
+            )
+        },
+        allowAnonymousAccess = true
+    )
+    @CreateDS(
+        name = "myDS",
+        partitions = {@CreatePartition(name = "test", suffix = LDAPTestUtils.LDAP_BASE_PATH)}
+    )
+    @ApplyLdifFiles({"data.ldif"})
+    public static class LDAPRoleServiceLdiffTest extends LDAPRoleServiceTest {
+
+        @Test
+        public void testGetRoles() throws Exception {
+            getService().setAllowAnonymousAccess(true);
+
+            checkAllRoles();
+        }
+
+        @Test
+        public void testGetRolesAuthenticated() throws Exception {
+            getService().setAllowAnonymousAccess(false);
+            configureAuthentication();
+            checkAllRoles();
+        }
+
+        @Test
+        public void testGetRolesCount() throws Exception {
+            getService().setAllowAnonymousAccess(true);
+
+            checkRoleCount();
+        }
+
+        @Test
+        public void testGetRolesCountAuthenticated() throws Exception {
+            getService().setAllowAnonymousAccess(true);
+            configureAuthentication();
+            checkRoleCount();
+        }
+
+        @Test
+        public void testGetRoleByName() throws Exception {
+            getService().setAllowAnonymousAccess(true);
+
+            checkRoleByName();
+        }
+
+        @Test
+        public void testGetRoleByNameAuthenticated() throws Exception {
+            getService().setAllowAnonymousAccess(false);
+            configureAuthentication();
+            checkRoleByName();
+        }
+
+        @Test
+        public void testGetAdminRoles() throws Exception {
+            getService().setAllowAnonymousAccess(true);
+
+            checkAdminRoles();
+        }
+
+        @Test
+        public void testGetAdminRolesAuthenticated() throws Exception {
+            getService().setAllowAnonymousAccess(false);
+            configureAuthentication();
+            checkAdminRoles();
+        }
+
+        @Test
+        public void testGetRolesForUser() throws Exception {
+            getService().setAllowAnonymousAccess(true);
+
+            checkUserRoles("admin", false);
+        }
+
+        @Test
+        public void testGetRolesForUserAuthenticated() throws Exception {
+            getService().setAllowAnonymousAccess(false);
+
+            configureAuthentication();
+            checkUserRoles("admin", false);
+        }
+
+        @Test
+        public void testGetUserNamesForRole() throws Exception {
+            getService().setAllowAnonymousAccess(true);
+
+            checkUserNamesForRole("admin", 1, false);
+            checkUserNamesForRole("other", 2, false);
+        }
+    }
+
+    @RunWith(FrameworkRunner.class)
+    @CreateLdapServer(
+        transports = {
+            @CreateTransport(
+                protocol = "LDAP",
+                address = "localhost",
+                port = LDAPTestUtils.LDAP_SERVER_PORT
+            )
+        },
+        allowAnonymousAccess = true
+    )
+    @CreateDS(
+        name = "myDS",
+        partitions = {@CreatePartition(name = "test", suffix = LDAPTestUtils.LDAP_BASE_PATH)}
+    )
+    @ApplyLdifFiles({"data2.ldif"})
+    public static class LDAPRoleServiceLdiff2Test extends LDAPRoleServiceTest {
+
+        @Test
+        public void testGetRolesForUserUsingUserFilter() throws Exception {
+
+            checkUserRoles("admin", true);
+        }
+
+        @Test
+        public void testGetRolesForUserAuthenticatedUsingUserFilter() throws Exception {
+            getService().setAllowAnonymousAccess(false);
+
+            configureAuthentication();
+            checkUserRoles("admin", true);
+        }
+
+        @Test
+        public void testGetUserNamesForRoleUsingUserFilter() throws Exception {
+            getService().setAllowAnonymousAccess(true);
+            checkUserNamesForRole("admin", 1, true);
+            checkUserNamesForRole("other", 2, true);
+        }
     }
 }

76e3bea94e6f30ecd2a3b982faf6543e277986e5
Fernando <fernandomino7@gmail.com>
2018-12-04 08:09:31 -0500

    Service configuration - Layer Group preview fix

--- src/web/demo/src/test/java/org/geoserver/web/demo/MapPreviewPageTest.java
+++ src/web/demo/src/test/java/org/geoserver/web/demo/MapPreviewPageTest.java
@@ -246,4 +246,20 @@ public class MapPreviewPageTest extends GeoServerWicketTestSupport {
             catalog.save(ft);
         }
     }
+
+    /** Test for layer group service support check */
+    @Test
+    public void testHasServiceSupport() throws Exception {
+        Catalog cat = getCatalog();
+        LayerGroupInfo lg = cat.getFactory().createLayerGroup();
+        lg.setName("linkgroup");
+        lg.setWorkspace(cat.getWorkspaceByName("sf"));
+        lg.getLayers().add(cat.getLayerByName(getLayerId(MockData.PRIMITIVEGEOFEATURE)));
+        new CatalogBuilder(cat).calculateLayerGroupBounds(lg);
+        cat.add(lg);
+        tester.startPage(MapPreviewPage.class);
+        tester.assertRenderedPage(MapPreviewPage.class);
+        MapPreviewPage page = (MapPreviewPage) tester.getLastRenderedPage();
+        assertTrue(page.hasServiceSupport("sf:linkgroup", "WMS"));
+    }
 }

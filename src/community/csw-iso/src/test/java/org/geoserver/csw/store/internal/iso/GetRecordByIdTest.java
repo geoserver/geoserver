6ccbc6db8ccd5372a524cf6861b98b60da116cc9
Niels Charlier <niels@scitus.be>
2019-03-02 16:37:22 +0100

    GEOS-9130 csw:support gmx tags

--- src/community/csw-iso/src/test/java/org/geoserver/csw/store/internal/iso/GetRecordByIdTest.java
+++ src/community/csw-iso/src/test/java/org/geoserver/csw/store/internal/iso/GetRecordByIdTest.java
@@ -45,6 +45,10 @@ public class GetRecordByIdTest extends MDTestSupport {
                 "//gmd:MD_Metadata[gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:title/gco:CharacterString='Forests']/gmd:dateStamp/gco:Date/@xsi:nil",
                 d);
         assertXpathEvaluatesTo(
+                "anchor",
+                "//gmd:MD_Metadata[gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:title/gco:CharacterString='Forests']/gmd:identificationInfo/gmd:MD_DataIdentification/gmd:purpose/gmx:Anchor",
+                d);
+        assertXpathEvaluatesTo(
                 "http://purl.org/dc/dcmitype/Dataset",
                 "//gmd:MD_Metadata[gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:title/gco:CharacterString='Forests']/gmd:hierarchyLevel/gmd:MD_ScopeCode/@codeListValue",
                 d);

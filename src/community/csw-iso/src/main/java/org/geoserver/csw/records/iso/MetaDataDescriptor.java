0ebf4f13a6a448ed854d7cab5e47d2d6c95e71a0
Niels Charlier <niels@scitus.be>
2019-03-06 20:17:56 +0100

    GEOS-9130 support xlink (2)

--- src/community/csw-iso/src/main/java/org/geoserver/csw/records/iso/MetaDataDescriptor.java
+++ src/community/csw-iso/src/main/java/org/geoserver/csw/records/iso/MetaDataDescriptor.java
@@ -52,6 +52,8 @@ public class MetaDataDescriptor extends AbstractRecordDescriptor {
     public static final String NAMESPACE_GMD = "http://www.isotc211.org/2005/gmd";
     public static final String NAMESPACE_APISO = "http://www.opengis.net/cat/csw/apiso/1.0";
 
+    public static final String NAMESPACE_XLINK = "http://www.w3.org/1999/xlink";
+
     public static FeatureType METADATA_TYPE;
     public static AttributeDescriptor METADATA_DESCRIPTOR;
 
@@ -74,6 +76,7 @@ public class MetaDataDescriptor extends AbstractRecordDescriptor {
         NAMESPACES.declarePrefix("gco", NAMESPACE_GCO);
         NAMESPACES.declarePrefix("gmd", NAMESPACE_GMD);
         NAMESPACES.declarePrefix("gmx", NAMESPACE_GMX);
+        NAMESPACES.declarePrefix("xlink", NAMESPACE_XLINK);
         NAMESPACES.declarePrefix("gfc", NAMESPACE_GFC);
 
         FeatureTypeFactory typeFactory = new FeatureTypeFactoryImpl();

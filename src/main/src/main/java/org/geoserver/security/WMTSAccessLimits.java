f0a194410e2d252b17b81e95e29d3d2e31b150d1
Andrea Aime <andrea.aime@gmail.com>
2019-04-25 09:47:42 +0200

    Dude, this is the two arguments constructor....

--- src/main/src/main/java/org/geoserver/security/WMTSAccessLimits.java
+++ src/main/src/main/java/org/geoserver/security/WMTSAccessLimits.java
@@ -23,7 +23,6 @@ public class WMTSAccessLimits extends DataAccessLimits {
      *
      * @param rasterFilter Used as a ROI on the returned data
      * @param readFilter generic filtering
-     * @deprecated use the 2 argument constructor
      */
     public WMTSAccessLimits(CatalogMode mode, Filter readFilter, MultiPolygon rasterFilter) {
         super(mode, readFilter);

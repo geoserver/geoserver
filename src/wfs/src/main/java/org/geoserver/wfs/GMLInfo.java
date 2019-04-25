203d4b76cfd65e34380adad416a6863a03edd1e8
Andrea Aime <andrea.aime@gmail.com>
2019-04-25 15:12:02 +0200

    Goodbye TransactionPlugin

--- src/wfs/src/main/java/org/geoserver/wfs/GMLInfo.java
+++ src/wfs/src/main/java/org/geoserver/wfs/GMLInfo.java
@@ -27,9 +27,7 @@ public interface GMLInfo extends Serializable {
      *   <li>{@link #URN} : urn:x-ogc:def:crs:EPSG:XXXX
      * </ul>
      *
-     * <p>
-     *
-     * Use {@link SrsSyntax} whenever possible
+     * <p>Use {@link SrsSyntax} whenever possible
      */
     public static enum SrsNameStyle {
         NORMAL {

09625eae96dd5c84206baf6f9d5eb296504e0434
Niels Charlier <niels@scitus.be>
2019-09-09 16:56:01 +0200

    GEOS-9107 CSW iso profile: repeating tags

--- src/extension/csw/api/src/main/java/org/geoserver/csw/records/GenericRecordBuilder.java
+++ src/extension/csw/api/src/main/java/org/geoserver/csw/records/GenericRecordBuilder.java
@@ -12,6 +12,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.geotools.data.complex.util.ComplexFeatureConstants;
 import org.geotools.feature.AttributeBuilder;
 import org.geotools.feature.ComplexFeatureBuilder;
@@ -42,6 +44,8 @@ public class GenericRecordBuilder implements RecordBuilder {
      * A user property of the boundingBox attribute containing the original envelopes of the Record.
      */
     public static final String ORIGINAL_BBOXES = "RecordOriginalBounds";
+
+    private static final Pattern PATTERN_ATT_WITH_INDEX = Pattern.compile("([^\\[]*)\\[(.*)\\]");
 
     protected ComplexFeatureBuilder fb;
     protected List<ReferencedEnvelope> boxes = new ArrayList<ReferencedEnvelope>();
@@ -160,7 +164,7 @@ public class GenericRecordBuilder implements RecordBuilder {
             int[] splitIndex) {
 
         AttributeDescriptor descriptor =
-                (AttributeDescriptor) Types.findDescriptor(type, path[index]);
+                (AttributeDescriptor) Types.findDescriptor(type, attName(path[index]));
 
         if (descriptor == null) {
             throw new IllegalArgumentException(
@@ -289,6 +293,15 @@ public class GenericRecordBuilder implements RecordBuilder {
                     }
                 }
             }
+        }
+    }
+
+    private String attName(String attWithIndex) {
+        Matcher matcher = PATTERN_ATT_WITH_INDEX.matcher(attWithIndex);
+        if (matcher.matches()) {
+            return matcher.group(1);
+        } else {
+            return attWithIndex;
         }
     }
 

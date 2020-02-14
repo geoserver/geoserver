365f9508653c2609755691656e9b0454e7ae3318
Andrea Aime <andrea.aime@gmail.com>
2020-02-14 17:15:35 +0100

    Default spatial CQL filters to WGS84 unless otherwise specified, following OGC API guidance

--- src/community/ogcapi/ogcapi-core/src/main/java/org/geoserver/api/APIFilterParser.java
+++ src/community/ogcapi/ogcapi-core/src/main/java/org/geoserver/api/APIFilterParser.java
@@ -4,15 +4,24 @@
  */
 package org.geoserver.api;
 
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.filter.spatial.DefaultCRSFilterVisitor;
 import org.geotools.filter.text.cql2.CQLException;
 import org.geotools.filter.text.ecql.ECQL;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.opengis.filter.Filter;
+import org.opengis.filter.FilterFactory2;
 
 /** Centralizes filter/filter-lang handling. */
 public class APIFilterParser {
 
+    private static final FilterFactory2 FF = CommonFactoryFinder.getFilterFactory2();
     public static String CQL_TEXT = "cql-text";
 
+    /**
+     * Parses the filter over the supported filter languages (right now, only {@link #CQL_TEXT}) and
+     * defaults the geometry liters in spatial filters to CRS84
+     */
     public Filter parse(String filter, String filterLang) {
         if (filter == null) {
             return null;
@@ -31,7 +40,12 @@ public class APIFilterParser {
         }
 
         try {
-            return ECQL.toFilter(filter);
+            Filter parsedFilter = ECQL.toFilter(filter);
+            // in OGC APIs assume CRS84 as the default, but the underlying machinery may default to
+            // the native CRS in EPSG axis order instead, best making the CRS explicit instead
+            DefaultCRSFilterVisitor crsDefaulter =
+                    new DefaultCRSFilterVisitor(FF, DefaultGeographicCRS.WGS84);
+            return (Filter) parsedFilter.accept(crsDefaulter, null);
         } catch (CQLException e) {
             throw new InvalidParameterValueException(e.getMessage(), e);
         }

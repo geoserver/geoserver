9ff886cb36c0b9d317e334b0f5647e850e0e668d
Imran <rajjad@gmail.com>
2020-02-21 15:54:47 +0500

    [GEOS-9498] Add Backward compatibility on Layergroups with legacy Cascaded WMS Layers (#4047) (#4080)

--- src/main/src/main/java/org/geoserver/catalog/impl/LayerInfoImpl.java
+++ src/main/src/main/java/org/geoserver/catalog/impl/LayerInfoImpl.java
@@ -160,8 +160,8 @@ public class LayerInfoImpl implements LayerInfo {
             // will be null if remote capability document
             // does not have any Style tags
             if (remoteDefaultStyleInfo != null) return remoteDefaultStyleInfo;
-            else
-                LOGGER.warning(
+            else if (LOGGER.isLoggable(Level.FINE))
+                LOGGER.fine(
                         "No Default Style found on cascaded WMS Resource"
                                 + ((WMSLayerInfo) getResource()).getName());
         }
@@ -180,8 +180,8 @@ public class LayerInfoImpl implements LayerInfo {
             // will be null if remote capability document
             // does not have any Style tags
             if (remoteStyles != null) return remoteStyles;
-            else
-                LOGGER.warning(
+            else if (LOGGER.isLoggable(Level.FINE))
+                LOGGER.fine(
                         "No Default Styles found on cascaded WMS Resource"
                                 + ((WMSLayerInfo) getResource()).getName());
         }

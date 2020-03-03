f8c27fd9a690702cef19e284578e1c2378bbd957
Jody Garnett <jody.garnett@gmail.com>
2020-03-02 22:08:31 -0800

    restore test using label graphic
    Signed-off-by: Jody Garnett <jody.garnett@gmail.com>

--- src/extension/mbstyle/src/test/java/org/geoserver/community/mbstyle/web/MBStyleEditPageTest.java
+++ src/extension/mbstyle/src/test/java/org/geoserver/community/mbstyle/web/MBStyleEditPageTest.java
@@ -14,10 +14,11 @@ import org.geoserver.community.mbstyle.MBStyleHandler;
 import org.geoserver.web.GeoServerWicketTestSupport;
 import org.geoserver.wms.web.data.OpenLayersPreviewPanel;
 import org.geoserver.wms.web.data.StyleEditPage;
-import org.geotools.styling.PointSymbolizer;
+import org.geotools.styling.ExternalGraphic;
 import org.geotools.styling.Style;
+import org.geotools.styling.TextSymbolizer2;
 import org.junit.Before;
-import org.junit.Ignore;
+import org.junit.Test;
 import org.opengis.style.Symbolizer;
 
 public class MBStyleEditPageTest extends GeoServerWicketTestSupport {
@@ -42,7 +43,7 @@ public class MBStyleEditPageTest extends GeoServerWicketTestSupport {
         tester.startPage(edit);
     }
 
-    @Ignore
+    @Test
     public void testMbstyleChange() throws Exception {
 
         String json =
@@ -84,12 +85,10 @@ public class MBStyleEditPageTest extends GeoServerWicketTestSupport {
         assertNotNull(getCatalog().getStyleByName("mbstyle").getSLD());
         Style style = getCatalog().getStyleByName("mbstyle").getStyle();
         Symbolizer sym = style.featureTypeStyles().get(0).rules().get(0).symbolizers().get(0);
-
-        assertTrue(sym instanceof PointSymbolizer);
-        //        ExternalGraphic eg = (ExternalGraphic) ps.getGraphic().graphicalSymbols().get(0);
-        //        assertEquals(
-        //                eg.getURI(),
-        //
-        // "http://localhost:8080/geoserver/styles/mbsprites#icon=${strURLEncode('circle')}&size=${strURLEncode(Interpolate(POP_MAX,0,0.7,40000000,3.7,'numeric'))}");
+        TextSymbolizer2 label = (TextSymbolizer2) sym;
+        ExternalGraphic eg = (ExternalGraphic) label.getGraphic().graphicalSymbols().get(0);
+        assertEquals(
+                eg.getURI(),
+                "http://localhost:8080/geoserver/styles/mbsprites#icon=${strURLEncode('circle')}&size=${strURLEncode(Interpolate(POP_MAX,0,0.7,40000000,3.7,'numeric'))}");
     }
 }

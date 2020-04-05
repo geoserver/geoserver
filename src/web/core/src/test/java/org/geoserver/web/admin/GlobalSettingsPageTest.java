69fdeca917dc659063bddb8bb3eced8eef21c57d
Andrea Aime <andrea.aime@gmail.com>
2020-04-05 15:04:00 +0200

    [GEOS-9585] Global settings page

--- src/web/core/src/test/java/org/geoserver/web/admin/GlobalSettingsPageTest.java
+++ src/web/core/src/test/java/org/geoserver/web/admin/GlobalSettingsPageTest.java
@@ -5,15 +5,32 @@
  */
 package org.geoserver.web.admin;
 
+import static org.junit.Assert.assertTrue;
+
 import org.apache.wicket.markup.html.form.CheckBox;
+import org.apache.wicket.util.tester.FormTester;
+import org.geoserver.config.GeoServer;
 import org.geoserver.config.GeoServerInfo;
+import org.geoserver.web.GeoServerHomePage;
 import org.geoserver.web.GeoServerWicketTestSupport;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GlobalSettingsPageTest extends GeoServerWicketTestSupport {
+
+    private GeoServer gs;
+
+    @Before
+    public void reset() {
+        gs = getGeoServerApplication().getGeoServer();
+        GeoServerInfo info = gs.getGlobal();
+        info.getSettings().setVerbose(false);
+        gs.save(info);
+    }
+
     @Test
     public void testValues() {
-        GeoServerInfo info = getGeoServerApplication().getGeoServer().getGlobal();
+        GeoServerInfo info = gs.getGlobal();
 
         login();
         tester.startPage(GlobalSettingsPage.class);
@@ -27,5 +44,33 @@ public class GlobalSettingsPageTest extends GeoServerWicketTestSupport {
         tester.assertModelValue(
                 "form:showModifiedTimeCols",
                 info.getSettings().isShowModifiedTimeColumnsInAdminList());
+    }
+
+    @Test
+    public void testSave() {
+        GeoServerInfo info = gs.getGlobal();
+
+        login();
+        tester.startPage(GlobalSettingsPage.class);
+        FormTester ft = tester.newFormTester("form");
+        ft.setValue("verbose", true);
+        ft.submit("submit");
+        
+        tester.assertRenderedPage(GeoServerHomePage.class);
+        assertTrue(gs.getSettings().isVerbose());
+    }
+
+    @Test
+    public void testApply() {
+        GeoServerInfo info = gs.getGlobal();
+
+        login();
+        tester.startPage(GlobalSettingsPage.class);
+        FormTester ft = tester.newFormTester("form");
+        ft.setValue("verbose", true);
+        ft.submit("apply");
+
+        tester.assertRenderedPage(GlobalSettingsPage.class);
+        assertTrue(gs.getSettings().isVerbose());
     }
 }

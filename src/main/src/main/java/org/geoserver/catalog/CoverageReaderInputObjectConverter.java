e1d27bf4ce4d20ab2e9e19115aecefcfc9cf3b6f
Josh Fix <josh@federal.planet.com>
2020-05-21 06:51:13 -0400

    [GEOS-9628] GSIP 188 - New extension point in ResourcePool for determining GridCoverageReader input object types
    * GSIP-188: Add extension point for ResourcePool to convert coverage input objects
    
    * [GSIP-188] Removed OpenPlans copyright headers
    
    * [GSIP-188] Fixed formatting issues
    
    * [GSIP-188] fix issue using resource loader input with no scheme

---
+++ src/main/src/main/java/org/geoserver/catalog/CoverageReaderInputObjectConverter.java
@@ -0,0 +1,37 @@
+/* (c) 2014 - 2020 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.catalog;
+
+import java.util.Optional;
+import javax.annotation.Nullable;
+import org.geotools.util.factory.Hints;
+import org.opengis.coverage.grid.GridCoverageReader;
+
+/**
+ * Extension point for {@link ResourcePool} that enables custom input object types when creating
+ * {@link GridCoverageReader}s. Implementations may return any custom Object which can in turn be
+ * used by {@link org.geotools.coverage.grid.io.AbstractGridFormat#getReader(Object)}
+ * implementations to instantiate the appropriate GridCoverageReader and by {@link
+ * javax.imageio.spi.ImageInputStreamSpi#getInputClass()} implementations to support the accurate
+ * selection of {@link javax.imageio.stream.ImageInputStream} implementations.
+ *
+ * @author joshfix Created on 2/13/20
+ */
+@FunctionalInterface
+public interface CoverageReaderInputObjectConverter<T> {
+
+    /**
+     * This method inspects the provided input object in an attempt to convert it to a custom class.
+     * Any of the accompanying method parameters may optionally be used to better inform the
+     * decision making logic. If an implementation does not support conversion for the given input
+     * object, the method should return an empty {@link Optional}.
+     *
+     * @param input The input object.
+     * @param coverageInfo The grid coverage metadata, may be <code>null</code>.
+     * @param hints Hints to use when loading the coverage, may be <code>null</code>.
+     * @return an {@link Optional} containing the converted value.
+     */
+    Optional<T> convert(Object input, @Nullable CoverageInfo coverageInfo, @Nullable Hints hints);
+}

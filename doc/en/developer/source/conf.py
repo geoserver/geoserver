541239aa01aee6b0c877ef14381344f9cee09ccf
jodygarnett <jody.garnett@gmail.com>
2020-06-02 01:46:49 +0000

    updating version numbers and release notes for 2.17.1

--- doc/en/developer/source/conf.py
+++ doc/en/developer/source/conf.py
@@ -50,7 +50,7 @@ copyright = u'{}, Open Source Geospatial Foundation'.format(now.year)
 # The short X.Y version.
 version = '2.17'
 # The full version, including alpha/beta/rc tags.
-release = '2.17-SNAPSHOT'
+release = '2.17.1'
 
 # Used in build and documentation links
 branch = '2.17.x'

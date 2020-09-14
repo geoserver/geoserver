73e9f46130dc628e5077c32ce8616220c0f7aed8
NielsCharlier <niels@scitus.be>
2020-09-14 12:54:40 +0200

    [GEOS-9730] WMSInfoImpl cannot be serialized any more (#4477)
    * [GEOS-9730] WMSInfoImpl cannot be serialized any more
    
    * [GEOS-9730] WMSInfoImpl cannot be serialized any more

--- src/wms/src/main/java/org/geoserver/wms/CacheConfiguration.java
+++ src/wms/src/main/java/org/geoserver/wms/CacheConfiguration.java
@@ -4,6 +4,7 @@
  */
 package org.geoserver.wms;
 
+import java.io.Serializable;
 import java.util.Objects;
 
 /**
@@ -12,7 +13,7 @@ import java.util.Objects;
  *
  * @author maurobartolomeoli@gmail.com
  */
-public class CacheConfiguration implements Cloneable {
+public class CacheConfiguration implements Cloneable, Serializable {
 
     private boolean enabled;
     private int maxEntries = 1000;

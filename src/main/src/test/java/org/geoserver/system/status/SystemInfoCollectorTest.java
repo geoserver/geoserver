8064599519d3e9a785a4124bced3579df1c10a3c
Mark Prins <1165786+mprins@users.noreply.github.com>
2020-10-25 11:37:44 +0100

    [GEOS-9779] Upgrade OSHI from 3.13.3 to 5.3.2
    - keep track of cpu ticks for processorload
    - mark all OSHI class members transient as these are not serializable and were throwing errors like
    - OSHI is available wunder MIT license, no need for exemptions in our license file

--- src/main/src/test/java/org/geoserver/system/status/SystemInfoCollectorTest.java
+++ src/main/src/test/java/org/geoserver/system/status/SystemInfoCollectorTest.java
@@ -19,21 +19,14 @@ public class SystemInfoCollectorTest extends GeoServerSystemTestSupport {
     @Rule public ErrorCollector collector = new ErrorCollector();
 
     @Test
-    public void testMetricCollector() throws Exception {
+    public void testMetricCollector() {
         final OSHISystemInfoCollector systemInfoCollector =
                 GeoServerExtensions.bean(OSHISystemInfoCollector.class);
         final Metrics collected = systemInfoCollector.retrieveAllSystemInfo();
         final List<MetricValue> metrics = collected.getMetrics();
         for (MetricValue m : metrics) {
-            //            if (m.getAvailable()) {
-            //                System.out.println(
-            //                        m.getName() + " IS available -> " + m.getValue() + " " +
-            // m.getUnit());
-            //            } else {
-            //                System.err.println(m.getName() + " IS NOT available");
-            //            }
             collector.checkThat(
-                    "Metric for " + m.getName() + " available but value is not retrived",
+                    "Metric for " + m.getName() + " available but value is not retrieved",
                     (m.getAvailable() && !m.getValue().equals(BaseSystemInfoCollector.DEFAULT_VALUE)
                             || (!m.getAvailable()
                                     && m.getValue().equals(BaseSystemInfoCollector.DEFAULT_VALUE))),

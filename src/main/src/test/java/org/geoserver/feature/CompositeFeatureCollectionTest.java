ce779e073c2379a99b584a58f40d2404c76834cd
Andrea Aime <andrea.aime@gmail.com>
2020-12-05 18:51:10 +0100

    More build fixes after merging master

--- src/main/src/test/java/org/geoserver/feature/CompositeFeatureCollectionTest.java
+++ src/main/src/test/java/org/geoserver/feature/CompositeFeatureCollectionTest.java
@@ -10,6 +10,8 @@ import org.geotools.data.DataTestCase;
 import org.geotools.data.DataUtilities;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
 
 public class CompositeFeatureCollectionTest extends DataTestCase {
 
@@ -18,15 +20,16 @@ public class CompositeFeatureCollectionTest extends DataTestCase {
     }
 
     public void testCompositeEmpty() {
-        CompositeFeatureCollection fc = new CompositeFeatureCollection(new ArrayList<>());
+        CompositeFeatureCollection<SimpleFeatureType, SimpleFeature> fc =
+                new CompositeFeatureCollection<>(new ArrayList<>());
         assertEquals(0, fc.size());
         assertEquals(null, fc.getBounds());
         assertEquals(null, DataUtilities.first(fc));
     }
 
     public void testComposeOne() {
-        CompositeFeatureCollection fc =
-                new CompositeFeatureCollection(
+        CompositeFeatureCollection<SimpleFeatureType, SimpleFeature> fc =
+                new CompositeFeatureCollection<>(
                         Arrays.asList(DataUtilities.collection(riverFeatures[0])));
         assertEquals(1, fc.size());
         assertEquals(
@@ -39,8 +42,8 @@ public class CompositeFeatureCollectionTest extends DataTestCase {
         SimpleFeatureCollection roads = DataUtilities.collection(roadFeatures);
         SimpleFeatureCollection rivers = DataUtilities.collection(riverFeatures);
         SimpleFeatureCollection lakes = DataUtilities.collection(lakeFeatures);
-        CompositeFeatureCollection fc =
-                new CompositeFeatureCollection(Arrays.asList(roads, rivers, lakes));
+        CompositeFeatureCollection<SimpleFeatureType, SimpleFeature> fc =
+                new CompositeFeatureCollection<>(Arrays.asList(roads, rivers, lakes));
         // 3 roads, 2 rivers, 1 lake
         assertEquals(6, fc.size());
         assertEquals(

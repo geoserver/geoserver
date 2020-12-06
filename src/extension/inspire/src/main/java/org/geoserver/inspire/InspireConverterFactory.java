3ef3ff89613c4abf2d058769b26a1ee716290467
Andrea Aime <andrea.aime@gmail.com>
2020-12-06 18:39:35 +0100

    Cleaned up inspire module

--- src/extension/inspire/src/main/java/org/geoserver/inspire/InspireConverterFactory.java
+++ src/extension/inspire/src/main/java/org/geoserver/inspire/InspireConverterFactory.java
@@ -94,7 +94,7 @@ public class InspireConverterFactory implements ConverterFactory {
                             new UniqueResourceIdentifier(code, namespace, metadataURL);
                     identifiers.add(id);
                 }
-                return (T) identifiers;
+                return target.cast(identifiers);
             } else if (source instanceof UniqueResourceIdentifiers && String.class.equals(target)) {
                 UniqueResourceIdentifiers ids = (UniqueResourceIdentifiers) source;
                 if (ids.size() > 0) {
@@ -114,9 +114,9 @@ public class InspireConverterFactory implements ConverterFactory {
                         sb.append(";");
                     }
                     sb.setLength(sb.length() - 1);
-                    return (T) sb.toString();
+                    return target.cast(sb.toString());
                 } else {
-                    return (T) "";
+                    return target.cast("");
                 }
             }
 

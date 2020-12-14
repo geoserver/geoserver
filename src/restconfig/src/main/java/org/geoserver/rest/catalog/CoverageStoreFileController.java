2aa31463a3b3689b8e8d130b03d31db6bffb185f
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2020-12-14 12:47:37 +0100

    [GEOS-9828]: REST support ImageMosaic Harvesting COG datasets

--- src/restconfig/src/main/java/org/geoserver/rest/catalog/CoverageStoreFileController.java
+++ src/restconfig/src/main/java/org/geoserver/rest/catalog/CoverageStoreFileController.java
@@ -5,7 +5,6 @@
 package org.geoserver.rest.catalog;
 
 import java.awt.RenderingHints;
-import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 import java.net.MalformedURLException;
@@ -29,6 +28,7 @@ import org.geoserver.platform.resource.Resources;
 import org.geoserver.rest.ResourceNotFoundException;
 import org.geoserver.rest.RestBaseController;
 import org.geoserver.rest.RestException;
+import org.geoserver.rest.util.IOUtils;
 import org.geoserver.rest.util.RESTUtils;
 import org.geoserver.rest.wrapper.RestWrapper;
 import org.geotools.coverage.grid.io.AbstractGridFormat;
@@ -111,14 +111,19 @@ public class CoverageStoreFileController extends AbstractStoreUploadController {
         }
 
         StructuredGridCoverage2DReader sr = (StructuredGridCoverage2DReader) reader;
-        // This method returns a List of the harvested files.
-        final List<File> uploadedFiles = new ArrayList<>();
-        for (Resource res :
-                doFileUpload(method, workspaceName, storeName, filename, format, request)) {
-            uploadedFiles.add(Resources.find(res));
+        // This method returns a List of the harvested sources.
+        final List<Object> harvestedResources = new ArrayList<>();
+        if (method == UploadMethod.remote) {
+            harvestedResources.add(handleRemoteUrl(request));
+
+        } else {
+            for (Resource res :
+                    doFileUpload(method, workspaceName, storeName, filename, format, request)) {
+                harvestedResources.add(Resources.find(res));
+            }
         }
         // File Harvesting
-        sr.harvest(null, uploadedFiles, GeoTools.getDefaultHints());
+        sr.harvest(null, harvestedResources, GeoTools.getDefaultHints());
         if (updateBBox) new MosaicInfoBBoxHandler(catalog).updateNativeBBox(info, sr);
     }
 
@@ -452,5 +457,21 @@ public class CoverageStoreFileController extends AbstractStoreUploadController {
         }
         return handleFileUpload(
                 storeName, workspaceName, filename, method, format, directory, request);
+    }
+
+    /** Return the remote URL provided in the request. */
+    protected URL handleRemoteUrl(HttpServletRequest request) {
+
+        try {
+            // get the URL to be harvested
+            final String stringURL = IOUtils.toString(request.getReader());
+            URL remoteUrl = new URL(stringURL);
+            return remoteUrl;
+        } catch (RestException re) {
+            throw re;
+        } catch (Throwable t) {
+            throw new RestException(
+                    "Error while retrieving the remote URL:", HttpStatus.INTERNAL_SERVER_ERROR, t);
+        }
     }
 }

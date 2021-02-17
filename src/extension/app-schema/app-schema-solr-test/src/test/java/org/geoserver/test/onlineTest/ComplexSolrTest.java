f2daf0b1036c575afaa131587a5fed00b8a152f5
taba90 <46672505+taba90@users.noreply.github.com>
2021-02-17 11:13:55 +0100

    [GEOS-9918] Integration test nested binary filter for AppSchema count query (#4744) (#4747)

--- src/extension/app-schema/app-schema-solr-test/src/test/java/org/geoserver/test/onlineTest/ComplexSolrTest.java
+++ src/extension/app-schema/app-schema-solr-test/src/test/java/org/geoserver/test/onlineTest/ComplexSolrTest.java
@@ -7,6 +7,7 @@ package org.geoserver.test.onlineTest;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+
 import static org.junit.Assume.assumeTrue;
 
 import java.awt.image.BufferedImage;

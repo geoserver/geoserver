d3feac5d19f4f06789ddb69c4d0997213ff27f26
Andrea Aime <andrea.aime@gmail.com>
2021-02-25 16:41:56 +0100

    [GEOS-9938] Split the Features template module in independent parts. Split classes in three modules.
    Moved files around, split tests between WFS and OGC API. Core module
    still depends on both WFS and OGCAPI, limiting its reuse.

---
+++ src/community/features-templating/features-templating-wfs/src/test/java/org/geoserver/featurestemplating/response/GeoJSONGetSimpleFeaturesResponseWFSTest.java
@@ -0,0 +1,65 @@
+/* (c) 2020 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.featurestemplating.response;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.junit.Test;
+
+public class GeoJSONGetSimpleFeaturesResponseWFSTest extends GeoJSONGetSimpleFeaturesResponseTest {
+
+    @Test
+    public void testGeoJSONResponse() throws Exception {
+        setUpSimple("NamedPlacesGeoJSON.json");
+        StringBuilder sb = new StringBuilder("wfs?request=GetFeature&version=2.0");
+        sb.append("&TYPENAME=cite:NamedPlaces&outputFormat=");
+        sb.append("application/json");
+        JSONObject result = (JSONObject) getJson(sb.toString());
+        JSONArray features = (JSONArray) result.get("features");
+        assertEquals(features.size(), 2);
+        for (int i = 0; i < features.size(); i++) {
+            checkFeature(features.getJSONObject(i));
+        }
+        checkAdditionalInfo(result);
+    }
+
+    @Test
+    public void testGeoJSONQueryPointingToExpr() throws Exception {
+        setUpSimple("NamedPlacesGeoJSON.json");
+        StringBuilder sb =
+                new StringBuilder("wfs?request=GetFeature&version=2.0")
+                        .append("&TYPENAME=cite:NamedPlaces&outputFormat=")
+                        .append("application/json")
+                        .append("&cql_filter= features.name = 'Name: Goose Island' ");
+        JSONObject result = (JSONObject) getJson(sb.toString());
+        JSONArray features = (JSONArray) result.get("features");
+        assertTrue(features.size() == 1);
+        assertEquals(features.getJSONObject(0).getString("name"), "Name: Goose Island");
+        checkAdditionalInfo(result);
+    }
+
+    @Test
+    public void testGeoJSONResponseWithFilter() throws Exception {
+        setUpSimple("NamedPlacesGeoJSON.json");
+        StringBuilder path =
+                new StringBuilder("wfs?request=GetFeature&version=2.0")
+                        .append("&TYPENAME=cite:NamedPlaces")
+                        .append("&outputFormat=application/json")
+                        .append("&cql_filter= features.id = '118'");
+        JSONObject result = (JSONObject) getJson(path.toString());
+        JSONArray features = (JSONArray) result.get("features");
+        assertEquals(features.size(), 1);
+        JSONObject feature = (JSONObject) features.get(0);
+        assertEquals(feature.getString("id"), "118");
+        assertNotNull(feature.getString("name"), "Goose Island");
+        JSONObject geometry = (JSONObject) feature.get("geometry");
+        assertEquals(geometry.getString("type"), "MultiPolygon");
+        checkAdditionalInfo(result);
+    }
+}

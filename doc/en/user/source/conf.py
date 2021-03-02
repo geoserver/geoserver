a52c4f4c1dbc14026d7acc43ee3a71f97f6f70ee
Jody Garnett <jody.garnett@gmail.com>
2021-03-02 12:58:27 -0800

    Updated version to 2.20-SNAPSHOT, updated GeoTools dependency to 26-SNAPSHOT, updated GeoWebCache dependency to 1.20-SNAPSHOT, and related changes

--- doc/en/user/source/conf.py
+++ doc/en/user/source/conf.py
@@ -48,17 +48,17 @@ copyright = u'{}, Open Source Geospatial Foundation'.format(now.year)
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '2.19'
+version = '2.20'
 
 # The full version, including alpha/beta/rc tags.
-release = '2.19-SNAPSHOT'
+release = '2.20-SNAPSHOT'
 
 # Used in build and documentation links
 branch = 'master'
 
 # Users don't need to see the "SNAPSHOT" notation when it's there
 if release.find('SNAPSHOT') != -1:
-   release = '2.19.x'
+   release = '2.20.x'
    download_release = 'https://build.geoserver.org/geoserver/'+branch+'/geoserver-'+version+'.x-latest-%s.zip'
    download_extension = 'https://build.geoserver.org/geoserver/'+branch+'/ext-latest/geoserver-'+version+'-SNAPSHOT-%s-plugin.zip'
    download_community = 'https://build.geoserver.org/geoserver/'+branch+'/community-latest/geoserver-'+version+'-SNAPSHOT-%s-plugin.zip'

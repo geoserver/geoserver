b32fe30b2b1f225d809722aef6c60499b01f8803
taba90 <46672505+taba90@users.noreply.github.com>
2021-04-01 18:13:59 +0200

    [GEOS-10002] Update url for Inspire Schemas in gs-inspire extension (#4922)
    * [GEOS-10002] Update url for Inspire Schemas in gs-inspire extension
    
    * review feedback

--- src/extension/inspire/src/main/java/org/geoserver/inspire/InspireSchema.java
+++ src/extension/inspire/src/main/java/org/geoserver/inspire/InspireSchema.java
@@ -6,14 +6,19 @@ package org.geoserver.inspire;
 
 public class InspireSchema {
 
-    public static final String COMMON_NAMESPACE = "http://inspire.ec.europa.eu/schemas/common/1.0";
+    public static final String COMMON_NAMESPACE = "https://inspire.ec.europa.eu/schemas/common/1.0";
+
     public static final String COMMON_SCHEMA =
-            "http://inspire.ec.europa.eu/schemas/common/1.0/common.xsd";
-    public static final String VS_NAMESPACE = "http://inspire.ec.europa.eu/schemas/inspire_vs/1.0";
+            "https://inspire.ec.europa.eu/schemas/common/1.0/common.xsd";
+
+    public static final String VS_NAMESPACE = "https://inspire.ec.europa.eu/schemas/inspire_vs/1.0";
+
     public static final String VS_SCHEMA =
-            "http://inspire.ec.europa.eu/schemas/inspire_vs/1.0/inspire_vs.xsd";
+            "https://inspire.ec.europa.eu/schemas/inspire_vs/1.0/inspire_vs.xsd";
+
     public static final String DLS_NAMESPACE =
-            "http://inspire.ec.europa.eu/schemas/inspire_dls/1.0";
+            "https://inspire.ec.europa.eu/schemas/inspire_dls/1.0";
+
     public static final String DLS_SCHEMA =
-            "http://inspire.ec.europa.eu/schemas/inspire_dls/1.0/inspire_dls.xsd";
+            "https://inspire.ec.europa.eu/schemas/inspire_dls/1.0/inspire_dls.xsd";
 }

826f4db9ea0bf778978946ced4f36dcc96fc366e
taba90 <46672505+taba90@users.noreply.github.com>
2021-04-22 17:51:12 +0200

    [GEOS-10028] Allow to customize ComplexGeoJsonWriter encoding of nested element and non mandatory attributes (#4987)
    * [GEOS-10028] Allow to customize ComplexGeoJsonWriter encoding of nested element and non mandatory attributes
    
    * review feedback

--- src/wfs/src/test/java/org/geoserver/wfs/json/ComplexGeoJsonWriterTest.java
+++ src/wfs/src/test/java/org/geoserver/wfs/json/ComplexGeoJsonWriterTest.java
@@ -5,12 +5,28 @@
 package org.geoserver.wfs.json;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+import org.geoserver.test.GeoServerSystemTestSupport;
+import org.geotools.feature.AttributeBuilder;
+import org.geotools.feature.ComplexFeatureBuilder;
+import org.geotools.feature.FakeTypes;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.LenientFeatureFactoryImpl;
+import org.geotools.feature.type.AttributeDescriptorImpl;
+import org.geotools.feature.type.FeatureTypeImpl;
 import org.junit.Test;
+import org.opengis.feature.Attribute;
 import org.opengis.feature.ComplexAttribute;
+import org.opengis.feature.Feature;
 import org.opengis.feature.IllegalAttributeException;
 import org.opengis.feature.Property;
 import org.opengis.feature.type.AttributeDescriptor;
@@ -20,7 +36,26 @@ import org.opengis.feature.type.PropertyDescriptor;
 import org.opengis.feature.type.PropertyType;
 import org.opengis.filter.identity.Identifier;
 
-public class ComplexGeoJsonWriterTest {
+public class ComplexGeoJsonWriterTest extends GeoServerSystemTestSupport {
+
+    private static final ComplexType MINENAMETYPE_TYPE =
+            new FeatureTypeImpl(
+                    /* name: */ FakeTypes.Mine.NAME_MineNameType,
+                    /* properties: */ FakeTypes.Mine.MINENAMETYPE_SCHEMA,
+                    /* identified: */ null,
+                    /* isAbstract: */ false,
+                    /* restrictions: */ Collections.emptyList(),
+                    /* superType: */ FakeTypes.ANYTYPE_TYPE,
+                    /* description: */ null);
+
+    private static final AttributeDescriptor MINENAME_DESCRIPTOR =
+            new AttributeDescriptorImpl(
+                    /* type: */ MINENAMETYPE_TYPE,
+                    /* name: */ FakeTypes.Mine.NAME_MineName,
+                    /* min: */ 1,
+                    /* max: */ 1,
+                    /* isNillable: */ false,
+                    /* defaultValue: */ null);
 
     /*
      * Tests Null safety when attribute parameter is null in checkIfFeatureIsLinked util method.
@@ -143,5 +178,97 @@ public class ComplexGeoJsonWriterTest {
                 return null;
             }
         };
+    }
+
+    @Test
+    public void testGeoJsonWriterOptions() {
+        // test the default behaviour of the ComplexGeoJsonWriter
+        // with respect to nested features
+        Feature f = buildComplexFeature();
+        StringWriter w = new StringWriter();
+        GeoJSONBuilder jWriter = new GeoJSONBuilder(w);
+        TestComplexGeoJsonWriter testWriter = new TestComplexGeoJsonWriter(jWriter);
+
+        testWriter.encodeFeature(f);
+        JSONObject jsonF = (JSONObject) JSONSerializer.toJSON(w.toString());
+        JSONObject mineName = jsonF.getJSONObject("properties").getJSONObject("MineName");
+        assertTrue(mineName.has("id"));
+        assertTrue(mineName.has("geometry"));
+        assertTrue(mineName.has("properties"));
+        assertTrue(mineName.getJSONObject("properties").has("@featureType"));
+    }
+
+    @Test
+    public void testGeoJsonWriterOptionsCustom() {
+        // test behaviour of the ComplexGeoJsonWriter with a
+        // ComplexGeoJsonWriterOptions object asking for the econding
+        // of nested features as complex properties
+        ComplexGeoJsonWriterOptions options =
+                new ComplexGeoJsonWriterOptions() {
+                    @Override
+                    public boolean canHandle(List<FeatureCollection> features) {
+                        return true;
+                    }
+
+                    @Override
+                    public boolean encodeComplexAttributeType() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean encodeNestedFeatureAsProperty(ComplexType complexType) {
+                        return true;
+                    }
+                };
+        Feature f = buildComplexFeature();
+        StringWriter w = new StringWriter();
+        GeoJSONBuilder jWriter = new GeoJSONBuilder(w);
+        TestComplexGeoJsonWriter testWriter = new TestComplexGeoJsonWriter(jWriter, options);
+
+        testWriter.encodeFeature(f);
+        JSONObject jsonF = (JSONObject) JSONSerializer.toJSON(w.toString());
+        JSONObject mineName = jsonF.getJSONObject("properties").getJSONObject("MineName");
+        assertFalse(mineName.has("id"));
+        assertFalse(mineName.has("geometry"));
+        assertFalse(mineName.has("properties"));
+        assertFalse(mineName.has("@dataType"));
+    }
+
+    /** used to expose a public encodeFeature method for testing purposes * */
+    class TestComplexGeoJsonWriter extends ComplexGeoJsonWriter {
+
+        public TestComplexGeoJsonWriter(
+                GeoJSONBuilder jsonWriter, ComplexGeoJsonWriterOptions settings) {
+            super(jsonWriter, settings);
+        }
+
+        public TestComplexGeoJsonWriter(GeoJSONBuilder jsonWriter) {
+            super(jsonWriter);
+        }
+
+        public void encodeFeature(Feature feature) {
+            super.encodeFeature(feature, true);
+        }
+    }
+
+    // build a complexFeature with a nested Feature
+    private Feature buildComplexFeature() {
+        ComplexFeatureBuilder fBuilder = new ComplexFeatureBuilder(FakeTypes.Mine.MINETYPE_TYPE);
+        ComplexAttribute attr = getNestedFeature("a name", true);
+        fBuilder.append(FakeTypes.Mine.NAME_mineName, attr);
+        return fBuilder.buildFeature("id");
+    }
+
+    private static ComplexAttribute getNestedFeature(String name, boolean isPreferred) {
+        ComplexFeatureBuilder complexFB = new ComplexFeatureBuilder(MINENAME_DESCRIPTOR);
+        AttributeBuilder builder = new AttributeBuilder(new LenientFeatureFactoryImpl());
+        builder.setDescriptor(FakeTypes.Mine.ISPREFERRED_DESCRIPTOR);
+        Attribute isPrefAttr = builder.buildSimple("isPreferred_testId", isPreferred);
+        builder.setDescriptor(FakeTypes.Mine.mineNAME_DESCRIPTOR);
+        Attribute nameAttr = builder.buildSimple("mineName_testId", name);
+        complexFB.append(FakeTypes.Mine.NAME_isPreferred, isPrefAttr);
+        complexFB.append(FakeTypes.Mine.NAME_mineName, nameAttr);
+
+        return complexFB.buildFeature(null);
     }
 }

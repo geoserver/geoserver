79347187e6b6f53bb542953cbdca0fa667eaa5be
Andrea Aime <andrea.aime@gmail.com>
2021-07-27 11:08:13 +0200

    [GEOS-10152] Features templating: allow also keys to depend on feature attributes. Follow up review

--- src/community/features-templating/features-templating-core/src/main/java/org/geoserver/featurestemplating/builders/flat/AttributeNameHelper.java
+++ src/community/features-templating/features-templating-core/src/main/java/org/geoserver/featurestemplating/builders/flat/AttributeNameHelper.java
@@ -24,7 +24,8 @@ class AttributeNameHelper {
 
     String getFinalAttributeName(TemplateBuilderContext context) {
         String parentKey = this.parentKey;
-        String key = this.key != null ? this.key.evaluate(context).toString() : null;
+        String key =
+                this.key != null ? this.key.evaluate(context.getCurrentObj()).toString() : null;
         if (parentKey != null && !parentKey.equals(PROPERTIES_KEY) && key != null)
             key = parentKey + separator + key;
         else if (key == null) key = parentKey;

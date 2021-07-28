21ee1d14379de7919b9fbda5385ddacc2f6f23ea
ydeliorman <yalin.deliorman@gmail.com>
2021-07-28 11:13:59 +0300

    [GEOS-10160] review feedbacks

--- src/main/src/main/java/org/geoserver/ows/LanguageURLMangler.java
+++ src/main/src/main/java/org/geoserver/ows/LanguageURLMangler.java
@@ -4,48 +4,53 @@
  */
 package org.geoserver.ows;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Map;
-import javax.servlet.http.HttpServletRequest;
 
 /** Mangles service URL's based on the AcceptLanguages and Languages parameters */
 public class LanguageURLMangler implements URLMangler {
 
     public static final String ACCEPT_LANGUAGES = "AcceptLanguages";
     public static final String LANGUAGE = "Language";
-    private String languageParameter = "";
-    private String acceptLanguagesParameter = "";
 
     @Override
     public void mangleURL(
             StringBuilder baseURL, StringBuilder path, Map<String, String> kvp, URLType type) {
         if (Dispatcher.REQUEST.get() == null || !type.equals(URLType.SERVICE)) return;
 
-        String language = "";
+        String languageParameter = null;
+        String acceptLanguagesParameter = null;
+
         HttpServletRequest httpRequest = Dispatcher.REQUEST.get().getHttpRequest();
         Enumeration<String> parameterNames = httpRequest.getParameterNames();
-        Collections.list(parameterNames)
-                .forEach(
-                        parameter -> {
-                            if (parameter.equalsIgnoreCase(LANGUAGE)) {
-                                languageParameter = parameter;
-                            } else if (parameter.equalsIgnoreCase(ACCEPT_LANGUAGES)) {
-                                acceptLanguagesParameter = parameter;
-                            }
-                        });
 
-        String acceptLanguages = httpRequest.getParameter(acceptLanguagesParameter);
-        language = httpRequest.getParameter(languageParameter);
-
-        if (acceptLanguages != null) {
-            String comaSplit = acceptLanguages.split(",")[0];
-            String spaceSplit = comaSplit.split(" ")[0];
-            if (spaceSplit.length() >= 1) {
-                language = spaceSplit;
-            } else {
-                language = comaSplit;
+        for (String parameter : Collections.list(parameterNames)) {
+            if (parameter.equalsIgnoreCase(LANGUAGE)) {
+                languageParameter = parameter;
+            } else if (parameter.equalsIgnoreCase(ACCEPT_LANGUAGES)) {
+                acceptLanguagesParameter = parameter;
             }
+        }
+
+        String language = "";
+
+        if (acceptLanguagesParameter != null) {
+            String acceptLanguages = httpRequest.getParameter(acceptLanguagesParameter);
+            if (acceptLanguages != null) {
+                String commaSplit = acceptLanguages.split(",")[0];
+                String spaceSplit = commaSplit.split(" ")[0];
+                if (spaceSplit.length() >= 1) {
+                    language = spaceSplit;
+                } else {
+                    language = commaSplit;
+                }
+            }
+        }
+
+        if (languageParameter != null) {
+            language = httpRequest.getParameter(languageParameter);
         }
 
         if (language != null && language.length() > 0) {

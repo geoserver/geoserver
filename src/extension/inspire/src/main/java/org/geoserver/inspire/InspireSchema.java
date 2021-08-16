9923d618af981574ab950f8461dd1a73c49c8a98
Niels Meijer <niels@n4m5.nl>
2021-08-16 10:39:16 +0200

    GEOS-10163 Incorrect INSPIRE namespace URI

--- src/extension/inspire/src/main/java/org/geoserver/inspire/InspireSchema.java
+++ src/extension/inspire/src/main/java/org/geoserver/inspire/InspireSchema.java
@@ -6,18 +6,18 @@ package org.geoserver.inspire;
 
 public class InspireSchema {
 
-    public static final String COMMON_NAMESPACE = "https://inspire.ec.europa.eu/schemas/common/1.0";
+    public static final String COMMON_NAMESPACE = "http://inspire.ec.europa.eu/schemas/common/1.0";
 
     public static final String COMMON_SCHEMA =
             "https://inspire.ec.europa.eu/schemas/common/1.0/common.xsd";
 
-    public static final String VS_NAMESPACE = "https://inspire.ec.europa.eu/schemas/inspire_vs/1.0";
+    public static final String VS_NAMESPACE = "http://inspire.ec.europa.eu/schemas/inspire_vs/1.0";
 
     public static final String VS_SCHEMA =
             "https://inspire.ec.europa.eu/schemas/inspire_vs/1.0/inspire_vs.xsd";
 
     public static final String DLS_NAMESPACE =
-            "https://inspire.ec.europa.eu/schemas/inspire_dls/1.0";
+            "http://inspire.ec.europa.eu/schemas/inspire_dls/1.0";
 
     public static final String DLS_SCHEMA =
             "https://inspire.ec.europa.eu/schemas/inspire_dls/1.0/inspire_dls.xsd";

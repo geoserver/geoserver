e3a52c96362d4421365f75c967402e8afe84b2df
Marco Volpini <tabareu90@gmail.com>
2021-08-17 16:30:51 +0200

    refact tests to run with thread count>1

--- src/community/features-templating/features-templating-wfs/src/test/java/org/geoserver/featurestemplating/response/GML2GetComplexFeatureTest.java
+++ src/community/features-templating/features-templating-wfs/src/test/java/org/geoserver/featurestemplating/response/GML2GetComplexFeatureTest.java
@@ -1,21 +1,40 @@
 package org.geoserver.featurestemplating.response;
 
 import java.io.IOException;
-import org.geoserver.featurestemplating.configuration.TemplateIdentifier;
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.FeatureTypeInfo;
+import org.geoserver.data.test.SystemTestData;
+import org.geoserver.featurestemplating.configuration.SupportedFormat;
 import org.junit.Test;
-import org.springframework.test.annotation.DirtiesContext;
 import org.w3c.dom.Document;
 
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class GML2GetComplexFeatureTest extends TemplateComplexTestSupport {
+
+    private static final String MF_GML2 = "MappedFeatureGML2";
+    private static final String MF_GML2_PARAM = "&" + MF_GML2 + "=true";
+
+    @Override
+    public void onSetUp(SystemTestData testData) throws IOException {
+        Catalog catalog = getCatalog();
+        FeatureTypeInfo mappedFeature = catalog.getFeatureTypeByName("gsml", "MappedFeature");
+        String templateMappedFeature = "MappedFeatureGML2.xml";
+        setUpTemplate(
+                "requestParam('" + MF_GML2 + "')='true'",
+                SupportedFormat.GML,
+                templateMappedFeature,
+                MF_GML2,
+                ".xml",
+                "gsml",
+                mappedFeature);
+    }
 
     @Test
     public void getMappedFeature() throws IOException {
-        setUpComplex("MappedFeatureGML2.xml", mappedFeature);
         Document doc =
                 getAsDOM(
                         "wfs?request=GetFeature&version=1.0.0&typename=gsml:MappedFeature"
-                                + "&outputFormat=text%2Fxml%3B%20subtype%3Dgml%2F2.1.2");
+                                + "&outputFormat=text%2Fxml%3B%20subtype%3Dgml%2F2.1.2"
+                                + MF_GML2_PARAM);
         assertXpathCount(4, "//gsml:MappedFeature", doc);
         assertXpathCount(4, "//gsml:samplingFrame//@xlink:href", doc);
         assertXpathCount(4, "//gsml:MappedFeature/gsml:geometry/gml:Polygon", doc);
@@ -32,16 +51,11 @@ public class GML2GetComplexFeatureTest extends TemplateComplexTestSupport {
 
     @Test
     public void testBackwardMappingOnFilterStaticAttribute() throws IOException {
-        setUpComplex("MappedFeatureGML2.xml", mappedFeature);
         Document doc =
                 getAsDOM(
                         "wfs?request=GetFeature&version=1.0.0&typename=gsml:MappedFeature"
-                                + "&outputFormat=text%2Fxml%3B%20subtype%3Dgml%2F2.1.2&cql_filter=wfs:FeatureCollection.gml:featureMember.gsml:MappedFeature.gsml:specification.gsml:GeologicUnit.xlink:title='a static xlink:title filtered'");
+                                + "&outputFormat=text%2Fxml%3B%20subtype%3Dgml%2F2.1.2&cql_filter=wfs:FeatureCollection.gml:featureMember.gsml:MappedFeature.gsml:specification.gsml:GeologicUnit.xlink:title='a static xlink:title filtered'"
+                                + MF_GML2_PARAM);
         assertXpathCount(1, "//gsml:MappedFeature", doc);
-    }
-
-    @Override
-    protected String getTemplateFileName() {
-        return TemplateIdentifier.GML2.getFilename();
     }
 }
--- src/community/features-templating/features-templating-wfs/src/test/java/org/geoserver/featurestemplating/response/ManagedComplexFeaturesResponseTest.java
+++ src/community/features-templating/features-templating-wfs/src/test/java/org/geoserver/featurestemplating/response/ManagedComplexFeaturesResponseTest.java
@@ -3,28 +3,73 @@ package org.geoserver.featurestemplating.response;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
-import org.geoserver.featurestemplating.configuration.TemplateIdentifier;
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.FeatureTypeInfo;
+import org.geoserver.data.test.SystemTestData;
+import org.geoserver.featurestemplating.configuration.SupportedFormat;
 import org.geoserver.test.AbstractAppSchemaMockData;
 import org.geoserver.test.FeatureChainingMockData;
 import org.junit.Test;
 import org.w3c.dom.Document;
 
-public class ManagedComplexFeaturesResponseTest extends JSONLDGetComplexFeaturesResponseTest {
+public class ManagedComplexFeaturesResponseTest extends TemplateComplexTestSupport {
+
+    private static final String MF_GML_MANAGED = "GMLManagedMappedFeature";
+    private static final String MF_GML_PARAM = "&" + MF_GML_MANAGED + "=true";
+
+    private static final String MF_GEOJSON_MANAGED = "GeoJSONManagedMappedFeature";
+    private static final String MF_GEOJSON_PARAM = "&" + MF_GEOJSON_MANAGED + "=true";
+
+    private static final String MF_JSONLD_MANAGED = "JSONLDManagedMappedFeature";
+    private static final String MF_JSONLD_PARAM = "&" + MF_JSONLD_MANAGED + "=true";
+
+    @Override
+    public void onSetUp(SystemTestData testData) throws IOException {
+        Catalog catalog = getCatalog();
+        FeatureTypeInfo mappedFeature = catalog.getFeatureTypeByName("gsml", "MappedFeature");
+        String templateMappedFeatureGML = "ManagedMappedFeatureGML32.xml";
+        setUpTemplate(
+                "requestParam('" + MF_GML_MANAGED + "')='true'",
+                SupportedFormat.GML,
+                templateMappedFeatureGML,
+                MF_GML_MANAGED,
+                ".xml",
+                "gsml",
+                mappedFeature);
+
+        String templateMappedFeatureGeoJSON = "ManagedMappedFeatureGeoJSON.json";
+        setUpTemplate(
+                "requestParam('" + MF_GEOJSON_MANAGED + "')='true'",
+                SupportedFormat.GEOJSON,
+                templateMappedFeatureGeoJSON,
+                MF_GEOJSON_MANAGED,
+                ".json",
+                "gsml",
+                mappedFeature);
+
+        String templateMappedFeatureJSONLD = "ManagedMappedFeatureJSONLD.json";
+
+        setUpTemplate(
+                "requestParam('" + MF_JSONLD_MANAGED + "')='true'",
+                SupportedFormat.JSONLD,
+                templateMappedFeatureJSONLD,
+                MF_JSONLD_MANAGED,
+                ".json",
+                "gsml",
+                mappedFeature);
+    }
 
     @Test
     public void testGMLResponse() throws Exception {
-        setUpComplex(
-                "ManagedMappedFeatureGML32.xml",
-                "gsml",
-                TemplateIdentifier.GML32.getFilename(),
-                mappedFeature);
         StringBuffer sb = new StringBuffer("wfs?request=GetFeature&version=2.0");
         sb.append("&TYPENAME=gsml:MappedFeature&outputFormat=");
         sb.append("application%2Fgml%2Bxml%3B%20version%3D3.2");
+        sb.append(MF_GML_PARAM);
         Document doc = getAsDOM(sb.toString());
         assertXpathCount(4, "//gsml:MappedFeature", doc);
         assertXpathCount(4, "//gsml:samplingFrame//@xlink:href", doc);
@@ -50,14 +95,10 @@ public class ManagedComplexFeaturesResponseTest extends JSONLDGetComplexFeatures
 
     @Test
     public void testJsonLdResponse() throws Exception {
-        setUpComplex(
-                "ManagedMappedFeatureJSONLD.json",
-                "gsml",
-                TemplateIdentifier.JSONLD.getFilename(),
-                mappedFeature);
         StringBuffer sb = new StringBuffer("wfs?request=GetFeature&version=2.0");
         sb.append("&TYPENAME=gsml:MappedFeature&outputFormat=");
         sb.append("application%2Fld%2Bjson");
+        sb.append(MF_JSONLD_PARAM);
         JSONObject result = (JSONObject) getJsonLd(sb.toString());
         Object context = result.get("@context");
         checkContext(context);
@@ -72,14 +113,10 @@ public class ManagedComplexFeaturesResponseTest extends JSONLDGetComplexFeatures
 
     @Test
     public void testGeoJSONResponse() throws Exception {
-        setUpComplex(
-                "ManagedMappedFeatureGeoJSON.json",
-                "gsml",
-                TemplateIdentifier.JSON.getFilename(),
-                mappedFeature);
         StringBuffer sb = new StringBuffer("wfs?request=GetFeature&version=2.0");
         sb.append("&TYPENAME=gsml:MappedFeature&outputFormat=");
         sb.append("application/json");
+        sb.append(MF_GEOJSON_PARAM);
         JSONObject result = (JSONObject) getJson(sb.toString());
         JSONArray features = (JSONArray) result.get("features");
         assertEquals(4, features.size());

8c2085157c4c1396a676ef19966ae78cd95b94ee
Marco Volpini <tabareu90@gmail.com>
2021-09-27 17:17:51 +0200

    [GEOS-10257] Features templating UI preview fails on secured layers

--- src/community/features-templating/features-templating-web/src/test/java/org/geoserver/featurestemplating/web/TemplateConfigurationPageTest.java
+++ src/community/features-templating/features-templating-web/src/test/java/org/geoserver/featurestemplating/web/TemplateConfigurationPageTest.java
@@ -2,19 +2,36 @@ package org.geoserver.featurestemplating.web;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.http.client.CookieStore;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.protocol.http.mock.MockHttpServletRequest;
 import org.apache.wicket.util.tester.FormTester;
 import org.geoserver.featurestemplating.configuration.SupportedFormat;
+import org.geoserver.featurestemplating.configuration.Template;
 import org.geoserver.featurestemplating.configuration.TemplateFileManager;
 import org.geoserver.featurestemplating.configuration.TemplateInfo;
 import org.geoserver.featurestemplating.configuration.TemplateInfoDAO;
+import org.geoserver.ows.Request;
+import org.geoserver.web.GeoServerApplication;
 import org.geoserver.web.GeoServerWicketTestSupport;
 import org.geoserver.web.wicket.CodeMirrorEditor;
 import org.junit.Test;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+
+import java.util.List;
 
 public class TemplateConfigurationPageTest extends GeoServerWicketTestSupport {
 
@@ -218,6 +235,18 @@ public class TemplateConfigurationPageTest extends GeoServerWicketTestSupport {
                     + "      </ul>\n"
                     + "    </li>\n"
                     + "  </ul>\n"
+                    + "</gft:Template>";
+
+
+    private static final String GML_STATIC_TEMPLATE =
+            "<gft:Template>\n"
+                    + "<gft:Options>\n"
+                    + "  <gft:Namespaces xmlns:topp=\"http://www.openplans.org/topp\"/>\n"
+                    + "  <gft:SchemaLocation xsi:schemaLocation=\"http://www.opengis.net/wfs/2.0 http://brgm-dev.geo-solutions.it/geoserver/schemas/wfs/2.0/wfs.xsd http://www.opengis.net/gml/3.2 http://schemas.opengis.net/gml/3.2.1/gml.xsd\"/>\n"
+                    + "</gft:Options>\n"
+                    + "  <theFeature gml:id=\"idVal\">\n"
+                    + "  \t<name>name</name>\n"
+                    + "  </theFeature>\n"
                     + "</gft:Template>";
 
     @Test
@@ -464,5 +493,77 @@ public class TemplateConfigurationPageTest extends GeoServerWicketTestSupport {
         } finally {
             TemplateInfoDAO.get().deleteAll();
         }
+    }
+
+    @Test
+    public void testPreviewRequestCanSendJsessionId() {
+        // tests that the http client requesting a wfs preview
+        // is able to send the JSESSIONID cookie to authenticate the wfs request according to the user
+        // preview the template fromUI.
+        TemplateInfo info = new TemplateInfo();
+        info.setWorkspace("cite");
+        info.setTemplateName("testGMLTemplate");
+        info.setExtension("xml");
+        info = TemplateInfoDAO.get().saveOrUpdate(info);
+        TemplateFileManager.get().saveTemplateFile(info, GML_STATIC_TEMPLATE);
+        try {
+            login();
+            MockHttpServletRequest request=(MockHttpServletRequest) GeoServerApplication.get().servletRequest();
+            RequestAttributes requestAttributes=new ServletRequestAttributes(request);
+            RequestContextHolder.setRequestAttributes(requestAttributes);
+            tester.startPage(new TemplateConfigurationPage(new Model<>(info), false));
+            tester.assertModelValue("theForm:tabbedPanel:panel:templateName", "testGMLTemplate");
+
+            tester.assertModelValue("theForm:tabbedPanel:panel:extension", "xml");
+            tester.assertModelValue("theForm:tabbedPanel:panel:workspace", "cite");
+            tester.assertModelValue("theForm:tabbedPanel:panel:featureTypeInfo", null);
+            tester.clickLink("theForm:tabbedPanel:tabs-container:tabs:1:link");
+            FormTester form2 = tester.newFormTester("theForm:tabbedPanel:panel:previewForm");
+            form2.select("outputFormats", 0);
+            tester.assertComponent(
+                    "theForm:tabbedPanel:panel:previewForm:workspaces", DropDownChoice.class);
+            tester.assertComponent(
+                    "theForm:tabbedPanel:panel:previewForm:featureTypes", DropDownChoice.class);
+            tester.assertComponent(
+                    "theForm:tabbedPanel:panel:previewForm:previewArea", CodeMirrorEditor.class);
+            tester.assertComponent(
+                    "theForm:tabbedPanel:panel:previewForm:preview", AjaxSubmitLink.class);
+            tester.assertComponent(
+                    "theForm:tabbedPanel:panel:previewForm:validate", AjaxSubmitLink.class);
+            DropDownChoice ws =
+                    (DropDownChoice)
+                            tester.getComponentFromLastRenderedPage(
+                                    "theForm:tabbedPanel:panel:previewForm:workspaces");
+            // template is Local to feature type no need to select ws
+            assertFalse(ws.isEnabled());
+
+            DropDownChoice ft =
+                    (DropDownChoice)
+                            tester.getComponentFromLastRenderedPage(
+                                    "theForm:tabbedPanel:panel:previewForm:featureTypes");
+            // template is not Local to feature type is needed select ft
+            assertTrue(ft.isEnabled());
+            form2.select("featureTypes", 0);
+
+            // checks that the JSESSIONID has been picked up by the HttpClient.
+            TemplatePreviewPanel previewPanel=(TemplatePreviewPanel)tester.getComponentFromLastRenderedPage("theForm:tabbedPanel:panel");
+            List<Cookie> httpCookies=getCookiesFromPreviewRequest(previewPanel);
+            Cookie cookie=httpCookies.stream().filter(c->c.getName().equals("JSESSIONID")).findFirst().get();
+            assertNotNull(cookie);
+            assertEquals(requestAttributes.getSessionId(),cookie.getValue());
+
+
+            tester.newFormTester("theForm").submit("save");
+            tester.assertNoErrorMessage();
+            tester.assertRenderedPage(TemplateInfoPage.class);
+        } finally {
+            TemplateInfoDAO.get().deleteAll();
+        }
+    }
+
+    private List<Cookie> getCookiesFromPreviewRequest(TemplatePreviewPanel previewPanel){
+            CookieStore cookieStore=new PropertyModel<CookieStore>(previewPanel.buildHttpClient(),"cookieStore").getObject();
+            return cookieStore.getCookies();
+
     }
 }

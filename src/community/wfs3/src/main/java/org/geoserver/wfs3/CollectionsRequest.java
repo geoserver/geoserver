775be6688c16c8506f38769559260b751d591ccf
Andrea Aime <andrea.aime@gmail.com>
2021-11-13 14:54:18 +0100

    [GEOS-10304] Remove the WFS3 community module

--- src/community/wfs3/src/main/java/org/geoserver/wfs3/CollectionsRequest.java
+++
@@ -1,8 +0,0 @@
-/* (c) 2018 Open Source Geospatial Foundation - all rights reserved
- * This code is licensed under the GPL 2.0 license, available at the root
- * application directory.
- */
-package org.geoserver.wfs3;
-
-/** Request for the server contents */
-public class CollectionsRequest extends BaseRequest {}

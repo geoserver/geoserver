abfa0ad76a5fd1df6a0928f598a5433076bc8a65
Maxime Werlen <maxime@werlen.fr>
2021-12-06 14:14:07 +0100

    [GEOS-10316] Fix startup.sh to handle multiple java options (#5415)

--- src/release/bin/shutdown.sh
+++ src/release/bin/shutdown.sh
@@ -65,4 +65,5 @@ if [ ! -r "${GEOSERVER_HOME}/start.jar" ]; then
 fi
 
 cd "${GEOSERVER_HOME}" || exit 1
-exec "${_RUNJAVA}" "${JAVA_OPTS:--DnoJavaOpts}" -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar start.jar --stop
+IFS=$(printf '\n\t ')
+exec "${_RUNJAVA}" ${JAVA_OPTS:--DnoJavaOpts} -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar start.jar --stop

4dbeca926aeb9ed1b92ea7e7520d75e5e3a2596d
Andrea Aime <andrea.aime@gmail.com>
2021-12-20 18:08:46 +0100

    [GEOS-10337] Harden importer against failed imports, make failures more evident

--- src/extension/importer/core/src/main/java/org/geoserver/importer/ImportContext.java
+++ src/extension/importer/core/src/main/java/org/geoserver/importer/ImportContext.java
@@ -55,8 +55,8 @@ public class ImportContext implements Serializable {
         PENDING,
         /** Import is running */
         RUNNING,
-        /** Import is complete */
-        COMPLETE;
+        /** Import completed succesfully */
+        COMPLETE
     }
 
     /** identifier */
@@ -232,18 +232,24 @@ public class ImportContext implements Serializable {
         } else {
             newState = State.COMPLETE;
         }
-        O:
+        boolean error = false;
         for (ImportTask task : tasks) {
             switch (task.getState()) {
                 case COMPLETE:
                     continue;
                 case RUNNING:
                     newState = State.RUNNING;
-                    break O;
+                    break;
+                case ERROR:
+                    error = true;
+                    break;
                 default:
                     newState = State.PENDING;
-                    break O;
+                    break;
             }
+        }
+        if (error && newState == State.COMPLETE) {
+            setMessage("At least one task failed, look for error details in the tasks");
         }
         state = newState;
 

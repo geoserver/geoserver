3d15ba64ecc96a8c4046ada87dd266ca4fedca8b
Marco Volpini <tabareu90@gmail.com>
2022-03-08 14:38:11 +0100

    [GEOS-10413] Schemaless Features- Allow usage of . separator for PropertyName evaluation

--- src/community/schemaless-features/schemaless-core/src/main/java/org/geoserver/schemalessfeatures/filter/SchemalessPropertyAccessorFactory.java
+++ src/community/schemaless-features/schemaless-core/src/main/java/org/geoserver/schemalessfeatures/filter/SchemalessPropertyAccessorFactory.java
@@ -77,7 +77,7 @@ public class SchemalessPropertyAccessorFactory implements PropertyAccessorFactor
             if (object instanceof ComplexAttribute) {
                 String[] pathParts;
                 if (xpath.indexOf('/') != -1) pathParts = xpath.split("/");
-                else pathParts = xpath.split(":");
+                else pathParts = xpath.split("\\.");
                 return (T) walkComplexAttribute((ComplexAttribute) object, pathParts);
             } else if (object instanceof DynamicComplexType) {
                 return (T) ANYTYPE_TYPE;

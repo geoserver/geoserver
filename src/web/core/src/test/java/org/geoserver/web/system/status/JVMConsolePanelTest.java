b558c861357f89d0f497c12fd112dd852adcac3b
fostash <fausto.bonacina@gmail.com>
2022-04-26 11:23:16 +0200

    [GEOS-4613] added test for text area content

--- src/web/core/src/test/java/org/geoserver/web/system/status/JVMConsolePanelTest.java
+++ src/web/core/src/test/java/org/geoserver/web/system/status/JVMConsolePanelTest.java
@@ -5,6 +5,7 @@
 package org.geoserver.web.system.status;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.apache.wicket.util.tester.TagTester;
 import org.geoserver.web.GeoServerWicketTestSupport;
@@ -24,10 +25,32 @@ public class JVMConsolePanelTest extends GeoServerWicketTestSupport {
     }
 
     @Test
-    public void testLoadConsoleTab() {
-        TagTester threadDumpBtn = tester.getTagByWicketId("dumpThread");
-        assertNotNull(threadDumpBtn);
-        TagTester heapDumpBtn = tester.getTagByWicketId("dumpHeap");
-        assertNotNull(heapDumpBtn);
+    public void testCheckButtonsLink() {
+        final TagTester threadDumpBtn = tester.getTagByWicketId("dumpThread");
+        assertNotNull("dump thread button not present", threadDumpBtn);
+        assertTrue(
+                "href wrong on dump head button",
+                threadDumpBtn.getAttribute("href").contains("tabs-panel-dumpThread"));
+        final TagTester heapDumpBtn = tester.getTagByWicketId("dumpHeap");
+        assertNotNull("heap thread button not present", heapDumpBtn);
+        assertTrue(
+                "href wrong on dump heap button",
+                heapDumpBtn.getAttribute("href").contains("tabs-panel-dumpHeap"));
+    }
+
+    @Test
+    public void testLoadThreadDump() {
+        tester.clickLink("tabs:panel:dumpThread");
+        final String threadDump = tester.getTagByWicketId("dumpContent").getValue();
+        assertTrue(threadDump.contains("RUNNABLE"));
+    }
+
+    @Test
+    public void testLoadHeapDump() {
+        tester.clickLink("tabs:panel:dumpHeap");
+        final String headDumpStr = tester.getTagByWicketId("dumpContent").getValue();
+        assertTrue("heap doesn't contains column num", headDumpStr.contains("num"));
+        assertTrue("heap doesn't contains column instances", headDumpStr.contains("#instances"));
+        assertTrue("heap doesn't contains column bytes", headDumpStr.contains("#bytes"));
     }
 }

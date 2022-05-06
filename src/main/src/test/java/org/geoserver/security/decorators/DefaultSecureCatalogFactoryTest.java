e9e09f27d992f6a202c945ec099f2a6ab7818b92
ahmed.ababneh@geosolutionsgroup.com <ahmed.ababneh@geosolutionsgroup.com>
2022-05-06 17:31:19 +0300

    [GEOS-10430] REST API failures

---
+++ src/main/src/test/java/org/geoserver/security/decorators/DefaultSecureCatalogFactoryTest.java
@@ -0,0 +1,44 @@
+package org.geoserver.security.decorators;
+
+import org.easymock.EasyMock;
+import org.geoserver.security.WrapperPolicy;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DefaultSecureCatalogFactoryTest {
+    DefaultSecureCatalogFactory defaultSecureCatalogFactory = new DefaultSecureCatalogFactory();
+    WrapperPolicy wrapperPolicy = EasyMock.createMock(WrapperPolicy.class);
+
+    @Before
+    public void setup() {
+        defaultSecureCatalogFactory = new DefaultSecureCatalogFactory();
+        wrapperPolicy = EasyMock.createMock(WrapperPolicy.class);
+    }
+
+    @Test
+    public void secureWMSStoreInfo() {
+        // Given
+        SecuredWMSStoreInfo securedWMSStoreInfo = EasyMock.createMock(SecuredWMSStoreInfo.class);
+
+        // When
+        Object object = defaultSecureCatalogFactory.secure(securedWMSStoreInfo, wrapperPolicy);
+
+        // Then
+        Assert.assertTrue(object instanceof SecuredWMSStoreInfo);
+    }
+
+    @Test
+    public void secureWMTSStoreInfo() {
+        // Given
+        DefaultSecureCatalogFactory defaultSecureCatalogFactory = new DefaultSecureCatalogFactory();
+        WrapperPolicy wrapperPolicy = EasyMock.createMock(WrapperPolicy.class);
+        SecuredWMTSStoreInfo securedWMTSStoreInfo = EasyMock.createMock(SecuredWMTSStoreInfo.class);
+
+        // When
+        Object object = defaultSecureCatalogFactory.secure(securedWMTSStoreInfo, wrapperPolicy);
+
+        // Then
+        Assert.assertTrue(object instanceof SecuredWMTSStoreInfo);
+    }
+}

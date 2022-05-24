a7c73bbff4eade5ebd28ba81d9eaf11a44d2c48e
Faroque Mohiuddin Ahmed <faroque@afrigis.co.za>
2022-05-24 13:50:57 +0600

    [GEOS-10505] Add license header

--- src/web/core/src/main/java/org/geoserver/web/wicket/StyleFormatLabel.java
+++ src/web/core/src/main/java/org/geoserver/web/wicket/StyleFormatLabel.java
@@ -1,4 +1,7 @@
-/** */
+/* (c) 2022 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.web.wicket;
 
 import java.util.logging.Logger;
@@ -9,7 +12,6 @@ import org.apache.wicket.model.IModel;
 import org.geoserver.catalog.Styles;
 import org.geotools.util.logging.Logging;
 
-/** @author hp */
 public class StyleFormatLabel extends Panel {
 
     static final Logger LOGGER = Logging.getLogger(StyleFormatLabel.class);

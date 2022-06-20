99f3ead992312159583f1ea786ca969ea97745fa
Marco Volpini <46672505+taba90@users.noreply.github.com>
2022-06-20 16:51:38 +0200

    [GEOS-10555] OpenID Filter - Add possibility to configure sending of client secret in token response (#5973) (#5977)

---
+++ src/community/security/oauth2-openid-connect/src/main/java/org/geoserver/security/oauth2/ClientSecretRequestEnhancer.java
@@ -0,0 +1,24 @@
+/* (c) 2022 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.security.oauth2;
+
+import java.util.Arrays;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.AccessTokenRequest;
+import org.springframework.security.oauth2.client.token.RequestEnhancer;
+import org.springframework.util.MultiValueMap;
+
+/** This class is used to add to Token Requests the client_secret in the query string. */
+public class ClientSecretRequestEnhancer implements RequestEnhancer {
+    @Override
+    public void enhance(
+            AccessTokenRequest request,
+            OAuth2ProtectedResourceDetails resource,
+            MultiValueMap<String, String> form,
+            HttpHeaders headers) {
+        form.put("client_secret", Arrays.asList(resource.getClientSecret()));
+    }
+}

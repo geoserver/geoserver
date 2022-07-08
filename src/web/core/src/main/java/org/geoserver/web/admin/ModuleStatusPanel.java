b11ae4c1cce7ee0e8ecd8dc31f9d3144e3ae007b
Jody Garnett <jody.garnett@gmail.com>
2022-07-08 11:03:56 -0700

    [GEOS-10580] Module Status missing version, environment and property information
    Sort module status by module name

--- src/web/core/src/main/java/org/geoserver/web/admin/ModuleStatusPanel.java
+++ src/web/core/src/main/java/org/geoserver/web/admin/ModuleStatusPanel.java
@@ -53,9 +53,8 @@ public class ModuleStatusPanel extends Panel {
         // get the list of ModuleStatuses
         List<ModuleStatus> applicationStatus =
                 GeoServerExtensions.extensions(ModuleStatus.class).stream()
-                        .filter(status -> !status.getModule().matches("\\A[system-](.*)"))
                         .map(ModuleStatusImpl::new)
-                        .sorted(Comparator.comparing(ModuleStatus::getModule))
+                        .sorted(Comparator.comparing(ModuleStatus::getName))
                         .collect(Collectors.toList());
 
         final ListView<ModuleStatus> moduleView =

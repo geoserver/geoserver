49719d0537912b25f43bbd742a83d0d1fd02a050
Jody Garnett <jody.garnett@gmail.com>
2022-08-15 18:38:05 -0700

    Look up WMSServiceInfo

--- src/web/wms/src/main/java/org/geoserver/wms/web/WMSServiceDescriptionProvider.java
+++ src/web/wms/src/main/java/org/geoserver/wms/web/WMSServiceDescriptionProvider.java
@@ -5,56 +5,121 @@
 package org.geoserver.wms.web;
 
 import org.apache.wicket.util.string.Strings;
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.LayerGroupInfo;
+import org.geoserver.catalog.LayerInfo;
+import org.geoserver.catalog.NamespaceInfo;
+import org.geoserver.catalog.PublishedInfo;
+import org.geoserver.catalog.WorkspaceInfo;
+import org.geoserver.config.GeoServer;
+import org.geoserver.ows.LocalWorkspaceCallback;
 import org.geoserver.platform.GeoServerExtensions;
 import org.geoserver.platform.Service;
 import org.geoserver.util.InternationalStringUtils;
 import org.geoserver.web.ServiceDescriptionProvider;
 import org.geoserver.web.ServicesPanel;
+import org.geoserver.wms.WMS;
 import org.geoserver.wms.WMSInfo;
 import org.geoserver.wms.WebMapService;
+import org.geotools.feature.NameImpl;
+import org.geotools.util.logging.Logging;
 import org.opengis.util.InternationalString;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Provide description of WMS services for welcome page.
  */
 public class WMSServiceDescriptionProvider implements ServiceDescriptionProvider {
 
-    public List<ServicesPanel.ServiceDescription> getServices(String workspace, String layer){
-        List<ServicesPanel.ServiceDescription> descriptions = new ArrayList<>();
-        List<Service> extensions = GeoServerExtensions.extensions(Service.class);
+    static final Logger LOGGER = Logging.getLogger(LocalWorkspaceCallback.class);
 
-        for (Service service : extensions) {
-            if(service.getService() instanceof WebMapService){
-                WebMapService wms = (WebMapService) service.getService();
-                WMSInfo info = wms.getServiceInfo();
+    GeoServer geoserver;
+    Catalog catalog;
 
-                String serviceId = service.getId();
-                boolean available = info.isEnabled();
+    public WMSServiceDescriptionProvider(GeoServer gs) {
+        this.geoserver = gs;
+        catalog = gs.getCatalog();
+    }
 
-                InternationalString title = InternationalStringUtils.growable(
-                    info.getInternationalTitle(),
-                    Strings.isEmpty(info.getTitle()) ? serviceId.toUpperCase() : info.getTitle()
-                );
-                InternationalString description = InternationalStringUtils.growable(
-                    info.getInternationalAbstract(),
-                    Strings.isEmpty(info.getAbstract()) ? null : info.getAbstract()
-                );
-                if (workspace == null) {
-                    ServicesPanel.ServiceDescription serviceDescription =  new ServicesPanel.ServiceDescription(
-                            serviceId.toLowerCase(),
-                            title,
-                            description,
-                            available,
-                            null,
-                            null);
-                    if (!descriptions.contains(serviceDescription)) {
-                        descriptions.add(serviceDescription);
-                    }
-                }
+    /**
+     * Lookup WMSInfo using workspaceName / layerName conotext.
+     * @param workspaceName Name of workspace, or global layer group
+     * @param layerName Name of layer or layer group
+     * @return WMSInfo if available for workspaceName, or {@code null} for global WMSInfo
+     */
+    protected WMSInfo info(String workspaceName, String layerName){
+        WMSInfo wmsInfo = null;
+        WorkspaceInfo workspaceInfo = catalog.getWorkspaceByName(workspaceName);
+        if (workspaceInfo != null) {
+           return geoserver.getService(workspaceInfo,WMSInfo.class);
+        }
+        return null;
+    }
+
+    /**
+     * Look up published info using page workspace / layer context (see {@code LocalWorkspaceCallback}).
+     *
+     * @param workspaceName Name of workspace, or global layer group
+     * @param layerName Name of layer or layer group
+     * @return PublishedInfo representing layer info or group info, or {@code null} if not found
+     */
+    protected PublishedInfo layer(String workspaceName, String layerName){
+        WorkspaceInfo workspaceInfo = catalog.getWorkspaceByName(workspaceName);
+        if(workspaceInfo != null){
+            LayerGroupInfo groupInfo = catalog.getLayerGroupByName(null, workspaceName);
+            if (groupInfo!=null){
+                return groupInfo;
             }
+        }
+        else {
+            NamespaceInfo namespaceInfo = catalog.getNamespaceByPrefix(workspaceInfo.getName());
+            LayerInfo layerInfo = catalog.getLayerByName(new NameImpl(namespaceInfo.getURI(), layerName));
+            if(layerInfo != null){
+                return layerInfo;
+            }
+            LayerGroupInfo groupInfo = catalog.getLayerGroupByName(workspaceInfo, layerName);
+            if (groupInfo!=null){
+                return groupInfo;
+            }
+        }
+        return null;
+    }
+
+    public List<ServicesPanel.ServiceDescription> getServices(String workspaceName, String layerName){
+        List<ServicesPanel.ServiceDescription> descriptions = new ArrayList<>();
+
+        WMSInfo virutalWMS = info(workspaceName, layerName);
+        WMSInfo globalWMS = geoserver.getService(WMSInfo.class);
+        PublishedInfo layerInfo = layer(workspaceName,layerName);
+
+        LayerInfo
+
+        WMSInfo info = info(workspaceName);
+        String serviceId = "wms";
+        boolean available = info.isEnabled();
+
+
+
+        InternationalString title = InternationalStringUtils.growable(
+            info.getInternationalTitle(),
+            Strings.isEmpty(info.getTitle()) ? serviceId.toUpperCase() : info.getTitle()
+        );
+        InternationalString description = InternationalStringUtils.growable(
+            info.getInternationalAbstract(),
+            Strings.isEmpty(info.getAbstract()) ? null : info.getAbstract()
+        );
+        ServicesPanel.ServiceDescription serviceDescription =  new ServicesPanel.ServiceDescription(
+            serviceId.toLowerCase(),
+            title,
+            description,
+            available,
+            null,
+            null);
+        if (!descriptions.contains(serviceDescription)) {
+            descriptions.add(serviceDescription);
         }
         return descriptions;
     }

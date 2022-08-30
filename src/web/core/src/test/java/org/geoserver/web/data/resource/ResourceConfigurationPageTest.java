2cfd64231131a5d1b558b5ce87666aa76bc6f82b
Andrea Aime <andrea.aime@gmail.com>
2022-08-30 11:13:23 +0200

    [GEOS-10598] XSS vulnerability in the email address field - fix too eager escape

--- src/web/core/src/test/java/org/geoserver/web/data/resource/ResourceConfigurationPageTest.java
+++ src/web/core/src/test/java/org/geoserver/web/data/resource/ResourceConfigurationPageTest.java
@@ -172,7 +172,6 @@ public class ResourceConfigurationPageTest extends GeoServerWicketTestSupport {
                     }
                 });
         tester.executeAjaxEvent(page, "ondblclick");
-        print(tester.getLastRenderedPage(), true, true);
 
         // verify contents were updated
         tester.assertContains("the_geom");
@@ -367,8 +366,6 @@ public class ResourceConfigurationPageTest extends GeoServerWicketTestSupport {
 
         login();
         tester.startPage(new ResourceConfigurationPage(layer, false));
-
-        print(tester.getLastRenderedPage(), true, true);
 
         FormTester ft = tester.newFormTester("publishedinfo");
         String newTitle = "A test title";

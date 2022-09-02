e96c7994ef9af439523de59fc99f5e8c3ba89447
Marco Volpini <tabareu90@gmail.com>
2022-09-02 11:52:50 +0200

    [GEOS-10644] Keycloak - Improvements to Role Service

---
+++ src/community/security/keycloak/src/main/java/org/geoserver/security/keycloak/KeycloakUrlBuilder.java
@@ -0,0 +1,123 @@
+package org.geoserver.security.keycloak;
+
+/** A builder for a keycloak rest endpoint. */
+class KeycloakUrlBuilder {
+
+    private StringBuilder sb;
+    private String realm;
+    private String serverUrl;
+
+    KeycloakUrlBuilder(String realm, String serverUrl) {
+        this.realm = realm;
+        this.serverUrl = serverUrl;
+        init();
+    }
+
+    private void init() {
+        StringBuilder sb = new StringBuilder(serverUrl);
+        sb.append("/auth/admin/realms/").append(realm);
+        this.sb = sb;
+    }
+
+    /**
+     * Adds the client path part.
+     *
+     * @param clientId the client id.
+     * @return this builder.
+     */
+    KeycloakUrlBuilder client(String clientId) {
+        sb.append("/clients/").append(clientId);
+        return this;
+    }
+
+    /**
+     * add the path part and query string to retrieve a user by its username.
+     *
+     * @param userName the username.
+     * @return this builder.
+     */
+    KeycloakUrlBuilder userByName(String userName) {
+        users();
+        sb.append("?exact=true&username=").append(userName);
+        return this;
+    }
+
+    /**
+     * Adds the users path part.
+     *
+     * @return this builder.
+     */
+    KeycloakUrlBuilder users() {
+        sb.append("/users");
+        return this;
+    }
+
+    /**
+     * Adds the user and userid path part.
+     *
+     * @param userId the user id.
+     * @return this builder.
+     */
+    KeycloakUrlBuilder userById(String userId) {
+        sb.append("/users/").append(userId);
+        return this;
+    }
+
+    /**
+     * Adds the roles path part.
+     *
+     * @return this builder.
+     */
+    KeycloakUrlBuilder allRoles() {
+        sb.append("/roles");
+        return this;
+    }
+
+    /**
+     * Add the roles path part when requesting user's roles.
+     *
+     * @param realmOnly if true will append the realm path part to limit the result to realm roles
+     *     only.
+     * @return this builder.
+     */
+    KeycloakUrlBuilder allUserRoles(boolean realmOnly) {
+        sb.append("/role-mappings");
+        if (realmOnly) sb.append("/realm");
+        return this;
+    }
+
+    /**
+     * add the role path part.
+     *
+     * @param roleName the role name.
+     * @return this builder.
+     */
+    KeycloakUrlBuilder role(String roleName) {
+        allRoles();
+        sb.append("/").append(roleName);
+        return this;
+    }
+
+    /**
+     * Build the url.
+     *
+     * @return the url as a string.
+     */
+    String build() {
+        String result = sb.toString();
+        init();
+        return result;
+    }
+
+    /**
+     * Build the access token endpoint.
+     *
+     * @return the access token endpoint.
+     */
+    String buildTokenEndpoint() {
+        StringBuilder sb = new StringBuilder(serverUrl);
+        sb.append("/auth/realms/").append(realm);
+        String result = sb.append("/protocol/openid-connect/token").toString();
+        return result;
+    }
+}

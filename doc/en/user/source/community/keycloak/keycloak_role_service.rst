b8931d864dd9d1424d048c42852699fc1a2a6fd6
Marco Volpini <46672505+taba90@users.noreply.github.com>
2022-09-02 15:58:01 +0200

    [GEOS-10644] Keycloak - Improvements to Role Service (#6155)

--- doc/en/user/source/community/keycloak/keycloak_role_service.rst
+++ doc/en/user/source/community/keycloak/keycloak_role_service.rst
@@ -5,17 +5,22 @@ Keycloak Role Service
 
 This tutorial walks through how to set up Keycloak as a role service for GeoServer.
 
-.. note:: In this example the Keycloak service runs on port `8080` while GeoServer runs on port `8181`
+.. note:: In this example the Keycloak service runs on port `8080` while GeoServer runs on port `8181`.
+
+The Keycloak Role Service uses the Keycloak REST api in order to retrieve the roles for its various operations. By default it will retrieve roles with ``realm`` scope.
+However it can be configured to retrieve roles with a client scope in a specific realm.
 
 Keycloak Client Configuration
 -----------------------------
 
 Follow the `Authentication with Keycloak <https://docs.geoserver.org/latest/en/user/community/keycloak/index.html>`_
-guide to configure GeoServer to allow logging in via Keycloak. Two clients will need to be configured - one with the
-confidential access type, and another with the public access type. Only one authentication filter is required, and it is
-recommended to use the confidential client.
+guide to configure GeoServer to allow logging in via Keycloak. The Keycloak Role Service needs a client to be configured on Keycloak side having ``Access Type`` set to ``confidential``.
 
-For both clients, ensure that:
+
+If for your Keycloak Authentication Filter you have used a different ``Access Type`` i.e. ``barer-only``, a separate client will have then to be configured for the Keycloak Role Service.
+
+
+For the client, ensure that:
 
 * Standard flow, implicit flow, and direct access grants are enabled
 * The base URL is set to ``http://localhost:8181/geoserver/web``
@@ -24,36 +29,26 @@ For both clients, ensure that:
   * ``http://localhost:8181/geoserver*``
   * ``http://localhost:8080/auth/realms/master/broker/keycloak-oidc/endpoint*``
 
-For the public client, additionally ensure that:
+* The ``Access Type`` is set to **confidential** and the ``Service Accounts Enabled`` option is enabled.
 
-* The access type is set to **public**
-* The redirect URI ``http://localhost:8181/*`` is enabled
-* Web origins is set to ``*``
-
-For the confidential client, additionally ensure that:
-
-* The access type is set to **confidential**
-* Service accounts are enabled
-* Under the Service Account Roles tab, the **realm** role "admin" is assigned
-
-Under the Roles tab of the confidential client, create a ``ROLE_ADMINISTRATOR`` role. For any user that should have
-administrative privileges in GeoServer, assign them the ``ROLE_ADMINISTRATOR`` role.
-
-.. figure:: images/role_service/keycloak_client001.png
+.. figure:: images/role_service/access_type_confidential.png
    :align: center
 
-   *An example set of roles for GeoServer-client, with all required roles and an additional staff role.*
+* Under the ``Service Account Roles`` tab, ensure that the realm-admin, from the realm-management client role is addedd to the ``Assigned roles``.
+
+.. figure:: images/role_service/service_account_roles.png
+   :align: center
 
 To assign a user a role:
 
 #. Under the users section in Keycloak, click the user's ID (if there are missing users, click "View all users").
 #. In the role mappings tab, select the GeoStore client from the client roles dropdown.
-#. Select the ``ROLE_ADMINISTRATOR`` role from the available roles, and click add selected.
+#. Select the role from the available roles, and click add selected.
 
 .. figure:: images/role_service/keycloak_client002.png
    :align: center
 
-   *An example set of role mappings for a user who should be an administrator and staff in GeoServer.*
+   *An example set of role mappings for a user.*
 
 When creating custom roles, ensure they begin with ``ROLE_`` e.g. ROLE_STAFF.
 
@@ -62,15 +57,20 @@ GeoServer Configuration for Role Syncing
 Role syncing with Keycloak will be tied to the confidential client.
 
 #. In GeoServer as an admin, on the 'Users, Groups, Roles' page, add a new role service.
-#. Select Keycloak from the list of provided options. All fillable fields are required, excluding the administrator and group administrator role dropdowns, which should initially have no options.
+#. Select Keycloak from the list of provided options. All fillable fields are required, excluding the ``Comma separated list of ID of client (not client-id)``.
 
-   * Base URL for Keycloak e.g. http://localhost:8080
-   * The ID of the client may be retrieved from the URL when viewing the GeoServer client in Keycloak. URL format: ``/auth/admin/master/console/#/realms/master/clients/{ID of client}``
-   * The client secret is under the GeoServer client's credentials tab in Keycloak
+   * ``Base URL for Keycloak`` is the keycloak host name eg. http://localhost:8080.
+   * The ``Realm Name`` is the realm from which the roles should be retrieved eg. master.
+   * The ``Client ID`` can be retrieved from the ``Settings`` tab of the client configuration on Keycloak.
+   * The ``Client secret`` can be retrieved from the ``Credentials`` tab of the client configuration on Keycloak.
+   * The ``Comma separated list of ID of client (not client-id)`` is meant to allow the Role Service to retrieve also roles with client scope. By default indeed the Keycloak Role Service will retrieve realm roles only. The id of a client can be retrieved from the URL when viewing the client configuration page in Keycloak. URL format: eg. ``/auth/admin/master/console/#/realms/master/clients/{ID of client}``
+
+ ``Administrator Role`` and ``Group administator`` role dropdown should be empty at the beginning. They can be filled once saved the role service with the Keycloak role that we want to map to the GeoServer ADMIN and GROUP ADMIN.
 
 #. Ensure you click save to create the Keycloak role service.
-#. Once the role service has been created, open the role service again and assign the administrator and group administrator roles to ``ADMINISTRATOR``.
-#. On the 'Security Settings' page, set the active role service to be the Keycloak role service
+#. Once the Role Service has been created and configured to have it active:
+   * it can be assigned as a RoleSource to the Keycloak Filter,
+   * it can be set as the ``Active Role Service``  in the ``Security Settings`` page.
 
 .. figure:: images/role_service/keycloak_role_service001.png
    :align: center
--- src/community/security/keycloak/src/main/java/org/geoserver/security/keycloak/KeycloakRoleService.java
+++ src/community/security/keycloak/src/main/java/org/geoserver/security/keycloak/KeycloakRoleService.java
@@ -4,31 +4,23 @@
  */
 package org.geoserver.security.keycloak;
 
-import com.google.gson.Gson;
-import com.google.gson.internal.LinkedTreeMap;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.StatusLine;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 import org.geoserver.platform.resource.Resource;
 import org.geoserver.security.GeoServerRoleService;
 import org.geoserver.security.GeoServerRoleStore;
@@ -48,22 +40,22 @@ public class KeycloakRoleService extends AbstractGeoServerSecurityService
     protected SortedSet<GeoServerRole> emptySet;
     protected SortedSet<String> emptyStringSet;
     protected Map<String, String> parentMappings;
-    protected HashMap<String, GeoServerRole> roleMap;
     protected SortedSet<GeoServerRole> roleSet;
-    protected Set<RoleLoadedListener> listeners =
-            Collections.synchronizedSet(new HashSet<RoleLoadedListener>());
+    protected Set<RoleLoadedListener> listeners = Collections.synchronizedSet(new HashSet<>());
 
-    private String serverURL;
-    private String realm;
-    private String clientID;
-    private String idOfClient;
-    private String clientSecret;
+    private KeycloakRESTClient client;
+
+    private LoadingCache<String, SortedSet<GeoServerRole>> userRolesCache;
+
+    private LoadingCache<String, Optional<GeoServerRole>> rolesCache;
+
+    private LoadingCache<String, SortedSet<String>> usersInRoleCache;
+
+    private Integer roleCacheExpiresAfter = 60;
 
     public KeycloakRoleService() throws IOException {
-        emptySet = Collections.unmodifiableSortedSet(new TreeSet<GeoServerRole>());
-        emptyStringSet = Collections.unmodifiableSortedSet(new TreeSet<String>());
-        parentMappings = new HashMap<String, String>();
-        load();
+        emptySet = Collections.unmodifiableSortedSet(new TreeSet<>());
+        emptyStringSet = Collections.unmodifiableSortedSet(new TreeSet<>());
     }
 
     @Override
@@ -72,15 +64,23 @@ public class KeycloakRoleService extends AbstractGeoServerSecurityService
         adminRoleName = ((SecurityRoleServiceConfig) config).getAdminRoleName();
         groupAdminRoleName = ((SecurityRoleServiceConfig) config).getGroupAdminRoleName();
 
-        if (config instanceof KeycloakSecurityServiceConfig) {
-            KeycloakSecurityServiceConfig keycloakConfig = (KeycloakSecurityServiceConfig) config;
-            serverURL = keycloakConfig.getServerURL();
-            realm = keycloakConfig.getRealm();
-            clientID = keycloakConfig.getClientID();
-            idOfClient = keycloakConfig.getIdOfClient();
-            clientSecret = keycloakConfig.getClientSecret();
+        if (config instanceof KeycloakRoleServiceConfig) {
+            KeycloakRoleServiceConfig keycloakConfig = (KeycloakRoleServiceConfig) config;
+            List<String> idsOfClients = null;
+            if (!StringUtils.isEmpty(keycloakConfig.getIdsOfClientsList())) {
+                idsOfClients = Arrays.asList(keycloakConfig.getIdsOfClientsList().split(","));
+            }
+            this.client =
+                    new KeycloakRESTClient(
+                            keycloakConfig.getServerURL(),
+                            keycloakConfig.getRealm(),
+                            keycloakConfig.getClientID(),
+                            keycloakConfig.getClientSecret(),
+                            idsOfClients);
         }
-
+        this.rolesCache = buildCache(new RoleCacheLoader(), roleCacheExpiresAfter);
+        this.userRolesCache = buildCache(new UserRoleCacheLoader(), roleCacheExpiresAfter);
+        this.usersInRoleCache = buildCache(new RoleUsersCacheLoader(), roleCacheExpiresAfter);
         load();
     }
 
@@ -130,7 +130,9 @@ public class KeycloakRoleService extends AbstractGeoServerSecurityService
      */
     @Override
     public SortedSet<GeoServerRole> getRoles() throws IOException {
-        if (roleSet != null) return roleSet;
+        if (roleSet != null) {
+            return roleSet;
+        }
         return emptySet;
     }
 
@@ -140,26 +142,10 @@ public class KeycloakRoleService extends AbstractGeoServerSecurityService
     @Override
     public synchronized void load() throws IOException {
         LOGGER.config("Start reloading roles for service named " + getName());
-
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            Gson gson = new Gson();
-            LOGGER.config("Obtaining access token for Keycloak");
-            String accessToken = getAccessToken(httpClient, gson);
-            if (accessToken == null || accessToken.trim().isEmpty()) {
-                return;
-            }
-
-            LOGGER.config("Retrieving roles from Keycloak");
-            List<GeoServerRole> roles = getRoles(httpClient, gson, accessToken);
-            if (roles == null) {
-                return;
-            }
-            roleSet = new TreeSet<GeoServerRole>();
-            roleSet.addAll(roles);
-
-            LOGGER.config("Reloading roles successful for service named " + getName());
-            fireRoleLoadedEvent();
-        }
+        roleSet = new TreeSet<>();
+        roleSet.addAll(client.getRoles());
+        LOGGER.config("Reloading roles successful for service named " + getName());
+        fireRoleLoadedEvent();
     }
 
     /* (non-Javadoc)
@@ -167,7 +153,15 @@ public class KeycloakRoleService extends AbstractGeoServerSecurityService
      */
     @Override
     public SortedSet<GeoServerRole> getRolesForUser(String username) throws IOException {
-        return emptySet;
+        SortedSet<GeoServerRole> roles = null;
+        try {
+            roles = userRolesCache.get(username);
+        } catch (ExecutionException e) {
+            LOGGER.log(Level.SEVERE, "Error while getting roles of user.", e.getCause());
+            throw new RuntimeException(e);
+        }
+        if (roles == null) roles = emptySet;
+        return roles;
     }
 
     /* (non-Javadoc)
@@ -199,8 +193,14 @@ public class KeycloakRoleService extends AbstractGeoServerSecurityService
      */
     @Override
     public GeoServerRole getRoleByName(String role) throws IOException {
-        if (roleMap != null) return roleMap.get(role);
-        return null;
+        try {
+            Optional<GeoServerRole> op = rolesCache.get(role);
+            if (op.isPresent()) return op.get();
+            return null;
+        } catch (ExecutionException e) {
+            LOGGER.log(Level.SEVERE, "Exception while retrieving role by name,", e.getCause());
+            throw new RuntimeException(e);
+        }
     }
 
     /** Fire {@link RoleLoadedEvent} for all listeners */
@@ -224,7 +224,15 @@ public class KeycloakRoleService extends AbstractGeoServerSecurityService
      */
     @Override
     public SortedSet<String> getUserNamesForRole(GeoServerRole role) throws IOException {
-        return emptyStringSet;
+        try {
+            return usersInRoleCache.get(role.getAuthority());
+        } catch (ExecutionException e) {
+            LOGGER.log(
+                    Level.SEVERE,
+                    "Error while retrieving user names in role " + role.getAuthority(),
+                    e.getCause());
+            throw new RuntimeException(e);
+        }
     }
 
     /* (non-Javadoc)
@@ -258,98 +266,52 @@ public class KeycloakRoleService extends AbstractGeoServerSecurityService
         return 0;
     }
 
-    /**
-     * Extract the message (body) from the given HTTP response.
-     *
-     * @param response the CloseableHttpResponse to retrieve the message from
-     * @return the content of the response entity, as a String
-     * @throws Exception
-     */
-    private String getStringResponseMessage(CloseableHttpResponse response) throws Exception {
-        HttpEntity responseEntity = response.getEntity();
-        if (responseEntity == null) {
-            throw new NullPointerException("HTTP response from Keycloak contained no message");
-        }
+    private <T> LoadingCache<String, T> buildCache(
+            CacheLoader<String, T> cacheLoader, Integer rolesCacheExpireAfter) {
+        return CacheBuilder.newBuilder()
+                .maximumSize(1000)
+                .initialCapacity(100)
+                .expireAfterAccess(rolesCacheExpireAfter, TimeUnit.MINUTES)
+                .build(cacheLoader);
+    }
 
-        try (InputStream contentStream = responseEntity.getContent()) {
-            return IOUtils.toString(contentStream, StandardCharsets.UTF_8);
+    private class RoleCacheLoader extends CacheLoader<String, Optional<GeoServerRole>> {
+
+        @Override
+        public Optional<GeoServerRole> load(String s) throws Exception {
+            return Optional.ofNullable(client.getRoleByName(s));
         }
     }
 
-    /**
-     * Obtain an access token from Keycloak, using the server URL, realm name, and client secret
-     * from the config. Returns null if an error occurs.
-     *
-     * @param httpClient a CloseableHttpClient to send the request through
-     * @return a String access token on success, null on error.
-     */
-    public String getAccessToken(CloseableHttpClient httpClient, Gson gson) {
-        HttpPost httpPost =
-                new HttpPost(
-                        this.serverURL
-                                + "/auth/realms/"
-                                + this.realm
-                                + "/protocol/openid-connect/token");
-        String body =
-                "client_id="
-                        + this.clientID
-                        + "&client_secret="
-                        + this.clientSecret
-                        + "&grant_type=client_credentials";
-        HttpEntity entity = new StringEntity(body, ContentType.APPLICATION_FORM_URLENCODED);
-        httpPost.setEntity(entity);
+    private class UserRoleCacheLoader extends CacheLoader<String, SortedSet<GeoServerRole>> {
 
-        try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
-            StatusLine statusLine = response.getStatusLine();
-            if (statusLine == null || statusLine.getStatusCode() != 200) {
-                LOGGER.info("Issue retrieving access token: " + statusLine);
-                return null;
+        @Override
+        public SortedSet<GeoServerRole> load(String key) {
+            try {
+                return client.getUserRoles(key);
+            } catch (IOException e) {
+                LOGGER.log(
+                        Level.SEVERE,
+                        "Error while retrieve user " + key + " roles from keycloak.",
+                        e);
             }
-            String jsonString = getStringResponseMessage(response);
-            Map<?, ?> map = gson.fromJson(jsonString, Map.class);
-            return map.get("access_token").toString();
-        } catch (Exception exception) {
-            LOGGER.info(exception.getMessage());
-            return null;
+            return emptySet;
         }
     }
 
-    /**
-     * Retrieve a List of GeoServerRole objects from Keyclaok, using the server URL, realm name, and
-     * ID of client from the config. Returns null if an error occurs.
-     *
-     * @param httpClient a CloseableHttpClient to send the request through
-     * @param accessToken an access token for the geoserver-client user
-     * @return a List of GeoServerRole objects from Keycloak on success, null on error.
-     */
-    public List<GeoServerRole> getRoles(
-            CloseableHttpClient httpClient, Gson gson, String accessToken) {
-        HttpGet httpGet =
-                new HttpGet(
-                        this.serverURL
-                                + "/auth/admin/realms/"
-                                + this.realm
-                                + "/clients/"
-                                + this.idOfClient
-                                + "/roles");
-        httpGet.setHeader("Authorization", "Bearer " + accessToken);
-        try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
-            StatusLine statusLine = response.getStatusLine();
-            if (statusLine == null || statusLine.getStatusCode() != 200) {
-                LOGGER.info("Issue retrieving roles: " + statusLine);
-                return null;
-            }
-            String jsonString = getStringResponseMessage(response);
-            List<GeoServerRole> roles = new ArrayList<>();
-            for (Object obj : gson.fromJson(jsonString, List.class)) {
-                LinkedTreeMap<?, ?> role = (LinkedTreeMap<?, ?>) obj;
-                roles.add(createRoleObject(role.get("name").toString()));
-            }
-            return roles;
-        } catch (Exception exception) {
-            LOGGER.info(exception.getMessage());
-        }
+    private class RoleUsersCacheLoader extends CacheLoader<String, SortedSet<String>> {
 
-        return null;
+        @Override
+        public SortedSet<String> load(String key) {
+            try {
+                return client.getUserInRole(key);
+            } catch (IOException e) {
+                LOGGER.log(
+                        Level.SEVERE,
+                        "Error while retrieve users in role " + key + " from keycloak.",
+                        e);
+            }
+            return emptyStringSet;
+        }
     }
 }

c94890853a135411d1057bcb21b4ee2189789579
david.blasby <david.blasby@geocat.net>
2022-11-29 16:37:58 -0800

    fix formatting issues

--- src/wfs/src/test/java/org/geoserver/wfs/response/HitsOutputFormatTest.java
+++ src/wfs/src/test/java/org/geoserver/wfs/response/HitsOutputFormatTest.java
@@ -4,25 +4,24 @@
  */
 package org.geoserver.wfs.response;
 
+import static org.junit.Assert.assertEquals;
+
 import org.geoserver.platform.Operation;
 import org.geoserver.platform.Service;
 import org.geotools.util.Version;
 import org.junit.Test;
 
-
-import static org.junit.Assert.assertEquals;
-
 public class HitsOutputFormatTest {
 
     @Test
     public void testMimeType() {
-        HitsOutputFormat hitsOutputFormat = new HitsOutputFormat(null,null);
+        HitsOutputFormat hitsOutputFormat = new HitsOutputFormat(null, null);
 
-        //Operation and Service are final classes - difficult to mock
-        Service servicewfs11 = new Service("wfs11",null,null,new Version("1.1.0"),null);
-        Operation opwfs11 = new Operation("opwfs11",servicewfs11,null,null);
+        // Operation and Service are final classes - difficult to mock
+        Service servicewfs11 = new Service("wfs11", null, null, new Version("1.1.0"), null);
+        Operation opwfs11 = new Operation("opwfs11", servicewfs11, null, null);
 
-        String mime = hitsOutputFormat.getMimeType(null,opwfs11);
+        String mime = hitsOutputFormat.getMimeType(null, opwfs11);
         assertEquals("text/xml; subtype=gml/3.1.1", mime);
     }
 }

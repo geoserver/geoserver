be7705290a71db95f46149f1db3f922932581e0b
david.blasby <david.blasby@geocat.net>
2023-01-09 14:43:27 -0800

    add test case

--- src/wcs1_1/src/test/java/org/geoserver/wcs/GetCoverageTest.java
+++ src/wcs1_1/src/test/java/org/geoserver/wcs/GetCoverageTest.java
@@ -75,6 +75,42 @@ public class GetCoverageTest extends AbstractGetCoverageTest {
     }
 
     @Test
+    public void testMissingInterpolation() throws Exception {
+        String request =
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n"
+                        + "<wcs:GetCoverage service=\"WCS\" "
+                        + "xmlns:ows=\"http://www.opengis.net/ows/1.1\"\r\n"
+                        + "  xmlns:wcs=\"http://www.opengis.net/wcs/1.1.1\"\r\n"
+                        + "  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" \r\n"
+                        + "  xsi:schemaLocation=\"http://www.opengis.net/wcs/1.1.1 "
+                        + "                       schemas/wcs/1.1.1/wcsAll.xsd\"\r\n"
+                        + "  version=\"1.1.1\" >\r\n"
+                        + "  <ows:Identifier>wcs:BlueMarble</ows:Identifier>\r\n"
+                        + "  <wcs:DomainSubset>\r\n"
+                        + "    <ows:BoundingBox crs=\"urn:ogc:def:crs:EPSG:6.6:4326\">\r\n"
+                        + "      <ows:LowerCorner>-90 -180</ows:LowerCorner>\r\n"
+                        + "      <ows:UpperCorner>90 180</ows:UpperCorner>\r\n"
+                        + "    </ows:BoundingBox>\r\n"
+                        + "  </wcs:DomainSubset>\r\n"
+                        + "  <wcs:RangeSubset>\n"
+                        + "    <wcs:FieldSubset>\n"
+                        + "      <ows:Identifier>contents</ows:Identifier>\n"
+                        + "      <wcs:InterpolationType></wcs:InterpolationType>\n"
+                        + "    </wcs:FieldSubset>\n"
+                        + "  </wcs:RangeSubset>"
+                        + "  <wcs:Output format=\"image/tiff\"/>\r\n"
+                        + "</wcs:GetCoverage>";
+        try {
+            executeGetCoverageXml(request);
+            fail("missing interpolation - should have thrown an exception");
+        } catch (WcsException e) {
+            assertEquals(ServiceException.INVALID_PARAMETER_VALUE, e.getCode());
+            assertEquals("RangeSubset", e.getLocator());
+            assertTrue(e.getMessage().contains("InterpolationMethod"));
+        }
+    }
+
+    @Test
     public void testKvpBasic() throws Exception {
         Map<String, Object> raw = baseMap();
         final String getLayerId = getLayerId(TASMANIA_BM);

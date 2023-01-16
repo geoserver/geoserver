1da2347e5da194ce4ac6cc5b90a4e11e7c711e77
NielsCharlier <niels@scitus.be>
2023-01-16 12:17:04 +0100

    [GEOS-10795] csw-iso: support custom queryable mappings + bounding box (#6443)
    * [GEOS-10795] csw-iso: support custom queryable mappings + bounding box
    
    * [GEOS-10795] csw-iso: support custom queryable mappings + bounding box: add javadoc

--- src/extension/csw/csw-iso/src/main/java/org/geoserver/csw/records/iso/MetaDataDescriptor.java
+++ src/extension/csw/csw-iso/src/main/java/org/geoserver/csw/records/iso/MetaDataDescriptor.java
@@ -6,23 +6,21 @@ package org.geoserver.csw.records.iso;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import net.opengis.cat.csw20.ElementSetType;
-import org.geoserver.csw.records.AbstractRecordDescriptor;
+import org.geoserver.config.GeoServer;
 import org.geoserver.csw.records.CSWRecordDescriptor;
 import org.geoserver.csw.records.RecordFeatureTypeRegistryConfiguration;
 import org.geoserver.csw.records.SpatialFilterChecker;
+import org.geoserver.csw.store.internal.CatalogStoreMapping;
 import org.geoserver.platform.GeoServerExtensions;
 import org.geotools.csw.CSW;
-import org.geotools.data.Query;
 import org.geotools.data.complex.feature.type.FeatureTypeRegistry;
 import org.geotools.data.complex.util.EmfComplexFeatureReader;
+import org.geotools.data.complex.util.XPathUtil;
 import org.geotools.feature.NameImpl;
 import org.geotools.feature.TypeBuilder;
 import org.geotools.feature.type.FeatureTypeFactoryImpl;
-import org.geotools.filter.SortByImpl;
 import org.geotools.xsd.SchemaIndex;
 import org.locationtech.jts.geom.MultiPolygon;
 import org.opengis.feature.type.AttributeDescriptor;
@@ -31,8 +29,6 @@ import org.opengis.feature.type.FeatureType;
 import org.opengis.feature.type.FeatureTypeFactory;
 import org.opengis.feature.type.Name;
 import org.opengis.filter.Filter;
-import org.opengis.filter.expression.PropertyName;
-import org.opengis.filter.sort.SortBy;
 import org.xml.sax.helpers.NamespaceSupport;
 
 /**
@@ -41,7 +37,7 @@ import org.xml.sax.helpers.NamespaceSupport;
  *
  * @author Niels Charlier
  */
-public class MetaDataDescriptor extends AbstractRecordDescriptor {
+public class MetaDataDescriptor extends QueryableMappingRecordDescriptor {
 
     public static final NamespaceSupport NAMESPACES;
 
@@ -56,6 +52,8 @@ public class MetaDataDescriptor extends AbstractRecordDescriptor {
 
     public static final String NAMESPACE_XLINK = "http://www.w3.org/1999/xlink";
 
+    public static final String QUERYABLE_BBOX = "BoundingBox";
+
     public static FeatureType METADATA_TYPE;
     public static AttributeDescriptor METADATA_DESCRIPTOR;
 
@@ -66,8 +64,6 @@ public class MetaDataDescriptor extends AbstractRecordDescriptor {
     public static final List<Name> SUMMARY_ELEMENTS;
 
     public static final List<Name> QUERYABLES;
-
-    public static Map<String, PropertyName> QUERYABLE_MAPPING = new HashMap<>();
 
     static {
 
@@ -138,7 +134,7 @@ public class MetaDataDescriptor extends AbstractRecordDescriptor {
                         "Identifier",
                         "Title",
                         "Type",
-                        "BoundingBox",
+                        QUERYABLE_BBOX,
                         "GraphicOverview",
                         "ServiceType",
                         "ServiceTypeVersion");
@@ -150,7 +146,7 @@ public class MetaDataDescriptor extends AbstractRecordDescriptor {
                         "Creator",
                         "Contributor",
                         "CouplingType",
-                        "BoundingBox",
+                        QUERYABLE_BBOX,
                         "Format",
                         "FormatVersion",
                         "GraphicOverview",
@@ -179,98 +175,10 @@ public class MetaDataDescriptor extends AbstractRecordDescriptor {
                         "Type");
 
         QUERYABLES = SUMMARY_ELEMENTS;
-
-        // brief
-        addQueryableMapping("Identifier", "gmd:fileIdentifier/gco:CharacterString");
-        addQueryableMapping(
-                "Title",
-                "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:title/gco:CharacterString");
-        addQueryableMapping("Type", "gmd:hierarchyLevel/gmd:MD_ScopeCode/@codeListValue");
-        addQueryableMapping(
-                "BoundingBox",
-                "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:extent/gmd:EX_Extent/gmd:geographicElement/gmd:EX_GeographicBoundingBox");
-        addQueryableMapping(
-                "GraphicOverview",
-                "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:graphicOverview/gmd:MD_BrowseGraphic/gmd:fileName/gco:CharacterString");
-        addQueryableMapping(
-                "ServiceType",
-                "gmd:identificationInfo/gmd:SV_ServiceIdentification/gmd:serviceType/gco:CharacterString");
-        addQueryableMapping(
-                "ServiceTypeVersion",
-                "gmd:identificationInfo/gmd:SV_ServiceIdentification/gmd:serviceTypeVersion/gco:CharacterString");
-        // :summary
-        addQueryableMapping(
-                "Abstract",
-                "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:abstract/gco:CharacterString");
-        addQueryableMapping(
-                "CharacterSet",
-                "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:characterSet/gmd:MD_CharacterSetCode/@codeListValue");
-        addQueryableMapping(
-                "Creator",
-                "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:pointOfContact/gmd:CI_ResponsibleParty/gmd:organisationName[role/gmd:CI_RoleCode/gmd:@codeListValue=’originator’]/gco:CharacterString");
-        addQueryableMapping(
-                "Contributor",
-                "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:pointOfContact/gmd:CI_ResponsibleParty/gmd:organisationName[role/gmd:CI_RoleCode/gmd:@codeListValue=’author’]/gco:CharacterString");
-        addQueryableMapping(
-                "CouplingType",
-                "gmd:identificationInfo/gmd:SV_ServiceIdentification/gmd:couplingType/gmd:SV_CouplingType/gmd:code/@codeListValue");
-        addQueryableMapping(
-                "Format",
-                "gmd:distributionInfo/gmd:MD_Distribution/gmd:distributionFormat/gmd:MD_Format/gmd:name/gco:CharacterString");
-        addQueryableMapping(
-                "FormatVersion",
-                "gmd:distributionInfo/gmd:MD_Distribution/gmd:distributionFormat/gmd:MD_Format/gmd:version/gco:CharacterString");
-        addQueryableMapping("HierarchyLevelName", "gmd:hierarchyLevelName/gco:CharacterString");
-        addQueryableMapping("Language", "gmd:language/gco:CharacterString");
-        addQueryableMapping(
-                "Lineage",
-                "gmd:dataQualityInfo/gmd:DQ_DataQuality/gmd:lineage/gmd:LI_Lineage/gmd:statement/gco:CharacterString");
-        addQueryableMapping(
-                "MetadataCharacterSet", "gmd:characterSet/gmd:MD_ScopeCode/@codeListValue");
-        addQueryableMapping("MetadataStandardName", "gmd:metadataStandardName/gco:CharacterString");
-        addQueryableMapping(
-                "MetadataStandardVersion", "gmd:metadataStandardVersion/gco:CharacterString");
-        addQueryableMapping("Modified", "gmd:dateStamp/gmd:Date");
-        addQueryableMapping(
-                "OnlineResource",
-                "gmd:distributionInfo/gmd:MD_Distribution/gmd:transferOptions/gmd:MD_DigitalTransferOption/gmd:onLine/gmd:CI_OnlineResource/gmd:linkage/gmd:URL/gco:CharacterString");
-        addQueryableMapping("ParentIdentifier", "gmd:parentIdentifier/gco:CharacterString");
-        addQueryableMapping(
-                "Publisher",
-                "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:pointOfContact/gmd:CI_ResponsibleParty/gmd:organisationName[role/gmd:CI_RoleCode/gmd:@codeListValue=’publisher’]/gco:CharacterString");
-        addQueryableMapping(
-                "ResourceLanguage",
-                "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:language/gco:CharacterString");
-        addQueryableMapping(
-                "ReferenceSystem",
-                "gmd:referenceSystemInfo/gmd:MD_ReferenceSystem/gmd:referenceSystemIdentifier/gmd:RS_Identifier/gco:CharacterString");
-        addQueryableMapping(
-                "RevisionDate",
-                "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:date/gmd:CI_Date[dateType/gmd:CI_DateTypeCode/gmd:@codeListValue='revision']/gmd:date/gmd:Date");
-        addQueryableMapping(
-                "Rights",
-                "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:resourceConstraints/gmd:MD_LegalConstraints/gmd:accessConstraints/@codeListValue");
-        addQueryableMapping(
-                "ServiceOperation",
-                "gmd:identificationInfo/gmd:SV_ServiceIdentification/gmd:containsOperations/gmd:SV_OperationMetadata/gco:CharacterString");
-        addQueryableMapping(
-                "SpatialResolution",
-                "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:spatialResolution/gco:CharacterString");
-        addQueryableMapping(
-                "SpatialRepresentationType",
-                "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:spatialRepresentationType/gmd:MD_SpatialRepresentationTypeCode/@codeListValue");
-        addQueryableMapping(
-                "TopicCategory",
-                "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:topicCategory/gco:CharacterString");
-        // required
-        addQueryableMapping(
-                "Contact",
-                "gmd:contact/gmd:CI_ResponsibleParty/gmd:individualName/gco:CharacterString");
     }
 
-    /** Helper method to add Mapping in to Queryables */
-    protected static void addQueryableMapping(String key, String path) {
-        QUERYABLE_MAPPING.put(key, ff.property(path, NAMESPACES));
+    public MetaDataDescriptor(GeoServer geoServer) {
+        super(geoServer);
     }
 
     @Override
@@ -296,37 +204,19 @@ public class MetaDataDescriptor extends AbstractRecordDescriptor {
     }
 
     @Override
-    public Query adaptQuery(Query query) {
-        Filter filter = query.getFilter();
-        if (filter != null && !Filter.INCLUDE.equals(filter)) {
-            query.setFilter((Filter) filter.accept(new MDQueryableFilterVisitor(), null));
-        }
-
-        SortBy[] sortBy = query.getSortBy();
-        if (sortBy != null && sortBy.length > 0) {
-            for (int i = 0; i < sortBy.length; i++) {
-                SortBy sb = sortBy[i];
-                if (!SortBy.NATURAL_ORDER.equals(sb) && !SortBy.REVERSE_ORDER.equals(sb)) {
-                    sortBy[i] =
-                            new SortByImpl(
-                                    MDQueryableFilterVisitor.property(sb.getPropertyName()),
-                                    sb.getSortOrder());
-                }
-            }
-            query.setSortBy(sortBy);
-        }
-
-        return query;
-    }
-
-    @Override
     public AttributeDescriptor getFeatureDescriptor() {
         return METADATA_DESCRIPTOR;
     }
 
     @Override
     public String getBoundingBoxPropertyName() {
-        return "identificationInfo.MD_DataIdentification.extent.EX_Extent.geographicElement.EX_GeographicBoundingBox";
+        XPathUtil.StepList steps =
+                XPathUtil.steps(
+                        getFeatureDescriptor(),
+                        queryableMapping.get(QUERYABLE_BBOX).getPropertyName(),
+                        getNamespaceSupport());
+
+        return CatalogStoreMapping.toDotPath(steps);
     }
 
     @Override
@@ -337,11 +227,6 @@ public class MetaDataDescriptor extends AbstractRecordDescriptor {
     @Override
     public String getQueryablesDescription() {
         return "SupportedISOQueryables";
-    }
-
-    @Override
-    public PropertyName translateProperty(Name name) {
-        return QUERYABLE_MAPPING.get(name.getLocalPart());
     }
 
     @Override

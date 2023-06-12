abc5d4ee4fc680ae0e8d0ccd7c4fbb4cc71db1e5
NielsCharlier <niels@scitus.be>
2023-06-12 13:53:35 +0200

    [GEOS-10931] taskmanager: fix missing serializable (#6927)

--- src/community/taskmanager/core/src/main/java/org/geoserver/taskmanager/data/LatestBatchRun.java
+++ src/community/taskmanager/core/src/main/java/org/geoserver/taskmanager/data/LatestBatchRun.java
@@ -1,9 +1,10 @@
 package org.geoserver.taskmanager.data;
 
+import java.io.Serializable;
 import org.geoserver.taskmanager.data.impl.BatchImpl;
 import org.geoserver.taskmanager.data.impl.BatchRunImpl;
 
-public interface LatestBatchRun {
+public interface LatestBatchRun extends Serializable {
 
     BatchRunImpl getBatchRun();
 

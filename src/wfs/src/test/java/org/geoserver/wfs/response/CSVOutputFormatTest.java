dbba1d40cfe1dcb6b93c9293b3e536dd2e300cd1
Pratik Bandal <pratik.bandal@geosolutionsgroup.com>
2023-08-16 22:47:38 +0100

    [GEOS-11102]:Allow configuration of the CSV date format

--- src/wfs/src/test/java/org/geoserver/wfs/response/CSVOutputFormatTest.java
+++ src/wfs/src/test/java/org/geoserver/wfs/response/CSVOutputFormatTest.java
@@ -27,8 +27,10 @@ import net.opengis.wfs.GetFeatureType;
 import net.opengis.wfs.WfsFactory;
 import org.geoserver.catalog.NamespaceInfo;
 import org.geoserver.catalog.impl.NamespaceInfoImpl;
+import org.geoserver.config.GeoServer;
 import org.geoserver.data.test.MockData;
 import org.geoserver.platform.Operation;
+import org.geoserver.wfs.WFSInfo;
 import org.geoserver.wfs.WFSTestSupport;
 import org.geoserver.wfs.request.FeatureCollectionResponse;
 import org.geotools.data.FeatureSource;
@@ -434,5 +436,109 @@ public class CSVOutputFormatTest extends WFSTestSupport {
 
         // check expected list of values as a comma separated string
         assertEquals("prop1,prop2", lines.get(1)[1]);
+    }
+    @Test
+    public void testIAULayer() throws Exception {
+
+        MockHttpServletResponse resp =
+                getAsServletResponse(
+                        "wfs?version=1.1.0&request=GetFeature&typeName=iau:MarsPoi&outputFormat=csv",
+                        UTF_8.name());
+
+        FeatureSource fs = getFeatureSource(SystemTestData.MARS_POI);
+
+        // check the mime type
+        assertEquals(CSV, getBaseMimeType(resp.getContentType()));
+
+        // check the charset encoding
+        assertEquals(UTF_8.name(), resp.getCharacterEncoding());
+
+        // check the content disposition
+        assertEquals("attachment; filename=MarsPoi.csv", resp.getHeader("Content-Disposition"));
+
+        // read the response back with a parser that can handle escaping, newlines and what not
+        List<String[]> lines = readLines(resp.getContentAsString(), ',');
+
+        // we should have one header line and then all the features in that feature type
+        assertEquals(fs.getCount(Query.ALL) + 1, lines.size());
+
+        for (String[] line : lines) {
+            // check each line has the expected number of elements (num of att + 1 for the id)
+            assertEquals(fs.getSchema().getDescriptors().size() + 1, line.length);
+        }
+    }
+
+    @Test
+    public void testDates() throws Exception {
+        SimpleFeatureTypeBuilder builder = new SimpleFeatureTypeBuilder();
+        builder.add("geom", Point.class);
+        builder.add("label", String.class);
+        builder.add("dtg", Date.class);
+        builder.add("n", Integer.class);
+        builder.add("d", Double.class);
+        builder.setName("funnyLabels");
+        SimpleFeatureType type = builder.buildFeatureType();
+
+        Date d = new Date(1483228800000L);
+        GeometryFactory gf = new GeometryFactory();
+        SimpleFeature f =
+                SimpleFeatureBuilder.build(
+                        type,
+                        new Object[] {
+                            gf.createPoint(new Coordinate(5, 8)),
+                            "A label with \"quotes\"",
+                            d,
+                            10,
+                            100.0
+                        },
+                        null);
+
+        MemoryDataStore data = new MemoryDataStore();
+        data.addFeature(f);
+        SimpleFeatureSource fs = data.getFeatureSource("funnyLabels");
+
+        // build the request objects and feed the output format
+        GetFeatureType gft = WfsFactory.eINSTANCE.createGetFeatureType();
+        Operation op =
+                new Operation("GetFeature", getServiceDescriptor10(), null, new Object[] {gft});
+
+        FeatureCollectionResponse fct =
+                FeatureCollectionResponse.adapt(WfsFactory.eINSTANCE.createFeatureCollectionType());
+        fct.getFeature().add(fs.getFeatures());
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        CSVOutputFormat format = setCSVDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        format.write(fct, bos, op);
+        assertDates("2017-01-01T00:00:00.000Z", bos);
+
+        ByteArrayOutputStream bos1 = new ByteArrayOutputStream();
+        CSVOutputFormat format1 = setCSVDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+        format1.write(fct, bos1, op);
+        assertDates("2017-01-01T00:00:00.000", bos1);
+
+        ByteArrayOutputStream bos2 = new ByteArrayOutputStream();
+        CSVOutputFormat format2 = setCSVDateFormat("EEE, MMM d, ''yy");
+        format2.write(fct, bos2, op);
+        assertDates("Sun, Jan 1, '17", bos2);
+
+        ByteArrayOutputStream bos3 = new ByteArrayOutputStream();
+        CSVOutputFormat format3 = setCSVDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
+        format3.write(fct, bos3, op);
+        assertDates("Sun, 1 Jan 2017 00:00:00 +0000", bos3);
+    }
+
+    private CSVOutputFormat setCSVDateFormat(String csvDateFormat) {
+        GeoServer gs = getGeoServer();
+        WFSInfo wfsInfo = gs.getService(WFSInfo.class);
+        wfsInfo.setCsvDateFormat(csvDateFormat);
+        gs.save(wfsInfo);
+        return new CSVOutputFormat(gs);
+    }
+
+    private void assertDates(String date, ByteArrayOutputStream bou) throws IOException {
+        // read the response back with a parser that can handle escaping, newlines and what not
+        List<String[]> lines = readLines(bou.toString(), ',');
+
+        assertEquals(date, lines.get(1)[3]);
     }
 }

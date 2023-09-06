6d2d76b147c33e6e642e90809b6d7349b1abfd63
Joseph Miller <millerj@pixia.com>
2023-09-06 13:40:42 -0400

    [GEOS-11118] Add Temporal Extents domain and datetime parameter to Maps API
    unit tests for caps and cleanup
    
    added datetime
    
    cleanup
    
    cleanup2
    
    more cleanup
    
    datetime info test
    
    cleanup
    
    hardcoded gwc version
    
    sort pom
    
    html format
    
    copyright

--- src/community/ogcapi/ogcapi-maps/src/main/java/org/geoserver/ogcapi/v1/maps/CollectionDocument.java
+++ src/community/ogcapi/ogcapi-maps/src/main/java/org/geoserver/ogcapi/v1/maps/CollectionDocument.java
@@ -5,6 +5,7 @@
 package org.geoserver.ogcapi.v1.maps;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.io.IOException;
 import java.util.logging.Logger;
 import org.geoserver.catalog.LayerGroupInfo;
 import org.geoserver.catalog.LayerInfo;
@@ -15,11 +16,13 @@ import org.geoserver.ogcapi.AbstractCollectionDocument;
 import org.geoserver.ogcapi.CollectionExtents;
 import org.geoserver.ogcapi.LinkInfoConverter;
 import org.geoserver.ogcapi.LinksBuilder;
+import org.geoserver.ogcapi.TimeExtentCalculator;
 import org.geoserver.platform.ServiceException;
 import org.geotools.api.referencing.FactoryException;
 import org.geotools.api.referencing.operation.TransformException;
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.geotools.util.DateRange;
 import org.geotools.util.logging.Logging;
 import org.springframework.http.HttpStatus;
 
@@ -30,7 +33,7 @@ public class CollectionDocument extends AbstractCollectionDocument<PublishedInfo
 
     PublishedInfo published;
 
-    public CollectionDocument(GeoServer geoServer, PublishedInfo published) {
+    public CollectionDocument(GeoServer geoServer, PublishedInfo published) throws IOException {
         super(published);
         LinkInfoConverter.addLinksToDocument(this, published, MapsService.class);
         // basic info
@@ -38,8 +41,9 @@ public class CollectionDocument extends AbstractCollectionDocument<PublishedInfo
         this.id = collectionId;
         this.title = published.getTitle();
         this.description = published.getAbstract();
-        ReferencedEnvelope bbox = getExtents(published);
-        setExtent(new CollectionExtents(bbox));
+        ReferencedEnvelope bbox = getSpatialExtents(published);
+        DateRange timeExtent = getTimeExtent(published);
+        setExtent(new CollectionExtents(bbox, timeExtent));
         this.published = published;
 
         addSelfLinks("ogc/maps/v1/collections/" + id);
@@ -53,7 +57,7 @@ public class CollectionDocument extends AbstractCollectionDocument<PublishedInfo
                 .add(this);
     }
 
-    private ReferencedEnvelope getExtents(PublishedInfo published) {
+    private ReferencedEnvelope getSpatialExtents(PublishedInfo published) {
         try {
             if (published instanceof LayerInfo) {
                 return ((LayerInfo) published).getResource().getLatLonBoundingBox();
@@ -70,5 +74,16 @@ public class CollectionDocument extends AbstractCollectionDocument<PublishedInfo
                     HttpStatus.INTERNAL_SERVER_ERROR,
                     e);
         }
+    }
+
+    private DateRange getTimeExtent(PublishedInfo published) throws IOException {
+        if (published instanceof LayerInfo) {
+            return TimeExtentCalculator.getTimeExtent(((LayerInfo) published).getResource());
+        } else if (published instanceof LayerGroupInfo) {
+            LOGGER.fine("Time extent not supported for Layer Groups");
+        } else {
+            throw new RuntimeException("Unexpected, don't know how to handle: " + published);
+        }
+        return null;
     }
 }

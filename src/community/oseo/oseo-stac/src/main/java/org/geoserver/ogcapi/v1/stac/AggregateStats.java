6d0ffa450bacded91faef32569abe73413f0954a
Joseph Miller <millerj@pixia.com>
2023-09-09 17:21:47 -0400

    [GEOS-11120] Create aggregates filterFunction in OSEO to support STAC Datacube extension implementation
    First pass with min
    
    field lookup
    
    added max and distinct
    
    bounds and distinct
    
    mappropertynametosource and bounds
    
    Start on cache
    
    started on cache
    
    cache key equals hash
    
    cache test
    
    added a bounds test
    
    added cache ttl to ui
    
    UI test
    
    added ttl units
    
    linked service ui to ttl settings
    
    actually zero ttl doesn't work
    
    docs
    
    cleanup
    
    more cleanup
    
    sortpom
    
    aggregate factory
    
    clean
    
    clean
    
    switched to LoadingCache
    
    PR changes
    
    visitor optimization test
    
    added html
    
    switched to model stored
    
    fixes to uniquevisitor results parsing
    
    docs
    
    doc cleanup

---
+++ src/community/oseo/oseo-stac/src/main/java/org/geoserver/ogcapi/v1/stac/AggregateStats.java
@@ -0,0 +1,23 @@
+/* (c) 2023 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.ogcapi.v1.stac;
+
+import java.io.IOException;
+import org.geotools.api.data.FeatureSource;
+
+/** Get Aggregate Statistics for a collection */
+public interface AggregateStats {
+    /**
+     * Get the aggregate statistics for a collection
+     *
+     * @param delegateProductSource The product source
+     * @param collectionIdentifier The collection identifier
+     * @param sourceProperty The source property
+     * @return The aggregate statistics
+     * @throws IOException If an error occurs
+     */
+    Object getStat(FeatureSource productSource, String collectionIdentifier, String sourceProperty)
+            throws IOException;
+}

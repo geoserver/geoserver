fb75c1d0519b6e8e3993a0d8218276662455afd4
Peter Rushforth <peter.rushforth@gmail.com>
2023-10-12 11:51:19 -0400

    [GEOS-11185] Update MapML viewer to latest release 0.12.1 - map-extent attributes

--- src/extension/mapml/src/main/resources/viewer/widget/map-caption.js
+++ src/extension/mapml/src/main/resources/viewer/widget/map-caption.js
@@ -1,4 +1,4 @@
-/*! @maps4html/web-map-custom-element 27-09-2023 */
+/*! @maps4html/web-map-custom-element 06-11-2023 */
 
 class MapCaption extends HTMLElement{constructor(){super()}connectedCallback(){if("MAPML-VIEWER"===this.parentElement.nodeName||"MAP"===this.parentElement.nodeName){let t=this.parentElement.querySelector("map-caption").textContent;var e;this.observer=new MutationObserver(()=>{this.parentElement.querySelector("map-caption").textContent!==t&&this.parentElement.setAttribute("aria-label",this.parentElement.querySelector("map-caption").textContent)}),this.observer.observe(this,{characterData:!0,subtree:!0,attributes:!0,childList:!0}),this.parentElement.hasAttribute("aria-label")||(e=this.textContent,this.parentElement.setAttribute("aria-label",e))}}disconnectedCallback(){this.observer.disconnect()}}export{MapCaption};
 //# sourceMappingURL=map-caption.js.map

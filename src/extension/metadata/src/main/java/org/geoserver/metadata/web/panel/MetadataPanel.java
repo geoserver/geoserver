c3eacf8cda8dcb9162b7771fe1a534490dd323be
NielsCharlier <niels@scitus.be>
2023-10-30 11:00:30 +0100

    [GEOS-11101] metadata: multiple tabs (#7073)
    * [GEOS-11101] metadata: multiple tabs
    
    * [GEOS-11101] metadata: multiple tabs - fix pmd violation
    
    * [GEOS-11101] metadata: multiple tabs - fix tests
    
    * [GEOS-11101] metadata: multiple tabs - documentation
    
    * [GEOS-11101] metadata: multiple tabs - fix formatting + add image

--- src/extension/metadata/src/main/java/org/geoserver/metadata/web/panel/MetadataPanel.java
+++ src/extension/metadata/src/main/java/org/geoserver/metadata/web/panel/MetadataPanel.java
@@ -4,14 +4,21 @@
  */
 package org.geoserver.metadata.web.panel;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
+import org.apache.wicket.extensions.markup.html.tabs.TabbedPanel;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.geoserver.catalog.ResourceInfo;
 import org.geoserver.metadata.data.model.ComplexMetadataMap;
+import org.geoserver.metadata.data.service.ConfigurationService;
 import org.geoserver.metadata.web.panel.attribute.AttributeDataProvider;
 import org.geoserver.metadata.web.panel.attribute.AttributesTablePanel;
+import org.geoserver.web.GeoServerApplication;
 
 /**
  * The dynamically generated metadata input panel. All fields are added on the fly based on the yaml
@@ -26,14 +33,18 @@ public class MetadataPanel extends Panel {
 
     private final ResourceInfo rInfo;
 
+    private final String tab;
+
     public MetadataPanel(
             String id,
             IModel<ComplexMetadataMap> metadataModel,
             Map<String, List<Integer>> derivedAtts,
-            ResourceInfo rInfo) {
+            ResourceInfo rInfo,
+            String tab) {
         super(id, metadataModel);
         this.derivedAtts = derivedAtts;
         this.rInfo = rInfo;
+        this.tab = tab;
     }
 
     @Override
@@ -43,7 +54,7 @@ public class MetadataPanel extends Panel {
         AttributesTablePanel attributesPanel =
                 new AttributesTablePanel(
                         "attributesPanel",
-                        new AttributeDataProvider(rInfo),
+                        new AttributeDataProvider(rInfo, tab),
                         getMetadataModel(),
                         derivedAtts,
                         rInfo);
@@ -55,5 +66,44 @@ public class MetadataPanel extends Panel {
     @SuppressWarnings("unchecked")
     public IModel<ComplexMetadataMap> getMetadataModel() {
         return (IModel<ComplexMetadataMap>) getDefaultModel();
+    }
+
+    public static Panel buildPanel(
+            String id,
+            IModel<ComplexMetadataMap> metadataModel,
+            Map<String, List<Integer>> derivedAtts,
+            ResourceInfo resource) {
+
+        List<String> tabs =
+                GeoServerApplication.get()
+                        .getApplicationContext()
+                        .getBean(ConfigurationService.class)
+                        .getMetadataConfiguration()
+                        .getTabs();
+
+        if (!tabs.isEmpty()) {
+            List<AbstractTab> tabPanels = new ArrayList<>();
+
+            for (String tab : tabs) {
+                tabPanels.add(
+                        new AbstractTab(new Model<>(tab)) {
+                            private static final long serialVersionUID = -6178140635455783732L;
+
+                            @Override
+                            public WebMarkupContainer getPanel(String panelId) {
+                                return new MetadataPanel(
+                                        panelId, metadataModel, derivedAtts, resource, tab);
+                            }
+                        });
+            }
+
+            TabbedPanel<AbstractTab> panel = new TabbedPanel<>(id, tabPanels);
+
+            return panel;
+        } else {
+            return (Panel)
+                    new MetadataPanel(id, metadataModel, derivedAtts, resource, null)
+                            .setOutputMarkupId(true);
+        }
     }
 }

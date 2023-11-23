efbaf00be4003590f3b1b55c9032588e69e74dd4
Jody Garnett <jody.garnett@gmail.com>
2023-11-22 21:29:09 -0800

    [GEOS-11212] Check signature validation using JWKS publicKey and access token expiration

--- src/community/security/oauth2-openid-connect/oauth2-openid-connect-core/src/main/java/org/geoserver/security/oauth2/services/OpenIdConnectTokenServices.java
+++ src/community/security/oauth2-openid-connect/oauth2-openid-connect-core/src/main/java/org/geoserver/security/oauth2/services/OpenIdConnectTokenServices.java
@@ -4,6 +4,15 @@
  */
 package org.geoserver.security.oauth2.services;
 
+import com.nimbusds.jose.JWSObject;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import java.io.IOException;
+import java.net.URL;
+import java.text.ParseException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import org.geoserver.security.oauth2.GeoServerAccessTokenConverter;
 import org.geoserver.security.oauth2.GeoServerOAuthRemoteTokenServices;
@@ -13,13 +22,19 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.jwt.crypto.sign.InvalidSignatureException;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 /** Remote Token Services for OpenId token details. */
 public class OpenIdConnectTokenServices extends GeoServerOAuthRemoteTokenServices {
+
+    private JwkTokenStore store = null;
+    private JWKSet publicKeys;
 
     public OpenIdConnectTokenServices() {
         super(new GeoServerAccessTokenConverter());
@@ -33,16 +48,69 @@ public class OpenIdConnectTokenServices extends GeoServerOAuthRemoteTokenService
     @SuppressWarnings("unchecked")
     @Override
     protected Map<String, Object> checkToken(String accessToken) {
+        if (this.checkTokenEndpointUrl != null) {
+            return checkTokenEndpoint(this.checkTokenEndpointUrl, accessToken);
+        } else if (this.store != null) {
+            return checkTokenJWKS(accessToken);
+        } else {
+            throw new IllegalStateException(
+                    "Unable to check access token: require check token endpoint url, or JWK URI");
+        }
+    }
+
+    /**
+     * According to the spec, the token can be verified issuing a GET request, and putting the token
+     * in the Authorization header. See
+     * https://openid.net/specs/openid-connect-core-1_0.html#UserInfo
+     */
+    protected Map<String, Object> checkTokenEndpoint(
+            String checkTokenEndpoint, String accessToken) {
         MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", getAuthorizationHeader(accessToken));
         return restTemplate
                 .exchange(
-                        checkTokenEndpointUrl,
+                        checkTokenEndpoint,
                         HttpMethod.GET,
                         new HttpEntity<>(formData, headers),
                         Map.class)
                 .getBody();
+    }
+
+    /**
+     * According to the spec, the token signature can be checked using public JSON Web Key set
+     * document.
+     */
+    protected Map<String, Object> checkTokenJWKS(String rawAccessToken) {
+        // use
+        try {
+            TokenSignatureValidator tokenValidator = new TokenSignatureValidator();
+            JWSObject jwsToken = tokenValidator.parseToken(rawAccessToken);
+            String keyId = jwsToken.getHeader().getKeyID();
+
+            RSAKey rsaKey = (RSAKey) publicKeys.getKeyByKeyId(keyId);
+            tokenValidator.validateSignature(rsaKey, jwsToken);
+        } catch (ParseException parseException) {
+            throw (InvalidSignatureException)
+                    new InvalidSignatureException(
+                                    "Could not verify signature of the JWT with the given RSA Public Key")
+                            .initCause(parseException);
+        }
+        // verify access token - this did not validate signature as initially assumed
+        OAuth2AccessToken accessToken = store.readAccessToken(rawAccessToken);
+
+        // To be careful check if accessToken has expired as we do not rely on check token endpoint
+        Date expiration = accessToken.getExpiration();
+        if (Instant.now().isAfter(expiration.toInstant())) {
+            throw new InvalidTokenException("Provided accessToken has expired");
+        }
+
+        // Extract information from token
+        Map<String, Object> map = new HashMap<>();
+        map.put("scope", accessToken.getScope());
+        map.putAll(accessToken.getAdditionalInformation());
+
+        return map;
     }
 
     @Override
@@ -68,6 +136,20 @@ public class OpenIdConnectTokenServices extends GeoServerOAuthRemoteTokenService
     }
 
     public void setConfiguration(OpenIdConnectFilterConfig config) {
+        if (config.getJwkURI() != null && !config.getJwkURI().isEmpty()) {
+            this.store = new JwkTokenStore(config.getJwkURI());
+            try {
+                this.publicKeys = JWKSet.load(new URL(config.getJwkURI()));
+            } catch (IOException e) {
+                this.publicKeys = null;
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            this.publicKeys = null;
+            this.store = null;
+        }
+
         setAccessTokenConverter(
                 new GeoServerAccessTokenConverter(
                         new GeoServerUserAuthenticationConverter(config.getPrincipalKey())));

a09c04b7506da2d2d8cc87979ae6ffa2f7039de9
Sebastian Frey <sebastian.frey@outlook.com>
2024-02-15 10:29:37 +0100

    Support lower case sortby parameter

--- src/community/ogcapi/ogcapi-features/src/test/java/org/geoserver/ogcapi/v1/features/FeatureTest.java
+++ src/community/ogcapi/ogcapi-features/src/test/java/org/geoserver/ogcapi/v1/features/FeatureTest.java
@@ -709,7 +709,7 @@ public class FeatureTest extends FeaturesTestSupport {
     @Test
     public void testSearchSortByJson() throws Exception {
         String roadSegments = getLayerId(MockData.PRIMITIVEGEOFEATURE);
-        String request = "{\"sortBy\":[\"name\"],\"limit\":2}";
+        String request = "{\"sortby\":[\"name\"],\"limit\":2}";
         DocumentContext json =
                 postAsJSONPath(
                         "ogc/features/v1/collections/" + roadSegments + "/search", request, 200);
@@ -722,7 +722,7 @@ public class FeatureTest extends FeaturesTestSupport {
     @Test
     public void testSearchSortByText() throws Exception {
         String roadSegments = getLayerId(MockData.PRIMITIVEGEOFEATURE);
-        String request = "{\"sortBy\":\"name\",\"limit\":2}";
+        String request = "{\"sortby\":\"name\",\"limit\":2}";
         DocumentContext json =
                 postAsJSONPath(
                         "ogc/features/v1/collections/" + roadSegments + "/search", request, 200);

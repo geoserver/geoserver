68bee668f96833bb2f0af766293d09e7cb6f440b
Alessio Fabiani <alessio.fabiani@geosolutionsgroup.com>
2024-04-15 14:22:19 +0200

    [Backport to 2.23.x][GEOS-11358] Feature-Autopopulate Update operation does not apply the Update Element filter (#7535)
    * [GEOS-11358] Feature-Autopopulate Update operation does not apply the Update Element filter
    
    * - Update jdoc and test case
    
    * - Get rid of heavy loggers
    
    * - Make sure the ecql expression are evaluated each time
    
    * - Search for the user into the SecurityContext if not able to find him on the local env
    
    * - Removing unneeded lop
    
    * - Make sure we correctly handle the FeatureCollection iterator
    
    (cherry picked from commit 1b126f40949ec1d682abeb5841ac46f3c56a85ea)
    
    # Conflicts:
    #	src/community/features-autopopulate/features-autopopulate-core/src/main/java/org/geoserver/autopopulate/AutopopulateTemplate.java
    #	src/community/features-autopopulate/features-autopopulate-core/src/main/java/org/geoserver/autopopulate/AutopopulateTransactionCallback.java

--- src/community/features-autopopulate/features-autopopulate-core/src/main/java/org/geoserver/autopopulate/AutopopulateTemplate.java
+++ src/community/features-autopopulate/features-autopopulate-core/src/main/java/org/geoserver/autopopulate/AutopopulateTemplate.java
@@ -10,13 +10,14 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.logging.Logger;
-import org.geoserver.platform.GeoServerExtensions;
-import org.geoserver.platform.GeoServerResourceLoader;
+import org.geoserver.platform.resource.Resource;
 import org.geoserver.security.PropertyFileWatcher;
 import org.geotools.filter.text.cql2.CQLException;
 import org.geotools.filter.text.ecql.ECQL;
 import org.geotools.util.logging.Logging;
 import org.opengis.filter.expression.Expression;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
  * AutopopulateTemplate class is used to load the properties from the file and store them in a map
@@ -29,8 +30,6 @@ import org.opengis.filter.expression.Expression;
 public class AutopopulateTemplate {
     /** logger */
     private static final Logger LOGGER = Logging.getLogger(AutopopulateTransactionCallback.class);
-    /** The properties map */
-    private final Map<String, String> propertiesMap;
 
     /** The file watcher */
     private final PropertyFileWatcher watcher;
@@ -38,26 +37,23 @@ public class AutopopulateTemplate {
     /**
      * Constructs the template loader.
      *
-     * @param filePath The file path to load the properties from
+     * @param templateResource The file path to load the properties from
      */
-    public AutopopulateTemplate(String filePath) {
-        this.propertiesMap = new HashMap<>();
-        GeoServerResourceLoader loader = GeoServerExtensions.bean(GeoServerResourceLoader.class);
-        this.watcher = new PropertyFileWatcher(loader.get(filePath));
-        loadProperties(filePath);
+    public AutopopulateTemplate(Resource templateResource) {
+        this.watcher = new PropertyFileWatcher(templateResource);
     }
 
     /**
      * Load the properties from the file and store them in the map.
      *
-     * @param filePath The file path to load the properties from
+     * @return propertiesMap
      */
-    private void loadProperties(String filePath) {
+    private Map<String, String> loadProperties() {
+        HashMap<String, String> propertiesMap = new HashMap<>();
         try {
             Properties properties = this.watcher.getProperties();
             if (properties == null) {
-                LOGGER.warning("Unable to load the properties file: " + filePath);
-                return;
+                LOGGER.warning("Unable to load the properties file: " + this.watcher.getFile());
             } else {
                 for (String key : properties.stringPropertyNames()) {
                     String expression = properties.getProperty(key);
@@ -65,10 +61,7 @@ public class AutopopulateTemplate {
 
                     // First check on the Syntax of the expression
                     try {
-                        Expression ecql = ECQL.toExpression(expression);
-                        if (ecql != null) {
-                            propertiesMap.put(key, ecql.evaluate(null, String.class));
-                        }
+                        propertiesMap.put(key, getValue(expression));
                     } catch (CQLException e) {
                         LOGGER.warning(
                                 "Unable to parse the following Expression" + e.getSyntaxError());
@@ -80,16 +73,19 @@ public class AutopopulateTemplate {
             LOGGER.severe("Unable to load the properties file: " + e.getMessage());
             throw new RuntimeException("Unable to load the properties file: " + e.getMessage());
         }
+        return propertiesMap;
     }
 
-    /**
-     * Get the property from the map.
-     *
-     * @param key The key to get the property
-     * @return The property value
-     */
-    public String getProperty(String key) {
-        return propertiesMap.get(key);
+    private static String getValue(String expression) throws CQLException {
+        Expression ecql = ECQL.toExpression(expression);
+        String value = ecql.evaluate(null, String.class);
+        if (value == null && expression.contains("GSUSER")) {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            if (auth != null) {
+                value = auth.getName();
+            }
+        }
+        return value;
     }
 
     /**
@@ -98,7 +94,7 @@ public class AutopopulateTemplate {
      * @return The properties map
      */
     public Map<String, String> getAllProperties() {
-        return propertiesMap;
+        return loadProperties();
     }
 
     /**
@@ -107,13 +103,12 @@ public class AutopopulateTemplate {
      * @return true if the template file has been modified and must be reloaded, false otherwise
      */
     public boolean needsReload() {
-        if (watcher != null) return watcher.isStale();
-        return true;
+        return watcher.isStale();
     }
 
     @Override
     public String toString() {
-        return "AutopopulateTemplate{" + "propertiesMap=" + propertiesMap + '}';
+        return "AutopopulateTemplate{" + "file=" + watcher.getFile().getAbsolutePath() + '}';
     }
 
     @Override
@@ -121,11 +116,12 @@ public class AutopopulateTemplate {
         if (this == o) return true;
         if (!(o instanceof AutopopulateTemplate)) return false;
         AutopopulateTemplate that = (AutopopulateTemplate) o;
-        return Objects.equals(propertiesMap, that.propertiesMap);
+        return Objects.equals(
+                watcher.getFile().getAbsolutePath(), that.watcher.getFile().getAbsolutePath());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(propertiesMap);
+        return Objects.hashCode(watcher.getFile().getAbsolutePath());
     }
 }

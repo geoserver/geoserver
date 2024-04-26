5676ed929f928e59e1466de88f6e34866a3f5089
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-04-26 10:58:37 +0200

    fix UnusedLocalVariable violations

--- src/main/src/test/java/org/geoserver/test/GeoServerSystemTestSupport.java
+++ src/main/src/test/java/org/geoserver/test/GeoServerSystemTestSupport.java
@@ -2344,10 +2344,9 @@ public class GeoServerSystemTestSupport extends GeoServerBaseTestSupport<SystemT
 
     /** Parses a stream into a String */
     protected String string(InputStream input) throws Exception {
-        StringBuffer sb = new StringBuffer();
-        char[] buf = new char[8192];
+        StringBuilder sb = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(input))) {
-            String line = null;
+            String line;
             while ((line = reader.readLine()) != null) {
                 sb.append(line);
                 sb.append("\n");

e360b0fbd96701f6b2da15c12e7e29dea33cb5ad
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2024-05-31 11:16:00 -0300

    [GEOS-11424] Speed up web-ui WorkspaceAdminComponentAuthorizer
    Logging in as a user other than `admin`, and having
    a large amount of workspaces results in potentlially
    substantial delays in loading almost every webui wicket
    page, as `WorkspaceAdminComponentAuthorizer` will be
    run several times, each of them potentially doing
    a full scan of workspaces, to determine if the user
    has admin privileges in at least one workspace.
    
    This patch addresses the long-standing TODO comment in
    `WorkspaceAdminComponentAuthorizer` by caching the result
    in the `RequestAttributes`, hence performing the check
    once per request.
    
    Also, moves the logic to a new default method in the
    `ResourceAccessManager` interface, giving implementations
    a chance to provide a more efficient check.

--- src/web/core/src/main/java/org/geoserver/web/WorkspaceAdminComponentAuthorizer.java
+++ src/web/core/src/main/java/org/geoserver/web/WorkspaceAdminComponentAuthorizer.java
@@ -1,4 +1,4 @@
-/* (c) 2014 - 2016 Open Source Geospatial Foundation - all rights reserved
+/* (c) 2014 - 2024 Open Source Geospatial Foundation - all rights reserved
  * (c) 2001 - 2013 OpenPlans
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
@@ -6,18 +6,28 @@
 package org.geoserver.web;
 
 import org.geoserver.catalog.Catalog;
-import org.geoserver.catalog.WorkspaceInfo;
 import org.geoserver.security.ResourceAccessManager;
 import org.geoserver.security.SecureCatalogImpl;
-import org.geoserver.security.WorkspaceAccessLimits;
+import org.springframework.lang.Nullable;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
 
 /**
  * Authorizer that allows access if the user has admin rights to any workspace.
  *
  * @author Justin Deoliveira, OpenGeo
+ * @see ResourceAccessManager#isWorkspaceAdmin(Authentication, Catalog)
  */
+@SuppressWarnings("serial")
 public class WorkspaceAdminComponentAuthorizer extends AdminComponentAuthorizer {
+
+    /**
+     * Key to cache the result of {@link #isWorkspaceAdmin(Authentication)} on the request's {@link
+     * RequestAttributes} in {@link RequestAttributes#SCOPE_REQUEST request scope}
+     */
+    static final String REQUEST_CONTEXT_CACHE_KEY = "WORKSPACEADMIN_COMPONENT_AUTHORIZER_VALUE";
+
     @Override
     public boolean isAccessAllowed(Class<?> componentClass, Authentication authentication) {
 
@@ -31,30 +41,48 @@ public class WorkspaceAdminComponentAuthorizer extends AdminComponentAuthorizer 
             return false;
         }
 
-        // TODO: we should cache this result somehow
-        return isWorkspaceAdmin(authentication);
+        // this authorizer may be called several times per request, use a per-request cached value
+        Boolean workspaceAdmin = getCachedValue();
+        if (null == workspaceAdmin) {
+            workspaceAdmin = isWorkspaceAdmin(authentication);
+            setCachedValue(workspaceAdmin.booleanValue());
+        }
+        return workspaceAdmin;
     }
 
     /** Check if the current user has any admin privilege on at least one workspace. */
     boolean isWorkspaceAdmin(Authentication authentication) {
+        ResourceAccessManager manager = getAccessManager();
+        return null != manager && manager.isWorkspaceAdmin(authentication, getCatalog());
+    }
 
-        Catalog catalog = getSecurityManager().getCatalog();
+    private Catalog getCatalog() {
+        return getSecurityManager().getCatalog();
+    }
 
+    ResourceAccessManager getAccessManager() {
         // the secure catalog builds and owns the ResourceAccessManager
         SecureCatalogImpl secureCatalog =
                 GeoServerApplication.get().getBeanOfType(SecureCatalogImpl.class);
-        ResourceAccessManager manager = secureCatalog.getResourceAccessManager();
+        if (null == secureCatalog) return null;
+        return secureCatalog.getResourceAccessManager();
+    }
 
-        if (manager != null) {
-            for (WorkspaceInfo workspace : catalog.getWorkspaces()) {
-                WorkspaceAccessLimits accessLimits =
-                        manager.getAccessLimits(authentication, workspace);
-                if (accessLimits != null && accessLimits.isAdminable()) {
-                    return true;
-                }
-            }
+    void setCachedValue(boolean workspaceAdmin) {
+        RequestAttributes atts = RequestContextHolder.getRequestAttributes();
+        if (null != atts) {
+            atts.setAttribute(
+                    REQUEST_CONTEXT_CACHE_KEY, workspaceAdmin, RequestAttributes.SCOPE_REQUEST);
         }
+    }
 
-        return false;
+    @Nullable
+    Boolean getCachedValue() {
+        RequestAttributes atts = RequestContextHolder.getRequestAttributes();
+        if (null != atts) {
+            return (Boolean)
+                    atts.getAttribute(REQUEST_CONTEXT_CACHE_KEY, RequestAttributes.SCOPE_REQUEST);
+        }
+        return null;
     }
 }
---
+++ src/web/core/src/test/java/org/geoserver/web/WorkspaceAdminComponentAuthorizerTest.java
@@ -0,0 +1,136 @@
+/* (c) 2024 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.web;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.geoserver.security.ResourceAccessManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+public class WorkspaceAdminComponentAuthorizerTest extends GeoServerWicketTestSupport {
+
+    private WorkspaceAdminComponentAuthorizer authorizer;
+    private ResourceAccessManager accessManager;
+
+    @Before
+    @SuppressWarnings("serial")
+    public void setUp() {
+        this.accessManager = new WorkspaceAdminComponentAuthorizer().getAccessManager();
+        authorizer =
+                new WorkspaceAdminComponentAuthorizer() {
+                    @Override
+                    ResourceAccessManager getAccessManager() {
+                        return accessManager;
+                    }
+                };
+    }
+
+    @After
+    public void after() {
+        RequestContextHolder.setRequestAttributes(null);
+    }
+
+    private boolean isAccessAllowed(Authentication user) {
+        Class<?> unused = GeoServerBasePage.class;
+        return authorizer.isAccessAllowed(unused, user);
+    }
+
+    @Test
+    public void testGetAccessManager() {
+        authorizer = new WorkspaceAdminComponentAuthorizer();
+        assertNotNull(authorizer.getAccessManager());
+    }
+
+    @Test
+    public void testAdmin() {
+        assertTrue(isAccessAllowed(admin()));
+    }
+
+    @Test
+    public void testNull() {
+        assertFalse(isAccessAllowed(null));
+    }
+
+    @Test
+    public void testNoAccessManager() {
+        this.accessManager = null;
+        assertNull(authorizer.getAccessManager());
+        Authentication user = user("test", "ROLE_1");
+        assertFalse(isAccessAllowed(user));
+    }
+
+    @Test
+    public void testNotAuthenticated() {
+        Authentication user = user("test");
+        user.setAuthenticated(false);
+        assertFalse(isAccessAllowed(user));
+    }
+
+    @Test
+    public void testNotWorkspaceAdmin() {
+        Authentication user = user("test", "ROLE_USER");
+        assertFalse(isAccessAllowed(user));
+    }
+
+    @Test
+    public void testIsWorkspaceAdmin() {
+        Authentication user = user("test", "ROLE_USER");
+        accessManager = mock(ResourceAccessManager.class);
+        when(accessManager.isWorkspaceAdmin(user, getCatalog())).thenReturn(true);
+        assertTrue(isAccessAllowed(user));
+    }
+
+    @Test
+    public void testRequestCache() {
+        Authentication user = user("test", "ROLE_USER");
+        accessManager = mock(ResourceAccessManager.class);
+        when(accessManager.isWorkspaceAdmin(user, getCatalog())).thenReturn(true);
+
+        RequestAttributes atts = new ServletRequestAttributes(new MockHttpServletRequest());
+        RequestContextHolder.setRequestAttributes(atts);
+
+        authorizer = spy(authorizer);
+
+        assertTrue(isAccessAllowed(user));
+        assertTrue(isAccessAllowed(user));
+        assertTrue(isAccessAllowed(user));
+
+        verify(authorizer, times(1)).setCachedValue(true);
+        verify(authorizer, times(1)).isWorkspaceAdmin(user);
+
+        Object cached =
+                atts.getAttribute(
+                        WorkspaceAdminComponentAuthorizer.REQUEST_CONTEXT_CACHE_KEY,
+                        RequestAttributes.SCOPE_REQUEST);
+        assertEquals(Boolean.TRUE, cached);
+    }
+
+    private Authentication admin() {
+        login();
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    private Authentication user(String name, String... roles) {
+        login(name, "pwd", roles);
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+}

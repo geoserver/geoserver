caa98548d1093584aab9d76bc3c0af5fffb32cbe
Alessio Fabiani <alessio.fabiani@geosolutionsgroup.com>
2024-07-02 09:34:40 +0200

    [GEOS-11446] [INSPIRE] Incorrect behavior for unsupported languages (#7752)
    * [GEOS-11446] [INSPIRE] Incorrect behavior for unsupported languages
    
    * - Fix QA checks (spotless:apply)
    
    * - title checks for configured i18n title with unsupported language and with default
    
    * - Remove unnneded changes
    
    (cherry picked from commit 62453b2267ea4f9a05775a54da128d93be1626ae)

--- src/extension/inspire/src/test/java/org/geoserver/inspire/wfs/WFSExtendedCapabilitiesTest.java
+++ src/extension/inspire/src/test/java/org/geoserver/inspire/wfs/WFSExtendedCapabilitiesTest.java
@@ -423,4 +423,49 @@ public class WFSExtendedCapabilitiesTest extends GeoServerSystemTestSupport {
         nodeList = suppLangs.getElementsByTagNameNS(COMMON_NAMESPACE, "SupportedLanguage");
         assertEquals("Number of Supported Languages", 2, nodeList.getLength());
     }
+
+    @Test
+    public void testUnSupportedLanguages() throws Exception {
+        final ServiceInfo serviceInfo = getGeoServer().getService(WFSInfo.class);
+        final MetadataMap metadata = serviceInfo.getMetadata();
+        clearInspireMetadata(metadata);
+        metadata.put(CREATE_EXTENDED_CAPABILITIES.key, true);
+        metadata.put(SERVICE_METADATA_URL.key, "http://foo.com?bar=baz");
+        metadata.put(SERVICE_METADATA_TYPE.key, "application/vnd.iso.19139+xml");
+        metadata.put(LANGUAGE.key, "fre");
+        metadata.put(OTHER_LANGUAGES.key, "ita,eng");
+        metadata.put(
+                SPATIAL_DATASET_IDENTIFIER_TYPE.key,
+                "one,http://www.geoserver.org/one;two,http://www.geoserver.org/two,http://metadata.geoserver.org/id?two");
+        getGeoServer().save(serviceInfo);
+
+        final Document dom = getAsDOM(WFS_1_1_0_GETCAPREQUEST + "&LANGUAGE=unsupported");
+
+        NodeList nodeList = dom.getElementsByTagNameNS(DLS_NAMESPACE, "ExtendedCapabilities");
+        assertEquals("Number of INSPIRE ExtendedCapabilities elements", 1, nodeList.getLength());
+
+        String schemaLocation = dom.getDocumentElement().getAttribute("xsi:schemaLocation");
+        assertSchemaLocationContains(schemaLocation, DLS_NAMESPACE, DLS_SCHEMA);
+
+        final Element extendedCaps = (Element) nodeList.item(0);
+
+        final Element suppLangs =
+                (Element)
+                        extendedCaps
+                                .getElementsByTagNameNS(COMMON_NAMESPACE, "SupportedLanguages")
+                                .item(0);
+
+        nodeList = suppLangs.getElementsByTagNameNS(COMMON_NAMESPACE, "DefaultLanguage");
+        assertEquals("Number of DefaultLanguage elements", 1, nodeList.getLength());
+        nodeList = suppLangs.getElementsByTagNameNS(COMMON_NAMESPACE, "SupportedLanguage");
+        assertEquals("Number of Supported Languages", 2, nodeList.getLength());
+
+        final String responseLanguage =
+                dom.getElementsByTagNameNS(COMMON_NAMESPACE, "ResponseLanguage")
+                        .item(0)
+                        .getFirstChild()
+                        .getFirstChild()
+                        .getNodeValue();
+        assertEquals("Unsupported LANGUAGE returns the Default one", "fre", responseLanguage);
+    }
 }

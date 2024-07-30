a8a97c4291c002fc8074e1a4908a176664f906d8
Andrea Aime <andrea.aime@gmail.com>
2024-07-30 19:25:35 +0200

    [Backport 2.24.x] Update Logging Statement for WMS Store Deletion (#7799)
    * Fix logging statement
    
    * Fix logging statement
    
    The original logging statement incorrectly concatenated workspaceName twice, which misrepresents the action of deleting a WMTS store. The update correctly identifies the workspace and the specific store being deleted, aligning with the code's intention and adhering to the principle of consistent meaning between static logging text and dynamic variables.
    
    * Apply suggestions from code review
    
    * formatting
    
    ---------
    
    Co-authored-by: logresearch <log.researchovo@gmail.com>
    Co-authored-by: Jody Garnett <jody.garnett@gmail.com>

--- src/restconfig/src/main/java/org/geoserver/rest/catalog/WMTSStoreController.java
+++ src/restconfig/src/main/java/org/geoserver/rest/catalog/WMTSStoreController.java
@@ -207,7 +207,7 @@ public class WMTSStoreController extends AbstractCatalogController {
         }
         clear(cs);
 
-        LOGGER.info("DELETE wmts store " + workspaceName + ":s" + workspaceName);
+        LOGGER.info("DELETE wmts store '" + storeName + "' from workspace '" + workspaceName + "'");
     }
 
     void clear(WMTSStoreInfo info) {

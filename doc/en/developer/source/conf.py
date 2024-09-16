6b6fb3075eb543ebef636622cd24fd254a48ece0
Peter Smythe <peter@afrigis.co.za>
2024-09-16 17:54:31 +0200

    Use build/rename.xml to update version in all pom.xml files to next: 2.27-SNAPSHOT, 33-SNAPSHOT, 1.27-SNAPSHOT

--- doc/en/developer/source/conf.py
+++ doc/en/developer/source/conf.py
@@ -48,17 +48,17 @@ copyright = u'{}, Open Source Geospatial Foundation'.format(now.year)
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '2.26'
+version = '2.27'
 
 # The full version, including alpha/beta/rc tags.
-release = '2.26-SNAPSHOT'
+release = '2.27-SNAPSHOT'
 
 # Used in build and documentation links
-branch = '2.26.x'
+branch = '2.27.x'
 
 # Users don't need to see the "SNAPSHOT" notation when it's there
 if release.find('SNAPSHOT') != -1:
-   release = '2.26.x'
+   release = '2.27.x'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:

e36b5f00ba44063da9568ec007e70489de6a8ec8
Andrea Aime <andrea.aime@gmail.com>
2024-10-04 11:59:10 +0200

    [GEOS-11563] Allow configuring a DGGS resolution offset on a layer basis

---
+++ src/community/ogcapi/dggs/dggs-core/src/test/java/org/geotools/dggs/gstore/DGGSResolutionCalculatorTest.java
@@ -0,0 +1,109 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2024, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This file is hereby placed into the Public Domain. This means anyone is
+ *    free to do whatever they wish with this file. Use it well and enjoy!
+ */
+package org.geotools.dggs.gstore;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.geotools.api.data.Query;
+import org.geotools.dggs.DGGSInstance;
+import org.geotools.dggs.h3.H3DGGSFactory;
+import org.geotools.filter.function.EnvFunction;
+import org.geotools.util.NumberRange;
+import org.geotools.util.factory.Hints;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DGGSResolutionCalculatorTest {
+
+    private DGGSInstance dggsInstance;
+    private DGGSResolutionCalculator calculator;
+
+    @Before
+    public void setUp() throws IOException {
+        dggsInstance = new H3DGGSFactory().createInstance(new HashMap<>());
+        calculator = new DGGSResolutionCalculator(dggsInstance);
+    }
+
+    @Test
+    public void testConstructor() {
+        assertNotNull(calculator);
+        assertEquals(16, calculator.levelThresholds.length);
+    }
+
+    @Test
+    public void testIsValid() {
+        for (int level = 0; level < 16; level++) {
+            assertTrue(calculator.isValid(level));
+        }
+        assertFalse(calculator.isValid(16));
+    }
+
+    @Test
+    public void testGetValidResolutions() {
+        NumberRange<Integer> range = calculator.getValidResolutions();
+        assertEquals(0, (int) range.getMinimum());
+        assertEquals(15, (int) range.getMaximum());
+    }
+
+    @Test
+    public void testGetTargetResolutionDistance() {
+        Query query = new Query("testLayer");
+        Hints hints = new Hints();
+        hints.put(Hints.GEOMETRY_DISTANCE, 10d); // 10 degrees
+        query.setHints(hints);
+
+        int resolution = calculator.getTargetResolution(query, 1);
+        assertEquals(0, resolution);
+    }
+
+    @Test
+    public void testGetTargetResolutionDistanceOffset() {
+        Query query = new Query("testLayer");
+        Hints hints = new Hints();
+        hints.put(Hints.GEOMETRY_DISTANCE, 10d); // 10 degrees
+        Hints.ConfigurationMetadataKey offsetKey =
+                Hints.ConfigurationMetadataKey.get(DGGSResolutionCalculator.CONFIGURED_OFFSET_KEY);
+        hints.put(offsetKey, 1);
+        query.setHints(hints);
+
+        int resolution = calculator.getTargetResolution(query, 1);
+        assertEquals(1, resolution);
+    }
+
+    @Test
+    public void testGetTargetResolutionExplicit() {
+        Query query = new Query("testLayer");
+        Hints hints = new Hints();
+        hints.put(Hints.VIRTUAL_TABLE_PARAMETERS, Map.of(DGGSStore.VP_RESOLUTION, 5));
+        query.setHints(hints);
+
+        int resolution = calculator.getTargetResolution(query, 1);
+        assertEquals(5, resolution);
+    }
+
+    @Test
+    public void testGetTargetResolutionWMSScale() {
+        EnvFunction.setLocalValues(
+                Map.of(DGGSResolutionCalculator.WMS_SCALE_DENOMINATOR, 10_000_000));
+        try {
+            Query query = new Query("testLayer");
+            int resolution = calculator.getTargetResolution(query, 1);
+            assertEquals(2, resolution);
+        } finally {
+            EnvFunction.clearLocalValues();
+        }
+    }
+}

238a16be32ad45de2d160c13dba778974f15a9a4
Jody Garnett <jody.garnett@gmail.com>
2024-11-28 01:00:33 -0800

    Headers

--- src/community/ogcapi/ogcapi-features/src/main/java/org/geoserver/ogcapi/v1/features/FeatureConformance.java
+++ src/community/ogcapi/ogcapi-features/src/main/java/org/geoserver/ogcapi/v1/features/FeatureConformance.java
@@ -1,3 +1,7 @@
+/* (c) 2024 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.ogcapi.v1.features;
 
 import static org.geoserver.ogcapi.APIConformance.Level.DRAFT_STANDARD;

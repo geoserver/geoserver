7b8f4a5d8f726e5e11681189fe4000e927633660
Ikeoka, Steve <Steve.Ikeoka@GDIT.com>
2024-12-30 12:25:34 -0800

    [GEOS-11664] Update REST security paths

--- src/security/security-tests/src/test/java/org/geoserver/security/auth/AuthenticationFilterTest.java
+++ src/security/security-tests/src/test/java/org/geoserver/security/auth/AuthenticationFilterTest.java
@@ -983,8 +983,8 @@ public class AuthenticationFilterTest extends AbstractAuthenticationProviderTest
 
         modifyChain(pattern, false, true, null);
 
-        prepareFilterChain(ConstantFilterChain.class, "/j_spring_security_check_foo/", testFilterName6);
-        modifyChain("/j_spring_security_check_foo/", false, true, null);
+        prepareFilterChain(ConstantFilterChain.class, "/j_spring_security_check_foo", testFilterName6);
+        modifyChain("/j_spring_security_check_foo", false, true, null);
 
         //        prepareFilterChain(LogoutFilterChain.class,"/j_spring_security_logout_foo",
         //                GeoServerSecurityFilterChain.SECURITY_CONTEXT_ASC_FILTER,
@@ -1173,8 +1173,8 @@ public class AuthenticationFilterTest extends AbstractAuthenticationProviderTest
                 GeoServerSecurityFilterChain.FORM_LOGIN_FILTER);
         modifyChain(pattern, false, true, null);
 
-        prepareFilterChain("/j_spring_security_check_foo/", testFilterName7);
-        modifyChain("/j_spring_security_check_foo/", false, true, null);
+        prepareFilterChain("/j_spring_security_check_foo", testFilterName7);
+        modifyChain("/j_spring_security_check_foo", false, true, null);
 
         SecurityContextHolder.getContext().setAuthentication(null);
 

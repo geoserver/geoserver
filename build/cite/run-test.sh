ae63d2cefb5114fec88c4831af61252fbef9dceb
Andrea Aime <andrea.aime@gmail.com>
2025-01-06 11:38:14 +0100

    Run CITE GeoTIFF tests

--- build/cite/run-test.sh
+++ build/cite/run-test.sh
@@ -112,10 +112,36 @@ ogcapi-features10() {
     run_rest_test_suite "ogcapi-features-1.0" "testng" "iut=http://geoserver:8080/geoserver/ogc/features/v1" "noofcollections=-1"
 }
 
+geotiff11() {
+  # RGB WMS
+  run_rest_test_suite_prefix "geotiff11" "01-wms-" "testng" "iut=http:%2F%2Fgeoserver:8080%2Fgeoserver%2Fwms?service%3DWMS%26version%3D1.1.0%26request%3DGetMap%26layers%3Dtopp%3Atazbm%26bbox%3D146.49999999999477%2C-44.49999999999785%2C147.99999999999474%2C-42.99999999999787%26width%3D767%26height%3D768%26srs%3DEPSG%3A4326%26styles%3D%26format%3Dimage%2Fgeotiff"
+
+  # single band WCS
+  run_rest_test_suite_prefix "geotiff11" "02-wcs-dem-basic-" "testng" "iut=http%3A%2F%2Fgeoserver%3A8080%2Fgeoserver%2Ftopp%2Fows%3Fservice%3DWCS%26version%3D2.0.1%26request%3DGetCoverage%26coverageId%3Dtopp__tazdem"
+
+  # single band rotated
+  run_rest_test_suite_prefix "geotiff11" "03-wcs-rotated-" "testng" "iut=http%3A%2F%2Fgeoserver%3A8080%2Fgeoserver%2Ftopp%2Fows%3Fservice%3DWCS%26version%3D2.0.1%26request%3DGetCoverage%26coverageId%3Dtopp__rotated"
+
+  # single band, deflate compressed, tiled, WCS
+  run_rest_test_suite_prefix "geotiff11" "04-wcs-dem-deflate-tiled-" "testng" "iut=http%3A%2F%2Fgeoserver%3A8080%2Fgeoserver%2Ftopp%2Fows%3Fservice%3DWCS%26version%3D2.0.1%26request%3DGetCoverage%26coverageId%3Dtopp__tazdem%26compression%3DDeflate%26tiling%3Dtrue%26tileheight%3D256%26tilewidth%3D256"
+
+  # three band, JPEG compressed, small tiles, WCS
+  run_rest_test_suite_prefix "geotiff11" "05-wcs-dem-jpeg-tinytiles-" "testng" "iut=http%3A%2F%2Fgeoserver%3A8080%2Fgeoserver%2Ftopp%2Fows%3Fservice%3DWCS%26version%3D2.0.1%26request%3DGetCoverage%26coverageId%3Dtopp__tazbm%26compression%3DJPEG%26compression%3DJPEG%26jpeg_quality%3D75%26tiling%3Dtrue%26tileheight%3D32%26tilewidth%3D32"
+}
+
 run_rest_test_suite() {
+  local suite_name=$1
+  local format=$2
+  shift 2
+  run_rest_test_suite_prefix "$suite_name" "" "$format" "$@"
+}
+
+run_rest_test_suite_prefix() {
+    set +x
+
     local suite_name=$1
-    local targetfile="/logs/$2-results.xml"
-    shift 2
+    local targetfile="/logs/$2$3-results.xml"
+    shift 3
     local params=("$@")
 
     echo $0
@@ -134,7 +160,6 @@ run_rest_test_suite() {
     echo $testurl
     credentials="ogctest:ogctest"
 
-    set +x
 
     echo
     echo Running tests
@@ -148,6 +173,7 @@ run_rest_test_suite() {
     # requesting application/xml as it's the one we can parse to fail the build if there are test failures
     curl -v -s -u "$credentials" "$testurl" -H "Accept: application/xml" > $targetfile
 }
+
 
 interactive () {
     /usr/local/tomcat/bin/startup.sh

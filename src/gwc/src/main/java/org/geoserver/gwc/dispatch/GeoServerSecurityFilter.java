df91e0786edf53ea2f2e908456c8a091ed1912c9
Ikeoka, Steve <Steve.Ikeoka@GDIT.com>
2025-01-14 09:41:49 -0800

    [GEOS-11677] Hide version info on GWC home page

--- src/gwc/src/main/java/org/geoserver/gwc/dispatch/GeoServerSecurityFilter.java
+++ src/gwc/src/main/java/org/geoserver/gwc/dispatch/GeoServerSecurityFilter.java
@@ -7,6 +7,8 @@ package org.geoserver.gwc.dispatch;
 
 import java.util.Objects;
 import org.geoserver.gwc.GWC;
+import org.geoserver.platform.GeoServerExtensions;
+import org.geoserver.security.GeoServerSecurityManager;
 import org.geotools.api.geometry.MismatchedDimensionException;
 import org.geotools.api.referencing.FactoryException;
 import org.geotools.geometry.jts.ReferencedEnvelope;
@@ -17,6 +19,9 @@ import org.geowebcache.filter.security.SecurityFilter;
 import org.geowebcache.grid.BoundingBox;
 import org.geowebcache.grid.SRS;
 import org.geowebcache.layer.TileLayer;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
  * Filter which applies GeoServer security to GWC requests
@@ -46,5 +51,32 @@ public class GeoServerSecurityFilter implements SecurityFilter {
                 throw new GeoWebCacheException(e);
             }
         }
+    }
+
+    // copied from org.geoserver.web.spring.security.GeoServerSession.isAdmin()
+    @Override
+    public boolean isAdmin() {
+        Authentication auth = getAuthentication();
+        if (auth == null || !auth.isAuthenticated() || auth instanceof AnonymousAuthenticationToken) {
+            return false;
+        } else {
+            return GeoServerExtensions.bean(GeoServerSecurityManager.class).checkAuthenticationForAdminRole(auth);
+        }
+    }
+
+    /**
+     * Spring authentication, or {@code null} if not set or anonymous.
+     *
+     * @return spring authentication, or {@code null} if not set or anonymous.
+     */
+    public Authentication getAuthentication() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null
+                && auth.getAuthorities().size() == 1
+                && "ROLE_ANONYMOUS"
+                        .equals(auth.getAuthorities().iterator().next().getAuthority())) {
+            return null;
+        }
+        return auth;
     }
 }

5b3b1fe82c1d2cb957f62e37e81490c1dc6725eb
Peter Smythe <peter@afrigis.co.za>
2025-02-24 16:57:13 +0200

    Run spotless:apply

--- src/main/src/main/java/org/geoserver/config/HealthCheckEndpoint.java
+++ src/main/src/main/java/org/geoserver/config/HealthCheckEndpoint.java
@@ -1,3 +1,8 @@
+/* (c) 2014 - 2025 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+
 package org.geoserver.config;
 
 import org.springframework.web.bind.annotation.GetMapping;

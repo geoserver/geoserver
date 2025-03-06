7ebc51a96118a54907c812abad14fd358948ea9b
root <root@geoserver-cite.lxd>
2025-03-06 16:13:35 +0000

    7 instance GeoServer docker-compose for cite.geoserver.org

---
+++ build/cite/postgis-data/init-scripts/03-init-user-wfs10.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+DB_NAME="cite_wfs10"
+
+psql -v ON_ERROR_STOP=0 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER cite PASSWORD 'cite';
+    CREATE DATABASE $DB_NAME;
+    GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO cite;
+EOSQL
+
+psql -v ON_ERROR_STOP=0 --username "$POSTGRES_USER" --dbname "$DB_NAME" <<-EOSQL
+    CREATE EXTENSION postgis;
+    GRANT ALL PRIVILEGES ON SCHEMA public TO cite;
+EOSQL
+

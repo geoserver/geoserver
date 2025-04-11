f1206012877c9380d02d73ab69006b0bbb5440d1
Andrea Aime <andrea.aime@gmail.com>
2025-04-11 18:50:17 +0200

    Adapt GeoServer build to GeoTools changes

--- src/main/src/main/java/org/geoserver/catalog/ResourcePool.java
+++ src/main/src/main/java/org/geoserver/catalog/ResourcePool.java
@@ -4,6 +4,8 @@
  */
 package org.geoserver.catalog;
 
+import static java.util.Collections.emptyList;
+
 import java.awt.RenderingHints;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
@@ -939,7 +941,8 @@ public class ResourcePool {
             List<XSDSchemaLocator> locators = Arrays.asList(GML.getInstance().createSchemaLocator());
             XSDSchema schema = null;
             try {
-                schema = Schemas.parse(schemaFile.getAbsolutePath(), locators, null);
+                schema = Schemas.parse(
+                        schemaFile.getAbsolutePath(), locators, emptyList(), emptyList(), getEntityResolver());
             } catch (Exception e) {
                 LOGGER.warning("Unable to parse "
                         + schemaFile.getAbsolutePath()

078821333208d683b7804ef3d86c4bb29c7e871a
Mike7311 <mykhailo.kotkov@geosolutionsgroup.com>
2025-04-28 11:50:58 +0400

    [GEOS-11805] Option to disable the management of stored queries

--- src/wfs/src/main/java/org/geoserver/wfs/WFSInfo.java
+++ src/wfs/src/main/java/org/geoserver/wfs/WFSInfo.java
@@ -321,5 +321,9 @@ public interface WFSInfo extends ServiceInfo {
      *
      * @param csvDateFormat Date Format pattern
      */
-    public void setCsvDateFormat(String csvDateFormat);
+    void setCsvDateFormat(String csvDateFormat);
+
+    boolean isDisableStoredQueriesManagement();
+
+    void setDisableStoredQueriesManagement(boolean disableStoredQueriesManagement);
 }

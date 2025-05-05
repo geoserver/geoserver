0df844e6fccdc260a23169d90f60c1551c341ff7
Ikeoka, Steve <Steve.Ikeoka@GDIT.com>
2025-05-05 13:25:34 -0700

    [GEOS-11817] GUI spinner remains after drag and drop

--- src/extension/geofence/geofence-server/src/main/java/org/geoserver/geofence/server/web/GeofenceServerPage.java
+++ src/extension/geofence/geofence-server/src/main/java/org/geoserver/geofence/server/web/GeofenceServerPage.java
@@ -14,6 +14,7 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.request.resource.PackageResourceReference;
 import org.geoserver.geofence.services.dto.ShortRule;
 import org.geoserver.web.GeoServerSecuredPage;
+import org.geoserver.web.wicket.GeoServerDNDBehavior;
 import org.geoserver.web.wicket.GeoServerDataProvider.Property;
 import org.geoserver.web.wicket.GeoServerTablePanel;
 import org.geoserver.web.wicket.ImageAjaxLink;
@@ -116,6 +117,7 @@ public class GeofenceServerPage extends GeoServerSecuredPage {
                         doReturn(GeofenceServerPage.class);
                     }
                 }.dropCenter("tr"));
+        rulesPanel.add(new GeoServerDNDBehavior());
         rulesPanel.setOutputMarkupId(true);
     }
 

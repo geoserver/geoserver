ffa1a4352965572910991a17a10490d03f6a794d
Alan McDade <alan.mcdade@geosolutionsgroup.com>
2025-05-08 15:45:36 +0200

    [GEOS-11810] New application setting to allow access by Workspace Admin (#8513)

--- src/web/wfs/src/test/java/org/geoserver/wfs/web/WFSAdminPageTest.java
+++ src/web/wfs/src/test/java/org/geoserver/wfs/web/WFSAdminPageTest.java
@@ -5,6 +5,7 @@
  */
 package org.geoserver.wfs.web;
 
+import static org.geoserver.web.services.BaseServiceAdminPage.WORKSPACE_ADMIN_SERVICE_ACCESS;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
@@ -12,18 +13,55 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Collections;
 import java.util.Locale;
+import java.util.Properties;
 import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.util.tester.FormTester;
+import org.geoserver.catalog.WorkspaceInfo;
+import org.geoserver.config.GeoServer;
+import org.geoserver.data.test.SystemTestData;
+import org.geoserver.ows.util.OwsUtils;
+import org.geoserver.platform.GeoServerExtensions;
+import org.geoserver.platform.resource.Resource;
+import org.geoserver.security.impl.DefaultFileAccessManager;
+import org.geoserver.web.GeoServerLoginPage;
 import org.geoserver.web.GeoServerWicketTestSupport;
+import org.geoserver.web.UnauthorizedPage;
 import org.geoserver.wfs.GMLInfo;
 import org.geoserver.wfs.WFSInfo;
 import org.geotools.api.util.InternationalString;
+import org.junit.Before;
 import org.junit.Test;
 
 public class WFSAdminPageTest extends GeoServerWicketTestSupport {
+    private static final String ROLE_CITE = "ROLE_CITE";
+    public static final String CITE_WFS_TITLE = "This is the CITE WFS service";
+    public static final String GLOBAL_WFS_TITLE = "This is the global WFS service";
+
+    @Override
+    protected void onSetUp(SystemTestData testData) throws Exception {
+        super.onSetUp(testData);
+    }
+
+    @Before
+    public void cleanupService() {
+        GeoServer gs = getGeoServer();
+        WFSInfo wfs = gs.getService(WFSInfo.class);
+        wfs.setTitle(null);
+        gs.save(wfs);
+
+        WorkspaceInfo citeWorkspace = getCatalog().getWorkspaceByName("cite");
+        WFSInfo citeWFS = gs.getService(citeWorkspace, WFSInfo.class);
+        if (citeWFS != null) {
+            gs.remove(citeWFS);
+        }
+    }
+
     @Test
     public void testValues() throws Exception {
         WFSInfo wfs = getGeoServerApplication().getGeoServer().getService(WFSInfo.class);
@@ -40,6 +78,7 @@ public class WFSAdminPageTest extends GeoServerWicketTestSupport {
 
     @Test
     public void testChangesToValues() throws Exception {
+
         String testValue1 = "100", testValue2 = "0";
         WFSInfo wfs = getGeoServerApplication().getGeoServer().getService(WFSInfo.class);
         login();
@@ -257,5 +296,106 @@ public class WFSAdminPageTest extends GeoServerWicketTestSupport {
         ft.submit("submit");
         assertNotNull(getGeoServer().getService(WFSInfo.class).getCsvDateFormat());
         assertEquals(getGeoServer().getService(WFSInfo.class).getCsvDateFormat(), "yyyy-MM-dd'T'HH:mm:ss'Z'");
+    }
+
+    @Test
+    public void testWorkspaceAdminFlagOn() throws IOException {
+        System.setProperty(WORKSPACE_ADMIN_SERVICE_ACCESS, "true");
+        // setup a CITE workspace WFS service
+        WorkspaceInfo citeWorkspace = getCatalog().getWorkspaceByName("cite");
+        GeoServer gs = getGeoServer();
+        WFSInfo wfs = gs.getService(WFSInfo.class);
+        wfs.setTitle(GLOBAL_WFS_TITLE);
+        gs.save(wfs);
+
+        WFSInfo citeWfs = gs.getFactory().create(WFSInfo.class);
+        OwsUtils.copy(wfs, citeWfs, WFSInfo.class);
+        citeWfs.setWorkspace(citeWorkspace);
+        citeWfs.setTitle(CITE_WFS_TITLE);
+        gs.add(citeWfs);
+
+        // setup a sandbox by security config
+        Resource layerSecurity = getDataDirectory().get("security/layers.properties");
+        Properties properties = new Properties();
+        properties.put("cite.*.a", ROLE_CITE);
+        try (OutputStream os = layerSecurity.out()) {
+            properties.store(os, "sandbox");
+        }
+        DefaultFileAccessManager fam = GeoServerExtensions.bean(DefaultFileAccessManager.class, applicationContext);
+        fam.reload();
+
+        logout();
+
+        // global service page, cannot be accessed as anonymous
+        tester.startPage(WFSAdminPage.class);
+        tester.assertRenderedPage(GeoServerLoginPage.class);
+
+        // login as workspace admin (logout happens as @After in base class)
+        login("cite", "pwd", ROLE_CITE);
+
+        // global service page, still no joy, not a global admin
+        tester.startPage(WFSAdminPage.class);
+        tester.assertRenderedPage(UnauthorizedPage.class);
+
+        // ok, can access with the right workspace
+        tester.startPage(WFSAdminPage.class, new PageParameters().add("workspace", "cite"));
+        tester.assertRenderedPage(WFSAdminPage.class);
+
+        // now log as admin and check the global service page is rendered
+        loginAsAdmin();
+        tester.startPage(WFSAdminPage.class);
+        tester.assertRenderedPage(WFSAdminPage.class);
+
+        System.clearProperty(WORKSPACE_ADMIN_SERVICE_ACCESS);
+    }
+
+    @Test
+    public void testWorkspaceAdminFlagOff() throws IOException {
+        System.clearProperty(WORKSPACE_ADMIN_SERVICE_ACCESS);
+
+        // setup a CITE workspace WFS service
+        WorkspaceInfo citeWorkspace = getCatalog().getWorkspaceByName("cite");
+        GeoServer gs = getGeoServer();
+        WFSInfo wfs = gs.getService(WFSInfo.class);
+        wfs.setTitle(GLOBAL_WFS_TITLE);
+        gs.save(wfs);
+
+        WFSInfo citeWfs = gs.getFactory().create(WFSInfo.class);
+        OwsUtils.copy(wfs, citeWfs, WFSInfo.class);
+        citeWfs.setWorkspace(citeWorkspace);
+        citeWfs.setTitle(CITE_WFS_TITLE);
+        gs.add(citeWfs);
+
+        // setup a sandbox by security config
+        Resource layerSecurity = getDataDirectory().get("security/layers.properties");
+        Properties properties = new Properties();
+        properties.put("cite.*.a", ROLE_CITE);
+        try (OutputStream os = layerSecurity.out()) {
+            properties.store(os, "sandbox");
+        }
+        DefaultFileAccessManager fam = GeoServerExtensions.bean(DefaultFileAccessManager.class, applicationContext);
+        fam.reload();
+
+        logout();
+
+        // global service page, cannot be accessed as anonymous
+        tester.startPage(WFSAdminPage.class);
+        tester.assertRenderedPage(GeoServerLoginPage.class);
+
+        // login as workspace admin (logout happens as @After in base class)
+        login("cite", "pwd", ROLE_CITE);
+
+        // global service page, still no joy, not a global admin
+        tester.startPage(WFSAdminPage.class);
+        tester.assertRenderedPage(UnauthorizedPage.class);
+
+        // cannot access with the workspace
+        tester.startPage(WFSAdminPage.class, new PageParameters().add("workspace", "cite"));
+        tester.assertRenderedPage(UnauthorizedPage.class);
+
+        // now log as admin and check the global service page is rendered
+        loginAsAdmin();
+        tester.startPage(WFSAdminPage.class);
+        tester.assertRenderedPage(WFSAdminPage.class);
     }
 }

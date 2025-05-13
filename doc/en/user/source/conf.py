c7e6e87c478a56427c5720a3493497759c0f53df
aaime <jody.garnett@gmail.com>
2025-05-13 18:01:10 +0000

    updating version numbers and release notes for 2.27.1

--- doc/en/user/source/conf.py
+++ doc/en/user/source/conf.py
@@ -58,7 +58,7 @@ version = '2.27'
 # check environmental variable to see if ant build.xml passed in project.version
 project_version = os.getenv("project.version")
 if project_version == None: 
-  release = '2.27-SNAPSHOT'
+  release = '2.27.1'
 else:
   release = project_version
 

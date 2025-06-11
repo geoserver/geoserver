625110cbf445c0b6a5f8593821c5278f577fa4c1
Ikeoka, Steve <11542093+sikeoka@users.noreply.github.com>
2025-06-11 14:41:37 -0700

    [GEOS-11862] Layer Preview and Tile Layers page dropdown links broken after updating table

--- src/web/demo/src/test/java/org/geoserver/web/demo/MapPreviewPageTest.java
+++ src/web/demo/src/test/java/org/geoserver/web/demo/MapPreviewPageTest.java
@@ -7,11 +7,15 @@ package org.geoserver.web.demo;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.matchesRegex;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
 import org.apache.wicket.MarkupContainer;
@@ -56,6 +60,14 @@ public class MapPreviewPageTest extends GeoServerWicketTestSupport {
     public void testValues() throws Exception {
         tester.startPage(MapPreviewPage.class);
         tester.assertRenderedPage(MapPreviewPage.class);
+        List<String> scripts = TagTester.createTags(
+                        tester.getLastResponseAsString(), tag -> tag.getName().equalsIgnoreCase("script"), false)
+                .stream()
+                .map(tag -> tag.getAttribute("src"))
+                .collect(Collectors.toList());
+        String regex =
+                "^.*/" + MapPreviewPage.class.getName() + "/" + MapPreviewPage.class.getSimpleName() + ".*\\.js$";
+        assertThat(scripts, hasItem(matchesRegex(regex)));
     }
 
     @Test
@@ -135,8 +147,9 @@ public class MapPreviewPageTest extends GeoServerWicketTestSupport {
 
         tester.startPage(MapPreviewPage.class);
         tester.assertRenderedPage(MapPreviewPage.class);
-
-        assertMaxFeaturesInData(tester.getLastResponseAsString(), maxFeatures);
+        TagTester maxFeaturesTag = tester.getTagById("maxFeatures");
+        assertNotNull("Missing maxFeatures field", maxFeaturesTag);
+        assertEquals(Integer.toString(maxFeatures), maxFeaturesTag.getAttribute("value"));
 
         maxFeatures = 0;
         wfsInfo.setMaxNumberOfFeaturesForPreview(maxFeatures);
@@ -144,22 +157,9 @@ public class MapPreviewPageTest extends GeoServerWicketTestSupport {
 
         tester.startPage(MapPreviewPage.class);
         tester.assertRenderedPage(MapPreviewPage.class);
-
-        assertMaxFeaturesInData(tester.getLastResponseAsString(), maxFeatures);
-    }
-
-    /**
-     * Max features is handled by a Javascript building the URLs client, so we need to check its code to make sure the
-     * maxFeatures parameter is being set correctly.
-     */
-    private void assertMaxFeaturesInData(String html, int maxFeatures) {
-        String PREFIX = "var maxFeature = '";
-        String END = "';";
-        if (maxFeatures > 0) {
-            assertThat(html, containsString(PREFIX + "&maxFeatures=" + maxFeatures + END));
-        } else {
-            assertThat(html, containsString(PREFIX + END));
-        }
+        maxFeaturesTag = tester.getTagById("maxFeatures");
+        assertNotNull("Missing maxFeatures field", maxFeaturesTag);
+        assertEquals(Integer.toString(maxFeatures), maxFeaturesTag.getAttribute("value"));
     }
 
     @Test

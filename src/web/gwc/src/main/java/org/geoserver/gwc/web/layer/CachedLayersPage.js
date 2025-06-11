c11885ce47f47ebbe0c72ceee3dbbea6cd5d0a28
Ikeoka, Steve <11542093+sikeoka@users.noreply.github.com>
2025-06-11 14:41:37 -0700

    [GEOS-11862] Layer Preview and Tile Layers page dropdown links broken after updating table

---
+++ src/web/gwc/src/main/java/org/geoserver/gwc/web/layer/CachedLayersPage.js
@@ -0,0 +1,10 @@
+
+"use strict";
+
+const CachedLayersPage_SetOnChange = () => {
+    $('.tile-layers-page-menu-select').on('change', event => {
+        const select = event.target;
+        window.open(select.options[select.selectedIndex].value);
+        select.selectedIndex = 0;
+    });
+};

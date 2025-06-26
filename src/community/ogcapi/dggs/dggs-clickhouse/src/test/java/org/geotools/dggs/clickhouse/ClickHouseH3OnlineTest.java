8d8168b040fb80a294a55fd6a2ee17893241768a
Andrea Aime <andrea.aime@gmail.com>
2025-06-26 17:22:00 +0200

    Reviewer's feedback

--- src/community/ogcapi/dggs/dggs-clickhouse/src/test/java/org/geotools/dggs/clickhouse/ClickHouseH3OnlineTest.java
+++ src/community/ogcapi/dggs/dggs-clickhouse/src/test/java/org/geotools/dggs/clickhouse/ClickHouseH3OnlineTest.java
@@ -2,7 +2,7 @@
  *    GeoTools - The Open Source Java GIS Toolkit
  *    http://geotools.org
  *
- *    (C) 2020, Open Source Geospatial Foundation (OSGeo)
+ *    (C) 2025, Open Source Geospatial Foundation (OSGeo)
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public

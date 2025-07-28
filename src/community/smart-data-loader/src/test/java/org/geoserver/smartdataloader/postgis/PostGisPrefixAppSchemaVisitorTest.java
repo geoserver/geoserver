caa5296ba9b6b9e793408e18dbe513e744a793e1
Fernando Mino <fernandomino7@gmail.com>
2025-07-28 13:37:46 -0500

    Review fixes

--- src/community/smart-data-loader/src/test/java/org/geoserver/smartdataloader/postgis/PostGisPrefixAppSchemaVisitorTest.java
+++ src/community/smart-data-loader/src/test/java/org/geoserver/smartdataloader/postgis/PostGisPrefixAppSchemaVisitorTest.java
@@ -1,3 +1,7 @@
+/* (c) 2025 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.smartdataloader.postgis;
 
 import org.geoserver.smartdataloader.visitors.JDBCAppSchemaVisitorTest;

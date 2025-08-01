c998ec0c8e034ff1a306a8c2fd46e400776471b3
afabiani <alessio.fabiani@geosolutionsgroup.com>
2025-08-01 15:24:39 +0200

    Review fixes

--- src/restconfig/src/test/java/org/geoserver/rest/security/SecurityConfigFilterHelper.java
+++ src/restconfig/src/test/java/org/geoserver/rest/security/SecurityConfigFilterHelper.java
@@ -1,3 +1,7 @@
+/* (c) 2025 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.rest.security;
 
 import java.io.IOException;

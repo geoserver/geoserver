41dfcf28c4d8fe3ddc08576629fec2dda6061253
Fernando Mino <fernandomino7@gmail.com>
2025-08-05 15:30:41 -0500

    [GEOS-11895] Features templates schemas overrides should be applied to OGC-API queryables output

--- src/community/features-templating/features-templating-core/src/test/java/org/geoserver/featurestemplating/response/TemplateComplexTestSupport.java
+++ src/community/features-templating/features-templating-core/src/test/java/org/geoserver/featurestemplating/response/TemplateComplexTestSupport.java
@@ -25,6 +25,7 @@ import org.geoserver.featurestemplating.configuration.TemplateInfo;
 import org.geoserver.featurestemplating.configuration.TemplateInfoDAO;
 import org.geoserver.featurestemplating.configuration.TemplateRule;
 import org.geoserver.featurestemplating.configuration.TemplateRuleService;
+import org.geoserver.featurestemplating.configuration.schema.*;
 import org.geoserver.test.AbstractAppSchemaMockData;
 import org.geoserver.test.AbstractAppSchemaTestSupport;
 import org.geoserver.test.FeatureChainingMockData;
@@ -293,5 +294,49 @@ public abstract class TemplateComplexTestSupport extends AbstractAppSchemaTestSu
                 assertTrue(lithology.size() > 0);
             }
         }
+    }
+
+    /**
+     * Sets up a schema override for a complex feature type.
+     *
+     * @param cqlRuleCondition the CQL rule condition
+     * @param profile the profile
+     * @param outputFormat the output format
+     * @param schemaFileName the schema file name
+     * @param schemaName the schema name
+     * @param schemaExtension the schema extension
+     * @param workspace the workspace
+     * @param ft the feature type info
+     * @throws IOException if an error occurs while setting up the schema override
+     */
+    protected void setUpSchemaOverride(
+            String cqlRuleCondition,
+            String profile,
+            SupportedFormat outputFormat,
+            String schemaFileName,
+            String schemaName,
+            String schemaExtension,
+            String workspace,
+            FeatureTypeInfo ft)
+            throws IOException {
+        // setup the schema override
+        String rawSchema =
+                org.apache.commons.io.IOUtils.toString(getClass().getResourceAsStream(schemaFileName), Charsets.UTF_8);
+        SchemaInfo info = new SchemaInfo();
+        info.setExtension(schemaExtension);
+        info.setSchemaName(schemaName);
+        info.setWorkspace(workspace);
+        info.setFeatureType(ft.getNativeName());
+        SchemaInfoDAO.get().saveOrUpdate(info);
+        SchemaFileManager.get().saveSchemaFile(info, rawSchema);
+        // setup the rule
+        SchemaRule rule = new SchemaRule();
+        rule.setSchemaName(info.getFullName());
+        rule.setCqlFilter(cqlRuleCondition);
+        rule.setProfileFilter(profile);
+        rule.setOutputFormat(outputFormat);
+        rule.setSchemaIdentifier(info.getIdentifier());
+        SchemaRuleService ruleService = new SchemaRuleService(ft);
+        ruleService.saveRule(rule);
     }
 }

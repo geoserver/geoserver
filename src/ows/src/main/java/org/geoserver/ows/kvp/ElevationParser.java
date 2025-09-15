6fdcbe394f687131cfa1da9e82c09a9d9a522272
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2025-09-15 16:48:41 -0300

    [GEOS-11933] Upgrade codebase to Java 17 language features - ows

--- src/ows/src/main/java/org/geoserver/ows/kvp/ElevationParser.java
+++ src/ows/src/main/java/org/geoserver/ows/kvp/ElevationParser.java
@@ -155,9 +155,7 @@ public class ElevationParser {
 
     private boolean addValue(Collection<Double> result, Double step) {
         for (final Object element : result) {
-            if (element instanceof Double) {
-                // convert
-                final Double local = (Double) element;
+            if (element instanceof Double local) {
                 if (local.equals(step)) return false;
             } else {
                 // convert
@@ -172,9 +170,9 @@ public class ElevationParser {
     private void addPeriod(Collection<Object> result, NumberRange<Double> newRange) {
         for (Iterator<Object> it = result.iterator(); it.hasNext(); ) {
             final Object element = it.next();
-            if (element instanceof Double) {
+            if (element instanceof Double double1) {
                 // convert
-                if (newRange.contains((Number) element)) {
+                if (newRange.contains((Number) double1)) {
                     it.remove();
                 }
             } else {

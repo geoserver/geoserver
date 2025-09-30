cd6f55c720ef9480e1d75a76fdd808e5e5a1f026
Jody Garnett <jody.garnett@gmail.com>
2025-09-30 13:57:38 -0700

    [GEOS-11948]  Adjust to use of USE_IMAGEN_IMAGEREAD
    While preserving USE_JAI_IMAGEREAD for rest api and coverate utils.

--- src/restconfig/src/main/java/org/geoserver/rest/catalog/CoverageStoreFileController.java
+++ src/restconfig/src/main/java/org/geoserver/rest/catalog/CoverageStoreFileController.java
@@ -128,7 +128,8 @@ public class CoverageStoreFileController extends AbstractStoreUploadController {
             @PathVariable UploadMethod method,
             @PathVariable String format,
             @RequestParam(name = "configure", required = false) String configure,
-            @RequestParam(name = "USE_IMAGEN_IMAGEREAD", required = false) Boolean useJaiImageRead,
+            @RequestParam(name = "USE_JAI_IMAGEREAD", required = false) Boolean useJaiImageRead,
+            @RequestParam(name = "USE_IMAGEN_IMAGEREAD", required = false) Boolean useImageNImageRead,
             @RequestParam(name = "coverageName", required = false) String coverageName,
             @RequestParam(required = false) String filename,
             HttpServletRequest request)
@@ -243,6 +244,10 @@ public class CoverageStoreFileController extends AbstractStoreUploadController {
             if (useJaiImageRead != null) {
                 customParameters.put(
                         AbstractGridFormat.USE_IMAGEN_IMAGEREAD.getName().toString(), useJaiImageRead);
+            }
+            if (useImageNImageRead != null) {
+                customParameters.put(
+                        AbstractGridFormat.USE_IMAGEN_IMAGEREAD.getName().toString(), useImageNImageRead);
             }
 
             // check if the name of the coverage was specified

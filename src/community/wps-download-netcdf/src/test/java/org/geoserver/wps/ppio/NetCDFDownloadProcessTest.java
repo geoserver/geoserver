7579cdd9577ba0a8e8b92fcba021fc304deab89d
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2025-10-21 18:04:37 +0200

    Fixing NetCDFDownload QA

--- src/community/wps-download-netcdf/src/test/java/org/geoserver/wps/ppio/NetCDFDownloadProcessTest.java
+++ src/community/wps-download-netcdf/src/test/java/org/geoserver/wps/ppio/NetCDFDownloadProcessTest.java
@@ -74,7 +74,7 @@ public class NetCDFDownloadProcessTest extends WPSTestSupport {
         }
 
         public AutoDisposableGridCoverage2D read() throws IOException {
-            GridCoverage2D gc = super.read(null);
+            GridCoverage2D gc = super.read();
             assertNotNull(gc);
             return new AutoDisposableGridCoverage2D("", gc);
         }

2bfc9552a208567e44a3065dc3fc3d5b7f2eb89e
Ian Turton <ian@ianturton.com>
2025-10-22 14:13:40 +0100

    increase threshold on raster legend integration test to allow for new font

--- src/wms/src/test/java/org/geoserver/wms/wms_1_1_1/GetMapIntegrationTest.java
+++ src/wms/src/test/java/org/geoserver/wms/wms_1_1_1/GetMapIntegrationTest.java
@@ -1963,7 +1963,7 @@ public class GetMapIntegrationTest extends WMSTestSupport {
                 "image/png");
 
         File expected = new File("src/test/resources/org/geoserver/wms/wms_1_1_1/dem_with_legend.png");
-        ImageAssert.assertEquals(expected, image, 3400);
+        ImageAssert.assertEquals(expected, image, 3700);
     }
 
     @Test

8c5e8bf41adda9ceabd1e4ad01f49c76632cd33e
Andrea Aime <andrea.aime@gmail.com>
2025-10-26 18:45:33 +0100

    Skip tests randomly failing on Github alone

--- src/security/jdbc/src/test/java/org/geoserver/security/jdbc/H2JNDIUserDetailsServiceTest.java
+++ src/security/jdbc/src/test/java/org/geoserver/security/jdbc/H2JNDIUserDetailsServiceTest.java
@@ -8,12 +8,20 @@ package org.geoserver.security.jdbc;
 
 import org.geoserver.security.GeoServerRoleService;
 import org.geoserver.security.GeoServerUserGroupService;
+import org.junit.Assume;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 
 public class H2JNDIUserDetailsServiceTest extends JDBCUserDetailsServiceTest {
 
     @ClassRule
     public static final H2JNDITestConfig jndiConfig = new H2JNDITestConfig();
+
+    @BeforeClass
+    public static void checkGithub() {
+        // on Github this test randomly fails with a H2 lock issue
+        Assume.assumeFalse(System.getProperty("github-build") != null);
+    }
 
     @Override
     protected String getFixtureId() {

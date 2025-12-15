819b8d7623aecbb7098cdaa5d28b43f82ac827a0
Niels Charlier <niels@scitus.be>
2025-12-15 16:26:04 +0100

    taskmanager: fix merge issues

--- src/community/taskmanager/core/src/main/java/org/geoserver/taskmanager/data/impl/ParameterImpl.java
+++ src/community/taskmanager/core/src/main/java/org/geoserver/taskmanager/data/impl/ParameterImpl.java
@@ -4,16 +4,17 @@
  */
 package org.geoserver.taskmanager.data.impl;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Index;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.io.Serial;
 import org.geoserver.taskmanager.data.Parameter;
 import org.geoserver.taskmanager.data.Task;
 

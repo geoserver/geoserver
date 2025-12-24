0910f6c36e6dc195cc39eeadd8012dc6cd25d877
Peter Smythe <peter@afrigis.co.za>
2025-12-24 12:35:47 +0200

    Add Git branch name in GEOSERVER_NODE_OPTS
    - Implemented functionality to resolve and substitute the Git branch name in node IDs.
    - Added unit tests to verify Git branch token resolution from various directory structures.
    - Updated documentation to clarify the use of Application Properties (specifcally the order).

--- src/main/src/test/java/org/geoserver/GeoServerNodeDataTest.java
+++ src/main/src/test/java/org/geoserver/GeoServerNodeDataTest.java
@@ -9,6 +9,8 @@ import static org.junit.Assert.assertNotNull;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -61,5 +63,57 @@ public class GeoServerNodeDataTest {
         GeoServerNodeData data = GeoServerNodeData.createFromString("id:$host_compact_name");
         assertEquals("test.l", data.getId());
         assertNotNull(data.getIdStyle());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        GeoServerNodeData.clearOverrideGitHeadPath();
+    }
+
+    @Test
+    public void testGitBranchToken() throws Exception {
+        Path tmp = Files.createTempDirectory("githead");
+        Path head = tmp.resolve(".git").resolve("HEAD");
+        Files.createDirectories(head.getParent());
+        Files.writeString(head, "ref: refs/heads/feature/test\n");
+
+        GeoServerNodeData.setOverrideGitHeadPath(head);
+        GeoServerNodeData data = GeoServerNodeData.createFromString("id:$git_branch");
+        assertEquals("feature/test", data.getId());
+    }
+
+    @Test
+    public void testGitBranchFromDataDir() throws Exception {
+        Path tmp = Files.createTempDirectory("data-dir");
+        Path head = tmp.resolve(".git").resolve("HEAD");
+        Files.createDirectories(head.getParent());
+        Files.writeString(head, "ref: refs/heads/data-branch\n");
+
+        System.setProperty("GEOSERVER_DATA_DIR", tmp.toAbsolutePath().toString());
+        try {
+            GeoServerNodeData data = GeoServerNodeData.createFromString("id:$git_branch");
+            assertEquals("data-branch", data.getId());
+        } finally {
+            System.clearProperty("GEOSERVER_DATA_DIR");
+        }
+    }
+
+    @Test
+    public void testGitBranchFromParentOfDataDir() throws Exception {
+        Path parent = Files.createTempDirectory("parent-dir");
+        Path child = parent.resolve("child-data");
+        Files.createDirectories(child);
+
+        Path head = parent.resolve(".git").resolve("HEAD");
+        Files.createDirectories(head.getParent());
+        Files.writeString(head, "ref: refs/heads/parent-branch\n");
+
+        System.setProperty("GEOSERVER_DATA_DIR", child.toAbsolutePath().toString());
+        try {
+            GeoServerNodeData data = GeoServerNodeData.createFromString("id:$git_branch");
+            assertEquals("parent-branch", data.getId());
+        } finally {
+            System.clearProperty("GEOSERVER_DATA_DIR");
+        }
     }
 }

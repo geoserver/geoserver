338cd94057cb140414c85ebcf3178b7d4d7d6f03
Fernando Mino <fernandomino7@gmail.com>
2026-01-14 14:37:30 -0500

    Review fixes

--- src/community/features-templating/features-templating-core/src/test/java/org/geoserver/featurestemplating/configuration/schema/SchemaXStreamConfigurationMigrationTest.java
+++ src/community/features-templating/features-templating-core/src/test/java/org/geoserver/featurestemplating/configuration/schema/SchemaXStreamConfigurationMigrationTest.java
@@ -1,3 +1,7 @@
+/* (c) 2026 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.featurestemplating.configuration.schema;
 
 import static org.junit.Assert.assertSame;

7c2926fe3801da0a3f528bf51071b42d08d8398d
Mark Fodor <mark.fodor@geosolutionsgroup.com>
2026-01-19 11:45:08 +0100

    GEOS-12020 split WFS module to core and versioned modules

---
+++ src/wfs-core/src/main/java/org/geoserver/wfs/xml/SqlViewParamsExtractor.java
@@ -0,0 +1,44 @@
+/* (c) 2014 Open Source Geospatial Foundation - all rights reserved
+ * (c) 2001 - 2013 OpenPlans
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.wfs.xml;
+
+import java.util.List;
+import org.eclipse.emf.common.util.EList;
+import org.geoserver.ows.kvp.ViewParamsKvpParser;
+import org.geotools.xsd.Node;
+
+/** Static methods for accessing the ViewParams KVP parser. */
+public class SqlViewParamsExtractor {
+
+    /** Fully setup KVP parser for viewParams. Injected by spring at runtime */
+    private static ViewParamsKvpParser wfsSqlViewKvpParser = null;
+
+    public static ViewParamsKvpParser getWfsSqlViewKvpParser() {
+        return wfsSqlViewKvpParser;
+    }
+
+    public static void setWfsSqlViewKvpParser(ViewParamsKvpParser wfsSqlViewKvpParser) {
+        SqlViewParamsExtractor.wfsSqlViewKvpParser = wfsSqlViewKvpParser;
+    }
+
+    /**
+     * Fix the node object to store a parsed list of viewParams instead of a raw string. This prevents the parse()
+     * method choking later on...
+     */
+    @SuppressWarnings("unchecked") // no generics in EMF model
+    public static void fixNodeObject(Node node) throws Exception {
+        List viewParams = null;
+        if (node.hasAttribute("viewParams")) {
+            Node viewParamsAttribute = node.getAttribute("viewParams");
+            viewParams = (List) wfsSqlViewKvpParser.parse((String) viewParamsAttribute.getValue());
+
+            EList viewParamsList = new org.eclipse.emf.common.util.BasicEList<>();
+            viewParamsList.addAll(viewParams);
+
+            viewParamsAttribute.setValue(viewParamsList);
+        }
+    }
+}

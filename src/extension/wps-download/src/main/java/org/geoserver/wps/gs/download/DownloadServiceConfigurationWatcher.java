6ae580f533ae405e9bc8dc9e021f0f42a5c24df1
Peter Smythe <peter@afrigis.co.za>
2026-02-01 12:23:12 +0200

    Apply Spotless import order fixes: MosaicIndex, DownloadServiceConfigurationWatcher

--- src/extension/wps-download/src/main/java/org/geoserver/wps/gs/download/DownloadServiceConfigurationWatcher.java
+++ src/extension/wps-download/src/main/java/org/geoserver/wps/gs/download/DownloadServiceConfigurationWatcher.java
@@ -31,8 +31,8 @@ import org.geoserver.platform.resource.Paths;
 import org.geoserver.platform.resource.Resource;
 import org.geoserver.platform.resource.Resources;
 import org.geoserver.security.PropertyFileWatcher;
-import org.geoserver.util.LinkedProperties;
 import org.geoserver.util.IOUtils;
+import org.geoserver.util.LinkedProperties;
 import org.geotools.util.Utilities;
 import org.geotools.util.logging.Logging;
 

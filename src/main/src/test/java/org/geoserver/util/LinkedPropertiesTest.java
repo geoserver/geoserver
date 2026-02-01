5c366fb2ba486d5816ba2dfc9981449e8c500580
Peter Smythe <peter@afrigis.co.za>
2026-02-01 11:30:49 +0200

    Add missing assertNull and assertNotEquals imports to LinkedPropertiesTest

--- src/main/src/test/java/org/geoserver/util/LinkedPropertiesTest.java
+++ src/main/src/test/java/org/geoserver/util/LinkedPropertiesTest.java
@@ -6,7 +6,9 @@ package org.geoserver.util;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayOutputStream;

b0232f1e5f5d1b0dc41111da78b2e747d1152bb4
Eric Stadtherr <estadtherr@gmail.com>
2026-02-10 08:29:57 -0700

    [GS3] Updates to the GSR community extension to enable successful build and unit test

--- src/community/gsr/src/test/java/org/geoserver/gsr/validation/JSONValidator.java
+++ src/community/gsr/src/test/java/org/geoserver/gsr/validation/JSONValidator.java
@@ -9,7 +9,11 @@
  */
 package org.geoserver.gsr.validation;
 
-import java.io.File;
+import com.networknt.schema.*;
+import com.networknt.schema.Error;
+import com.networknt.schema.resource.IriResourceLoader;
+import java.io.*;
+import java.util.List;
 import java.util.logging.Logger;
 import org.geotools.util.logging.Logging;
 
@@ -18,47 +22,27 @@ public class JSONValidator {
     static final Logger LOGGER = Logging.getLogger(JSONValidator.class);
 
     public static boolean isValidSchema(String json, File schemaFile) {
-        //        boolean isValid = false;
-        //        final String baseURI = "file:///" + schemaFile.getAbsolutePath().replace(File.separator, "/");
-        //
-        //        JsonSchema schema;
-        //        try {
-        //            JsonSchemaFactory factory = JsonSchemaFactory.newBuilder()
-        //                    .setLoadingConfiguration(LoadingConfiguration.newBuilder()
-        //                            .setNamespace(baseURI)
-        //                            .freeze())
-        //                    .freeze();
-        //            JsonNode rawSchema = JsonLoader.fromFile(schemaFile);
-        //            schema = factory.getJsonSchema(rawSchema);
-        //        } catch (Exception e) {
-        //            throw new RuntimeException("Failed to load JSON Schema from " + baseURI, e);
-        //        }
-        //
-        //        JsonNode jsonNode;
-        //        try {
-        //            Reader reader = new StringReader(json);
-        //            jsonNode = JsonLoader.fromReader(reader);
-        //        } catch (Exception e) {
-        //            throw new RuntimeException("Couldn't load (" + json + ") as JSON", e);
-        //        }
-        //
-        //        ProcessingReport report;
-        //        try {
-        //            report = schema.validate(jsonNode);
-        //        } catch (ProcessingException e) {
-        //            LOGGER.log(Level.WARNING, "", e);
-        //            return false;
-        //        }
-        //
-        //        isValid = report.isSuccess();
-        //        if (!isValid) {
-        //            System.out.println("ERROR validating Json Schema in " + schemaFile);
-        //            for (ProcessingMessage msg : report) {
-        //                System.out.println(msg);
-        //            }
-        //        }
-        //        return isValid;
-        throw new RuntimeException(
-                "Please migrate code to another JSON schema validator that's either using Jackson 3, or is independent of Jackson.");
+        final String baseURI = "file://" + schemaFile.getAbsolutePath().replace(File.separator, "/");
+
+        Schema schema;
+        try {
+            SchemaRegistry schemaRegistry = SchemaRegistry.withDefaultDialect(
+                    SpecificationVersion.DRAFT_2020_12,
+                    builder -> builder.resourceLoaders(loaders -> loaders.add(new IriResourceLoader())));
+            schema = schemaRegistry.getSchema(SchemaLocation.of(baseURI));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load JSON Schema from " + baseURI, e);
+        }
+
+        List<Error> errors = schema.validate(json, InputFormat.JSON);
+
+        boolean isValid = errors.isEmpty();
+        if (!isValid) {
+            System.err.println("ERROR validating JSON Schema in " + schemaFile);
+            for (Error error : errors) {
+                System.err.println(error);
+            }
+        }
+        return isValid;
     }
 }

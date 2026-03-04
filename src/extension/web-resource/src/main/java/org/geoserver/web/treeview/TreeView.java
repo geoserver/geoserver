4de5113b3ef7b83d2ff04c5f7864bbf66c590738
David Blasby <david.blasby@geocat.net>
2026-03-03 17:19:42 -0800

    pmd fix

--- src/extension/web-resource/src/main/java/org/geoserver/web/treeview/TreeView.java
+++ src/extension/web-resource/src/main/java/org/geoserver/web/treeview/TreeView.java
@@ -447,6 +447,17 @@ public class TreeView<T> extends Panel {
 
         private boolean isCssEmpty = org.geoserver.web.util.WebUtils.IsWicketCssFileEmpty(getClass());
 
+        @Override
+        public void renderHead(org.apache.wicket.markup.head.IHeaderResponse response) {
+            super.renderHead(response);
+            // if the panel-specific CSS file contains actual css then have the browser load the css
+            if (!isCssEmpty) {
+                response.render(org.apache.wicket.markup.head.CssHeaderItem.forReference(
+                        new org.apache.wicket.request.resource.PackageResourceReference(
+                                getClass(), getClass().getSimpleName() + ".css")));
+            }
+        }
+
         @Serial
         private static final long serialVersionUID = 2940674057639126436L;
 
@@ -512,6 +523,17 @@ public class TreeView<T> extends Panel {
     protected class TreeLeafView extends TreeNodeView {
 
         private boolean isCssEmpty = org.geoserver.web.util.WebUtils.IsWicketCssFileEmpty(getClass());
+
+        @Override
+        public void renderHead(org.apache.wicket.markup.head.IHeaderResponse response) {
+            super.renderHead(response);
+            // if the panel-specific CSS file contains actual css then have the browser load the css
+            if (!isCssEmpty) {
+                response.render(org.apache.wicket.markup.head.CssHeaderItem.forReference(
+                        new org.apache.wicket.request.resource.PackageResourceReference(
+                                getClass(), getClass().getSimpleName() + ".css")));
+            }
+        }
 
         @Serial
         private static final long serialVersionUID = 2940674057639126436L;

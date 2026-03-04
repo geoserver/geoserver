b731b087c1dbc62bc9b87b7b8564dd1b0806d345
David Blasby <david.blasby@geocat.net>
2026-03-04 13:34:39 -0800

    minor cleanup of the proof-of-concept

--- src/web/gwc/src/main/java/org/geoserver/gwc/web/layer/GridSubsetsEditor.java
+++ src/web/gwc/src/main/java/org/geoserver/gwc/web/layer/GridSubsetsEditor.java
@@ -17,7 +17,6 @@ import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.form.OnChangeAjaxBehavior;
-import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
@@ -31,7 +30,6 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.ResourceModel;
-import org.apache.wicket.request.resource.PackageResourceReference;
 import org.apache.wicket.validation.IValidatable;
 import org.apache.wicket.validation.IValidator;
 import org.apache.wicket.validation.ValidationError;
@@ -145,8 +143,6 @@ class GridSubsetsEditor extends FormComponentPanel<Set<XMLGridSubset>> {
     @Override
     public void renderHead(IHeaderResponse response) {
         super.renderHead(response);
-        response.render(CssHeaderItem.forReference(
-                new PackageResourceReference(getClass(), getClass().getSimpleName() + ".css")));
         // if the panel-specific CSS file contains actual css then have the browser load the css
         if (!isCssEmpty) {
             response.render(org.apache.wicket.markup.head.CssHeaderItem.forReference(

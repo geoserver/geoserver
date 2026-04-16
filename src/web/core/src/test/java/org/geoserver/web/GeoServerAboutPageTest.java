74cc8159bc1fcfb46e22b3c0083046effd5f0cd8
Jody Garnett <jody.garnett@gmail.com>
2026-04-15 21:50:33 -0700

    [GEOS-12063] Double check context parameters are being respected

--- src/web/core/src/test/java/org/geoserver/web/GeoServerAboutPageTest.java
+++ src/web/core/src/test/java/org/geoserver/web/GeoServerAboutPageTest.java
@@ -11,7 +11,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.apache.wicket.util.tester.TagTester;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class GeoServerAboutPageTest extends GeoServerWicketTestSupport {

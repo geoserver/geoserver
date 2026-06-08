9c85c0c280ff891d0f49d7d648e1094eefdb743e
Jody Garnett <jody.garnett@gmail.com>
2026-06-08 11:34:37 -0700

    [GEOS-12110] Troubleshooting Cascading WMS tests which expect DTD use

--- src/main/src/main/java/org/geoserver/catalog/ResourcePool.java
+++ src/main/src/main/java/org/geoserver/catalog/ResourcePool.java
@@ -1885,14 +1885,15 @@ public class ResourcePool {
         hints.put(DocumentFactory.VALIDATION_HINT, Boolean.FALSE);
         if (entityResolver != null) {
             hints.put(XMLHandlerHints.ENTITY_RESOLVER, entityResolver);
-            //            if (entityResolver instanceof EntityResolver3 entityResolver3) {
-            //                String access = entityResolver3.getAccess();
-            //                if (access.equals("all") || access.contains("file")) {
-            //                    // trusted location
-            //                    hints.put(DocumentFactory.ENABLE_DTD, Boolean.TRUE);
-            //                    hints.put(DocumentFactory.DISABLE_EXTERNAL_ENTITIES, Boolean.FALSE);
-            //                }
-            //            }
+            if (entityResolver.getClass().getSimpleName().equals("DevModeEntityResolver")) {
+                hints.put(DocumentFactory.ENABLE_DTD, Boolean.TRUE);
+                hints.put(DocumentFactory.DISABLE_EXTERNAL_ENTITIES, Boolean.FALSE);
+            }
+        }
+        if (System.getProperty(EntityResolverProvider.ENTITY_RESOLUTION_UNRESTRICTED, "")
+                .equalsIgnoreCase("true")) {
+            hints.put(DocumentFactory.ENABLE_DTD, Boolean.TRUE);
+            hints.put(DocumentFactory.DISABLE_EXTERNAL_ENTITIES, Boolean.FALSE);
         }
         WebMapServer wms;
         if (StringUtils.isNotEmpty(expandedStore.getHeaderName())

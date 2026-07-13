f3ec1600d9c95d8b84d138b029d55bf88a54685e
Andrea Aime <andrea.aime@gmail.com>
2026-07-13 17:10:39 +0200

    [GEOS-12150] OpenSearch REST API logs index creation errors when a collection has dots in its name

--- src/community/oseo/web-oseo/src/test/java/org/geoserver/opensearch/eo/web/OSEOAdminPageTest.java
+++ src/community/oseo/web-oseo/src/test/java/org/geoserver/opensearch/eo/web/OSEOAdminPageTest.java
@@ -78,6 +78,17 @@ public class OSEOAdminPageTest extends OSEOWebTestSupport {
     }
 
     @Test
+    public void testProductClassValidation() throws Exception {
+        openAdminPage();
+        // the add link appends a product class with empty name, prefix and namespace, which
+        // must be rejected on save
+        tester.clickLink("form:" + PANEL + "addClass");
+        FormTester formTester = tester.newFormTester("form");
+        formTester.submit("submit");
+        assertEquals(1, tester.getMessages(FeedbackMessage.ERROR).size());
+    }
+
+    @Test
     public void testCacheTTLValidatation() throws Exception {
         setupCacheValues(-10);
         assertEquals(1, tester.getMessages(FeedbackMessage.ERROR).size());

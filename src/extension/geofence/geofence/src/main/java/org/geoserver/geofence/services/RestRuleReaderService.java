960fa0c09d3ee69fc882ae63e49c0638804c5726
Emanuele Tajariol <etj@geo-solutions.it>
2026-07-29 13:35:45 +0200

    Fix assembly

--- src/extension/geofence/geofence/src/main/java/org/geoserver/geofence/services/RestRuleReaderService.java
+++ src/extension/geofence/geofence/src/main/java/org/geoserver/geofence/services/RestRuleReaderService.java
@@ -5,36 +5,210 @@
 
 package org.geoserver.geofence.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.geofence.core.services.RuleReaderService;
 import org.geofence.core.services.dto.AccessInfo;
+import org.geofence.core.services.dto.AccessTypeDTO;
+import org.geofence.core.services.dto.CatalogModeDTO;
+import org.geofence.core.services.dto.GrantTypeDTO;
+import org.geofence.core.services.dto.LayerAttributeDTO;
 import org.geofence.core.services.dto.RuleFilter;
+import org.geofence.core.services.dto.RuleFilter.FilterType;
+import org.geofence.core.services.dto.RuleFilter.IdNameFilter;
+import org.geofence.core.services.dto.RuleFilter.TextFilter;
 import org.geofence.core.services.dto.ShortRule;
+import org.geofence.web.rest.api.interfaces.params.RESTRuleFilter;
+import org.geofence.web.rest.api.model.RESTAccessInfo;
+import org.geofence.web.rest.api.model.RESTLayerAttribute;
+import org.geofence.web.rest.api.model.RESTShortRule;
+import org.geofence.web.rest.api.model.enums.RESTAccessType;
+import org.geofence.web.rest.api.model.enums.RESTCatalogMode;
+import org.geofence.web.rest.api.model.enums.RESTGrantType;
+import org.geofence.web.rest.client.GeoFenceClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+/**
+ * REST-based {@link RuleReaderService}: talks to a remote GeoFence server's {@code /rulereader} endpoint, for
+ * deployments where the embedded engine (see {@code geofence-server}) isn't used.
+ */
 @Component
 public class RestRuleReaderService implements RuleReaderService {
 
     @Value("${servicesUrl}")
     private String serviceUrl;
 
+    private GeoFenceClient client;
+    private String clientUrl;
+
     @Override
     public AccessInfo getAccessInfo(RuleFilter filter) {
-        return null;
+        RESTAccessInfo restInfo = ruleReaderClient().getAccessInfo(toRestFilter(filter));
+        return toAccessInfo(restInfo);
     }
 
     @Override
     public AccessInfo getAdminAuthorization(RuleFilter filter) {
-        return null;
+        RESTAccessInfo restInfo = ruleReaderClient().getAdminAuthorization(toRestFilter(filter));
+        return toAccessInfo(restInfo);
     }
 
     @Override
     public List<ShortRule> getMatchingRules(RuleFilter filter) {
-        return List.of();
+        List<ShortRule> rules = new ArrayList<>();
+        ruleReaderClient().getMatchingRules(toRestFilter(filter)).forEach(restRule -> rules.add(toShortRule(restRule)));
+        return rules;
     }
 
     public void setServiceUrl(String serviceUrl) {
         this.serviceUrl = serviceUrl;
+    }
+
+    private synchronized org.geofence.web.rest.api.interfaces.RESTRuleReaderService ruleReaderClient() {
+        if (client == null || !serviceUrl.equals(clientUrl)) {
+            GeoFenceClient newClient = new GeoFenceClient();
+            newClient.setRestUrl(serviceUrl);
+            client = newClient;
+            clientUrl = serviceUrl;
+        }
+        return client.getRuleReaderService();
+    }
+
+    // ==========================================================================
+    // RuleFilter -> RESTRuleFilter
+
+    static RESTRuleFilter toRestFilter(RuleFilter filter) {
+        RESTRuleFilter query = new RESTRuleFilter();
+
+        TextQuery user = toQuery(filter.getUser());
+        query.userName = user.name();
+        query.userDefault = user.includeDefault();
+
+        TextQuery role = toQuery(filter.getRole());
+        query.groupName = role.name();
+        query.groupDefault = role.includeDefault();
+
+        IdNameQuery instance = toQuery(filter.getInstance());
+        query.instanceId = instance.id();
+        query.instanceName = instance.name();
+        query.instanceDefault = instance.includeDefault();
+
+        TextQuery sourceAddress = toQuery(filter.getSourceAddress());
+        query.ipAddress = sourceAddress.name();
+        query.ipAddressDefault = sourceAddress.includeDefault();
+
+        TextQuery date = toQuery(filter.getDate());
+        query.date = date.name();
+        query.dateDefault = date.includeDefault();
+
+        TextQuery service = toQuery(filter.getService());
+        query.serviceName = service.name();
+        query.serviceDefault = service.includeDefault();
+
+        TextQuery request = toQuery(filter.getRequest());
+        query.requestName = request.name();
+        query.requestDefault = request.includeDefault();
+
+        TextQuery subfield = toQuery(filter.getSubfield());
+        query.subfieldName = subfield.name();
+        query.subfieldDefault = subfield.includeDefault();
+
+        TextQuery workspace = toQuery(filter.getWorkspace());
+        query.workspace = workspace.name();
+        query.workspaceDefault = workspace.includeDefault();
+
+        TextQuery layer = toQuery(filter.getLayer());
+        query.layer = layer.name();
+        query.layerDefault = layer.includeDefault();
+
+        return query;
+    }
+
+    private record TextQuery(String name, Boolean includeDefault) {}
+
+    private static TextQuery toQuery(TextFilter filter) {
+        if (filter.getType() == FilterType.NAMEVALUE) {
+            return new TextQuery(filter.getText(), filter.isIncludeDefault());
+        } else if (filter.getType() == FilterType.DEFAULT) {
+            return new TextQuery(null, true);
+        } else {
+            return new TextQuery(null, null);
+        }
+    }
+
+    private record IdNameQuery(Long id, String name, Boolean includeDefault) {}
+
+    private static IdNameQuery toQuery(IdNameFilter filter) {
+        if (filter.getType() == FilterType.IDVALUE) {
+            return new IdNameQuery(filter.getId(), null, filter.isIncludeDefault());
+        } else if (filter.getType() == FilterType.NAMEVALUE) {
+            return new IdNameQuery(null, filter.getName(), filter.isIncludeDefault());
+        } else if (filter.getType() == FilterType.DEFAULT) {
+            return new IdNameQuery(null, null, true);
+        } else {
+            return new IdNameQuery(null, null, null);
+        }
+    }
+
+    // ==========================================================================
+    // RESTAccessInfo -> AccessInfo / RESTShortRule -> ShortRule
+
+    static AccessInfo toAccessInfo(RESTAccessInfo in) {
+        AccessInfo out = new AccessInfo(map(in.getGrant()));
+        out.setAdminRights(in.isAdminRights());
+        out.setAreaWkt(in.getAreaWkt());
+        out.setClipAreaWkt(in.getClipAreaWkt());
+        out.setCatalogMode(map(in.getCatalogMode()));
+        out.setDefaultStyle(in.getDefaultStyle());
+        out.setCqlFilterRead(in.getCqlFilterRead());
+        out.setCqlFilterWrite(in.getCqlFilterWrite());
+        out.setAllowedStyles(in.getAllowedStyles());
+        if (in.getAttributes() != null) {
+            out.setAttributes(in.getAttributes().stream()
+                    .map(RestRuleReaderService::map)
+                    .collect(java.util.stream.Collectors.toSet()));
+        }
+        return out;
+    }
+
+    static ShortRule toShortRule(RESTShortRule in) {
+        ShortRule out = new ShortRule();
+        out.setId(in.getId());
+        out.setPriority(in.getPriority());
+        out.setUserName(in.getUserName());
+        out.setRoleName(in.getRoleName());
+        out.setInstanceId(in.getInstanceId());
+        out.setInstanceName(in.getInstanceName());
+        out.setAddressRange(in.getAddressRange());
+        out.setValidAfter(in.getValidAfter());
+        out.setValidBefore(in.getValidBefore());
+        out.setService(in.getService());
+        out.setRequest(in.getRequest());
+        out.setSubfield(in.getSubfield());
+        out.setWorkspace(in.getWorkspace());
+        out.setLayer(in.getLayer());
+        out.setAccess(map(in.getAccess()));
+        return out;
+    }
+
+    private static GrantTypeDTO map(RESTGrantType in) {
+        return in == null ? null : GrantTypeDTO.valueOf(in.name());
+    }
+
+    private static AccessTypeDTO map(RESTAccessType in) {
+        return in == null ? null : AccessTypeDTO.valueOf(in.name());
+    }
+
+    private static CatalogModeDTO map(RESTCatalogMode in) {
+        return in == null ? null : CatalogModeDTO.valueOf(in.name());
+    }
+
+    private static LayerAttributeDTO map(RESTLayerAttribute in) {
+        LayerAttributeDTO out = new LayerAttributeDTO();
+        out.setName(in.getName());
+        out.setDatatype(in.getDatatype());
+        out.setAccess(map(in.getAccess()));
+        return out;
     }
 }
--- src/extension/geofence/geofence/src/test/java/org/geoserver/geofence/GeofencePageTest.java
+++ src/extension/geofence/geofence/src/test/java/org/geoserver/geofence/GeofencePageTest.java
@@ -21,7 +21,6 @@ import org.geoserver.geofence.web.GeofencePage;
 import org.geoserver.web.GeoServerHomePage;
 import org.geoserver.web.GeoServerWicketTestSupport;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.core.io.UrlResource;
 
@@ -90,9 +89,6 @@ public class GeofencePageTest extends GeoServerWicketTestSupport {
         tester.assertContains("is required");
     }
 
-    // RestRuleReaderService is currently a stub (doesn't make real REST calls yet), so it can't detect
-    // a bad URL and raise a RemoteAccessException - see GeofencePage.getRuleReaderService(). Revisit later.
-    @Ignore("RestRuleReaderService is a stub; can't validate the URL yet, see method comment")
     @Test
     public void testErrorWrongURL() {
         @SuppressWarnings("unchecked")
@@ -101,7 +97,7 @@ public class GeofencePageTest extends GeoServerWicketTestSupport {
 
         tester.clickLink("form:test", true);
 
-        tester.assertContains("RemoteAccessException");
+        tester.assertContains("ProcessingException");
     }
 
     @Test

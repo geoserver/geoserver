b5f9f724fab0aef1762ca11264d71e2255c8c522
Jody Garnett <jody.garnett@gmail.com>
2026-09-23 16:10:59 -0700

    Add header to maps-collection.js

--- src/community/ogcapi/ogcapi-maps/src/main/resources/webresources/ogcapi/maps-collections.js
+++ src/community/ogcapi/ogcapi-maps/src/main/resources/webresources/ogcapi/maps-collections.js
@@ -1,3 +1,7 @@
+/* (c) 2026 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 
 window.addEventListener('load', function() {
   const inputs = document.getElementsByClassName('form-select-open-limit');
